88a7ff62af8323bfca13008e971b525e7a7751f3
Arik Fraimovich <arik@arikfr.com>
2013-11-29 21:28:04 +0200

    Job: when cancelling send SIGKILL instead of SIGINT.

--- rd_service/data/worker.py
+++ rd_service/data/worker.py
@@ -78,7 +78,7 @@ class Job(object):
             return
 
         if self.status == self.PROCESSING:
-            os.kill(self.process_id, signal.SIGINT)
+            os.kill(self.process_id, signal.SIGKILL)
         else:
             self.done(None, "Interrupted/Cancelled while running.")
 
