0bf6e39c66946abc117e9c12a4cc1680347068b1
Arik Fraimovich <arik@arikfr.com>
2013-12-08 15:26:35 +0200

    Improvements to queries page: (#6)
    1. Search (client side).
    2. Stats about queries.
    3. Pagination.

--- rd_ui/app/scripts/filters.js
+++ rd_ui/app/scripts/filters.js
@@ -1,19 +1,31 @@
+var durationHumanize = function (duration) {
+    var humanized = "";
+    if (duration == undefined) {
+        humanized = "-";
+    } else if (duration < 60) {
+        humanized = Math.round(duration) + "s";
+    } else if (duration >= 3600) {
+        var hours = Math.round(parseFloat(duration) / 60.0 / 60.0)
+        humanized = hours + "h";
+    } else {
+        var minutes = Math.round(parseFloat(duration) / 60.0);
+        humanized = minutes + "m";
+    }
+    return humanized;
+}
+
 angular.module('redash.filters', []).
     filter('durationHumanize', function () {
-        return function (duration) {
-            var humanized = "";
-            if (duration == undefined) {
-                humanized = "-";
-            } else if (duration < 60) {
-                humanized = Math.round(duration) + "s";
-            } else if (duration >= 3600) {
-                var hours = Math.round(parseFloat(duration) / 60.0 / 60.0)
-                humanized = hours + "h";
+        return durationHumanize;
+    })
+
+    .filter('refreshRateHumanize', function () {
+        return function (ttl) {
+            if (ttl==-1) {
+                return "Never";
             } else {
-                var minutes = Math.round(parseFloat(duration) / 60.0);
-                humanized = minutes + "m";
+                return "Every " + durationHumanize(ttl);
             }
-            return humanized;
         }
     })
 
