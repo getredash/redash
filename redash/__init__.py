288d1f7e5a457b271035b5b54729e14eab42a764
Arik Fraimovich <arik@arikfr.com>
2014-01-17 11:28:34 +0200

    Fix import issues (renamed api to controllers).

--- redash/__init__.py
+++ redash/__init__.py
@@ -37,3 +37,5 @@ def json_representation(data, code, headers=None):
 redis_url = urlparse.urlparse(settings.REDIS_URL)
 redis_connection = redis.StrictRedis(host=redis_url.hostname, port=redis_url.port, db=0, password=redis_url.password)
 data_manager = data.Manager(redis_connection, settings.INTERNAL_DB_CONNECTION_STRING, settings.MAX_CONNECTIONS)
+
+from redash import controllers
