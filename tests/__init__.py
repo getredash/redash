c0ca6020179e380101e8fe636b61b0e28ee76b07
Arik Fraimovich <arik@arikfr.com>
2014-02-06 11:10:04 +0200

    Factories for all models for tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,44 +11,13 @@ settings.DATABASE_CONFIG = {
 app.config['DATABASE'] = settings.DATABASE_CONFIG
 db.load_database()
 
-
-def model_factory(model, **kwargs):
-    def factory(**properties):
-        kwargs.update(properties)
-
-        return model(**kwargs)
-
-    return factory
-
-
-class ModelFactory(object):
-    def __init__(self, model, **kwargs):
-        self.model = model
-        self.kwargs = kwargs
-
-    def _get_kwargs(self, override_kwargs):
-        kwargs = self.kwargs.copy()
-        kwargs.update(override_kwargs)
-        return kwargs
-
-    def instance(self, **override_kwargs):
-        kwargs = self._get_kwargs(override_kwargs)
-
-        return self.model(**kwargs)
-
-    def create(self, **override_kwargs):
-        kwargs = self._get_kwargs(override_kwargs)
-
-        return self.model.create(**kwargs)
-
-DashboardFactory = ModelFactory(redash.models.Dashboard,
-                                name='test', user='test@everything.me', layout='[]')
-
-dashboard_factory = model_factory(redash.models.Dashboard, name='test', user='test@everything.me', layout='[]')
+for model in redash.models.all_models:
+    model._meta.database = db.database
 
 
 class BaseTestCase(TestCase):
     def setUp(self):
+
         redash.models.create_db(True, True)
 
     def tearDown(self):
--- tests/models.py
+++ tests/models.py
@@ -1,14 +1,15 @@
-from tests import BaseTestCase, DashboardFactory
+from tests import BaseTestCase
+from factories import dashboard_factory
 
 
 class DashboardTest(BaseTestCase):
     def test_appends_suffix_to_slug_when_duplicate(self):
-        d1 = DashboardFactory.create()
+        d1 = dashboard_factory.create()
         self.assertEquals(d1.slug, 'test')
 
-        d2 = DashboardFactory.create()
+        d2 = dashboard_factory.create()
         self.assertNotEquals(d1.slug, d2.slug)
 
-        d3 = DashboardFactory.create()
+        d3 = dashboard_factory.create()
         self.assertNotEquals(d1.slug, d3.slug)
         self.assertNotEquals(d2.slug, d3.slug)
