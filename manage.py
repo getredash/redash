d3ee55a971caa53f496894d0b627260241fcb84d
Arik Fraimovich <arik@arikfr.com>
2014-02-09 17:09:07 +0200

    Switch to Flask-Script.

--- manage.py
+++ manage.py
@@ -7,13 +7,23 @@ atfork.monkeypatch_os_fork_functions()
 import atfork.stdlib_fixer
 atfork.stdlib_fixer.fix_logging_module()
 
-import argparse
 import logging
 import time
-from redash import settings, app, data_manager, __version__
+from redash import settings, app, db, models, data_manager, __version__
+from flask.ext.script import Manager
 
+manager = Manager(app)
+database_manager = Manager(help="Manages the database (create/drop tables).")
 
-def start_workers():
+@manager.command
+def version():
+    """Displays re:dash version."""
+    print __version__
+
+@manager.command
+def runworkers():
+    """Starts the re:dash query executors/workers."""
+
     try:
         old_workers = data_manager.redis_connection.smembers('workers')
         data_manager.redis_connection.delete('workers')
@@ -34,49 +44,25 @@ def start_workers():
         logging.warning("Exiting; waiting for threads")
         data_manager.stop_workers()
 
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, models=models)
 
-def start_server(port, debug):
-    app.run(debug=debug, port=port)
-
-
-def create_db(create_tables, drop_tables):
+@database_manager.command
+def create_tables():
+    """Creates the database tables."""
     from redash.models import create_db
 
-    create_db(create_tables, drop_tables)
+    create_db(True, False)
 
+@database_manager.command
+def drop_tables():
+    """Drop the database tables."""
+    from redash.models import create_db
+
+    create_db(False, True)
+
+manager.add_command("database", database_manager)
 
 if __name__ == '__main__':
-    channel = logging.StreamHandler()
-    logging.getLogger().addHandler(channel)
-    logging.getLogger().setLevel(settings.LOG_LEVEL)
-
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(title='command', dest='command')
-
-    subparsers.add_parser('version', help='display version')
-    subparsers.add_parser('worker', help='start query execution workers')
-    server_parser = subparsers.add_parser('server', help='start api server')
-    server_parser.add_argument('--debug',
-                               action='store_true',
-                               help='start in debug mode (code reload)')
-    server_parser.add_argument('--port',
-                               default=8888,
-                               help='port to bind to')
-    db_parser = subparsers.add_parser('db', help='database operations')
-    db_parser.add_argument('--create-tables',
-                           action='store_true',
-                           help='Create all tables')
-    db_parser.add_argument('--drop-tables',
-                           action='store_true',
-                           help='Drop all tables')
-
-    args = parser.parse_args()
-
-    if args.command == 'version':
-        print __version__
-    elif args.command == "worker":
-        start_workers()
-    elif args.command == 'db':
-        create_db(args.create_tables, args.drop_tables)
-    elif args.command == 'server':
-        start_server(args.port, args.debug)
+    manager.run()
