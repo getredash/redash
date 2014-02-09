b7827f3eea98c36e0f901f18168340da1c19cb19
Arik Fraimovich <arik@arikfr.com>
2014-02-09 15:14:46 +0200

    Update visualizations migration.

--- migrations/create_visualizations.py
+++ migrations/create_visualizations.py
@@ -1,66 +1,64 @@
 import json
+import peewee
+from playhouse.migrate import Migrator
+from redash import db
 from redash import models
-
-# first run:
-
-# CREATE TABLE "visualizations" (
-#     "id" serial NOT NULL PRIMARY KEY,
-#     "type" varchar(100) NOT NULL,
-#     "query_id" integer NOT NULL REFERENCES "queries" ("id") DEFERRABLE INITIALLY DEFERRED,
-#     "name" varchar(255) NOT NULL,
-#     "description" varchar(4096),
-#     "options" text NOT NULL
-# )
-# ;
-
-# ALTER TABLE widgets ADD COLUMN "visualization_id" integer REFERENCES "visualizations" ("id") DEFERRABLE INITIALLY DEFERRED;
 
 if __name__ == '__main__':
     default_options = {"series": {"type": "bar"}}
 
-    # create 'table' visualization for all queries
-    print 'creating TABLE visualizations ...'
-    for query in Query.objects.all():
-        vis = Visualization(query=query, name="Table",
-                            description=query.description,
-                            type="TABLE", options="{}")
+    db.connect_db()
+
+    if not models.Visualization.table_exists():
+        print "Creating visualization table..."
+        models.Visualization.create_table()
+
+    with db.database.transaction():
+        migrator = Migrator(db.database)
+        print "Adding visualization_id to widgets:"
+        migrator.add_column(models.Widget, models.Widget.visualization, 'visualization_id')
+
+    print 'Creating TABLE visualizations for all queries...'
+    for query in models.Query.select():
+        vis = models.Visualization(query=query, name="Table",
+                                   description=query.description,
+                                   type="TABLE", options="{}")
         vis.save()
 
-
-    # create 'cohort' visualization for all queries named with 'cohort'
-    print 'creating COHORT visualizations ...'
-    for query in Query.objects.filter(name__icontains="cohort"):
-        vis = Visualization(query=query, name="Cohort",
-                            description=query.description,
-                            type="COHORT", options="{}")
+    print 'Creating COHORT visualizations for all queries named like %cohort%...'
+    for query in models.Query.select().where(peewee.fn.Icontains(models.Query.name)=="cohort"):
+        vis = models.Visualization(query=query, name="Cohort",
+                                   description=query.description,
+                                   type="COHORT", options="{}")
         vis.save()
 
-
-    # create visualization for every widget (unless it already exists)
-    print 'migrating Widgets -> Visualizations ...'
-    for widget in Widget.objects.all():
-        print 'processing widget %d:' % widget.id,
+    print 'Create visualization for all widgets (unless exists already):'
+    for widget in models.Widget.select():
+        print 'Processing widget id: %d:' % widget.id
         query = widget.query
         vis_type = widget.type.upper()
+        if vis_type == 'GRID':
+            vis_type = 'TABLE'
 
-        vis = query.visualizations.filter(type=vis_type)
+        vis = query.visualizations.where(type=vis_type).first()
         if vis:
-            print 'visualization exists'
+            print '... visualization type (%s) found.' % vis_type
             widget.visualization = vis[0]
             widget.save()
-
         else:
-            vis_name = widget.type.title()
+            vis_name = vis_type.title()
 
             options = json.loads(widget.options)
             vis_options = {"series": options} if options else default_options
             vis_options = json.dumps(vis_options)
 
-            vis = Visualization(query=query, name=vis_name,
-                                description=query.description,
-                                type=vis_type, options=vis_options)
+            vis = models.Visualization(query=query, name=vis_name,
+                                       description=query.description,
+                                       type=vis_type, options=vis_options)
 
-            print 'created visualization %s' % vis_type
+            print '... Created visualization for type: %s' % vis_type
             vis.save()
             widget.visualization = vis
             widget.save()
+
+        db.close_db(None)
