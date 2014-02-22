791f2e0b34e2d2273e4dfddc749fb98dbcf91192
Arik Fraimovich <arik@arikfr.com>
2014-02-22 14:42:08 +0200

    Use of user object (fix views, update migrations and some).

--- migrations/create_users.py
+++ migrations/create_users.py
@@ -1,5 +1,6 @@
 import json
 import itertools
+import peewee
 from playhouse.migrate import Migrator
 from redash import db, settings
 from redash import models
@@ -14,14 +15,21 @@ if __name__ == '__main__':
     migrator = Migrator(db.database)
     with db.database.transaction():
         print "Creating user field on dashboard and queries..."
-        migrator.rename_column(models.Query, '"user"', "user_email")
-        migrator.rename_column(models.Dashboard, '"user"', "user_email")
+        try:
+            migrator.rename_column(models.Query, '"user"', "user_email")
+            migrator.rename_column(models.Dashboard, '"user"', "user_email")
+        except peewee.ProgrammingError:
+            print "Failed to rename user column -- assuming it already exists"
 
+    with db.database.transaction():
         models.Query.user.null = True
         models.Dashboard.user.null = True
 
-        migrator.add_column(models.Query, models.Query.user, "user_id")
-        migrator.add_column(models.Dashboard, models.Dashboard.user, "user_id")
+        try:
+            migrator.add_column(models.Query, models.Query.user, "user_id")
+            migrator.add_column(models.Dashboard, models.Dashboard.user, "user_id")
+        except peewee.ProgrammingError:
+            print "Failed to create user_id column -- assuming it already exists"
 
     print "Creating user for all queries and dashboards..."
     for obj in itertools.chain(models.Query.select(), models.Dashboard.select()):
@@ -35,12 +43,14 @@ if __name__ == '__main__':
             user = models.User.get(models.User.email == email)
         except models.User.DoesNotExist:
             is_admin = email in settings.ADMINS
-            user = models.User.create(email=email, name="", is_admin=is_admin)
+            user = models.User.create(email=email, name=email, is_admin=is_admin)
 
         obj.user = user
         obj.save()
 
     print "Set user_id to non null..."
     with db.database.transaction():
-        migrator.set_nullable(models.Query, models.Query.user, True)
-        migrator.set_nullable(models.Dashboard, models.Dashboard.user, True)
+        migrator.set_nullable(models.Query, models.Query.user, False)
+        migrator.set_nullable(models.Dashboard, models.Dashboard.user, False)
+        migrator.set_nullable(models.Query, models.Query.user_email, True)
+        migrator.set_nullable(models.Dashboard, models.Dashboard.user_email, True)
