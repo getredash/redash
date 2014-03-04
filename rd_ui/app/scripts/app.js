f8280552a0f56d7d4dd9fda03d154ee2da339cf6
Amir Nissim <amirnissim@gmail.com>
2014-03-04 16:17:52 +0200

    #121: QueryViewCtrl with 'strict mode'

--- rd_ui/app/scripts/app.js
+++ rd_ui/app/scripts/app.js
@@ -35,14 +35,14 @@ angular.module('redash', [
             controller: 'QueryFiddleCtrl',
             reloadOnSearch: false
         });
-        $routeProvider.when('/queries/view/:queryId', {
-            templateUrl: '/views/queryview.html',
-            controller: 'QueryFiddleCtrl',
-            reloadOnSearch: false
-        });
         $routeProvider.when('/queries/:queryId', {
             templateUrl: '/views/queryfiddle.html',
             controller: 'QueryFiddleCtrl',
+            reloadOnSearch: false
+        });
+        $routeProvider.when('/queries/view/:queryId', {
+            templateUrl: '/views/queryview.html',
+            controller: 'QueryViewCtrl',
             reloadOnSearch: false
         });
         $routeProvider.when('/admin/status', {
