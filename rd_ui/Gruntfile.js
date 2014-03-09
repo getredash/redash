30b4628593febdb5feff46f3379e7782825a5c68
Arik Fraimovich <arik@arikfr.com>
2014-03-09 12:52:33 +0200

    Fix: compile all views.

--- rd_ui/Gruntfile.js
+++ rd_ui/Gruntfile.js
@@ -231,7 +231,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>',
-          src: ['*.html', 'views/*.html'],
+          src: ['*.html', 'views/**/*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
