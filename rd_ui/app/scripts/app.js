259ea39d557ebd49182a674eeeb218d23e632f0e
Arik Fraimovich <arik@arikfr.com>
2014-03-19 11:44:50 +0200

    Move Highcharts color definitions to highchart's code file

--- rd_ui/app/scripts/app.js
+++ rd_ui/app/scripts/app.js
@@ -85,9 +85,6 @@ angular.module('redash', [
             redirectTo: '/'
         });
 
-        Highcharts.setOptions({
-            colors: ["#4572A7", "#AA4643", "#89A54E", "#80699B", "#3D96AE",
-                        "#DB843D", "#92A8CD", "#A47D7C", "#B5CA92"]
-        });
+
     }
 ]);
--- rd_ui/app/scripts/ng_highchart.js
+++ rd_ui/app/scripts/ng_highchart.js
@@ -1,6 +1,11 @@
 (function () {
     'use strict';
 
+    Highcharts.setOptions({
+        colors: ["#4572A7", "#AA4643", "#89A54E", "#80699B", "#3D96AE",
+            "#DB843D", "#92A8CD", "#A47D7C", "#B5CA92"]
+    });
+
     var defaultOptions = {
         title: {
             "text": null
