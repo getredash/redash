29f01a5780f69c43eb1c89cfff040efe8154a31d
Amir Nissim <amirnissim@gmail.com>
2014-03-27 15:03:51 +0200

    [#90] clear visualization hash when redirecting to forked query

--- rd_ui/app/scripts/controllers/query_edit.js
+++ rd_ui/app/scripts/controllers/query_edit.js
@@ -8,6 +8,10 @@
     var
     isNewQuery = !$scope.query.id,
     queryText = $scope.query.query,
+
+    // ref to QueryViewCtrl.saveQuery
+    saveQuery = $scope.saveQuery,
+
     shortcuts = {
       'meta+s': function() {
           if ($scope.canEdit) {
@@ -24,9 +28,34 @@
 
     KeyboardShortcuts.bind(shortcuts);
 
-    $scope.onQuerySave = function(savedQuery) {
-      queryText = savedQuery.query;
-      $scope.isDirty = $scope.query.query !== queryText;
+    // @override
+    $scope.saveQuery = function(options, data) {
+      var savePromise = saveQuery(options, data);
+
+      savePromise.then(function(savedQuery) {
+        queryText = savedQuery.query;
+        $scope.isDirty = $scope.query.query !== queryText;
+
+        if (isNewQuery) {
+          // redirect to new created query (keep hash)
+          $location.path(savedQuery.getSourceLink()).replace();
+        }
+      });
+
+      return savePromise;
+    };
+
+    $scope.duplicateQuery = function() {
+      $scope.query.id = null;
+      $scope.query.ttl = -1;
+
+      $scope.saveQuery({
+        successMessage: 'Query forked',
+        errorMessage: 'Query could not be forked'
+      }).then(function redirect(savedQuery) {
+        // redirect to forked query (clear hash)
+        $location.url(savedQuery.getSourceLink()).replace()
+      });
     };
 
     $scope.deleteVisualization = function($e, vis) {
