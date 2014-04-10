8615429e0c6615ee8ae5968bec45dc94f2064b96
Arik Fraimovich <arik@arikfr.com>
2014-04-10 13:02:24 +0300

    Logging setup for events.

--- redash/events.py
+++ redash/events.py
@@ -1,7 +1,21 @@
 import logging
 import json
 
-logger = logging.getLogger("events")
+logger = logging.getLogger("redash.events")
+logger.propagate = False
+
+
+def setup_logging(log_path, console_output=False):
+    fh = logging.FileHandler(log_path)
+    formatter = logging.Formatter('%(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    if console_output:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('[%(name)s] %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
 
 def record_event(event):
