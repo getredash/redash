9b34464f6a6972331883fd3bd13a24d23788dac3
Christopher Valles <christopher.valles@hailocab.com>
2014-05-14 11:16:10 +0100

    Removing unused permission

--- redash/models.py
+++ redash/models.py
@@ -34,7 +34,7 @@ class ApiUser(UserMixin):
 
 class Group(BaseModel):
     DEFAULT_PERMISSIONS = ['create_dashboard', 'create_query', 'edit_dashboard', 'edit_query',
-                           'view_query', 'view_source', 'execute_query', 'delete_query']
+                           'view_query', 'view_source', 'execute_query']
     
     id = peewee.PrimaryKeyField()
     name = peewee.CharField(max_length=100)
