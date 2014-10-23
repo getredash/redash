dcf0d2cbe38397c641192f2219118b8177b198ee
Amir Nissim <amirnissim@gmail.com>
2014-10-23 13:46:43 +0300

    #292: Customizable series colors

---
+++ rd_ui/app/scripts/constants.js
@@ -0,0 +1,13 @@
+angular.module('redash')
+  .constant('ColorPalette', {
+    'Black': '#0d233a',
+    'Blue': '#2f7ed8',
+    'Cyan': '#1aadce',
+    'Green': '#8bbc21',
+    'Light Blue': '#77a1e5',
+    'Lime': '#a6c96a',
+    'Orange': '#f28f43',
+    'Purple': '#492970',
+    'Red': '#910000',
+    'Red': '#c42525',
+  });
