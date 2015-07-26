b14b6d1773e7e274125cbeca82309dac52f75b3b
Arik Fraimovich <arik@arikfr.com>
2015-07-26 11:45:58 +0300

    Give permission to read user(id, name) to redash_reader

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -143,6 +143,7 @@ if [ $pg_user_exists -ne 0 ]; then
     REDASH_READER_PASSWORD=$(pwgen -1)
     sudo -u postgres psql -c "CREATE ROLE redash_reader WITH PASSWORD '$REDASH_READER_PASSWORD' NOCREATEROLE NOCREATEDB NOSUPERUSER LOGIN"
     sudo -u redash psql -c "grant select(id,name,type) ON data_sources to redash_reader;" redash
+    sudo -u redash psql -c "grant select(id,name) ON users to redash_reader;" redash
     sudo -u redash psql -c "grant select on activity_log, events, queries, dashboards, widgets, visualizations, query_results to redash_reader;" redash
 
     cd /opt/redash/current
