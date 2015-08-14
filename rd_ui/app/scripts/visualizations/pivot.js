9f0716c0d0043c5f541b261464763881473804ef
maflorvega <fvega@devspark.com>
2015-08-14 15:15:49 -0300

    minor changes

--- rd_ui/app/scripts/visualizations/pivot.js
+++ rd_ui/app/scripts/visualizations/pivot.js
@@ -2,13 +2,11 @@
 
 (function () {
 	var module = angular.module('redash.visualization');
-	
 	module.config(['VisualizationProvider', function (VisualizationProvider) {
 	var renderTemplate = 
 	'<pivot-renderer '  + 
 	' options="visualization.options" query-result="queryResult"> ' +
-	' </pivot-renderer>';
-	
+	' </pivot-renderer>';	
 	var editTemplate = '<pivot-editor></pivot-editor>';
 	var defaultOptions = {};
 	
@@ -24,14 +22,12 @@
 	  
 	module.directive('pivotRenderer', function() {
 		return {
-			restrict: 'E',
-		
+			restrict: 'E',	
 			template:'',
 			replace: false,
 			link: function($scope, element, attrs) {		
 				$scope.visualization.options.cols = 
-				$scope.visualization.options.rows = 
-						
+				$scope.visualization.options.rows = 		
 				$scope.$watch('[queryResult && queryResult.getData(), visualization.options]', 
 				function (data) {
 						var data = $scope.queryResult.getData();				
@@ -40,14 +36,13 @@
 							}
 							else {
 								var data = $.extend(true, [], $scope.queryResult.getData());	
-							     var cols= $scope.visualization.options.cols || 'cols';
-							     var rows= $scope.visualization.options.rows || 'rows';
-                                 var options = {"rows":rows, "cols":cols}
-		
+							    var cols= $scope.visualization.options.cols || 'cols';
+							    var rows= $scope.visualization.options.rows || 'rows';
+                                var options = {"rows":rows, "cols":cols}
 								$(element).pivotUI(data, options, true);						
 							}
 						});
-					}
+				}
 		}
 	});
 	
@@ -56,6 +51,6 @@
 	 restrict: 'E',
 	 templateUrl: '/views/visualizations/pivot_editor.html'
 	}
-	 });
+	});
  
 })();
