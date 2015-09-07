59cbafa7245525fe6187186f81bfffd8c1ed08d9
Arik Fraimovich <arik@arikfr.com>
2015-09-07 08:49:04 +0300

    Fix migration - always set target row number

--- migrations/0013_update_counter_options.py
+++ migrations/0013_update_counter_options.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
             if 'targetColName' not in options:
                 options['targetColName'] = 'target'
-                options['targetRowNumber'] = options['rowNumber']
+            options['targetRowNumber'] = options['rowNumber']
 
             print "After: ", options
             vis.options = json.dumps(options)
