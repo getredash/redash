5b024a35186ced2f168c686fee748ac933ff3311
jvasquez <jvasquez@devspark.com>
2015-09-09 14:39:43 -0300

    Hidden widgets

--- rd_ui/app/scripts/visualizations/pivot.js
+++ rd_ui/app/scripts/visualizations/pivot.js
@@ -1,29 +1,54 @@
-var renderers = angular.module('redash.renderers', []);
+'use strict';
 
-renderers.directive('pivotTableRenderer', function () {
-    return {
-        restrict: 'E',
-        scope: {
-            queryResult: '='
-        },
-        template: "",
-        replace: false,
-        link: function($scope, element, attrs) {
-            $scope.$watch('queryResult && queryResult.getData()', function (data) {
-                if (!data) {
-                    return;
-                }
+(function () {
+	var module = angular.module('redash.visualization');
+	module.config(['VisualizationProvider', function (VisualizationProvider) {
+	var renderTemplate = 
+	'<pivot-renderer '  + 
+	' options="visualization.options" query-result="queryResult"> ' +
+	' </pivot-renderer>';	
+	var editTemplate = '<pivot-editor></pivot-editor>';
+	var defaultOptions = {};
 
-                if ($scope.queryResult.getData() == null) {
-                } else {
-                    // We need to give the pivot table its own copy of the data, because its change
-                    // it which interferes with other visualizations.
-                    var data = $.extend(true, [], $scope.queryResult.getData());
-                    $(element).pivotUI(data, {
-                         renderers: $.pivotUtilities.renderers
-                    }, true);
-                }
-            });
-        }
-    }
-});
+	VisualizationProvider.registerVisualization({
+	 type: 'PIVOT',
+	 name: 'Pivot',
+	 renderTemplate: renderTemplate,
+	 editorTemplate: editTemplate,
+	 defaultOptions: defaultOptions
+	});
+	}
+]);
+	  
+	module.directive('pivotRenderer', function() {
+		return {
+			restrict: 'E',	
+			template:'',
+			replace: false,
+			link: function($scope, element, attrs) {		
+				$scope.$watch('[queryResult && queryResult.getData(), visualization.options]', 
+				function (data) {
+						var data = $scope.queryResult.getData();				
+						if ($scope.queryResult.getData() == null) {
+								return;
+							}
+							else {
+							    var cols= $scope.visualization.options.cols || 'cols';
+							    var rows= $scope.visualization.options.rows || 'rows';
+                                var optionsTable = {"rows":rows, "cols":cols}
+								var data = $.extend(true, [], $scope.queryResult.getData());
+								$(element).pivotUI(data, optionsTable, true);					
+							}
+						}, true);
+				}
+		}
+	});
+          
+	module.directive('pivotEditor', function() {
+	return {
+	 restrict: 'E',
+	 templateUrl: '/views/visualizations/pivot_editor.html'
+	}
+	});
+ 
+})();
