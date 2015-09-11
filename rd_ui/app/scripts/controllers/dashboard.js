078ab1a7f6a6a62a7b66dd2776a6236a246c7fb0
jvasquez <jvasquez@devspark.com>
2015-09-11 15:36:34 -0300

    Roll back for hidden widgets. Removing behavior from controller.

--- rd_ui/app/scripts/controllers/dashboard.js
+++ rd_ui/app/scripts/controllers/dashboard.js
@@ -126,10 +126,9 @@
 
   var WidgetCtrl = function($scope, $location, Events, Query) {
     $scope.disableWidget = {'visibility': 'visible'};
-    $scope.checkWidth = function (width) {
-     if (width == 0)
-        $scope.disableWidget = {'visibility': 'hidden'};
-    }
+
+    if ($scope.widget.width == 0) {
+      $scope.disableWidget = {'visibility': 'hidden'};
 
     $scope.deleteWidget = function() {
       if (!confirm('Are you sure you want to remove "' + $scope.widget.getName() + '" from the dashboard?')) {
