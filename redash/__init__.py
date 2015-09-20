ec46312bf6e2d06707ce3257a04baece0d7ab8fa
Arik Fraimovich <arik@arikfr.com>
2015-09-20 11:05:56 +0300

    Bump version.

--- redash/__init__.py
+++ redash/__init__.py
@@ -7,7 +7,7 @@ from flask_mail import Mail
 from redash import settings
 from redash.query_runner import import_query_runners
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 
 
 def setup_logging():
