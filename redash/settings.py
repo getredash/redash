410c5671f041c44680980e21a9cbf637d4d79fc0
John Wu <webmaster@leapoahead.com>
2015-10-13 11:42:42 -0700

    Revert: python data source in setting

--- redash/settings.py
+++ redash/settings.py
@@ -127,8 +127,7 @@ QUERY_RUNNERS = array_from_string(os.environ.get("REDASH_ENABLED_QUERY_RUNNERS",
     'redash.query_runner.hive_ds',
     'redash.query_runner.impala_ds',
     'redash.query_runner.vertica',
-    'redash.query_runner.treasuredata',
-    'redash.query_runner.python'
+    'redash.query_runner.treasuredata'
 ])))
 
 # Support for Sentry (http://getsentry.com/). Just set your Sentry DSN to enable it:
