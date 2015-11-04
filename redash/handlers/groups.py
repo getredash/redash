e60fb979a78bd409eef7a6eceafff8ebe9c93ea2
John Wu <webmaster@leapoahead.com>
2015-11-04 10:55:57 -0800

    Return groups array directly

--- redash/handlers/groups.py
+++ redash/handlers/groups.py
@@ -1,17 +1,10 @@
-import json
-
 from redash.handlers.base import BaseResource
 from redash.wsgi import api
-from flask import make_response
-from redash import utils
 from redash.models import Group
+
 
 class GroupsAPI(BaseResource):
     def get(self):
-        return self.make_json_response(Group.select())
-
-    def make_json_response(self, groups):
-        data = json.dumps({'groups': [group.to_dict() for group in groups]}, cls=utils.JSONEncoder)
-        return make_response(data, 200, {})
+        return [group.to_dict() for group in Group.select()]
 
 api.add_resource(GroupsAPI, '/api/groups', endpoint='groups')
