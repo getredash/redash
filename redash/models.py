51a10e5a2064d8800e8610bc16075f3f68a0e892
Kosei Moriyama <cou929@gmail.com>
2015-11-23 02:06:39 +0900

    Add AlertSubscription to all_models

--- redash/models.py
+++ redash/models.py
@@ -875,7 +875,7 @@ class Event(BaseModel):
         return event
 
 
-all_models = (DataSource, User, QueryResult, Query, Alert, Dashboard, Visualization, Widget, ActivityLog, Group, Event)
+all_models = (DataSource, User, QueryResult, Query, Alert, AlertSubscription, Dashboard, Visualization, Widget, ActivityLog, Group, Event)
 
 
 def init_db():
