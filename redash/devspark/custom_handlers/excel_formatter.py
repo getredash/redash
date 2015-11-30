4a73e2b7bee65570e33396a7d28e5b1c832033b3
Martín Redolatti <mredolatti@devspark.com>
2015-11-30 17:27:36 -0300

    Fixed sheet naming

--- redash/devspark/custom_handlers/excel_formatter.py
+++ redash/devspark/custom_handlers/excel_formatter.py
@@ -15,19 +15,31 @@ def get_tasks():
 
     with tempfile.NamedTemporaryFile() as tmp_flo:
         workbook = xlsxwriter.Workbook(tmp_flo.name)
+
+        # Add a bold format to use to highlight cells.
+        format1 = workbook.add_format({'bold': True, 'font_color': 'white', 'bg_color': 'black', 'border': True})
+        format2 = workbook.add_format({'border': True})
+
+        # For each widget to be exported, creates a new sheet
+        page_count = 0
         for sheet in data['data']:
-            worksheet = workbook.add_worksheet(sheet['option']['sheet'][:31])
+            sheet_name = "%s_%s" % (page_count, sheet['option']['sheet'])
+            page_count += 1
+            worksheet = workbook.add_worksheet(sheet_name[:31])
+     
             rowIdx = 0
             colIdx = 0
+            # Defines the header
             for column in sheet['option']['columnNames']:
-                worksheet.write(rowIdx, colIdx, column)
+                worksheet.write(rowIdx, colIdx, column, format1)
                 colIdx +=1
-
+            # Writes each row for each column
             for row in sheet['data']:
                 colIdx = 0
                 rowIdx +=1
                 for column in sheet['option']['columnNames']:
-                    worksheet.write(rowIdx, colIdx, row[column])
+                    worksheet.write(rowIdx, colIdx, row[column], format2)
+                    worksheet.set_column(rowIdx, colIdx, 25)
                     colIdx +=1
 
         workbook.close()
@@ -37,12 +49,8 @@ def get_tasks():
     response = make_response(contents)
     filename = "%s.xlsx" % data['name']
     response.headers['Content-Disposition'] = 'attachment; filename=%s' % filename
-#    response.headers['Content-Type'] = 'application/download'
     response.headers['Content-type'] = ('application/vnd.openxmlformats-'
                                         'officedocument.spreadsheetml.sheet')
     response.headers['Content-Description'] = 'File Transfer'
     response.headers['Content-Transfer-Encoding'] = 'binary'
-
-
-#header('Content-Length: ' . filesize($file));
     return response
