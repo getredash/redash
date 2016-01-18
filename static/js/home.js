43c4a160defe3c4a56581f4bad13d10db80e5a28
Ran Byron <ran@ranbena.com>
2016-01-18 21:09:13 +0200

    Added ‘Get started’ (hosted/opensource)

---
+++ static/js/home.js
@@ -0,0 +1,69 @@
+var SignUp = new function() {
+  var self = this,
+      el = document.getElementById('signup'),
+      emailEl = document.getElementById('email');
+
+  emailEl.addEventListener('keypress', function(e) {
+    if (e.keyCode == 13) {
+      self.send();
+    }
+  });
+
+  Object.defineProperty(this, 'status', {
+    set: function (status) {
+      el.dataset.status = status;
+      Feedback.clear();
+    }
+  });
+
+  this.send = function() {
+    // validate email
+    if (!validateEmail(emailEl.value)) {
+      // ouch
+      Feedback.set({
+        success: false,
+        text: 'Oops.. your email isn\'t valid :/'
+      });
+      return;
+    }
+
+    // sending
+    Feedback.set({text: 'Sending...'});
+
+    // ajax placeholder
+    setTimeout(function() {
+      // success!
+      self.status = 'success';
+    }, 2000);
+  }
+
+  function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  var Feedback = new function() {
+    var el = document.getElementById('feedback');
+
+    this.set = function(data) {
+      if (el.dataset.show) {
+        el.dataset.show = false;
+        setTimeout(function() {
+          set(data);
+        }, 200); // animation time
+      } else {
+        set(data);
+      }
+    };
+
+    this.clear = function() {
+      el.dataset.show = false;
+    };
+
+    function set(data) {
+      el.innerHTML = data.text;
+      el.dataset.success = data.success;
+      el.dataset.show = true;
+    }
+  }
+}
