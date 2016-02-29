5208abd072d747c0da9dbd599ee50ea63c61842e
Jeff Widman <jeff@jeffwidman.com>
2016-02-29 01:58:19 -0800

    Make lines indented by four spaces instead of three

--- migrations/0010_create_alerts.py
+++ migrations/0010_create_alerts.py
@@ -2,7 +2,7 @@ from redash.models import db, Alert, AlertSubscription
 
 if __name__ == '__main__':
     with db.database.transaction():
-       Alert.create_table()
-       AlertSubscription.create_table()
+        Alert.create_table()
+        AlertSubscription.create_table()
 
     db.close_db(None)
