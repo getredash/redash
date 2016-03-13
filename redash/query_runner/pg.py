605a12c34b43d43cbcd55517fe76cf328d1c4679
Arik Fraimovich <arik@arikfr.com>
2016-03-13 15:38:00 +0200

    PostgreSQL query runner: add timeout for connection

--- redash/query_runner/pg.py
+++ redash/query_runner/pg.py
@@ -28,16 +28,16 @@ types_map = {
 }
 
 
-def _wait(conn):
+def _wait(conn, timeout=None):
     while 1:
         try:
             state = conn.poll()
             if state == psycopg2.extensions.POLL_OK:
                 break
             elif state == psycopg2.extensions.POLL_WRITE:
-                select.select([], [conn.fileno()], [])
+                select.select([], [conn.fileno()], [], timeout)
             elif state == psycopg2.extensions.POLL_READ:
-                select.select([conn.fileno()], [], [])
+                select.select([conn.fileno()], [], [], timeout)
             else:
                 raise psycopg2.OperationalError("poll() returned %s" % state)
         except select.error:
@@ -115,7 +115,7 @@ class PostgreSQL(BaseSQLQueryRunner):
 
     def run_query(self, query):
         connection = psycopg2.connect(self.connection_string, async=True)
-        _wait(connection)
+        _wait(connection, timeout=10)
 
         cursor = connection.cursor()
 
