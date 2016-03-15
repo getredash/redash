537b729589d9383efffc4c325ed53f34e07e88db
Andrea Leesley <aleesley@gmail.com>
2016-03-15 10:38:38 -0500

    Style hover on edit description.

--- rd_ui/app/scripts/ng_smart_table.js
+++ rd_ui/app/scripts/ng_smart_table.js
@@ -663,7 +663,7 @@ angular.module("partials/defaultHeader.html", []).run(["$templateCache", functio
 
 angular.module("partials/editableCell.html", []).run(["$templateCache", function ($templateCache) {
   $templateCache.put("partials/editableCell.html",
-    "<div ng-dblclick=\"toggleEditMode($event)\">\n" +
+    "<div ng-click=\"toggleEditMode($event)\" class=\"edit-toggle\">\n" +
       "<p ng-show=\"(value == '' || value == null) && !isEditMode\" title=\"Double click to edit\"'><em>Empty</em></p>" +
     "    <span ng-hide=\"isEditMode\">{{value | format:column.formatFunction:column.formatParameter}}</span>\n" +
     "\n" +
