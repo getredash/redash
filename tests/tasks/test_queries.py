727295303836e928a55856e5d2532571f520a30c
Arik Fraimovich <arik@arikfr.com>
2016-05-19 09:58:30 +0300

    Fix: old task trackers were not really removed

--- tests/tasks/test_queries.py
+++ tests/tasks/test_queries.py
@@ -7,8 +7,12 @@ class TestPrune(TestCase):
     def setUp(self):
         self.list = "test_list"
         redis_connection.delete(self.list)
+        self.keys = []
         for score in range(0, 100):
-            redis_connection.zadd(self.list, score, 'k:{}'.format(score))
+            key = 'k:{}'.format(score)
+            self.keys.append(key)
+            redis_connection.zadd(self.list, score, key)
+            redis_connection.set(key, 1)
 
     def test_does_nothing_when_below_threshold(self):
         remove_count = QueryTaskTracker.prune(self.list, 100)
@@ -22,3 +26,6 @@ class TestPrune(TestCase):
 
         self.assertEqual(redis_connection.zscore(self.list, 'k:99'), 99.0)
         self.assertIsNone(redis_connection.zscore(self.list, 'k:1'))
+
+        for k in self.keys[0:50]:
+            self.assertFalse(redis_connection.exists(k))
