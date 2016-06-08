4758207d111b50ca761b0062274c73ff260e7181
Arik Fraimovich <arik@arikfr.com>
2016-06-08 20:28:35 +0300

    Remove potnetially concurrency not safe code form enqueue_query.
    This might have been causing the behavior described in #1097.

--- tests/tasks/test_queries.py
+++ tests/tasks/test_queries.py
@@ -1,9 +1,10 @@
+from tests import BaseTestCase
 from redash import redis_connection
 from redash.tasks.queries import QueryTaskTracker, enqueue_query, execute_query
 from unittest import TestCase
-from tests import BaseTestCase
 from mock import MagicMock
 from collections import namedtuple
+import uuid
 
 
 class TestPrune(TestCase):
@@ -34,17 +35,36 @@ class TestPrune(TestCase):
             self.assertFalse(redis_connection.exists(k))
 
 
-result = namedtuple('Result', 'id')
+FakeResult = namedtuple('FakeResult', 'id')
+
+
+def gen_hash(*args, **kwargs):
+    return FakeResult(uuid.uuid4().hex)
 
 
 class TestEnqueueTask(BaseTestCase):
-    def test_enqueue(self):
+    def test_multiple_enqueue_of_same_query(self):
         query = self.factory.create_query()
-        execute_query.apply_async = MagicMock(return_value=result('testing'))
+        execute_query.apply_async = MagicMock(side_effect=gen_hash)
 
         enqueue_query(query.query, query.data_source, True, {'Username': 'Arik', 'Query ID': query.id})
         enqueue_query(query.query, query.data_source, True, {'Username': 'Arik', 'Query ID': query.id})
         enqueue_query(query.query, query.data_source, True, {'Username': 'Arik', 'Query ID': query.id})
-        
+
         self.assertEqual(1, execute_query.apply_async.call_count)
         self.assertEqual(1, redis_connection.zcard(QueryTaskTracker.WAITING_LIST))
+        self.assertEqual(0, redis_connection.zcard(QueryTaskTracker.IN_PROGRESS_LIST))
+        self.assertEqual(0, redis_connection.zcard(QueryTaskTracker.DONE_LIST))
+
+    def test_multiple_enqueue_of_different_query(self):
+        query = self.factory.create_query()
+        execute_query.apply_async = MagicMock(side_effect=gen_hash)
+
+        enqueue_query(query.query, query.data_source, True, {'Username': 'Arik', 'Query ID': query.id})
+        enqueue_query(query.query + '2', query.data_source, True, {'Username': 'Arik', 'Query ID': query.id})
+        enqueue_query(query.query + '3', query.data_source, True, {'Username': 'Arik', 'Query ID': query.id})
+
+        self.assertEqual(3, execute_query.apply_async.call_count)
+        self.assertEqual(3, redis_connection.zcard(QueryTaskTracker.WAITING_LIST))
+        self.assertEqual(0, redis_connection.zcard(QueryTaskTracker.IN_PROGRESS_LIST))
+        self.assertEqual(0, redis_connection.zcard(QueryTaskTracker.DONE_LIST))
