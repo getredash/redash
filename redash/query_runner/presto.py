233838faa634acb70b8ab36eb40c2d70844c3023
simo7 <simone.alessi@hotmail.com>
2016-09-29 15:22:14 +0200

    only show bi & default schemas

--- redash/query_runner/presto.py
+++ redash/query_runner/presto.py
@@ -75,7 +75,7 @@ class Presto(BaseQueryRunner):
         query = """
         SELECT table_schema, table_name, column_name
         FROM information_schema.columns
-        WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
+        WHERE table_schema in ('bi', 'default')
         ORDER BY table_schema, table_name, column_name asc
         """
 
