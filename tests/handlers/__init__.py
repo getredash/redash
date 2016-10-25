40cc59259140f4ba5a5efca287e92aedccc6eba2
Arik Fraimovich <arik@arikfr.com>
2016-10-25 11:58:25 +0300

    Make sure error are logged in tests

--- tests/handlers/__init__.py
+++ tests/handlers/__init__.py
@@ -1,10 +1,12 @@
 import json
+import logging
 from contextlib import contextmanager
 
 from tests.factories import user_factory
 from redash.utils import json_dumps
 from redash.wsgi import app
 
+app.config['TESTING'] = True
 
 def authenticate_request(c, user):
     with c.session_transaction() as sess:
