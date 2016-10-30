fb2f0f025f13cd50c2f5351be404cbedb7294c3b
Arik Fraimovich <arik@arikfr.com>
2016-10-30 15:52:46 +0200

    Migrated services & app header component

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,16 +1,62 @@
+import 'material-design-iconic-font/dist/css/material-design-iconic-font.css';
+import 'font-awesome/css/font-awesome.css';
+
 import angular from 'angular';
 import ngRoute from 'angular-route';
+import ngResource from 'angular-resource';
+import uiBootstrap from 'angular-ui-bootstrap';
 import { each } from 'underscore';
 
 import * as pages from './pages';
 import * as components from './components';
 import * as filters from './filters';
+import * as services from './services';
 import markdownFilter from './filters/markdown';
 
-const ngModule = angular.module('app', [ngRoute]);
+const ngModule = angular.module('app', [ngRoute, ngResource, uiBootstrap]);
+
+const user = {
+  name: 'Arik Fraimovich',
+  gravatar_url: 'https://www.gravatar.com/avatar/ca410c2e27337c8d7075bb1b098ac70f?s=40',
+  id: 1,
+  groups: [
+    3,
+    1,
+  ],
+  email: 'arik@redash.io',
+  permissions: [
+    'admin',
+    'super_admin',
+    'create_dashboard',
+    'create_query',
+    'edit_dashboard',
+    'edit_query',
+    'view_query',
+    'view_source',
+    'list_users',
+    'execute_query',
+    'schedule_query',
+    'list_dashboards',
+    'list_alerts',
+    'create_alerts',
+    'list_dashboards',
+    'list_alerts',
+    'list_data_sources',
+  ],
+  isAdmin: true,
+};
+
+user.hasPermission = () => true;
+ngModule.constant('currentUser', user);
 
 function registerComponents() {
   each(components, (register) => {
+    register(ngModule);
+  });
+}
+
+function registerServices() {
+  each(services, (register) => {
     register(ngModule);
   });
 }
@@ -33,6 +79,7 @@ function registerFilters() {
   });
 }
 
+registerServices();
 registerFilters();
 markdownFilter(ngModule);
 registerComponents();
