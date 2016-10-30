da790de60d6890e3c45f9d97426b915a9a370263
Arik Fraimovich <arik@arikfr.com>
2016-10-30 16:20:09 +0200

    First page implemented (home)

--- frontend/app/pages/home/index.js
+++ frontend/app/pages/home/index.js
@@ -1,13 +1,21 @@
-class HomeController {
-  constructor(Dashboard) {
-    console.log(Dashboard.query());
-  }
+import template from './home.html';
+
+function HomeCtrl($scope, currentUser, Events, Dashboard, Query) {
+  Events.record(currentUser, 'view', 'page', 'personal_homepage');
+  // $scope.$parent.pageTitle = 'Home';
+
+  // todo: maybe this should come from some serivce as we have this logic elsewhere.
+  this.canCreateQuery = currentUser.hasPermission('create_query');
+  this.canCreateDashboard = currentUser.hasPermission('create_dashboard');
+
+  this.recentQueries = Query.recent();
+  this.recentDashboards = Dashboard.recent();
 }
 
 export default function (ngModule) {
   ngModule.component('pageHome', {
-    template: '<div>Home {{1923 | durationHumanize}} </div>',
-    controller: HomeController,
+    template,
+    controller: HomeCtrl,
   });
 
   return {
