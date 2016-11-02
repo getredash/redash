5856a9e109d0b301351019312cde9dfe517c7276
Arik Fraimovich <arik@arikfr.com>
2016-11-02 12:56:44 +0200

    Add alerts and admin pages

---
+++ frontend/app/pages/admin/outdated-queries/index.js
@@ -0,0 +1,45 @@
+import moment from 'moment';
+import template from './outdated-queries.html';
+
+function OutdatedQueriesCtrl($scope, NgTableParams, currentUser, Events, $http, $timeout) {
+  Events.record(currentUser, 'view', 'page', 'admin/outdated_queries');
+  // $scope.$parent.pageTitle = 'Outdated Queries';
+  $scope.autoUpdate = true;
+
+  this.tableParams = new NgTableParams({ count: 50 }, {});
+
+  const refresh = () => {
+    if ($scope.autoUpdate) {
+      $scope.refresh_time = moment().add(1, 'minutes');
+      $http.get('/api/admin/queries/outdated').success((data) => {
+        this.tableParams.settings({
+          dataset: data.queries,
+        });
+        $scope.updatedAt = data.updated_at * 1000.0;
+      });
+    }
+
+    const timer = $timeout(refresh, 59 * 1000);
+
+    $scope.$on('$destroy', () => {
+      if (timer) {
+        $timeout.cancel(timer);
+      }
+    });
+  };
+
+  refresh();
+}
+
+export default function (ngModule) {
+  ngModule.component('outdatedQueriesPage', {
+    template,
+    controller: OutdatedQueriesCtrl,
+  });
+
+  return {
+    '/admin/queries/outdated': {
+      template: '<outdated-queries-page></outdated-queries-page>',
+    },
+  };
+}
---
+++ frontend/app/pages/admin/status/index.js
@@ -0,0 +1,40 @@
+import template from './status.html';
+
+// TODO: switch to $ctrl instead of $scope.
+function AdminStatusCtrl($scope, $http, $timeout, currentUser, Events) {
+  Events.record(currentUser, 'view', 'page', 'admin/status');
+  // $scope.$parent.pageTitle = 'System Status';
+
+  const refresh = function () {
+    $http.get('/status.json').success((data) => {
+      $scope.workers = data.workers;
+      delete data.workers;
+      $scope.manager = data.manager;
+      delete data.manager;
+      $scope.status = data;
+    });
+
+    const timer = $timeout(refresh, 59 * 1000);
+
+    $scope.$on('$destroy', () => {
+      if (timer) {
+        $timeout.cancel(timer);
+      }
+    });
+  };
+
+  refresh();
+}
+
+export default function (ngModule) {
+  ngModule.component('statusPage', {
+    template,
+    controller: AdminStatusCtrl,
+  });
+
+  return {
+    '/admin/status': {
+      template: '<status-page></status-page>',
+    },
+  };
+}
