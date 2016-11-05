2184f53277829e3f3172e1c936a09192254681b2
Joshua Dechant <josh@codecrafting.org>
2016-11-04 23:46:54 -0400

    JS vars should be camelCase

--- rd_ui/app/scripts/directives/query_directives.js
+++ rd_ui/app/scripts/directives/query_directives.js
@@ -52,7 +52,7 @@
           if (scope.queryResult.getId() == null) {
             element.attr('href', '');
           } else {
-            element.attr('href', 'api/queries/' + scope.query.id + '/results/' + scope.queryResult.getId() + '.' + fileType + (scope.embed ? '?api_key=' + scope.api_key : ''));
+            element.attr('href', 'api/queries/' + scope.query.id + '/results/' + scope.queryResult.getId() + '.' + fileType + (scope.embed ? '?api_key=' + scope.apiKey : ''));
             element.attr('download', scope.query.name.replace(" ", "_") + moment(scope.queryResult.getUpdatedAt()).format("_YYYY_MM_DD") + "." + fileType);
           }
         });
