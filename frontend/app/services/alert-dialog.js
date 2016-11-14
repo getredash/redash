7c201b24ba638d845d23d6a1e068850c0c714e94
Arik Fraimovich <arik@arikfr.com>
2016-11-14 16:51:23 +0200

    Add widget dialog

--- frontend/app/services/alert-dialog.js
+++ frontend/app/services/alert-dialog.js
@@ -1,16 +1,14 @@
-import { wrap } from 'underscore';
-
 const AlertDialogComponent = {
   template: `
 <div class="modal-header">
-    <h4 class="modal-title">{{$ctrl.title}}</h4>
+    <h4 class="modal-title" ng-if="$ctrl.title">{{$ctrl.title}}</h4>
 </div>
 <div class="modal-body">
   <p ng-bind-html="$ctrl.message"></p>
 </div>
 <div class="modal-footer">
-  <button class="btn btn-default" ng-click="$ctrl.close()">Cancel</button>
-  <button class="btn" ng-class="action.class" ng-click="action.callback()" ng-repeat="action in $ctrl.actions">{{action.title}}</button>
+  <button class="btn btn-default" ng-click="$ctrl.dismiss()">Cancel</button>
+  <button class="btn" ng-class="$ctrl.confirm.class" ng-click="$ctrl.close()" ng-if="$ctrl.confirm.show">{{$ctrl.confirm.title}}</button>
 </div>
   `,
   bindings: {
@@ -21,28 +19,21 @@ const AlertDialogComponent = {
   controller() {
     this.title = this.resolve.title;
     this.message = this.resolve.message;
-    this.actions = this.resolve.actions.map((action) => {
-      action.callback = wrap(action.callback, (callback) => {
-        callback();
-        this.close();
-      });
-      return action;
-    });
+    this.confirm = Object.assign({}, { class: 'btn-sucess', show: true, title: 'OK' }, this.resolve.confirm);
   },
 };
 
 function AlertDialog($uibModal) {
   const service = {
-    open(title, message, actions) {
-      $uibModal.open({
-        // windowClass: 'modal-sm',
+    open(title, message, confirm) {
+      return $uibModal.open({
         component: 'alertDialog',
         resolve: {
           title: () => title,
           message: () => message,
-          actions: () => actions,
+          confirm: () => confirm,
         },
-      });
+      }).result;
     },
   };
 
