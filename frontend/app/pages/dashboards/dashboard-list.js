78f1bb816acaf2d052af4bb1c841b7817f668e36
luke14free <luca.giacomel@gmail.com>
2016-11-18 10:18:35 +0100

    Removing tabs and making the changes ES6 compliant

--- frontend/app/pages/dashboards/dashboard-list.js
+++ frontend/app/pages/dashboards/dashboard-list.js
@@ -1,5 +1,5 @@
+import _ from 'underscore';
 import template from './dashboard-list.html';
-import {_} from 'underscore';
 
 function DashboardListCtrl($scope, Dashboard, $location, currentUser, clientConfig, NgTableParams) {
   const self = this;
@@ -12,27 +12,23 @@ function DashboardListCtrl($scope, Dashboard, $location, currentUser, clientConf
   this.dashboards = Dashboard.query({}); // shared promise
 
   this.selectedTags = []; // in scope because it needs to be accessed inside a table refresh
-  this.searchText = "";
+  this.searchText = '';
 
-  this.tagIsSelected = (tag) => {
-    return this.selectedTags.indexOf(tag) > -1;
-  }
+  this.tagIsSelected = tag => this.selectedTags.indexOf(tag) > -1;
 
   this.toggleTag = (tag) => {
-    if(this.tagIsSelected(tag)){
-      this.selectedTags = this.selectedTags.filter((e) => e!=tag);
-    }else{
+    if (this.tagIsSelected(tag)) {
+      this.selectedTags = this.selectedTags.filter(e => e !== tag);
+    } else {
       this.selectedTags.push(tag);
     }
     this.tableParams.reload();
-  }
+  };
 
   this.allTags = [];
   this.dashboards.$promise.then((data) => {
-    const out = data.map((dashboard) => {
-      return dashboard.name.match(/(^\w+):|(#\w+)/ig);
-    });
-    this.allTags = _.unique(_.flatten(out)).filter((e) => e);
+    const out = data.map(dashboard => dashboard.name.match(/(^\w+):|(#\w+)/ig));
+    this.allTags = _.unique(_.flatten(out)).filter(e => e);
   });
 
   this.tableParams = new NgTableParams({ page, count }, {
@@ -40,8 +36,6 @@ function DashboardListCtrl($scope, Dashboard, $location, currentUser, clientConf
       const options = params.url();
       $location.search('page', options.page);
 
-      const request = {};
-      
       return self.dashboards.$promise.then((data) => {
         params.total(data.count);
         return data.map((dashboard) => {
@@ -49,30 +43,24 @@ function DashboardListCtrl($scope, Dashboard, $location, currentUser, clientConf
           dashboard.untagged_name = dashboard.name.replace(/(\w+):|(#\w+)/ig, '').trim();
           return dashboard;
         }).filter((value) => {
-          if(self.selectedTags.length){
-            const value_tags = new Set(value.tags);
-            const tag_match = self.selectedTags;
-            const filtered_match = tag_match.filter(x => value_tags.has(x));
-            if(tag_match.length != filtered_match.length){
+          if (self.selectedTags.length) {
+            const valueTags = new Set(value.tags);
+            const tagMatch = self.selectedTags;
+            const filteredMatch = tagMatch.filter(x => valueTags.has(x));
+            if (tagMatch.length !== filteredMatch.length) {
               return false;
             }
           }
-          if(self.searchText && self.searchText.length){
-            if(!value.untagged_name.toLowerCase().includes(self.searchText)){
+          if (self.searchText && self.searchText.length) {
+            if (!value.untagged_name.toLowerCase().includes(self.searchText)) {
               return false;
             }
           }
           return true;
         });
       });
-    }
+    },
   });
-
-  this.tabs = [
-    { name: 'All Dashboards', path: 'dashboards' },
-  ];
-
-  this.currentUser = currentUser;
 }
 
 export default function (ngModule) {
