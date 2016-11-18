4a2770f3cf6c52927a5b3852cfae1022ada888c7
luke14free <luca.giacomel@gmail.com>
2016-11-18 12:17:54 +0100

    Adding capabilities to the original script runner

--- redash/query_runner/script.py
+++ redash/query_runner/script.py
@@ -18,17 +18,26 @@ class Script(BaseQueryRunner):
                 'path': {
                     'type': 'string',
                     'title': 'Scripts path'
+                },
+                'shell': {
+                    'type': 'boolean',
+                    'title': 'Execute command through the shell'
                 }
             },
             'required': ['path']
         }
 
     @classmethod
-    def annotate_query(cls):
-        return False
+    def type(cls):
+        return "insecure_script"
+
 
     def __init__(self, configuration):
         super(Script, self).__init__(configuration)
+
+        # If path is * allow any execution path
+        if self.configuration["path"] == "*":
+            return
 
         # Poor man's protection against running scripts from outside the scripts directory
         if self.configuration["path"].find("../") > -1:
@@ -44,13 +53,16 @@ class Script(BaseQueryRunner):
 
             query = query.strip()
 
-            script = os.path.join(self.configuration["path"], query.split(" ")[0])
-            if not os.path.exists(script):
-                return None, "Script '%s' not found in script directory" % query
+            if self.configuration["path"] != "*":
+                script = os.path.join(self.configuration["path"], query.split(" ")[0])
+                if not os.path.exists(script):
+                    return None, "Script '%s' not found in script directory" % query
 
-            script = os.path.join(self.configuration["path"], query)
+                script = os.path.join(self.configuration["path"], query).split(" ")
+            else:
+                script = query.split("*/ ")[1]
 
-            output = subprocess.check_output(script.split(" "), shell=False)
+            output = subprocess.check_output(script, shell=self.configuration['shell'])
             if output is not None:
                 output = output.strip()
                 if output != "":
@@ -66,5 +78,6 @@ class Script(BaseQueryRunner):
             raise sys.exc_info()[1], None, sys.exc_info()[2]
 
         return json_data, error
+
 
 register(Script)
