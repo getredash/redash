cd670b677aa3e5279905f12d853e541463adc79a
Arik Fraimovich <arik@arikfr.com>
2016-11-27 12:00:47 +0200

    Implement sorting for data tables

--- client/app/components/dynamic-table/index.js
+++ client/app/components/dynamic-table/index.js
@@ -1,9 +1,13 @@
+import { sortBy } from 'underscore';
 import template from './dynamic-table.html';
+import './dynamic-table.css';
 
 function DynamicTable() {
   this.itemsPerPage = this.count = 15;
   this.page = 1;
   this.rowsCount = 0;
+  this.orderByField = undefined;
+  this.orderByReverse = false;
 
   this.pageChanged = () => {
     const first = this.count * (this.page - 1);
@@ -18,6 +22,35 @@ function DynamicTable() {
     this.rowsCount = this.allRows.length;
 
     this.pageChanged();
+  };
+
+  this.orderBy = (column) => {
+    if (column === this.orderByField) {
+      this.orderByReverse = !this.orderByReverse;
+    } else {
+      this.orderByField = column;
+      this.orderByReverse = false;
+    }
+
+    if (this.orderByField) {
+      this.allRows = sortBy(this.allRows, this.orderByField);
+      if (this.orderByReverse) {
+        this.allRows = this.allRows.reverse();
+      }
+      this.pageChanged();
+    }
+  };
+
+  this.sortIcon = (column) => {
+    if (column !== this.orderByField) {
+      return null;
+    }
+
+    if (this.orderByReverse) {
+      return 'desc';
+    }
+
+    return 'asc';
   };
 }
 
