b390cd2e3d459ccf69dee45437cf95e07d1a2a80
Arik Fraimovich <arik@arikfr.com>
2016-11-28 11:21:45 +0200

    Close DB connection between tests.
    Otherwise we were running out of connections.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,8 @@
 import os
+import logging
+import datetime
+from unittest import TestCase
+
 os.environ['REDASH_REDIS_URL'] = "redis://localhost:6379/5"
 # Use different url for Celery to avoid DB being cleaned up:
 os.environ['REDASH_CELERY_BROKER'] = "redis://localhost:6379/6"
@@ -8,19 +12,10 @@ os.environ['REDASH_GOOGLE_CLIENT_ID'] = "dummy"
 os.environ['REDASH_GOOGLE_CLIENT_SECRET'] = "dummy"
 os.environ['REDASH_MULTI_ORG'] = "true"
 
-import logging
-from unittest import TestCase
-import datetime
-from redash import create_app, settings
-from factories import Factory
-
-settings.DATABASE_CONFIG = {
-    'name': 'circle_test',
-    'threadlocals': True
-}
-
-from redash import redis_connection
 import redash.models
+from redash import create_app
+from redash import redis_connection
+from factories import Factory
 from tests.handlers import make_request
 
 logging.disable("INFO")
@@ -29,12 +24,12 @@ logging.getLogger("metrics").setLevel("ERROR")
 
 class BaseTestCase(TestCase):
     def setUp(self):
+        redash.models.db.session.close()
         self.app = create_app()
         self.app_ctx = self.app.app_context()
         self.app_ctx.push()
         redash.models.create_db(True, True)
         self.factory = Factory()
-
 
     def tearDown(self):
         redash.models.create_db(False, True)
