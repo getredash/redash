420973c0154a8cb21eab8b707bdd2f6b6b7f2d47
syerushalmy <syerushalmy@outbrain.com>
2016-12-22 11:32:10 +0200

    Added get tables query for cassandra and expilicit protocol version to 3 (Also working with cassandra version 2.1 and above)

--- redash/query_runner/cass.py
+++ redash/query_runner/cass.py
@@ -1,7 +1,7 @@
 import json
 import logging
 
-from redash.query_runner import BaseQueryRunner, register
+from redash.query_runner import BaseSQLQueryRunner, register
 from redash.utils import JSONEncoder
 
 logger = logging.getLogger(__name__)
@@ -14,7 +14,7 @@ except ImportError:
     enabled = False
 
 
-class Cassandra(BaseQueryRunner):
+class Cassandra(BaseSQLQueryRunner):
     noop_query = "SELECT * FROM system.compactions_in_progress"
 
     @classmethod
@@ -54,12 +54,26 @@ class Cassandra(BaseQueryRunner):
         return "Cassandra"
 
     def _get_tables(self, schema):
-        query = """
+        CF_query = """
         select columnfamily_name from system.schema_columnfamilies where keyspace_name = '{}';
         """.format(self.configuration['keyspace'])
 
-        results, error = self.run_query(query, None)
-        return results, error
+        results, error = self.run_query(CF_query, None)
+        results = json.loads(results)
+        for row in results['rows']:
+            table_name = row['columnfamily_name']
+            if table_name not in schema:
+                schema[table_name] = {'name': table_name, 'columns': []}
+            CN_query = """
+            SELECT column_name FROM system.schema_columns where keyspace_name ='{}' and columnfamily_name ='{}';
+            """.format(self.configuration['keyspace'], table_name)
+            columns, error = self.run_query(CN_query, None)
+            columns = json.loads(columns)
+            for col in columns['rows']:
+                column_name = col['column_name']
+                schema[table_name]['columns'].append(col['column_name'])
+
+        return schema.values()
 
     def run_query(self, query, user):
         connection = None
@@ -67,9 +81,9 @@ class Cassandra(BaseQueryRunner):
             if self.configuration.get('username', '') and self.configuration.get('password', ''):
                 auth_provider = PlainTextAuthProvider(username='{}'.format(self.configuration.get('username', '')),
                                                       password='{}'.format(self.configuration.get('password', '')))
-                connection = Cluster([self.configuration.get('host', '')], auth_provider=auth_provider)
+                connection = Cluster([self.configuration.get('host', '')], auth_provider=auth_provider, protocol_version=3)
             else:
-                connection = Cluster([self.configuration.get('host', '')])
+                connection = Cluster([self.configuration.get('host', '')], protocol_version=3)
 
             session = connection.connect()
             session.set_keyspace(self.configuration['keyspace'])
