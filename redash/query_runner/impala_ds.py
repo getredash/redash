78408e50c52e0068b8f2523391252864dc0aa5ab
yuua <mumin.nonnon0216@gmail.com>
2016-12-29 10:54:57 +0900

    impala query_runner get_tables error fix

--- redash/query_runner/impala_ds.py
+++ redash/query_runner/impala_ds.py
@@ -82,11 +82,11 @@ class Impala(BaseSQLQueryRunner):
     def _get_tables(self, schema_dict):
         schemas_query = "show schemas;"
         tables_query = "show tables in %s;"
-        columns_query = "show column stats %s;"
+        columns_query = "show column stats %s.%s;"
 
-        for schema_name in map(lambda a: a['name'], self._run_query_internal(schemas_query)):
-            for table_name in map(lambda a: a['name'], self._run_query_internal(tables_query % schema_name)):
-                columns = map(lambda a: a['Column'], self._run_query_internal(columns_query % table_name))
+        for schema_name in filter(lambda a: len(a) > 0, map(lambda a: str(a['name']), self._run_query_internal(schemas_query))):
+            for table_name in filter(lambda a: len(a) > 0, map(lambda a: str(a['name']), self._run_query_internal(tables_query % schema_name))):
+                columns = filter(lambda a: len(a) > 0, map(lambda a: str(a['Column']), self._run_query_internal(columns_query % (schema_name, table_name))))
 
                 if schema_name != 'default':
                     table_name = '{}.{}'.format(schema_name, table_name)
