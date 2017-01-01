fe363e27a338454bbede4a7f1390654274fe5af1
Arik Fraimovich <arik@arikfr.com>
2017-01-01 10:39:42 +0200

    Flatten dashboards list in menu

--- client/app/components/app-header/index.js
+++ client/app/components/app-header/index.js
@@ -1,16 +1,15 @@
-import { omit, groupBy, sortBy } from 'underscore';
-
 import template from './app-header.html';
 import logoUrl from '../../assets/images/redash_icon_small.png';
 
 function controller($scope, $location, $uibModal, Auth, currentUser, Dashboard) {
-  this.dashboards = [];
   // TODO: logoUrl should come from clientconfig
   this.logoUrl = logoUrl;
+  this.currentUser = currentUser;
   this.showQueriesMenu = currentUser.hasPermission('view_query');
   this.showNewQueryMenu = currentUser.hasPermission('create_query');
   this.showSettingsMenu = currentUser.hasPermission('list_users');
-  this.currentUser = currentUser;
+  this.showDashboardsMenu = currentUser.hasPermission('list_dashboards');
+  this.dashboards = Dashboard.recent();
 
   this.newDashboard = () => {
     $uibModal.open({
@@ -21,23 +20,6 @@ function controller($scope, $location, $uibModal, Auth, currentUser, Dashboard) 
     });
   };
 
-  this.reloadDashboards = () => {
-    Dashboard.recent((dashboards) => {
-      this.dashboards = sortBy(dashboards, 'name');
-      this.allDashboards = groupBy(this.dashboards, (d) => {
-        const parts = d.name.split(':');
-        if (parts.length === 1) {
-          return 'Other';
-        }
-        return parts[0];
-      });
-
-      this.otherDashboards = this.allDashboards.Other || [];
-      this.groupedDashboards = omit(this.allDashboards, 'Other');
-      this.showDashboardsMenu = this.groupedDashboards.length > 0 || this.otherDashboards.length > 0 || currentUser.hasPermission('create_dashboard');
-    });
-  };
-
   this.searchQueries = () => {
     $location.path('/queries/search').search({ q: $scope.term });
   };
@@ -45,8 +27,6 @@ function controller($scope, $location, $uibModal, Auth, currentUser, Dashboard) 
   this.logout = () => {
     Auth.logout();
   };
-
-  this.reloadDashboards();
 }
 
 export default function (ngModule) {
