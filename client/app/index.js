931c6b92ef14e2d5776453af75c1de94e64e19be
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2017-01-25 16:17:37 +0200

    Transfer all css/less dependencies to main.less file

--- client/app/index.js
+++ client/app/index.js
@@ -1,11 +1,3 @@
-import 'material-design-iconic-font/dist/css/material-design-iconic-font.css';
-import 'font-awesome/css/font-awesome.css';
-import 'ui-select/dist/select.css';
-import 'angular-toastr/dist/angular-toastr.css';
-import 'angular-resizable/src/angular-resizable.css';
-import 'angular-gridster/dist/angular-gridster.css';
-import 'pace-progress/themes/blue/pace-theme-minimal.css';
-
 import 'pace-progress';
 import debug from 'debug';
 import angular from 'angular';
