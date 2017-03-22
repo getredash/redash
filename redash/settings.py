6d5b89fae140046ee929905ddad150d4dd21e5d0
imsally <imsally98@gmail.com>
2017-03-22 16:51:40 +0900

    add hive query runner for kerberos + selected impyla instead of pyhive to use another auth mechanism + support default encoding to UTF-8

--- redash/settings.py
+++ redash/settings.py
@@ -176,6 +176,7 @@ default_query_runners = [
     'redash.query_runner.elasticsearch',
     'redash.query_runner.presto',
     'redash.query_runner.hive_ds',
+    'redash.query_runner.hive_kerberos',
     'redash.query_runner.impala_ds',
     'redash.query_runner.vertica',
     'redash.query_runner.clickhouse',
