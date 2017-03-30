37ff9f9d16ffa9d55c45819cca6503373e3af7bf
Renjith Nair <shrirenjith@gmail.com>
2017-03-30 19:15:58 -0400

    Fixed DB2 Runner Issues . Added the DB2 DataSource to settings.py  (Note that you will need to install libxml2 dependencies to get this work)

--- redash/query_runner/DB2.py
+++ redash/query_runner/DB2.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 import json
 import logging
@@ -5,37 +6,34 @@ import logging
 from redash.utils import JSONEncoder
 from redash.query_runner import *
 
+try:
+    import ibm_db_dbi
+    import ibm_db
+
+    types_map = {
+       ibm_db_dbi.FLOAT: TYPE_FLOAT,
+       ibm_db_dbi.NUMBER: TYPE_INTEGER,
+       ibm_db_dbi.STRING: TYPE_STRING,
+       ibm_db_dbi.DATE: TYPE_DATE,
+       ibm_db_dbi.DATETIME: TYPE_DATETIME,
+    }
+    ENABLED = True
+except ImportError:
+    ENABLED = False
+
 logger = logging.getLogger(__name__)
 
-"""
- NAME                  	TYPEID	REDASH TYPE 
- --------------------- 	------	 
-FLOAT - DECFLOAT             	4 : TYPE_FLOAT,
-FLOAT - DOUBLE               	8 :	TYPE_FLOAT,
-FLOAT - REAL                 	10 : TYPE_FLOAT,
-DECIMAL -  DECIMAL             	16 : TYPE_FLOAT,
-BIGINT - BIGINT               	20 : TYPE_INTEGER,
-NUMBER - INTEGER              	24 : TYPE_INTEGER,
-NUMBER - SMALLINT             	28 : TYPE_INTEGER,
-STRING - LONG VARCHAR         	52 : TYPE_STRING,
-STRING - VARCHAR              	56 : TYPE_STRING,
-STRING - CHARACTER            	60 : TYPE_STRING,
-DATE -  DATE                 	100 : TYPE_DATE,
-DATETIME - TIMESTAM            	108 : TYPE_DATETIME,
-			BOOLEAN            	112 : TYPE_BOOLEAN 
-"""
-types_map = {
- 	ibm_db_dbi.FLOAT : TYPE_FLOAT,
- 	ibm_db_dbi.NUMBER : TYPE_INTEGER,
- 	ibm_db_dbi.STRING : TYPE_STRING,
- 	ibm_db_dbi.DATE : TYPE_DATE,
- 	ibm_db_dbi.DATETIME : TYPE_DATETIME
-	#ibm_db_dbi. : TYPE_BOOLEAN 
-}
 
-
-class DB2(BaseSQLQueryRunner):
+class Db2(BaseSQLQueryRunner):
     noop_query = "SELECT 1 FROM sysibm.sysdummy1 "
+
+    @classmethod
+    def enabled(cls):
+        return True
+
+    @classmethod
+    def name(cls):
+        return "IBM DB2"        
 
     @classmethod
     def configuration_schema(cls):
@@ -68,32 +66,13 @@ class DB2(BaseSQLQueryRunner):
             'secret': ['password']
         }
 
-    @classmethod
-    def enabled(cls):
-        try:
-            import ibm_db
-        except ImportError:
-            return False
 
-        return True
 
     def __init__(self, configuration):
-        super(DB2, self).__init__(configuration)
+        super(Db2, self).__init__(configuration)
 
     def _get_tables(self, schema):
-        query = """
-        Select 
-			t.creator table_schema,
-			t.name table_name,
-			c.name column_name   
-		FROM 
-			sysibm.systables t
-		INNER JOIN sysibm.syscolumns  c 
-			on t.name=c.tbname 
-			and t.creator=c.tbcreator
-		WHERE 
-			t.creator NOT LIKE 'SYS%';
-        """
+        query = "select NAME,TBNAME,TBCREATOR from sysibm.syscolumns WHERE TBCREATOR NOT LIKE 'SYS%'"
 
         results, error = self.run_query(query, None)
 
@@ -103,12 +82,12 @@ class DB2(BaseSQLQueryRunner):
         results = json.loads(results)
 
         for row in results['rows']:
-            table_name = '{}.{}'.format(row['table_schema'], row['table_name'])
+            table_name = '{}.{}'.format(row['TBCREATOR'].rstrip(), row['TBNAME'])
 
             if table_name not in schema:
                 schema[table_name] = {'name': table_name, 'columns': []}
 
-            schema[table_name]['columns'].append(row['column_name'])
+            schema[table_name]['columns'].append(row['NAME'])
 
         return schema.values()
 
@@ -122,24 +101,18 @@ class DB2(BaseSQLQueryRunner):
 
         connection = None
         try:
-            conn_info = 
-				"DATABASE={};HOSTNAME={};PORT={};PROTOCOL=TCPIP;UID={};PWD{}".format(
-					self.configuration.get('database', ''),
-					self.configuration.get('host', ''),
-					self.configuration.get('port', 60000),
-					self.configuration.get('user', ''),
-					self.configuration.get('password', ''))
+            conn_info = "DATABASE={};HOSTNAME={};PORT={};PROTOCOL=TCPIP;UID={};PWD={}".format(
+                self.configuration.get('database', ''),
+                self.configuration.get('host', ''),
+                self.configuration.get('port', 60000),
+                self.configuration.get('user', ''),
+                self.configuration.get('password', ''))
+
             connection = ibm_db_dbi.connect(conn_info,"","")
-			logger.debug("DB2 running query: %s", query)
+
+            logger.debug("DB2 running query: %s", query)
             cursor = connection.cursor()
-			cursor.execute(sql)
-            # Process results
-			result = ibm_db.fetch_both(statement)
-			
-			# Get Column Data
-			results.keys()
-			
-			# Loop over data
+            cursor.execute(query)
             if cursor.description is not None:
                 columns_data = [(i[0], i[1]) for i in cursor.description]
 
@@ -154,7 +127,6 @@ class DB2(BaseSQLQueryRunner):
             else:
                 json_data = None
                 error = "No data was returned."
-
             cursor.close()
         except KeyboardInterrupt:
             error = "Query cancelled by user."
@@ -167,4 +139,4 @@ class DB2(BaseSQLQueryRunner):
 
         return json_data, error
 
-register(DB2)
+register(Db2)
