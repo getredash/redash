5e3440da912541557bea1446651f409d7c49ca58
Blake Imsland <blake@retroco.de>
2017-04-19 10:23:14 -0700

    Update Circle CI for our workflow
    - Use new master / rc release release strategy (#440)
    - Migrate Circle CI 2.0 (#488, #502, #923)
    - Install redash-stmo.
    
    In the long run we'll be able to install additional dependencies by
    having an own Dockerfile to build images based on the Redash image
    but that installs additional Python dependencies. But until we have
    a fork with lots of changes ourselves we need to do it this way.
    
    Redash-stmo contains the ability to hook up our own Dockerflow
    library.
    
    Disable handlers test that tests the login page and the existence of the
    remote auth link there. We override this functionality for user
    experience reasons via redash-stmo and redirect from the login page to
    the remote auth URL.
    
    Update metrics test since our auth changes add a database call which
    also gets timed, and therefore assert this was called only once fails.
    
    Refs #13
    Refs #37

--- tests/metrics/test_request.py
+++ tests/metrics/test_request.py
@@ -6,4 +6,4 @@ from tests import BaseTestCase
 class TestRequestMetrics(BaseTestCase):
     def test_flask_request_records_statsd_metrics(self, timing):
         self.client.get("/ping")
-        timing.assert_called_once_with("requests.redash_ping.get", ANY)
+        timing.assert_called_with("requests.redash_ping.get", ANY)
--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -101,25 +101,25 @@ class TestLogin(BaseTestCase):
         rv = self.client.get("/default/login")
         self.assertEqual(rv.status_code, 200)
 
-    def test_get_login_form_remote_auth(self):
-        """Make sure the remote auth link can be rendered correctly on the
-        login page when the remote user login feature is enabled"""
-        old_remote_user_enabled = settings.REMOTE_USER_LOGIN_ENABLED
-        old_ldap_login_enabled = settings.LDAP_LOGIN_ENABLED
-        try:
-            settings.REMOTE_USER_LOGIN_ENABLED = True
-            settings.LDAP_LOGIN_ENABLED = True
-            rv = self.client.get("/default/login")
-            self.assertEqual(rv.status_code, 200)
-            self.assertIn(
-                "/{}/remote_user/login".format(self.factory.org.slug), rv.data.decode()
-            )
-            self.assertIn(
-                "/{}/ldap/login".format(self.factory.org.slug), rv.data.decode()
-            )
-        finally:
-            settings.REMOTE_USER_LOGIN_ENABLED = old_remote_user_enabled
-            settings.LDAP_LOGIN_ENABLED = old_ldap_login_enabled
+    # def test_get_login_form_remote_auth(self):
+    #     """Make sure the remote auth link can be rendered correctly on the
+    #     login page when the remote user login feature is enabled"""
+    #     old_remote_user_enabled = settings.REMOTE_USER_LOGIN_ENABLED
+    #     old_ldap_login_enabled = settings.LDAP_LOGIN_ENABLED
+    #     try:
+    #         settings.REMOTE_USER_LOGIN_ENABLED = True
+    #         settings.LDAP_LOGIN_ENABLED = True
+    #         rv = self.client.get("/default/login")
+    #         self.assertEqual(rv.status_code, 200)
+    #         self.assertIn(
+    #             "/{}/remote_user/login".format(self.factory.org.slug), rv.data.decode()
+    #         )
+    #         self.assertIn(
+    #             "/{}/ldap/login".format(self.factory.org.slug), rv.data.decode()
+    #         )
+    #     finally:
+    #         settings.REMOTE_USER_LOGIN_ENABLED = old_remote_user_enabled
+    #         settings.LDAP_LOGIN_ENABLED = old_ldap_login_enabled
 
     def test_submit_non_existing_user(self):
         with patch("redash.handlers.authentication.login_user") as login_user_mock:
