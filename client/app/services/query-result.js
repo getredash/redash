f6802bccd6876f0cfaf69cdecc21507e6f3bbd52
Arik Fraimovich <arik@arikfr.com>
2017-05-04 10:39:39 +0300

    use relative links

--- client/app/services/query-result.js
+++ client/app/services/query-result.js
@@ -414,7 +414,7 @@ function QueryResultService($resource, $timeout, $q) {
     }
 
     getLink(queryId, fileType, apiKey) {
-      let link = `/api/queries/${queryId}/results/${this.getId()}.${fileType}`;
+      let link = `api/queries/${queryId}/results/${this.getId()}.${fileType}`;
       if (apiKey) {
         link = `${link}?api_key=${apiKey}`;
       }
