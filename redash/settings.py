8be9613640899e80db355d2ff59dfebbdb304a9c
Amar Ramachandran <amarramachandran@Amars-MacBook-Pro.local>
2017-06-20 12:07:48 -0700

    Add ldap env. config settings

--- redash/settings.py
+++ redash/settings.py
@@ -124,6 +124,27 @@ SAML_CALLBACK_SERVER_NAME = os.environ.get("REDASH_SAML_CALLBACK_SERVER_NAME", "
 REMOTE_USER_LOGIN_ENABLED = parse_boolean(os.environ.get("REDASH_REMOTE_USER_LOGIN_ENABLED", "false"))
 REMOTE_USER_HEADER = os.environ.get("REDASH_REMOTE_USER_HEADER", "X-Forwarded-Remote-User")
 
+
+
+# If REDASH_PASSWORD_LOGIN_ENABLED is not false, then users will still be able to login through Redash instead of the LDAP server
+LDAP_LOGIN_ENABLED = parse_boolean(os.environ.get("REDASH_LDAP_LOGIN_ENABLED", 'false'))
+# Includes port (ex. 10.0.10.1:389)
+LDAP_HOST_URL = os.environ.get('REDASH_LDAP_URL', None)
+# The DN of users who should be allowed to login (ex. cn=users,dc=organiztion,dc=local)
+LDAP_BIND_DN = os.environ.get('REDASH_LDAP_BIND_DN', None)
+# NTSM for Active Directory
+LDAP_AUTHENTICATION_METHOD = os.environ.get('REDASH_LDAP_AUTHENTICATION_METHOD', "SIMPLE")
+# Namespace when logging in org\{username} ~needed for Active Directory
+LDAP_NAMESPACE = os.environ.get('REDASH_LDAP_NAMESPACE', "")
+LDAP_UID_KEY = os.environ.get('REDASH_LDAP_UID_KEY', "sAMAccountName")
+# AD/LDAP email and display name keys
+LDAP_DISPLAY_NAME_KEY = os.environ.get('REDASH_LDAP_DISPLAY_NAME_KEY', "displayName")
+LDAP_EMAIL_KEY = os.environ.get('REDASH_LDAP_EMAIL_KEY', "mail")
+# Prompt that should be shown above username/email field.
+LDAP_CUSTOM_USERNAME_PROMPT = os.environ.get('REDASH_LDAP_LDAP_CUSTOM_USERNAME_PROMPT', "LDAP/AD/SSO username:")
+
+
+
 # Usually it will be a single path, but we allow to specify additional ones to override the default assets. Only the
 # last one will be used for Flask templates.
 STATIC_ASSETS_PATHS = [fix_assets_path(path) for path in os.environ.get("REDASH_STATIC_ASSETS_PATH", "../client/dist/").split(',')]
@@ -252,3 +273,7 @@ COMMON_CLIENT_CONFIG = {
     'mailSettingsMissing': MAIL_DEFAULT_SENDER is None,
     'logoUrl': LOGO_URL
 }
+
+
+
+
