f84068137709105b7a0de91f2da26f46cd7eb8a9
mitsuhiro_yamashita <m.yamashita@denryoku.co.jp>
2017-06-29 08:21:22 +0900

    change this to WRITER_ENCODING and REDASH_CSV_WRITER_ENCODING

--- redash/utils/__init__.py
+++ redash/utils/__init__.py
@@ -18,7 +18,7 @@ from .human_time import parse_human_time
 from redash import settings
 
 COMMENTS_REGEX = re.compile("/\*.*?\*/")
-WRITER_ENCODE = os.environ.get('REDASH_WRITER_ENCODE', 'utf-8')
+WRITER_ENCODING = os.environ.get('REDASH_CSV_WRITER_ENCODING', 'utf-8')
 
 
 def utcnow():
@@ -104,7 +104,7 @@ class UnicodeWriter:
     which is encoded in the given encoding.
     """
 
-    def __init__(self, f, dialect=csv.excel, encoding=WRITER_ENCODE, **kwds):
+    def __init__(self, f, dialect=csv.excel, encoding=WRITER_ENCODING, **kwds):
         # Redirect output to a queue
         self.queue = cStringIO.StringIO()
         self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
@@ -113,7 +113,7 @@ class UnicodeWriter:
 
     def _encode_utf8(self, val):
         if isinstance(val, (unicode, str)):
-            return val.encode(WRITER_ENCODE)
+            return val.encode(WRITER_ENCODING)
 
         return val
 
@@ -121,7 +121,7 @@ class UnicodeWriter:
         self.writer.writerow([self._encode_utf8(s) for s in row])
         # Fetch UTF-8 output from the queue ...
         data = self.queue.getvalue()
-        data = data.decode(WRITER_ENCODE)
+        data = data.decode(WRITER_ENCODING)
         # ... and reencode it into the target encoding
         data = self.encoder.encode(data)
         # write to the target stream
