92f93f8ff6a8c7919b7759f7ca019c46eca81250
Arik Fraimovich <arik@arikfr.com>
2017-06-28 19:10:26 +0300

    No need to load user for recent queries.

--- redash/handlers/queries.py
+++ redash/handlers/queries.py
@@ -2,10 +2,11 @@ from itertools import chain
 
 import sqlparse
 from flask import jsonify, request
-from funcy import distinct, take
-
 from flask_login import login_required
 from flask_restful import abort
+from funcy import distinct, take
+from sqlalchemy.orm.exc import StaleDataError
+
 from redash import models, settings
 from redash.handlers.base import (BaseResource, get_object_or_404,
                                   org_scoped_rule, paginate, routes)
@@ -15,7 +16,6 @@ from redash.permissions import (can_modify, not_view_only, require_access,
                                 require_object_modify_permission,
                                 require_permission, view_only)
 from redash.utils import collect_parameters_from_request
-from sqlalchemy.orm.exc import StaleDataError
 
 
 @routes.route(org_scoped_rule('/api/queries/format'), methods=['POST'])
@@ -60,14 +60,14 @@ class QueryRecentResource(BaseResource):
 
         if settings.FEATURE_DUMB_RECENTS:
             results = models.Query.by_user(self.current_user).order_by(models.Query.updated_at.desc()).limit(10)
-            queries = [q.to_dict(with_last_modified_by=False) for q in results]
+            queries = [q.to_dict(with_last_modified_by=False, with_user=False) for q in results]
         else:
             queries = models.Query.recent(self.current_user.group_ids, self.current_user.id)
-            recent = [d.to_dict(with_last_modified_by=False) for d in queries]
+            recent = [d.to_dict(with_last_modified_by=False, with_user=False) for d in queries]
 
             global_recent = []
             if len(recent) < 10:
-                global_recent = [d.to_dict(with_last_modified_by=False) for d in models.Query.recent(self.current_user.group_ids)]
+                global_recent = [d.to_dict(with_last_modified_by=False, with_user=False) for d in models.Query.recent(self.current_user.group_ids)]
 
             queries = take(20, distinct(chain(recent, global_recent), key=lambda d: d['id']))
 
