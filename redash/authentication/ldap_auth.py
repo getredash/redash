98c3eac490189cfd8008e597263bb4ce0bdbfb67
Amar Ramachandran <amarramachandran@Amars-MacBook-Pro.local>
2017-06-29 10:32:13 -0700

    Move ldap auth logic to function

--- redash/authentication/ldap_auth.py
+++ redash/authentication/ldap_auth.py
@@ -1,9 +1,8 @@
 import logging
 
 from flask import flash, redirect, render_template, request, url_for, Blueprint
-from ldap3 import Server, Connection, ALL, NTLM, SIMPLE
-from ldap3.core.exceptions import LDAPBindError, LDAPException
 from flask_login import current_user, login_required, login_user, logout_user
+from ldap3 import Server, Connection, SIMPLE
 
 from redash import settings
 from redash.authentication.google_oauth import create_and_login_user
@@ -28,25 +27,34 @@ def login(org_slug=None):
         return redirect(next_path)
 
     if request.method == 'POST':
-        try:
-            server = Server(settings.LDAP_HOST_URL)
-            conn = Connection(server, settings.LDAP_NAMESPACE+request.form['email'], password=request.form['password'], authentication=settings.LDAP_AUTHENTICATION_METHOD, auto_bind=True)
+        user = auth_ldap_user(request.form['email'], request.form['password'])
 
-            conn.search(settings.LDAP_BIND_DN, '(' + settings.LDAP_UID_KEY + '=' + request.form['email'] + ')', attributes=[settings.LDAP_DISPLAY_NAME_KEY, settings.LDAP_EMAIL_KEY])
-
-            if conn.entries[0] is not None:
-                user = create_and_login_user(current_org, conn.entries[0][settings.LDAP_DISPLAY_NAME_KEY].value, conn.entries[0][settings.LDAP_EMAIL_KEY].value)
-                return redirect(next_path or url_for('redash.index'))
-            else:
-                flash("Incorrect credentials.")
-        except LDAPBindError:
+        if user is not None:
+            create_and_login_user(current_org, user[settings.LDAP_DISPLAY_NAME_KEY][0], user[settings.LDAP_EMAIL_KEY][0])
+            return redirect(next_path or url_for('redash.index'))
+        else:
             flash("Incorrect credentials.")
-        except LDAPException:
-            logging.exception("Unkown error connecting to LDAP.")
-            flash("Error connecting to LDAP.")
 
     return render_template("login.html",
                            org_slug=org_slug,
                            next=next_path,
                            email=request.form.get('email', ''),
-                           username_prompt=settings.LDAP_CUSTOM_USERNAME_PROMPT)
+                           username_prompt=settings.LDAP_CUSTOM_USERNAME_PROMPT,
+                           hide_forgot_password=True)
+
+
+def auth_ldap_user(username, password):
+    server = Server(settings.LDAP_HOST_URL)
+    conn = Connection(server, settings.LDAP_BIND_DN, password=settings.LDAP_BIND_DN_PASSWORD, authentication=SIMPLE, auto_bind=True)
+
+    conn.search(settings.LDAP_SEARCH_DN, settings.LDAP_SEARCH_TEMPLATE % {"username": username}, attributes=[settings.LDAP_DISPLAY_NAME_KEY, settings.LDAP_EMAIL_KEY])
+
+    if len(conn.entries) == 0:
+        return None
+
+    user = conn.entries[0]
+
+    if not conn.rebind(user=user.entry_dn, password=password):
+        return None
+
+    return user
