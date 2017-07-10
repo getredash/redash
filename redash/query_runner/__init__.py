f55219c4bbb3ea21b47264c49636c039e6440faa
Alison <github@bankofknowledge.net>
2017-07-10 17:42:09 -0500

    updates based on PR comments

--- redash/query_runner/__init__.py
+++ redash/query_runner/__init__.py
@@ -77,16 +77,21 @@ class BaseQueryRunner(object):
         if self.data_source_version_query is None:
             raise NotImplementedError
         data, error = self.run_query(self.data_source_version_query, None)
-        version = json.loads(data)['rows'][0]['version']
-        if(self.data_source_version_post_process == "split by space take second"):
-            version = version.split(" ")[1]
-        elif(self.data_source_version_post_process == "split by space take last"):
-            version = version.split(" ")[-1]
-        elif(self.data_source_version_post_process == "none"):
-            version = version
-        
+
         if error is not None:
             raise Exception(error)
+
+        try:
+            version = json.loads(data)['rows'][0]['version']
+        except KeyError as e:
+            raise Exception(e)
+            
+        if self.data_source_version_post_process == "split by space take second":
+            version = version.split(" ")[1]
+        elif self.data_source_version_post_process == "split by space take last":
+            version = version.split(" ")[-1]
+        elif self.data_source_version_post_process == "none":
+            version = version
 
         return version
 
