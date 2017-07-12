6d8880c10dab2614e0c0856da68a0dd1413fb655
Tyler Brock <tyler.brock@gmail.com>
2017-07-12 09:35:28 -0700

    Add SSL configuration option for PostgreSQL

--- redash/query_runner/pg.py
+++ redash/query_runner/pg.py
@@ -70,6 +70,11 @@ class PostgreSQL(BaseSQLQueryRunner):
                 "dbname": {
                     "type": "string",
                     "title": "Database Name"
+                },
+                "sslmode": {
+                   "type": "string",
+                   "title": "SSL Mode",
+                   "default": "prefer"
                 }
             },
             "order": ['host', 'port', 'user', 'password'],
