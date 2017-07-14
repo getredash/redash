0302414362fb0b02ae4058b2e659a7d87b963455
Alison <github@bankofknowledge.net>
2017-07-14 18:50:09 -0500

    small bug fix

--- redash/query_runner/athena.py
+++ redash/query_runner/athena.py
@@ -58,7 +58,7 @@ class AthenaUpstream(BaseQueryRunner):
 
     @classmethod
     def configuration_schema(cls):
-        schema = return {
+        schema = {
             'type': 'object',
             'properties': {
                 'region': {
