948c5fc2d2f257e4425bb222a97409c0d20df672
Alison <github@bankofknowledge.net>
2017-07-28 06:36:19 -0500

    add column type info to query runners (re #152, #23)

--- redash/query_runner/mysql.py
+++ redash/query_runner/mysql.py
@@ -107,7 +107,8 @@ class Mysql(BaseSQLQueryRunner):
         query = """
         SELECT col.table_schema,
                col.table_name,
-               col.column_name
+               col.column_name,
+               col.column_type
         FROM `information_schema`.`columns` col
         WHERE col.table_schema NOT IN ('information_schema', 'performance_schema', 'mysql');
         """
@@ -128,7 +129,7 @@ class Mysql(BaseSQLQueryRunner):
             if table_name not in schema:
                 schema[table_name] = {'name': table_name, 'columns': []}
 
-            schema[table_name]['columns'].append(row['column_name'])
+            schema[table_name]['columns'].append(row['column_name'] + ' (' + row['column_type'] + ')')
 
         return schema.values()
 
