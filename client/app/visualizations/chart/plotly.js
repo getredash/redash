1a8078ab0345108ef91ca5a17835b3c6816f15ba
deecay <daiju.kito@gmail.com>
2017-07-31 19:22:25 +0900

    Fix: Custom code keeps appending trace per refresh

--- client/app/visualizations/chart/plotly.js
+++ client/app/visualizations/chart/plotly.js
@@ -499,9 +499,9 @@ const CustomPlotlyChart = (clientConfig) => {
         return;
       }
       const refresh = () => {
-        // Clear existing data
-        element[0].children[0].calcdata = [];
-        element[0].children[0].data = [];
+        // Clear existing data with blank data for succeeding codeCall adds data to existing plot.
+        Plotly.newPlot(element[0].children[0]);
+
         // eslint-disable-next-line no-eval
         const codeCall = eval(`codeCall = function(x, ys, element, Plotly){ ${scope.options.customCode} }`);
         codeCall(scope.x, scope.ys, element[0].children[0], Plotly);
