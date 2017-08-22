4f830b2e52e8083b8ac8e3c17f23aa180962f6d6
Maxime Fouilleul <maxime.fouilleul@blablacar.com>
2017-08-22 16:58:49 +0200

    Fix codeclimate

--- redash/query_runner/mysql.py
+++ redash/query_runner/mysql.py
@@ -158,9 +158,8 @@ class Mysql(BaseSQLQueryRunner):
                 else:
                     if rows_count is not None:
                         transaction = True
-                        columns = [{'name': 'Row(s) Count', 
-                                    'type': types_map.get(str(type(rows_count)).upper(), 
-                                    None)}]
+                        columns = [{'name': 'Row(s) Count',
+                                    'type': types_map.get(str(type(rows_count)).upper(),None)}]
                         transaction_rows += rows_count
 
             if transaction and transaction_rows > 0:
