f337f5ea8db21dad86d35b80245438b487a955e9
Nima <nsanati@surexdirect.com>
2017-09-07 16:52:20 -0600

    Add Apache Drill for running query on Zendesk JSON Output

--- redash/query_runner/zendesk.py
+++ redash/query_runner/zendesk.py
@@ -1,8 +1,8 @@
 import requests
 import json
 import string
-from redash.utils import JSONEncoder
 from redash.query_runner import BaseQueryRunner, register
+from pydrill.client import PyDrill
 class Zendesk(BaseQueryRunner):
     @classmethod
     def configuration_schema(cls):
@@ -46,30 +46,45 @@ class Zendesk(BaseQueryRunner):
             response = requests.get(url,auth=(email, pwd))
             response.raise_for_status()
             json_data = response.content.strip()
-            column_names = []
-            column_names.append('created_at')
             columns = []
             columns.append({
             "friendly_name": "created_at",
-            "type": "date",
+            "type": "string",
             "name": "created_at"
             })
             columns.append({
-            "friendly_name": "updated_at",
-            "type": "date",
-            "name": "updated_at"
+            "friendly_name": "tags",
+            "type": "string",
+            "name": "tags"
             })
-            temp=json.loads(json_data)
-            while(temp['next_page'] is not None):
-                response = requests.get(temp['next_page'],auth=(email, pwd))
-                response.raise_for_status()
-                temp = response.content.strip()
-                json_data=json_data+temp
-            temp = None 
-            json_data = string.replace(json_data,'results','rows')
             json_data = json.loads(json_data)
-            json_data['columns']=columns
-            json_data = json.dumps(json_data,encoding='latin1')
+            row={}
+            rows=[]
+            for i in json_data['results']:
+                row['id']= i['id']
+                row['created_at']= i['created_at']
+                row['status']= i['status']
+                row['tags']= i['tags']
+                rows.append(row)
+            while(json_data['next_page'] is not None):
+                response = requests.get(json_data['next_page'],auth=(email, pwd))
+                response.raise_for_status()
+                json_data = response.content.strip()
+                json_data = json.loads(json_data)
+                for i in json_data['results']:
+                    row['id']= i['id']
+                    row['created_at']= i['created_at']
+                    row['status']= i['status']
+                    row['tags']= i['tags']
+                    rows.append(row)
+                    row={}
+            drill = PyDrill(host='localhost', port=8047)
+            yelp_reviews = drill.query('''SELECT t.flatdata.id FROM (select flatten(Rows1) AS flatdata FROM dfs.`/home/nima/Desktop/data.json`) t''')
+            for result in yelp_reviews:
+            print result       
+            #json_data = {'columns':columns,'rows':rows}
+            
+            #json_data = json.dumps(json_data,encoding='latin1')
             return json_data, error
             if not json_data:
                 error = "Got empty response from '{}'.".format(url)
@@ -81,4 +96,3 @@ class Zendesk(BaseQueryRunner):
             json_data = None
         return json_data, error
 register(Zendesk)
-
