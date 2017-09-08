8f11b7bf0c0a54d85b5f6ab3eb926f620bc6e620
Nima <nsanati@surexdirect.com>
2017-09-08 16:52:43 -0600

    modify Apache Drill data source

--- redash/query_runner/zendesk.py
+++ redash/query_runner/zendesk.py
@@ -3,6 +3,7 @@ import json
 import string
 from redash.query_runner import BaseQueryRunner, register
 from pydrill.client import PyDrill
+from random import randint
 class Zendesk(BaseQueryRunner):
     @classmethod
     def configuration_schema(cls):
@@ -11,7 +12,7 @@ class Zendesk(BaseQueryRunner):
             "properties": {
                 "url": {
                     "type": "string",
-                    "default": "https://domain.zendesk.com/api/v2/groups.json"
+                    "default": "https://domain.zendesk.com/api/v2/search.json?"
                 },
                 "email": {
                     "type": "string",
@@ -21,42 +22,32 @@ class Zendesk(BaseQueryRunner):
                     "type": "string"
                 }
             },
-            "required": ["email"],
+            "required": ["email","pwd"],
             "secret": ["pwd"]
         }
 
     @classmethod
     def annotate_query(cls):
         return False
+    def __init__(self, configuration):
+        super(Zendesk, self).__init__(configuration)
+        self.auth = (self.configuration["email"], self.configuration["pwd"])
+        self.base_url = self.configuration.get("url", None)
     def test_connection(self):
-        pass
+        response = requests.get(self.base_url,auth=(self.configuration.get("email", None), self.configuration.get("pwd", None)))
+        if response.status_code != 200:
+            raise Exception("Not Authorized! (http status code: {0}).".format(response.status_code))
     def run_query(self, query, user):
-        base_url = self.configuration.get("url", None)
-        pwd = self.configuration.get("pwd", None)
-        email = self.configuration.get("email", None)
         try:
+            jsonfilepath='/tmp/'+randint(0, 1000)+'.json'
             error = None
             query = query.strip()
-            if base_url is not None and base_url != "":
-                if query.find("://") > -1:
-                    return None, "Accepting only relative URLs to '%s'" % base_url
-            if base_url is None:
-                base_url = ""
-            url = base_url + query
-            response = requests.get(url,auth=(email, pwd))
+            if query.find("://") > -1:
+                    return None, "Accepting only relative URLs to '%s'" % self.base_url
+            url = self.base_url + query
+            response = requests.get(url,auth=self.auth )
             response.raise_for_status()
             json_data = response.content.strip()
-            columns = []
-            columns.append({
-            "friendly_name": "created_at",
-            "type": "string",
-            "name": "created_at"
-            })
-            columns.append({
-            "friendly_name": "tags",
-            "type": "string",
-            "name": "tags"
-            })
             json_data = json.loads(json_data)
             row={}
             rows=[]
@@ -67,7 +58,7 @@ class Zendesk(BaseQueryRunner):
                 row['tags']= i['tags']
                 rows.append(row)
             while(json_data['next_page'] is not None):
-                response = requests.get(json_data['next_page'],auth=(email, pwd))
+                response = requests.get(json_data['next_page'],auth=self.auth)
                 response.raise_for_status()
                 json_data = response.content.strip()
                 json_data = json.loads(json_data)
@@ -78,13 +69,27 @@ class Zendesk(BaseQueryRunner):
                     row['tags']= i['tags']
                     rows.append(row)
                     row={}
+            json_data1 = {'temp':rows}
+            with open(jsonfilepath,'w+') as outfile:
+                json.dump(json_data1,outfile)
+            rows=[]
             drill = PyDrill(host='localhost', port=8047)
-            yelp_reviews = drill.query('''SELECT t.flatdata.id FROM (select flatten(Rows1) AS flatdata FROM dfs.`/home/nima/Desktop/data.json`) t''')
-            for result in yelp_reviews:
-            print result       
-            #json_data = {'columns':columns,'rows':rows}
-            
-            #json_data = json.dumps(json_data,encoding='latin1')
+            data = drill.query('''SELECT * (select flatten(temp) AS flatdata FROM dfs.`'''+jsonfilepath+'''`) t''')
+            for result in data:
+                columnnames = list(result.keys())
+                for column in columnnames:
+                    row[column]=result[column]
+                rows.append(row)
+                row={}
+            columns = []  
+            for column in columnnames:
+                columns.append({
+                        "friendly_name": column,
+                        "type": "string",
+                        "name": column
+                        })
+            json_data = {'columns':columns,'rows':rows}
+            json_data = json.dumps(json_data,encoding='latin1')
             return json_data, error
             if not json_data:
                 error = "Got empty response from '{}'.".format(url)
