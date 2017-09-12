bfedcac1ee1d537fd5ff6a81498be7e451a41f35
Nima <nsanati@surexdirect.com>
2017-09-12 15:58:59 -0600

    modify Zendesk data source

--- redash/query_runner/zendesk.py
+++ redash/query_runner/zendesk.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import string
+import os
 from redash.query_runner import BaseQueryRunner, register
 from pydrill.client import PyDrill
 from random import randint
@@ -25,33 +26,39 @@ class Zendesk(BaseQueryRunner):
             "required": ["email","pwd"],
             "secret": ["pwd"]
         }
-
     @classmethod
     def annotate_query(cls):
         return False
+    
     def __init__(self, configuration):
         super(Zendesk, self).__init__(configuration)
         self.auth = (self.configuration["email"], self.configuration["pwd"])
         self.base_url = self.configuration.get("url", None)
+        
     def test_connection(self):
         response = requests.get(self.base_url,auth=(self.configuration.get("email", None), self.configuration.get("pwd", None)))
-        if response.status_code != 200:
+        if response.status_code != 200 and response.status_code != 422 :
             raise Exception("Not Authorized! (http status code: {0}).".format(response.status_code))
+        
     def run_query(self, query, user):
         try:
-            jsonfilepath="/tmp/"+str(randint(0, 1000))+".json"
-            zendeskcolumn=['via',
+            query = query.strip()
+            sqlquery = None
+            jsonfile="/tmp/"+str(randint(100, 1000))+".json"
+            
+            columnnames=['id',
+              'created_at',
               'updated_at',
-              'submitter_id',
+              'status',
+              'tags',
               'assignee_id',
+              'submitter_id',
               'brand_id',
-              'id',
+              'via',
               'subject',
               'priority',
               'satisfaction_rating',
               'type',
-              'status',
-              'tags',
               'forum_topic_id',
               'organization_id',
               'due_at',
@@ -60,63 +67,82 @@ class Zendesk(BaseQueryRunner):
               'recipient',
               'problem_id',
               'url',
-              'created_at',
               'raw_subject',
               'allow_channelback',
               'has_incidents',
               'group_id',
               'external_id',
               'result_type'] 
+              
             error = None
-            query = query.strip()
+            
             if query.find("://") > -1:
                     return None, "Accepting only relative URLs to '%s'" % self.base_url
-            url = self.base_url + query
+                
+            queries = query.split(";")
+            
+            if len(queries ) > 1 :
+                sqlquery=queries[1]
+                
+            zendeskquery = queries[0]
+            url = self.base_url + zendeskquery
             response = requests.get(url,auth=self.auth )
             response.raise_for_status()
             json_data = response.content.strip()
             json_data = json.loads(json_data)
             row={}
             rows=[]
-            for i in json_data['results']:
-                for column in zendeskcolumn:
-                    row[column]=i[column]
+            
+            for result in json_data['results']:
+                for column in columnnames:
+                    row[column]=result[column]
                 rows.append(row)
                 row={}
+                
             while(json_data['next_page'] is not None):
                 response = requests.get(json_data['next_page'],auth=self.auth)
                 response.raise_for_status()
                 json_data = response.content.strip()
                 json_data = json.loads(json_data)
-                for i in json_data['results']:
-                    for column in zendeskcolumn:
-                        row[column]=i[column]
+                for result in json_data['results']:
+                    for column in columnnames:
+                        row[column]=result[column]
                     rows.append(row)
                     row={}
-            json_temp = {'temp':rows}
-            with open(jsonfilepath,'w+') as outfile:
-                json.dump(json_temp,outfile)
-            rows=[]
-            drill = PyDrill(host='localhost', port=8047)
-            data = drill.query('''SELECT t.flatdata.id as id  FROM (select flatten(temp) AS flatdata FROM dfs.`'''+jsonfilepath+'''`) t''')
-            for result in data:
-                columnnames = list(result.keys())
-                for column in columnnames:
-                    row[column]=result[column]
-                rows.append(row)
-                row={}
-            columns = []  
+                    
+            if sqlquery is not None and len(sqlquery)>1:
+                json_temp = {'temp':rows}
+                with open(jsonfile,'w+') as outfile:
+                    json.dump(json_temp,outfile)
+                rows=[]
+                drill = PyDrill(host='localhost', port=8047)
+                table="(select flatten(temp) AS flatdata FROM dfs.`"+jsonfile+"`)"
+                sqlquery=sqlquery.replace('report',table) 
+                data = drill.query(sqlquery)
+                for result in data:
+                    columnnames = list(result.keys())
+                    for column in columnnames:
+                        row[column]=result[column]
+                    rows.append(row)
+                    row={}
+                os.remove(jsonfile)
+                
+            columns = []
+            
             for column in columnnames:
                 columns.append({
                         "friendly_name": column,
                         "type": "string",
                         "name": column
                         })
+                        
             json_data = {'columns':columns,'rows':rows}
             json_data = json.dumps(json_data,encoding='latin1')
             return json_data, error
+        
             if not json_data:
                 error = "Got empty response from '{}'.".format(url)
+                
             return json_data, error
         except requests.RequestException as e:
             return None, str(e)
@@ -124,4 +150,5 @@ class Zendesk(BaseQueryRunner):
             error = "Query cancelled by user."
             json_data = None
         return json_data, error
+    
 register(Zendesk)
