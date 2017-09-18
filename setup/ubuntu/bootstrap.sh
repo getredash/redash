1c6d1d664683e2939320e78c94e1cab838a3832f
Nima <nsanati@surexdirect.com>
2017-09-18 12:04:59 -0600

    change LATEST_URL variable

--- setup/ubuntu/bootstrap.sh
+++ setup/ubuntu/bootstrap.sh
@@ -12,7 +12,7 @@ set -eu
 REDASH_BASE_PATH=/opt/redash
 REDASH_BRANCH="${REDASH_BRANCH:-SUR-2805_zendesk_data_source}" # Default branch/version to master if not specified in REDASH_BRANCH env var
 REDASH_VERSION=${REDASH_VERSION-2.0.0.b2990} # Install latest version if not specified in REDASH_VERSION env var
-LATEST_URL="https://s3.amazonaws.com/redash-releases/redash.${REDASH_VERSION}.tar.gz"
+LATEST_URL="https://github.com/surexdirect/redash/archive/SUR-2805_zendesk_data_source.tar.gz"
 VERSION_DIR="/opt/redash/redash.${REDASH_VERSION}"
 REDASH_TARBALL=/tmp/redash.tar.gz
 FILES_BASE_URL=https://raw.githubusercontent.com/surexdirect/redash/SUR-2805_zendesk_data_source/setup/ubuntu/files
