aaa1bc3658eccc098c4d540ea735a11ca53a6def
Levko Kravets <levko.ne@gmail.com>
2017-11-15 19:58:53 +0200

    Chart: fit to container on resize

--- client/app/directives/resize-event.js
+++ client/app/directives/resize-event.js
@@ -2,40 +2,26 @@ import * as _ from 'underscore';
 import { requestAnimationFrame } from './utils';
 
 const items = [];
-let running = false;
 
 function checkItems() {
-  running = items.length > 0;
-  if (running) {
-    _.each(items, (item) => {
-      const offsetWidth = item.node.offsetWidth;
-      const offsetHeight = item.node.offsetHeight;
-      const clientWidth = item.node.clientWidth;
-      const clientHeight = item.node.clientHeight;
-      const scrollWidth = item.node.scrollWidth;
-      const scrollHeight = item.node.scrollHeight;
+  _.each(items, (item) => {
+    const offsetWidth = item.node.offsetWidth;
+    const offsetHeight = item.node.offsetHeight;
 
-      if (
-        (item.offsetWidth !== offsetWidth) ||
-        (item.offsetHeight !== offsetHeight) ||
-        (item.clientWidth !== clientWidth) ||
-        (item.clientHeight !== clientHeight) ||
-        (item.scrollWidth !== scrollWidth) ||
-        (item.scrollHeight !== scrollHeight)
-      ) {
-        item.offsetWidth = offsetWidth;
-        item.offsetHeight = offsetHeight;
-        item.clientWidth = clientWidth;
-        item.clientHeight = clientHeight;
-        item.scrollWidth = scrollWidth;
-        item.scrollHeight = scrollHeight;
-        item.callback(item);
-      }
+    if (
+      (item.offsetWidth !== offsetWidth) ||
+      (item.offsetHeight !== offsetHeight)
+    ) {
+      item.offsetWidth = offsetWidth;
+      item.offsetHeight = offsetHeight;
+      item.callback(item);
+    }
+  });
 
-      requestAnimationFrame(checkItems);
-    });
-  }
+  requestAnimationFrame(checkItems);
 }
+
+checkItems(); // ensure it was called only once!
 
 function resizeEvent() {
   return {
@@ -57,10 +43,6 @@ function resizeEvent() {
             items.splice(index, 1); // remove item
           }
         });
-
-        if (!running) {
-          checkItems();
-        }
       }
     },
   };
