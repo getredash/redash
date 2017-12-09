8edb0532e82e8d2ce57beed2d4754e1245879b77
Allen Short <ashort@mozilla.com>
2017-12-09 05:50:15 +0000

    make autocomplete for large schemas faster (re #232)

--- client/app/components/queries/query-editor.js
+++ client/app/components/queries/query-editor.js
@@ -93,7 +93,7 @@ function queryEditor(QuerySnippet, $timeout) {
               editor.getSession().setMode(newMode);
             });
 
-            $scope.$watch('schema', (newSchema, oldSchema) => {
+            $scope.$watch('autoCompleteSchema', (newSchema, oldSchema) => {
               if (newSchema !== oldSchema) {
                 if (newSchema === undefined) {
                   return;
@@ -103,8 +103,10 @@ function queryEditor(QuerySnippet, $timeout) {
                 // as it makes typing slower.
                 if (tokensCount > 5000) {
                   editor.setOption('enableLiveAutocompletion', false);
+                  editor.setOption('enableBasicAutocompletion', false);
                 } else {
                   editor.setOption('enableLiveAutocompletion', true);
+                  editor.setOption('enableBasicAutocompletion', true);
                 }
               }
             });
@@ -119,31 +121,44 @@ function queryEditor(QuerySnippet, $timeout) {
 
         const schemaCompleter = {
           getCompletions(state, session, pos, prefix, callback) {
-            if (prefix.length === 0 || !$scope.schema) {
+            // make a variable for the auto completion in the query editor
+            $scope.autoCompleteSchema = $scope.schema; // removeExtraSchemaInfo(
+
+            if (prefix.length === 0 || !$scope.autoCompleteSchema) {
               callback(null, []);
               return;
             }
 
-            if (!$scope.schema.keywords) {
+            if (!$scope.autoCompleteSchema.keywords) {
               const keywords = {};
 
-              $scope.schema.forEach((table) => {
+              $scope.autoCompleteSchema.forEach((table) => {
                 keywords[table.name] = 'Table';
 
-                table.columns.forEach((c) => {
-                  keywords[c] = 'Column';
+                table.columns.forEach((c) => { // autoCompleteColumns
+                  if (c.charAt(c.length - 1) === ')') {
+                    let parensStartAt = c.indexOf('(') - 1;
+                    c = c.substring(0, parensStartAt);
+                    parensStartAt = 1; // linter complains without this line
+                  }
+                  // remove '[P] ' for partition keys
+                  if (c.charAt(0) === '[') {
+                    c = c.substring(4, c.length);
+                  }
+                  // keywords[c] = 'Column'; // dups columns
                   keywords[`${table.name}.${c}`] = 'Column';
                 });
               });
 
-              $scope.schema.keywords = map(keywords, (v, k) => ({
-                name: k,
-                value: k,
-                score: 0,
-                meta: v,
-              }));
+              $scope.autoCompleteSchema.keywords = map(keywords, (v, k) =>
+                ({
+                  name: k,
+                  value: k,
+                  score: 0,
+                  meta: v,
+                }));
             }
-            callback(null, $scope.schema.keywords);
+            callback(null, $scope.autoCompleteSchema.keywords);
           },
         };
 
