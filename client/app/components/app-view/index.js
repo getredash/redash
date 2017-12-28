3e6f90376f5ed78728b28df4ec7b468df3323b77
Levko Kravets <levko.ne@gmail.com>
2017-12-28 18:39:14 +0200

    getredash/redash#2162 CR1

--- client/app/components/app-view/index.js
+++ client/app/components/app-view/index.js
@@ -1,31 +1,54 @@
+import debug from 'debug';
 import template from './template.html';
 
+const logger = debug('redash:app-view');
+
 export default function init(ngModule) {
+  let customHandler = null;
+
+  ngModule.factory('$exceptionHandler', () => function exceptionHandler(exception) {
+    if (customHandler) {
+      customHandler(exception);
+    } else {
+      // eslint-disable-next-line no-console
+      console.error(exception);
+    }
+  });
+
   ngModule.component('appView', {
     template,
     controller($rootScope, $route, Auth) {
       this.showHeaderAndFooter = false;
 
+      this.error = null;
+
+      customHandler = (error) => {
+        if (!(error instanceof Error)) {
+          if (error.status && error.data) {
+            switch (error.status) {
+              case 403: error = new Error(''); break;
+              default: error = new Error(error.data.message); break;
+            }
+          }
+        }
+        this.error = error;
+        // eslint-disable-next-line no-console
+        console.error(error);
+      };
+
       $rootScope.$on('$routeChangeStart', (event, route) => {
         if (route.$$route.authenticated) {
           // For routes that need authentication, check if session is already
           // loaded, and load it if not.
-          Auth.logger('Requested authenticated route: ', route);
+          logger('Requested authenticated route: ', route);
           if (Auth.isAuthenticated()) {
             this.showHeaderAndFooter = true;
           } else {
             event.preventDefault();
-            Auth.loadSession().then(() => {
-              if (Auth.isAuthenticated()) {
-                this.showHeaderAndFooter = true;
-                Auth.logger('Loaded session');
-                $route.reload();
-              } else {
-                throw new Error('Need to login');
-              }
-            }).catch(() => {
-              Auth.logger('Need to login, redirecting');
-              Auth.login();
+            // Auth.requireSession resolves only if session loaded
+            Auth.requireSession().then(() => {
+              this.showHeaderAndFooter = true;
+              $route.reload();
             });
           }
         } else {
@@ -33,31 +56,12 @@ export default function init(ngModule) {
         }
       });
 
-      this.error = null;
-
-      $rootScope.$on('appViewError', (event, error) => {
-        if ((error !== null) && (error !== undefined) && (error !== '')) {
-          this.error = error instanceof Error ? error : new Error('' + error);
-        } else {
-          this.error = null;
-        }
-      });
-
       $rootScope.$on('$routeChangeSuccess', () => {
-        $rootScope.$broadcast('appViewError', null);
-      });
-
-      $rootScope.$on('appViewRejection', (event, rejection) => {
-        let error = null;
-        switch (rejection.status) {
-          case 403: error = new Error(''); break;
-          default: error = new Error(rejection.data.message); break;
-        }
-        $rootScope.$broadcast('appViewError', error);
+        this.error = null;
       });
 
       $rootScope.$on('$routeChangeError', (event, current, previous, rejection) => {
-        $rootScope.$broadcast('appViewRejection', rejection);
+        throw rejection;
       });
     },
   });
