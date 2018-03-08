aafad6d9c9ba3cfb9ef3b3b15485944ad27fd32c
fqiang <f.qiang@gmail.com>
2018-03-08 14:03:22 +0800

    打印定向null以及相关的SQL配置

--- build4ccpg.sh
+++ build4ccpg.sh
@@ -25,18 +25,26 @@ case $1 in
     echo "Run ..."
     echo "to kill the redash related processes..." 
     ps aux|grep redash|awk '{print $2}'|xargs kill -9
+    ps aux|grep celery|awk '{print $2}'|xargs kill -9
+
     echo "@ to run the celery and server"
-    nohup ./bin/run  celery worker --app=redash.worker --beat -Qscheduled_queries,queries,celery -c2  | tee redash_worker.log & nohup ./bin/run ./manage.py runserver -h 0.0.0.0 -p 8080 | tee redash_server.log &
+    export SQLALCHEMY_MAX_OVERFLOW=200
+    export SQLALCHEMY_POOL_SIZE=100
+    nohup ./bin/run  celery worker --app=redash.worker --beat -Qscheduled_queries,queries,celery -c2 2>&1| tee redash_worker.log >/dev/null & nohup ./bin/run ./manage.py runserver -h 0.0.0.0 -p 8080 2>&1 | tee redash_server.log >/dev/null &
     ;;
 -D | -d)
      echo "to kill the redash related process..."
      ps aux|grep redash|awk '{print $2}'|xargs kill -9
+     ps aux|grep celery|awk '{print $2}'|xargs kill -9
+     export SQLALCHEMY_MAX_OVERFLOW=200
+     export SQLALCHEMY_POOL_SIZE=100
+   
    #  ps aux|grep node|awk '{print $2}'|xargs kill -9&
      
      echo "Debug Mode.. you may modify the webpack related files first : webpack.config.js and package.json "
 #     unset $REDASH_BACKEND
      echo "to run server"
-     nohup ./bin/run ./manage.py runserver --debugger --reload | tee redash_server.log & nohup ./bin/run celery worker --app=redash.worker --beat -Qscheduled_queries,queries,celery -c2  | tee redash_worker.log & nohup  npm run start | tee npm_run.log &
+     nohup ./bin/run ./manage.py runserver --debugger --reload 2>&1| tee redash_server.log >/dev/null & nohup ./bin/run celery worker --app=redash.worker --beat -Qscheduled_queries,queries,celery -c2 2>&1| tee redash_worker.log >/dev/null& nohup npm run start 2>&1| tee npm_run.log >/dev/null&
       ;;
 *)
     echo "illegal command!"  
