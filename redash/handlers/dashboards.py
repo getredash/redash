6f3d5139bb1c452c5df3d43da57e8d98df76e038
Allen Short <ashort@mozilla.com>
2018-03-21 20:38:48 +0000

    properly rollback failed db commits

--- redash/handlers/dashboards.py
+++ redash/handlers/dashboards.py
@@ -10,6 +10,7 @@ from redash.serializers import serialize_dashboard
 from redash.permissions import (can_modify, require_admin_or_owner,
                                 require_object_modify_permission,
                                 require_permission)
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import StaleDataError
 
 
@@ -127,7 +128,7 @@ class DashboardResource(BaseResource):
 
         require_object_modify_permission(dashboard, self.current_user)
 
-        updates = project(dashboard_properties, ('name', 'layout', 'version', 'tags', 
+        updates = project(dashboard_properties, ('name', 'layout', 'version', 'tags',
                                                  'is_draft', 'dashboard_filters_enabled'))
 
         # SQLAlchemy handles the case where a concurrent transaction beats us
@@ -143,7 +144,11 @@ class DashboardResource(BaseResource):
         try:
             models.db.session.commit()
         except StaleDataError:
+            models.db.session.rollback()
             abort(409)
+        except IntegrityError:
+            models.db.session.rollback()
+            abort(400)
 
         result = serialize_dashboard(dashboard, with_widgets=True, user=self.current_user)
         self.record_event({
