b3abab99159a0509f09eb48bd050acae97e1b0fb
Allen Short <ashort@mozilla.com>
2018-03-21 20:38:48 +0000

    properly rollback failed db commits

--- redash/handlers/dashboards.py
+++ redash/handlers/dashboards.py
@@ -10,6 +10,7 @@ from redash.serializers import serialize_dashboard
 from redash.permissions import (can_modify, require_admin_or_owner,
                                 require_object_modify_permission,
                                 require_permission)
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import StaleDataError
 
 
@@ -198,7 +199,11 @@ class DashboardResource(BaseResource):
         try:
             models.db.session.commit()
         except StaleDataError:
+            models.db.session.rollback()
             abort(409)
+        except IntegrityError:
+            models.db.session.rollback()
+            abort(400)
 
         result = serialize_dashboard(dashboard, with_widgets=True, user=self.current_user)
 
--- redash/handlers/users.py
+++ redash/handlers/users.py
@@ -105,6 +105,7 @@ class UserListResource(BaseResource):
             models.db.session.add(user)
             models.db.session.commit()
         except IntegrityError as e:
+            models.db.session.rollback()
             if "email" in e.message:
                 abort(400, message='Email already taken.')
             abort(500)
@@ -199,7 +200,7 @@ class UserResource(BaseResource):
                 message = "Email already taken."
             else:
                 message = "Error updating record"
-
+            models.db.session.rollback()
             abort(400, message=message)
 
         self.record_event({
