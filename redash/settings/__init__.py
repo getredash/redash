c426ef323dde6f01620bdb89f980f140357d5fe5
matsumo <matsumo@ce.ns0.it>
2018-04-23 17:14:01 +0900

    add support ChatWork Alert Destination.

--- redash/settings/__init__.py
+++ redash/settings/__init__.py
@@ -186,6 +186,7 @@ default_destinations = [
     'redash.destinations.webhook',
     'redash.destinations.hipchat',
     'redash.destinations.mattermost',
+    'redash.destinations.chatwork',
 ]
 
 enabled_destinations = array_from_string(os.environ.get("REDASH_ENABLED_DESTINATIONS", ",".join(default_destinations)))
