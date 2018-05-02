a6bda9d403f2227119d2d245b41a71e6bad12ea6
Allen Short <ashort@mozilla.com>
2018-05-02 17:42:25 +0000

    react rules for eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: "airbnb-base",
+  extends: "airbnb",
   settings: {
     "import/resolver": "webpack"
   },
@@ -23,6 +23,13 @@ module.exports = {
     "no-lonely-if": "off",
     "consistent-return": "off",
     "no-control-regex": "off",
+    "react/jsx-filename-extension": "off",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react/prefer-stateless-function": "warn",
+    "react/forbid-prop-types": "warn",
+    "react/prop-types": "warn",
+    "jsx-a11y/anchor-is-valid": "off",
     "max-len": ['error', 120, 2, {
       ignoreUrls: true,
       ignoreComments: false,
