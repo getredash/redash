3fe8272ce4305fa88dc1c454c6c90e9b2c95e1a2
Levko Kravets <levko.ne@gmail.com>
2018-05-28 17:05:39 +0300

    getredash/redash#2375 Better error message when trying to disable self user; set disabled user avattar URL on backend and remove profileImage filter

--- client/app/filters/profile-image.js
+++
@@ -1,9 +0,0 @@
-import defaultProfileImageUrl from '@/assets/images/avatar.svg';
-
-function getUserProfileImageUrl(user) {
-  return user.is_disabled ? defaultProfileImageUrl : user.profile_image_url;
-}
-
-export default function init(ngModule) {
-  ngModule.filter('profileImage', () => getUserProfileImageUrl);
-}
