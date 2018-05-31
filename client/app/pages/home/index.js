40c60feacb82efb70abc65f105ac839c9302398c
Arik Fraimovich <arik@arikfr.com>
2018-05-31 10:58:04 +0300

    Remove unused code.

--- client/app/pages/home/index.js
+++ client/app/pages/home/index.js
@@ -3,11 +3,6 @@ import template from './home.html';
 function HomeCtrl($scope, $uibModal, currentUser, Events, Dashboard, Query) {
   Events.record('view', 'page', 'personal_homepage');
 
-  // todo: maybe this should come from some serivce as we have this logic elsewhere.
-  this.canCreateQuery = currentUser.hasPermission('create_query');
-  this.canCreateDashboard = currentUser.hasPermission('create_dashboard');
-  this.canCreateAlert = currentUser.hasPermission('list_alerts');
-
   this.recentQueries = Query.recent();
   this.recentDashboards = Dashboard.recent();
 
