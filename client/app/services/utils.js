eaa9c2261c5a6baeb2109017691c6fa64cdc3c05
Arik Fraimovich <arik@arikfr.com>
2018-06-20 18:45:44 +0300

    Refactor: create tags list component to be shared between dashboard and queries list page.

--- client/app/services/utils.js
+++ client/app/services/utils.js
@@ -1,15 +1,7 @@
-import { extend, map, sortBy } from 'underscore';
-
+// eslint-disable-next-line import/prefer-default-export
 export function absoluteUrl(url) {
   const urlObj = new URL(url, window.location);
   urlObj.protocol = window.location.protocol;
   urlObj.host = window.location.host;
   return urlObj.toString();
-}
-
-export function processTags(tags) {
-  return map(sortBy(
-    map(extend({}, tags), (count, tag) => ({ tag, count })),
-    item => item.count,
-  ), item => item.tag);
 }
