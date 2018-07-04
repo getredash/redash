633eba7eb30bab96db3b00ebb4008bbff75524fd
Alison <astanton+git@bankofknowledge.net>
2018-07-04 16:57:10 -0500

    refactor based on codeclimate review of PR

--- redash/metrics/database.py
+++ redash/metrics/database.py
@@ -13,7 +13,8 @@ metrics_logger = logging.getLogger("metrics")
 
 
 def _table_name_from_select_element(elt):
-    t = elt.froms[0]
+    if elt.froms[0]:
+        t = elt.froms[0]
 
     if isinstance(t, Alias):
         t = t.original.froms[0]
--- redash/monitor.py
+++ redash/monitor.py
@@ -1,42 +1,47 @@
 from redash import redis_connection, models, __version__, settings
 
-
-def get_status():
-    status = {}
+def get_redis_status():
     info = redis_connection.info()
-    status['redis_used_memory'] = info['used_memory']
-    status['redis_used_memory_human'] = info['used_memory_human']
-    status['version'] = __version__
+    return {'redis_used_memory': info['used_memory'], 'redis_used_memory_human': info['used_memory_human']}
+
+def get_object_counts():
+    status = {}
     status['queries_count'] = models.db.session.query(models.Query).count()
     if settings.FEATURE_SHOW_QUERY_RESULTS_COUNT:
         status['query_results_count'] = models.db.session.query(models.QueryResult).count()
         status['unused_query_results_count'] = models.QueryResult.unused().count()
     status['dashboards_count'] = models.Dashboard.query.count()
     status['widgets_count'] = models.Widget.query.count()
+    return status
 
-    status['workers'] = []
-
-    status['manager'] = redis_connection.hgetall('redash:status')
-
+def get_queues():
     queues = {}
     for ds in models.DataSource.query:
         for queue in (ds.queue_name, ds.scheduled_queue_name):
             queues.setdefault(queue, set())
             queues[queue].add(ds.name)
 
-    status['manager']['queues'] = {}
+    return queues
+
+def get_queues_status():
+    queues = get_queues()
+
+    #status['manager']['queues'] = {}
     for queue, sources in queues.iteritems():
-        status['manager']['queues'][queue] = {
+        queues[queue] = {
             'data_sources': ', '.join(sources),
             'size': redis_connection.llen(queue)
         }
     
-    status['manager']['queues']['celery'] = {
+    queues['celery'] = {
         'size': redis_connection.llen('celery'),
         'data_sources': ''
     }
 
-    status['database_metrics'] = []
+    return queues
+
+def get_db_sizes():
+    database_metrics = []
     # have to include the fake FROM in the SQL to prevent an IndexError
     queries = [
         ['Query Results Size', "pg_total_relation_size('query_results') as size from (select 1) as a"],
@@ -44,6 +49,19 @@ def get_status():
     ]
     for query_name, query in queries:
         result = models.db.session.query(query).first()
-        status['database_metrics'].append([query_name, result[0]])
+        database_metrics.append([query_name,result[0]])
+
+    return database_metrics
+
+def get_status():
+    status = {
+        'version': __version__,
+        'workers': []
+    }
+    status.update(get_redis_status())
+    status.update(get_object_counts())
+    status['manager'] = redis_connection.hgetall('redash:status')
+    status['manager']['queues'] = get_queues_status() 
+    status['database_metrics'] = get_db_sizes()
 
     return status
