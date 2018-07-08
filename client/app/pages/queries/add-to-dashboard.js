6438b7fbc2dfec41ebaaa3bca702a37ef879d147
Alison <astanton+git@bankofknowledge.net>
2018-07-08 14:51:28 -0500

    remove unused lines from controller
    removes 14 lines

--- client/app/pages/queries/add-to-dashboard.js
+++ client/app/pages/queries/add-to-dashboard.js
@@ -1,37 +1,25 @@
 import template from './add-to-dashboard.html';
 
 const AddToDashboardForm = {
-  controller($sce, Dashboard, currentUser, toastr, Query, Widget) {
+  controller($sce, Dashboard, currentUser, toastr, Widget) {
     'ngInject';
 
-    this.query = this.resolve.query;
     this.vis = this.resolve.vis;
-    this.saveAddToDashbosard = this.resolve.saveAddToDashboard;
     this.saveInProgress = false;
-
     this.trustAsHtml = html => $sce.trustAsHtml(html);
 
     this.onDashboardSelected = (dash) => {
-      // add widget to dashboard
       this.saveInProgress = true;
-      this.widgetSize = 1;
-      this.selectedVis = null;
-      this.query = {};
-      this.selected_query = this.query.id;
-      this.type = 'visualization';
-      this.isVisualization = () => this.type === 'visualization';
-
+      this.selected_query = this.resolve.query.id;
       const widget = new Widget({
         visualization_id: this.vis && this.vis.id,
         dashboard_id: dash.id,
         options: {},
-        width: this.widgetSize,
-        type: this.type,
+        width: 1,
+        type: 'visualization',
       });
 
-      // (response)
       widget.save().then(() => {
-        // (dashboard)
         this.selectedDashboard = Dashboard.get({ slug: dash.slug }, () => {});
         this.close();
         toastr.success('Widget added to dashboard.');
@@ -51,8 +39,7 @@ const AddToDashboardForm = {
 
       Dashboard.search({
         q: term,
-        user_id: currentUser.id,
-        // limit_to_users_dashboards: limitToUsersDashboards,
+        user_id: currentUser.id, // limit_to_users_dashboards: limitToUsersDashboards,
         include_drafts: true,
       }, (results) => {
         this.dashboards = results;
