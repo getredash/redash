9f489f5acb0689f87f970599b301763349ce4681
Allen Short <ashort@mozilla.com>
2018-07-09 13:20:36 -0500

    Filter tables from schema browser (re #31)

--- redash/query_runner/clickhouse.py
+++ redash/query_runner/clickhouse.py
@@ -36,6 +36,12 @@ class ClickHouse(BaseSQLQueryRunner):
                     "type": "number",
                     "title": "Request Timeout",
                     "default": 30
+                },
+                "toggle_table_string": {
+                    "type": "string",
+                    "title": "Toggle Table String",
+                    "default": "_v",
+                    "info": "This string will be used to toggle visibility of tables in the schema browser when editing a query in order to remove non-useful tables from sight."
                 }
             },
             "required": ["dbname"],
--- redash/query_runner/presto.py
+++ redash/query_runner/presto.py
@@ -59,6 +59,12 @@ class Presto(BaseQueryRunner):
                 'password': {
                     'type': 'string'
                 },
+                "toggle_table_string": {
+                    "type": "string",
+                    "title": "Toggle Table String",
+                    "default": "_v",
+                    "info": "This string will be used to toggle visibility of tables in the schema browser when editing a query in order to remove non-useful tables from sight."
+                },
             },
             'order': ['host', 'protocol', 'port', 'username', 'password', 'schema', 'catalog'],
             'required': ['host']
