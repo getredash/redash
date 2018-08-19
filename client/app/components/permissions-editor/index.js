354a931a657bcda0d562c301bcdc88b471fafc37
Alison <astanton+git@bankofknowledge.net>
2018-08-19 18:20:31 -0500

    fix indentation

--- client/app/components/permissions-editor/index.js
+++ client/app/components/permissions-editor/index.js
@@ -54,12 +54,12 @@ const PermissionsEditorComponent = {
         user.alreadyGrantee = true;
         loadGrantees();
       }).catch((error) => {
-          if (error.status === 403) {
-            toastr.error('You cannot add a user to this dashboard. Ask the dashboard owner to grant them permissions.');
-          } else {
-            toastr.error('Something went wrong.');
-          }
-        });
+        if (error.status === 403) {
+          toastr.error('You cannot add a user to this dashboard. Ask the dashboard owner to grant them permissions.');
+        } else {
+          toastr.error('Something went wrong.');
+        }
+      });
     };
 
     // Remove user from grantees list
