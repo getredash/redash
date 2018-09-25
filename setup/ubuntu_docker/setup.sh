6e1c3ed742f2f0113efc4d5e0552007af4cdb300
Arik Fraimovich <arik@arikfr.com>
2018-09-25 20:31:41 +0300

    setup.sh changes
    1. Don't use .env file to set the Redash image version, as this is path dependant. Instead use sed to update the image version.
    2. Don't assume running script as root, but rather use sudo.
    3. Create env file from scratch.

--- setup/ubuntu_docker/setup.sh
+++ setup/ubuntu_docker/setup.sh
@@ -4,64 +4,57 @@ set -eu
 
 REDASH_BASE_PATH=/opt/redash
 REDASH_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
-FILES_BASE_URL=https://raw.githubusercontent.com/getredash/redash/${REDASH_BRANCH}/setup/ubuntu_docker
-
-verify_root() {
-    # Verify running as root:
-    if [[ "$(id -u)" != "0" ]]; then
-        if [[ $# -ne 0 ]]; then
-            echo "Failed running with sudo. Exiting." 1>&2
-            exit 1
-        fi
-        echo "This script must be run as root. Trying to run with sudo."
-        sudo bash "$0" --with-sudo
-        exit 1
-    fi
-}
 
 install_docker(){
-	apt-get update && apt-get -yy install apt-transport-https ca-certificates curl software-properties-common wget 
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-	apt-get update && apt-get -y install docker-ce
-	curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-	chmod +x /usr/local/bin/docker-compose
+    # Install Docker
+    sudo apt-get update 
+    sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common wget 
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update && sudo apt-get -y install docker-ce
+
+    # Install Docker Compose
+    curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+
+    # Allow current user to run Docker commands
+    sudo usermod -aG docker $USER
 }
 
 create_directories() {
     if [[ ! -e $REDASH_BASE_PATH ]]; then
-    	mkdir -p $REDASH_BASE_PATH
-    fi
-   	
-   	mkdir $REDASH_BASE_PATH/upgrade
-   	wget $FILES_BASE_URL/upgrade.sh -O $REDASH_BASE_PATH/upgrade/upgrade.sh
-
-    # Default config file
-    if [[ ! -f "$REDASH_BASE_PATH/env" ]]; then
-        wget "$FILES_BASE_URL/files/env" -O $REDASH_BASE_PATH/env
+        sudo mkdir -p $REDASH_BASE_PATH
+        sudo chown $USER:$USER $REDASH_BASE_PATH
     fi
 
-    touch $REDASH_BASE_PATH/.env
+    if [[ ! -e $REDASH_BASE_PATH/postgres-data ]]; then
+        mkdir $REDASH_BASE_PATH/postgres-data
+    fi
+       
+    if [[ -e $REDASH_BASE_PATH/env ]]; then
+        rm $REDASH_BASE_PATH/env
+        touch $REDASH_BASE_PATH/env
+    fi
 
     COOKIE_SECRET=$(pwgen -1s 32)
     POSTGRES_PASSWORD=$(pwgen -1s 32)
     REDASH_DATABASE_URL="postgresql://postgres:${POSTGRES_PASSWORD}@postgres/postgres"
+
+    echo "PYTHONUNBUFFERED=0" >> $REDASH_BASE_PATH/env
+    echo "REDASH_LOG_LEVEL=INFO" >> $REDASH_BASE_PATH/env
+    echo "REDASH_REDIS_URL=redis://redis:6379/0" >> $REDASH_BASE_PATH/env
     echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> $REDASH_BASE_PATH/env
     echo "REDASH_COOKIE_SECRET=$COOKIE_SECRET" >> $REDASH_BASE_PATH/env
     echo "REDASH_DATABASE_URL=$REDASH_DATABASE_URL" >> $REDASH_BASE_PATH/env
-
-    if [[ ! -e $REDASH_BASE_PATH/postgres-data ]]; then
-    	mkdir $REDASH_BASE_PATH/postgres-data
-    fi
 }
 
 setup_compose() {
     REQUESTED_CHANNEL=stable
-    AVAILABLE_IMAGE_VERSION=`curl -s "https://version.redash.io/releases?channel=$REQUESTED_CHANNEL"  | json_pp  | grep "docker_image" | head -n 1 | awk 'BEGIN{FS=":"}{print $3}' | awk 'BEGIN{FS="\""}{print $1}'`
-    echo "TAG=${AVAILABLE_IMAGE_VERSION}" >> $REDASH_BASE_PATH/.env
+    LATEST_VERSION=`curl -s "https://version.redash.io/api/releases?channel=$REQUESTED_CHANNEL"  | json_pp  | grep "docker_image" | head -n 1 | awk 'BEGIN{FS=":"}{print $3}' | awk 'BEGIN{FS="\""}{print $1}'`
 
     cd $REDASH_BASE_PATH
-    wget $FILES_BASE_URL/files/docker-compose.yml
+    wget https://raw.githubusercontent.com/getredash/redash/${REDASH_BRANCH}/setup/ubuntu_docker/docker-compose.yml
+    sed -ri "s/image: redash\/redash:([A-Za-z0-9.-]*)/image: redash\/redash:$LATEST_VERSION/" docker-compose.yml
     echo "export COMPOSE_PROJECT_NAME=redash" >> ~/.profile
     echo "export COMPOSE_FILE=/opt/redash/docker-compose.yml" >> ~/.profile
     source ~/.profile
@@ -70,7 +63,6 @@ setup_compose() {
 }
 
 verify_root
-verify_ubuntu
 install_docker
 create_directories
 setup_compose
