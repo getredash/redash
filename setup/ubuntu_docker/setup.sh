ed3af44b34c45e1425a46600a716f9e68fe5502e
Arik Fraimovich <arik@arikfr.com>
2018-09-25 20:39:31 +0300

    Install pwgen & use sudo for docker-compose install.

--- setup/ubuntu_docker/setup.sh
+++ setup/ubuntu_docker/setup.sh
@@ -7,14 +7,14 @@ REDASH_BASE_PATH=/opt/redash
 install_docker(){
     # Install Docker
     sudo apt-get update 
-    sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common wget 
+    sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common wget pwgen
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt-get update && sudo apt-get -y install docker-ce
 
     # Install Docker Compose
-    curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 
     # Allow current user to run Docker commands
     sudo usermod -aG docker $USER
