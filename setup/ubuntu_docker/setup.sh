eda2251ca2ede75b174c048b8afe5c0596f240f0
Arik Fraimovich <arik@arikfr.com>
2018-09-25 18:51:11 +0300

    Update setup script:
    1. Add password to Postgres server.
    2. Remove Ubuntu check.
    3. Move setup commands into a function.

--- setup/ubuntu_docker/setup.sh
+++ setup/ubuntu_docker/setup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
-# This script setups dockerized Redash on Ubuntu 16.04.
-
+# This script setups dockerized Redash on Ubuntu 18.04.
 set -eu
 
 REDASH_BASE_PATH=/opt/redash
@@ -21,24 +19,16 @@ verify_root() {
     fi
 }
 
-verify_ubuntu() {
-	if [[ "$(cat /etc/lsb-release | grep "DISTRIB_ID" | awk 'BEGIN{FS="="}{print $2}')" != "Ubuntu" ]]; then
-		echo "This operating system is not Ubuntu. Exiting."
-		exit 1
-	fi
-}
-
 install_docker(){
 	apt-get update && apt-get -yy install apt-transport-https ca-certificates curl software-properties-common wget 
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 	apt-get update && apt-get -y install docker-ce
-	curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+	curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 	chmod +x /usr/local/bin/docker-compose
 }
 
 create_directories() {
-    
     if [[ ! -e $REDASH_BASE_PATH ]]; then
     	mkdir -p $REDASH_BASE_PATH
     fi
@@ -54,29 +44,33 @@ create_directories() {
     touch $REDASH_BASE_PATH/.env
 
     COOKIE_SECRET=$(pwgen -1s 32)
+    POSTGRES_PASSWORD=$(pwgen -1s 32)
+    REDASH_DATABASE_URL="postgresql://postgres:${POSTGRES_PASSWORD}@postgres/postgres"
+    echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> $REDASH_BASE_PATH/env
     echo "REDASH_COOKIE_SECRET=$COOKIE_SECRET" >> $REDASH_BASE_PATH/env
+    echo "REDASH_DATABASE_URL=$REDASH_DATABASE_URL" >> $REDASH_BASE_PATH/env
 
     if [[ ! -e $REDASH_BASE_PATH/postgres-data ]]; then
     	mkdir $REDASH_BASE_PATH/postgres-data
     fi
 }
 
+setup_compose() {
+    REQUESTED_CHANNEL=stable
+    AVAILABLE_IMAGE_VERSION=`curl -s "https://version.redash.io/releases?channel=$REQUESTED_CHANNEL"  | json_pp  | grep "docker_image" | head -n 1 | awk 'BEGIN{FS=":"}{print $3}' | awk 'BEGIN{FS="\""}{print $1}'`
+    echo "TAG=${AVAILABLE_IMAGE_VERSION}" >> $REDASH_BASE_PATH/.env
+
+    cd $REDASH_BASE_PATH
+    wget $FILES_BASE_URL/files/docker-compose.yml
+    echo "export COMPOSE_PROJECT_NAME=redash" >> ~/.profile
+    echo "export COMPOSE_FILE=/opt/redash/docker-compose.yml" >> ~/.profile
+    source ~/.profile
+    docker-compose run --rm server create_db
+    docker-compose up -d
+}
+
 verify_root
 verify_ubuntu
 install_docker
 create_directories
-
-wget -O /usr/local/bin/semver $FILES_BASE_URL/files/semver
-chmod +x /usr/local/bin/semver
-
-REQUESTED_CHANNEL=stable
-AVAILABLE_IMAGE_VERSION=`curl -s "https://version.redash.io/releases?channel=$REQUESTED_CHANNEL"  | json_pp  | grep "docker_image" | head -n 1 | awk 'BEGIN{FS=":"}{print $3}' | awk 'BEGIN{FS="\""}{print $1}'`
-echo "TAG=${AVAILABLE_IMAGE_VERSION}" >> $REDASH_BASE_PATH/.env
-
-cd $REDASH_BASE_PATH
-wget $FILES_BASE_URL/files/docker-compose.yml
-echo "export COMPOSE_PROJECT_NAME=redash" >> ~/.profile
-echo "export COMPOSE_FILE=/opt/redash/docker-compose.yml" >> ~/.profile
-source ~/.profile
-docker-compose run --rm server create_db
-docker-compose up -d
+setup_compose
