11738f73ac937204ec7d256eb082bcb3e8a12701
Jannis Leidel <jannis@leidel.info>
2018-09-27 17:31:31 +0200

    Removed redundant exception handling since execute_query task handles that.

--- redash/query_runner/mssql_odbc.py
+++ redash/query_runner/mssql_odbc.py
@@ -152,8 +152,6 @@ class SQLServerODBC(BaseSQLQueryRunner):
             connection.cancel()
             error = "Query cancelled by user."
             json_data = None
-        except Exception as e:
-            raise sys.exc_info()[1], None, sys.exc_info()[2]
         finally:
             if connection:
                 connection.close()
