669f3548b6757fc1261b801b2da8c288b140429a
cclauss <cclauss@bluewin.ch>
2018-10-04 18:15:57 +0200

    git add flake8_tests.sh

---
+++ flake8_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+flake8 --version ; pip --version
+# stop the build if there are Python syntax errors or undefined names
+flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics
+# exit-zero treats all errors as warnings.  The GitHub editor is 127 chars wide      
+flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
