375e61e712359de4b75820e2bf20eec3188e910e
bvenkatr <bvenkatr2@gmail.com>
2018-10-14 16:59:39 +0530

    show service account email id to which file needs to be shared if you get PERMISSION_DENIED

--- redash/query_runner/google_spreadsheets.py
+++ redash/query_runner/google_spreadsheets.py
@@ -217,7 +217,7 @@ class GoogleSpreadsheet(BaseQueryRunner):
             if json.loads(e.response.text)["error"]["status"] == "PERMISSION_DENIED":
                 raise Exception(
                     f"Looks like you don't have the permission to access this file,"
-                    f"probably you might need to share your file with this service account {gClient.auth._service_account_email}"
+                    f"probably you might need to share your file with this service account {spreadsheet_service.auth._service_account_email}"
                 )
             return None, "Spreadsheet ({}) not found. Make sure you used correct id.".format(key)
 
