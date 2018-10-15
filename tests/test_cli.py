a9c0d9235702aec57945aa294885712df6fbe3dc
sumito.tsukada <sumito.tsukada@rarejob.co.jp>
2018-10-15 13:38:08 +0900

    fixed test code

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -391,6 +391,7 @@ class UserCommandTests(BaseTestCase):
         Name: Fred Foobar
         Email: foobar@example.com
         Organization: Default
+        Effective: True
         """
         self.assertMultiLineEqual(result.output,
                                   textwrap.dedent(output).lstrip())
