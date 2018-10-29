391fbe130bdf96e1c36aeb2144ceadcacc10309e
Levko Kravets <levko.ne@gmail.com>
2018-10-29 22:08:35 +0200

    getredash/redash#2998 Charts lose responsive features after refreshing the dashboard (#3024)

--- client/app/visualizations/chart/plotly/index.js
+++ client/app/visualizations/chart/plotly/index.js
@@ -46,7 +46,9 @@ const PlotlyChart = () => ({
       layout = prepareLayout(plotlyElement, scope.series, scope.options, data);
 
       // It will auto-purge previous graph
-      Plotly.newPlot(plotlyElement, data, layout, plotlyOptions);
+      Plotly.newPlot(plotlyElement, data, layout, plotlyOptions).then(() => {
+        updateLayout(plotlyElement, layout, (e, u) => Plotly.relayout(e, u));
+      });
 
       plotlyElement.on('plotly_restyle', (updates) => {
         // This event is triggered if some plotly data/layout has changed.
