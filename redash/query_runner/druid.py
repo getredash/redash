c298101bd4793a5204e606d181f18f40f4889465
Omer Lachish <omer@rauchy.net>
2018-11-07 13:36:56 +0200

    implements run_query

--- redash/query_runner/druid.py
+++ redash/query_runner/druid.py
@@ -5,6 +5,10 @@ except ImportError:
     enabled = False
 
 from redash.query_runner import BaseQueryRunner, register
+from redash.utils import json_dumps, json_loads
+
+TYPES_MAP = {
+}
 
 class Druid(BaseQueryRunner):
     noop_query = "SELECT 1"
@@ -43,6 +47,21 @@ class Druid(BaseQueryRunner):
         return enabled
 
     def run_query(self, query, user):
-        pass
+        connection = connect(host=self.configuration['host'], port=self.configuration['port'], path='/druid/v2/sql/', scheme=self.configuration['scheme'])
+        cursor = connection.cursor()
+
+        try:
+            cursor.execute(query)
+            columns = self.fetch_columns([(i[0], TYPES_MAP.get(i[1], None)) for i in cursor.description])
+            rows = [dict(zip((c['name'] for c in columns), row)) for row in cursor]
+
+            data = {'columns': columns, 'rows': rows}
+            error = None
+            json_data = json_dumps(data)
+            print(json_data)
+        finally:
+            connection.close()
+
+        return json_data, error
 
 register(Druid)
