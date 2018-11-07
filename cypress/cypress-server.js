ef65ed417e081983e00b03705e8f3b75b4ce604f
Arik Fraimovich <arik@arikfr.com>
2018-11-07 14:28:40 +0100

    CircleCI config cleanup

--- cypress/cypress-server.js
+++ cypress/cypress-server.js
@@ -19,6 +19,8 @@ function execSetup() {
 
 function startServer() {
   console.log('Starting the server...');
+            
+  execSync('docker-compose -p cypress build --build-arg skip_ds_deps=true', {stdio: 'inherit'});
   execSync('docker-compose -p cypress up -d', { stdio: 'inherit' });
   execSync('docker-compose -p cypress run server create_db', { stdio: 'inherit' });
 }
