09180aefcbc5ecfb9990d5b0cd5ef725f62a244a
Alison <astanton+git@bankofknowledge.net>
2018-11-11 16:50:07 -0600

    codeclimate changes for saving query errors

--- redash/handlers/query_results.py
+++ redash/handlers/query_results.py
@@ -203,7 +203,8 @@ class QueryResultResource(BaseResource):
 
             if query_result is None and query is not None:
                 if settings.ALLOW_PARAMETERS_IN_EMBEDS and parameter_values:
-                    query_result = run_query_sync(query.data_source, parameter_values, query.query_text, max_age=max_age, get_errors=get_errors)
+                    query_result = run_query_sync(query.data_source, parameter_values, 
+                                                    query.query_text, max_age=max_age, get_errors=get_errors)
                 elif query.latest_query_data_id is not None:
                     query_result = get_object_or_404(models.QueryResult.get_by_id_and_org, query.latest_query_data_id, self.current_org)
 
