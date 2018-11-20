4387f3f1ce9b5a28f31fcc55afda24d5cd064ec4
Arik Fraimovich <arik@arikfr.com>
2018-11-20 18:45:33 +0200

    Autocomplete toggle improvements (#3091)
    * Autocomplete toggle improvements:
    
    * Refactor to its own component.
    * Show state in tooltip (enabled/disabled).
    * Disable the toggle if autocomplete is not possible (no schema/too many tokens).
    
    * Remove unsued code.
    
    * Custom icons font (currently has only two icons).
    
    Generated with Icomoon. If we extend its use, we should probably automate this and move to its own package.
    
    * Don't disable live autocomplete for data sources without schema.
    
    It can still be useful to autocomplete from local keywords.
    
    * Differentiate between autocomplete toggle states with an icon.
    
    Also added explicit message for the disabled state.
    
    * Remember thes state of autocomplete.
    
    * Only auto register init functions.

--- client/app/services/http.js
+++ client/app/services/http.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-mutable-exports
 export let $http = null;
 
-export default function register(ngModule) {
+export default function init(ngModule) {
   ngModule.run(($injector) => {
     $http = $injector.get('$http');
   });
