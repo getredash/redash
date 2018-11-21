1555c201aef57896aa4197528155ad4c97b032e1
Omer Lachish <omer@rauchy.net>
2018-11-21 13:51:19 +0200

    split .apply calls to newline

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -65,21 +65,27 @@ class TestSkipNones(TestCase):
 
 class TestSQLQuery(TestCase):
     def test_marks_simple_queries_as_safe(self):
-        query = SQLQuery("SELECT * FROM users WHERE userid='{{userid}}'").apply({"userid": 22})
+        query = SQLQuery("SELECT * FROM users WHERE userid='{{userid}}'")
+        query.apply({"userid": 22})
 
         self.assertTrue(query.is_safe())
 
     def test_marks_multiple_simple_queries_as_safe(self):
-        query = SQLQuery("SELECT * FROM users WHERE userid='{{userid}}' ; SELECT * FROM profiles").apply({"userid": 22})
+        query = SQLQuery("SELECT * FROM users WHERE userid='{{userid}}' ; SELECT * FROM profiles")
+        query.apply({"userid": 22})
 
         self.assertTrue(query.is_safe())
 
     def test_marks_tautologies_as_not_safe(self):
-        query = SQLQuery("SELECT * FROM users WHERE userid={{userid}}").apply({"userid": "22 OR 1==1"})
+        query = SQLQuery("SELECT * FROM users WHERE userid={{userid}}")
+        query.apply({"userid": "22 OR 1==1"})
 
         self.assertFalse(query.is_safe())
 
     def test_marks_union_queries_as_not_safe(self):
-        query = SQLQuery("SELECT * FROM users WHERE userid={{userid}}").apply({"userid": "22 UNION SELECT body, results, 1 FROM reports"})
+        query = SQLQuery("SELECT * FROM users WHERE userid={{userid}}")
+        query.apply({"userid": "22 UNION SELECT body, results, 1 FROM reports"})
+
+        self.assertFalse(query.is_safe())
 
         self.assertFalse(query.is_safe())
