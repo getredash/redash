7bade16070b9eb8ffb908f5789f37189d09b2c92
Arik Fraimovich <arik@arikfr.com>
2018-11-26 15:26:04 +0200

    Refactor Rockset qury runner:
    * More idomatic names for configuration.
    * Move API code to separate class to make it easier removing it when we
      switch to official library.
    * Make Test Connection work.

--- redash/query_runner/rockset.py
+++ redash/query_runner/rockset.py
@@ -2,81 +2,41 @@ import requests
 import os
 from redash.query_runner import *
 from redash.utils import JSONEncoder
-import logging
 import json
 
-logger = logging.getLogger(__name__)
 
-class Rockset(BaseSQLQueryRunner):
-    @classmethod
-    def configuration_schema(cls):
-        return {
-            "type": "object",
-            "properties": {
-                "apiserver": {
-                    "type": "string",
-                    "default": "https://api.rs2.usw2.rockset.com"
-                },
-                "apikey": {
-                    "type": "string",
-                },
-            },
-            "required": ["apiserver", "apikey"],
-            "secret": ["apikey"]
-        }
 
-    @classmethod
-    def type(cls):
-        return "rockset"
-
-    def _get_tables(self, schema):
-        for col in self.list():
-            table_name = col['name']
-            describe = self.query('DESCRIBE "{}"'.format(table_name))
-            columns = list(set(map(lambda x: x['field'][0], describe['results'])))
-            schema[table_name] = {'name': table_name, 'columns': columns}
-        return schema.values()
-
-    def _get_type(self, value):
-        if isinstance(value, int):
-            return TYPE_INTEGER
-        elif isinstance(value, float):
-            return TYPE_FLOAT
-        elif isinstance(value, bool):
-            return TYPE_BOOLEAN
-        elif isinstance(value, str):
-            return TYPE_STRING
+def _get_type(value):
+    if isinstance(value, int):
+        return TYPE_INTEGER
+    elif isinstance(value, float):
+        return TYPE_FLOAT
+    elif isinstance(value, bool):
+        return TYPE_BOOLEAN
+    elif isinstance(value, str):
         return TYPE_STRING
+    return TYPE_STRING
 
-    def run_query(self, query, user):
-        logger.info('Rockset running query: %s', query)
-        results = self.query(query)
-        if 'code' in results and results['code'] != 200:
-            return None, '{}: {}'.format(results['type'], results['message'])
-        rows = results['results']
-        columns = []
-        if len(rows) > 0:
-            columns = []
-            for k in rows[0]:
-                columns.append({'name': k, 'friendly_name': k, 'type': self._get_type(rows[0][k])})
-        data = json.dumps({'columns': columns, 'rows': rows}, cls=JSONEncoder)
-        return data, None
 
-    # ####### BELOW ARE ROCKSET-SPECIFIC METHODS ########
+# The following is here, because Rockset's PyPi package is Python 3 only.
+# Should be removed once we move to Python 3.
+class RocksetAPI(object):
+    def __init__(self, api_key, api_server):
+        self.api_key = api_key
+        self.api_server = api_server
+
     def _request(self, endpoint, method='GET', body=None):
-        apikey = self.configuration['apikey']
-        apiserver = self.configuration['apiserver']
-        headers = {'Authorization': 'ApiKey {}'.format(apikey)}
+        headers = {'Authorization': 'ApiKey {}'.format(self.api_key)}
+        url = '{}/v1/orgs/self/{}'.format(self.api_server, endpoint)
+
         if method == 'GET':
-            r =  requests.get('{}/v1/orgs/self/{}'.format(apiserver, endpoint),
-                                headers=headers)
+            r =  requests.get(url, headers=headers)
             return r.json()
         elif method == 'POST':
-            r =  requests.post('{}/v1/orgs/self/{}'.format(apiserver, endpoint),
-                                headers=headers, json=body)
+            r =  requests.post(url, headers=headers, json=body)
             return r.json()
         else:
-            raise 'Unknown method {}'.format(method)
+            raise 'Unknown method: {}'.format(method)
 
     def list(self):
         response = self._request('ws/commons/collections')
@@ -84,5 +44,60 @@ class Rockset(BaseSQLQueryRunner):
 
     def query(self, sql):
         return self._request('queries', 'POST', {'sql': {'query': sql}})
+
+class Rockset(BaseSQLQueryRunner):
+    noop_query = 'SELECT 1'
+
+    @classmethod
+    def configuration_schema(cls):
+        return {
+            "type": "object",
+            "properties": {
+                "api_server": {
+                    "type": "string",
+                    "title": "API Server",
+                    "default": "https://api.rs2.usw2.rockset.com"
+                },
+                "api_key": {
+                    "title": "API Key",
+                    "type": "string",
+                },
+            },
+            "order": ["api_key", "api_server"],
+            "required": ["api_server", "api_key"],
+            "secret": ["api_key"]
+        }
+
+    @classmethod
+    def type(cls):
+        return "rockset"
+    
+    def __init__(self, configuration):
+        super(Rockset, self).__init__(configuration)
+        self.api = RocksetAPI(self.configuration.get('api_key'), self.configuration.get('api_server', "https://api.rs2.usw2.rockset.com"))
+
+    def _get_tables(self, schema):
+        for col in self.api.list():
+            table_name = col['name']
+            describe = self.api.query('DESCRIBE "{}"'.format(table_name))
+            columns = list(set(map(lambda x: x['field'][0], describe['results'])))
+            schema[table_name] = {'name': table_name, 'columns': columns}
+        return schema.values()
+
+
+    def run_query(self, query, user):
+        results = self.api.query(query)
+        if 'code' in results and results['code'] != 200:
+            return None, '{}: {}'.format(results['type'], results['message'])
+
+        rows = results['results']
+        columns = []
+        if len(rows) > 0:
+            columns = []
+            for k in rows[0]:
+                columns.append({'name': k, 'friendly_name': k, 'type': _get_type(rows[0][k])})
+        data = json.dumps({'columns': columns, 'rows': rows}, cls=JSONEncoder)
+        return data, None
+
 
 register(Rockset)
