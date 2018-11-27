d36d18f85b5556071f7812574dd19f4c0b8546a0
Arik Fraimovich <arik@arikfr.com>
2018-11-27 09:06:44 +0200

    Support unicode in Postgres/Redshift schema (#3124)

--- redash/query_runner/pg.py
+++ redash/query_runner/pg.py
@@ -95,7 +95,7 @@ class PostgreSQL(BaseSQLQueryRunner):
 
         for row in results['rows']:
             if row['table_schema'] != 'public':
-                table_name = '{}.{}'.format(row['table_schema'], row['table_name'])
+                table_name = u'{}.{}'.format(row['table_schema'], row['table_name'])
             else:
                 table_name = row['table_name']
 
