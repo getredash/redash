ba487a828e66feb3ba4984535f5571da9d4712b3
Omer Lachish <omer@rauchy.net>
2018-12-02 14:26:49 +0200

    refactor out methods that don't involve any state

--- redash/utils/sql_query.py
+++ redash/utils/sql_query.py
@@ -9,13 +9,51 @@ def _replace_params(template):
     return re.sub('-?{{.+?}}', 'param', template)
 
 
+def _inside_a_where_clause(a):
+    if a is None:
+        return False
+    else:
+        return type(a.parent) is sqlparse.sql.Where or _inside_a_where_clause(a.parent)
+
+
+def _populating_an_in_operator(a, b):
+    if type(a) is sqlparse.sql.Identifier and \
+       type(b) is sqlparse.sql.IdentifierList and \
+       _inside_a_where_clause(a):
+        return True
+
+
+def _equivalent_leaves(a, b):
+    return type(a) == type(b) or \
+        (type(a) is sqlparse.sql.Identifier and type(b) is sqlparse.sql.Token)
+
+
+def _filter_noise(tokens):
+    skippable_tokens = [sqlparse.tokens.Error, sqlparse.tokens.Whitespace]
+    return [t for t in tokens if t.ttype not in skippable_tokens]
+
+
+def _same_type(a, b):
+    if _populating_an_in_operator(a, b):
+        return True
+    elif not _equivalent_leaves(a, b):
+        return False
+    elif type(a) in (list, tuple):
+        children_are_same = [_same_type(child_a, child_b) for (child_a, child_b) in zip(a, b)]
+        return len(a) == len(b) and all(children_are_same)
+    elif (hasattr(a, 'tokens') and hasattr(b, 'tokens')):
+        return _same_type(_filter_noise(a.tokens), _filter_noise(b.tokens))
+    else:
+        return True
+
+
 class SQLQuery(object):
     def __init__(self, template):
         self.template = template
         self.query = template
 
     def apply(self, parameters):
-        if self.__dict__.has_key('text'):
+        if 'text' in self.__dict__:
             del self.__dict__['text']
 
         self.query = mustache_render(self.template, parameters)
@@ -24,20 +62,8 @@ class SQLQuery(object):
     def is_safe(self):
         template_tree = sqlparse.parse(_replace_params(self.template))
         query_tree = sqlparse.parse(self.query)
-        return self._same_type(template_tree, query_tree)
 
-    def _same_type(self, a, b):
-        if type(a) != type(b):
-            return False
-        elif type(a) in (list, tuple):
-            children_are_same = [self._same_type(child_a, child_b) for (child_a, child_b) in zip(a, b)]
-            return len(a) == len(b) and all(children_are_same)
-        elif hasattr(a, 'tokens'):
-            template_tokens = filter(lambda t: t.ttype is not sqlparse.tokens.Error, a.tokens)
-            query_tokens = b.tokens
-            return self._same_type(template_tokens, query_tokens)
-        else:
-            return True
+        return _same_type(template_tree, query_tree)
 
     @cached_property
     def text(self):
