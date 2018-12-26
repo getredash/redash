2e610134f2787ee44ce3f64a99634c3abb749c5d
Giovanni Sollazzo <giovanni_sollazzo@exsigma.com>
2018-12-26 20:58:40 +0100

    FIX: regression in single org login

--- redash/handlers/__init__.py
+++ redash/handlers/__init__.py
@@ -1,19 +1,27 @@
 from flask import jsonify, redirect
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from redash.handlers.api import api
 from redash.handlers.base import routes
+from redash.handlers.static import render_index
 from redash.monitor import get_status
 from redash.permissions import require_super_admin
+from redash import settings
 
 
 @routes.route('/ping', methods=['GET'])
 def ping():
     return 'PONG.'
 
+
 @routes.route('/', methods=['GET'])
 def default_route():
-    return redirect("/default/", code=302)
+    if settings.MULTI_ORG:
+        return redirect("/default/", code=302)
+    elif current_user.is_authenticated:
+        return render_index()
+    else:
+    	return redirect("/login/", code=302)
 
 
 @routes.route('/status.json')
