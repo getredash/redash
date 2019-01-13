25efffe5f7bd03acc0b7b0cf928972fde7423e44
GabrielSEBAG <gabriel.sebag@efrei.net>
2019-01-13 10:04:56 +0100

    added mixpanel datasource

--- redash/settings/__init__.py
+++ redash/settings/__init__.py
@@ -190,6 +190,7 @@ default_query_runners = [
     'redash.query_runner.druid',
     'redash.query_runner.kylin',
     'redash.query_runner.drill',
+    'redash.query_runner.mixpanel',
 ]
 
 enabled_query_runners = array_from_string(os.environ.get("REDASH_ENABLED_QUERY_RUNNERS", ",".join(default_query_runners)))
