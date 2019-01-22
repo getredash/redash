ae8220a4e86f9fa96f4ad72e85218e139e31c891
Vibhor Kumar <vkumar@uptycs.com>
2019-01-22 07:56:19 -0500

    fixed some indentation issues

--- redash/query_runner/uptycs.py
+++ redash/query_runner/uptycs.py
@@ -1,3 +1,6 @@
+from redash.query_runner import *
+from redash.utils import JSONEncoder, json_dumps
+
 import os
 import sys
 import json
@@ -8,14 +11,13 @@ import urllib3
 import logging
 logger = logging.getLogger(__name__)
 
-from redash.query_runner import *
-from redash.utils import JSONEncoder, json_dumps
 
 enabled = True
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class Uptycs(BaseSQLQueryRunner):
+
 
     noop_query = "SELECT 1"
 
@@ -27,7 +29,7 @@ class Uptycs(BaseSQLQueryRunner):
                 "url": {
                     "type": "string"
                 },
-                 "customer_id": {
+                "customer_id": {
                     "type": "string"
                 },
                 "key": {
@@ -38,23 +40,22 @@ class Uptycs(BaseSQLQueryRunner):
                 },
             },
             "order": ['url', 'customer_id', 'key', 'secret'],
-            "required": ["url", "customer_id", "key","secret"],
+            "required": ["url", "customer_id", "key", "secret"],
             "secret": ["secret", "customer_id", "key"]
         }
-
 
     @classmethod
     def annotate_query(cls):
         return False
 
     def generate_header(self, key, secret):
-        header={}
-        utcnow=datetime.datetime.utcnow()
-        date=utcnow.strftime("%a, %d %b %Y %H:%M:%S GMT")
+        header = {}
+        utcnow = datetime.datetime.utcnow()
+        date = utcnow.strftime("%a, %d %b %Y %H:%M:%S GMT")
         auth_var = jwt.encode({'iss': key}, secret, algorithm='HS256')
-        authorization="Bearer %s" % (auth_var)
-        header['date']=date
-        header['Authorization']=authorization
+        authorization = "Bearer %s" % (auth_var)
+        header['date'] = date
+        header['Authorization'] = authorization
         return header
 
     def transformed_to_redash_json(self, data):
@@ -65,42 +66,42 @@ class Uptycs(BaseSQLQueryRunner):
         if 'columns' in data:
             for json_each in data['columns']:
                 name = json_each['name']
-                new_json = {"name": name, 
-                            "type": "string", 
+                new_json = {"name": name,
+                            "type": "string",
                             "friendly_name": name}
                 transformed_columns.append(new_json)
         # Transfored items into rows.
         if 'items' in data:
             rows = data['items']
 
-        redash_json_data = {"columns": transformed_columns, 
-                            "rows": rows}  
+        redash_json_data = {"columns": transformed_columns,
+                            "rows": rows}
         return redash_json_data
 
     def api_call(self, sql):
         # JWT encoded header
-        header = self.generate_header( self.configuration.get('key'), 
+        header = self.generate_header(self.configuration.get('key'),
                                        self.configuration.get('secret'))
 
         # URL form using API key file based on GLOBAL
-        url=("%s/public/api/customers/%s/query" %
-            (self.configuration.get('url'), 
+        url = ("%s/public/api/customers/%s/query" %
+            (self.configuration.get('url'),
              self.configuration.get('customer_id')));
 
         # post data base sql
-        post_data_json = { "query": sql }
+        post_data_json = {"query": sql}
 
-        response = requests.post(url, headers=header, 
+        response = requests.post(url, headers=header,
                 json=post_data_json, verify=False)
 
         if response.status_code == 200:
             response_output = json.loads(response.content)
         else:
-            error = 'status_code ' + str(response.status_code) + '\n' 
+            error = 'status_code ' + str(response.status_code) + '\n'
             error = error + "failed to connect"
             json_data = {}
             return json_data, error
-     
+    
         json_data = self.transformed_to_redash_json(response_output)
         error = None
         # if we got error from Uptycs include error information
@@ -116,11 +117,11 @@ class Uptycs(BaseSQLQueryRunner):
         return json_data, error
 
     def get_schema(self, get_stats=False):
-        header = self.generate_header(self.configuration.get('key'), 
+        header = self.generate_header(self.configuration.get('key'),
                                      self.configuration.get('secret'))
         url = ("%s/public/api/customers/%s/schema/global" %
-                (self.configuration.get('url'), 
-                self.configuration.get('customer_id')))
+            (self.configuration.get('url'),
+            self.configuration.get('customer_id')))
         response = requests.get(url, headers=header, verify=False)
         redash_json = []
         schema = json.loads(response.content)
