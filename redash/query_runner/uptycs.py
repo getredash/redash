f166936ab488bb3bbde52d0e44ebebeafa337634
Vibhor Kumar <vkumar@uptycs.com>
2019-01-22 07:35:00 -0500

    as per comment from Arik comment fixed the code

--- redash/query_runner/uptycs.py
+++ redash/query_runner/uptycs.py
@@ -9,7 +9,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 from redash.query_runner import *
-from redash.utils import JSONEncoder
+from redash.utils import JSONEncoder, json_dumps
 
 enabled = True
 
@@ -27,7 +27,7 @@ class Uptycs(BaseSQLQueryRunner):
                 "url": {
                     "type": "string"
                 },
-                 "customerid": {
+                 "customer_id": {
                     "type": "string"
                 },
                 "key": {
@@ -37,32 +37,22 @@ class Uptycs(BaseSQLQueryRunner):
                     "type": "string",
                 },
             },
-            "order": ['url', 'customerid', 'key', 'secret'],
-            "required": ["url", "customerid", "key","secret"],
-            "secret": ["secret", "customerid", "key"]
+            "order": ['url', 'customer_id', 'key', 'secret'],
+            "required": ["url", "customer_id", "key","secret"],
+            "secret": ["secret", "customer_id", "key"]
         }
 
-
-    @classmethod
-    def enabled(cls):
-        return enabled
 
     @classmethod
     def annotate_query(cls):
         return False
 
-
-    @classmethod
-    def type(cls):
-        return "uptycs"
-
-
     def generateHeaders(self, key, secret):
         header={}
         utcnow=datetime.datetime.utcnow()
         date=utcnow.strftime("%a, %d %b %Y %H:%M:%S GMT")
-        authVar = jwt.encode({'iss': key}, secret, algorithm='HS256')
-        authorization="Bearer %s" % (authVar)
+        auth_var = jwt.encode({'iss': key}, secret, algorithm='HS256')
+        authorization="Bearer %s" % (auth_var)
         header['date']=date
         header['Authorization']=authorization
         return header
@@ -77,7 +67,7 @@ class Uptycs(BaseSQLQueryRunner):
                 name = json_each['name']
                 new_json = {"name": name, 
                             "type": "string", 
-                            "friendly_name": "null"}
+                            "friendly_name": name}
                 transformed_columns.append(new_json)
         # Transfored items into rows.
         if 'items' in data:
@@ -87,7 +77,7 @@ class Uptycs(BaseSQLQueryRunner):
                             "rows": rows}  
         return redash_json_data
 
-    def restcall(self, sql):
+    def api_call(self, sql):
         # JWT encoded header
         header = self.generateHeaders( self.configuration.get('key'), 
                                        self.configuration.get('secret'))
@@ -95,14 +85,21 @@ class Uptycs(BaseSQLQueryRunner):
         # URL form using API key file based on GLOBAL
         url=("%s/public/api/customers/%s/query" %
             (self.configuration.get('url'), 
-             self.configuration.get('customerid')));
+             self.configuration.get('customer_id')));
 
         # post data base sql
         post_data_json = { "query": sql }
 
         response = requests.post(url, headers=header, 
-                json=post_data_json, verify=False )
-        response_output = json.loads(response.content)
+                json=post_data_json, verify=False)
+
+        if response.status_code == 200:
+            response_output = json.loads(response.content)
+        else:
+            error = 'status_code ' + str(response.status_code) + '\n' 
+            error = error + "failed to connect"
+            json_data = {}
+            return json_data, error
      
         json_data = self.transformed_to_redash_json(response_output)
         error = None
@@ -113,8 +110,8 @@ class Uptycs(BaseSQLQueryRunner):
         return json_data, error
 
     def run_query(self, query, user):
-        data, error = self.restcall(query)
-        json_data = json.dumps(data, cls=JSONEncoder)
+        data, error = self.api_call(query)
+        json_data = json_dumps(data, cls=JSONEncoder)
         logger.debug("%s",json_data)
         return json_data, error
 
@@ -123,8 +120,8 @@ class Uptycs(BaseSQLQueryRunner):
                                      self.configuration.get('secret'))
         url = ("%s/public/api/customers/%s/schema/global" %
                 (self.configuration.get('url'), 
-                self.configuration.get('customerid')))
-        response = requests.get(url, headers=header, verify=False )
+                self.configuration.get('customer_id')))
+        response = requests.get(url, headers=header, verify=False)
         redash_json = []
         schema = json.loads(response.content)
         for each_def in schema['tables']:
@@ -137,5 +134,5 @@ class Uptycs(BaseSQLQueryRunner):
         logger.debug("%s", schema.values())
         return redash_json
 
-register(Uptycs)
 
+register(Uptycs)
