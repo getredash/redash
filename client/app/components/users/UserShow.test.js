8cfb157b241476fae0c6aadb19c08ab67f6329b8
Gabriel Dutra <nesk.frz@gmail.com>
2019-01-26 14:04:09 -0200

    Update UserShow to use UserProfile prop

--- client/app/components/users/UserShow.test.js
+++ client/app/components/users/UserShow.test.js
@@ -3,7 +3,13 @@ import renderer from 'react-test-renderer';
 import { UserShow } from './UserShow';
 
 test('renders correctly', () => {
-  const component = renderer.create(<UserShow name="John Doe" email="john@doe.com" profileImageUrl="http://www.images.com/llama.jpg" />);
+  const user = {
+    name: 'John Doe',
+    email: 'john@doe.com',
+    profileImageUrl: 'http://www.images.com/llama.jpg',
+  };
+
+  const component = renderer.create(<UserShow user={user} />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
