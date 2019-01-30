9e2f8e2461799ffd4c882b7b8a05601064d1fd10
Ran Byron <ranbena@gmail.com>
2019-01-30 10:43:38 +0200

    Fix: Escape button in tag edit modal (#3363)

--- client/app/components/tags-control/TagsEditorModal.jsx
+++ client/app/components/tags-control/TagsEditorModal.jsx
@@ -1,12 +1,12 @@
-import { map, trim, uniq } from 'lodash';
+import { map, trim, chain } from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { react2angular } from 'react2angular';
 import Select from 'antd/lib/select';
+import Modal from 'antd/lib/modal';
 
 const { Option } = Select;
 
-class TagsEditorModal extends React.Component {
+export default class TagsEditorModal extends React.Component {
   static propTypes = {
     tags: PropTypes.arrayOf(PropTypes.string),
     availableTags: PropTypes.arrayOf(PropTypes.string),
@@ -23,81 +23,41 @@ class TagsEditorModal extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      result: uniq(map(this.props.tags, trim)),
-    };
-    this.selectRef = React.createRef();
-  }
 
-  componentDidMount() {
-    // `autoFocus` does not work on Select because its `componentDidMount` is fired before the component
-    // is actually visible, so it cannot get focus. This hack should be replaced with `autoFocus` prop
-    // when Angular will finally gone
-    setTimeout(() => {
-      if (
-        this.selectRef.current &&
-        this.selectRef.current.rcSelect &&
-        this.selectRef.current.rcSelect.inputRef
-      ) {
-        this.selectRef.current.rcSelect.inputRef.focus();
-      }
-    });
+    this.state = {
+      result: chain(this.props.tags).map(trim).uniq().value(),
+    };
+
+    this.selectOptions =
+      chain(this.props.availableTags)
+        .map(trim)
+        .uniq()
+        .map(tag => <Option key={tag}>{tag}</Option>)
+        .value();
   }
 
   render() {
-    const {
-      availableTags,
-      close,
-      dismiss,
-    } = this.props;
-
-    const uniqueAvailableTags = uniq(map(availableTags, trim));
+    const { close, dismiss } = this.props;
+    const { result } = this.state;
 
     return (
-      <div>
-        <div className="modal-header">
-          <button type="button" className="close" aria-hidden="true" onClick={dismiss}>&times;</button>
-          <h4 className="modal-title">Add/Edit Tags</h4>
-        </div>
-        <div className="modal-body">
-          <Select
-            ref={this.selectRef}
-            mode="tags"
-            className="w-100"
-            placeholder="Add some tags..."
-            defaultValue={this.state.result}
-            onChange={values => this.setState({ result: map(values, trim) })}
-            dropdownClassName="ant-dropdown-in-bootstrap-modal"
-          >
-            {uniqueAvailableTags.map(tag => (<Option key={tag}>{tag}</Option>))}
-          </Select>
-        </div>
-        <div className="modal-footer">
-          <button type="button" className="btn btn-default" onClick={dismiss}>Close</button>
-          <button type="button" className="btn btn-primary" onClick={() => close({ $value: this.state.result })}>Save</button>
-        </div>
-      </div>
+      <Modal
+        visible
+        title="Add/Edit Tags"
+        onOk={() => close(result)}
+        onCancel={dismiss}
+      >
+        <Select
+          mode="tags"
+          className="w-100"
+          placeholder="Add some tags..."
+          defaultValue={result}
+          onChange={values => this.setState({ result: map(values, trim) })}
+          autoFocus
+        >
+          {this.selectOptions}
+        </Select>
+      </Modal>
     );
   }
 }
-
-export default function init(ngModule) {
-  ngModule.component('tagsEditorModal', {
-    template: `
-      <tags-editor-modal-impl 
-        tags="$ctrl.resolve.tags"
-        available-tags="$ctrl.resolve.availableTags"
-        close="$ctrl.close"
-        dismiss="$ctrl.dismiss"
-      ></tags-editor-modal-impl>
-    `,
-    bindings: {
-      resolve: '<',
-      close: '&',
-      dismiss: '&',
-    },
-  });
-  ngModule.component('tagsEditorModalImpl', react2angular(TagsEditorModal));
-}
-
-init.init = true;
