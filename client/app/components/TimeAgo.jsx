06fc2662fee60d3f739646d368a6e4f9aa1bf819
Levko Kravets <levko.ne@gmail.com>
2019-02-01 10:10:09 +0200

    Replace <am-time-ago> (angular-moment) and <rd-timer> with React component

---
+++ client/app/components/TimeAgo.jsx
@@ -0,0 +1,91 @@
+import moment from 'moment';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const interactiveComponents = new Set();
+
+function updateInteractiveComponents() {
+  interactiveComponents.forEach(component => component.forceUpdate());
+  setTimeout(updateInteractiveComponents, 3000);
+}
+updateInteractiveComponents();
+
+export class TimeAgo extends React.Component {
+  static propTypes = {
+    date: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.instanceOf(Date),
+      // `moment`
+      (props, propName, componentName) => {
+        const value = props[propName];
+        if ((value !== null) && !moment.isMoment(value)) {
+          return new Error('Prop `' + propName + '` supplied to `' + componentName + '` should be a Moment.js instance.');
+        }
+      },
+    ]),
+    interactive: PropTypes.bool,
+    placeholder: PropTypes.string,
+  };
+
+  static defaultProps = {
+    date: null,
+    interactive: true,
+    placeholder: '',
+  };
+
+  constructor(props) {
+    super(props);
+    this._value = moment(this.props.date);
+  }
+
+  componentDidMount() {
+    if (this.props.interactive) {
+      interactiveComponents.add(this);
+    }
+  }
+
+  shouldComponentUpdate(nextProps) {
+    this._value = moment(nextProps.date);
+    if (nextProps.interactive) {
+      interactiveComponents.add(this);
+    } else {
+      interactiveComponents.delete(this);
+    }
+    return true; // default behaviour
+  }
+
+  componentWillUnmount() {
+    interactiveComponents.delete(this);
+  }
+
+  render() {
+    return this._value.isValid() ? this._value.fromNow() : this.props.placeholder;
+  }
+}
+
+export default function init(ngModule) {
+  ngModule.directive('amTimeAgo', () => ({
+    link($scope, element, attr) {
+      const modelName = attr.amTimeAgo;
+      $scope.$watch(modelName, (value) => {
+        ReactDOM.render(<TimeAgo date={value} />, element[0]);
+      });
+    },
+  }));
+
+  ngModule.component('rdTimeAgo', {
+    bindings: {
+      value: '=',
+    },
+    controller($scope, $element) {
+      $scope.$watch('$ctrl.value', () => {
+        // Initial render will occur here as well
+        ReactDOM.render(<TimeAgo date={this.value} placeholder="-" />, $element[0]);
+      });
+    },
+  });
+}
+
+init.init = true;
