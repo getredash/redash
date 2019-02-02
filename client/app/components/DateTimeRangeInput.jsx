807e6aaaa61b876abf77eea5168c7e659db00c9a
Levko Kravets <levko.ne@gmail.com>
2019-02-02 18:46:48 +0200

    Migrate "time ago" components to React (#3385)
    * Replace <am-time-ago> (angular-moment) and <rd-timer> with React component
    
    * PropTypes: Moment validation
    
    * Increase polling interval
    
    * Refine component implementation
    
    * Add tooltip with formatted date/time
    
    * Refine component implementation

--- client/app/components/DateTimeRangeInput.jsx
+++ client/app/components/DateTimeRangeInput.jsx
@@ -1,10 +1,10 @@
-import moment from 'moment';
 import { isArray } from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { react2angular } from 'react2angular';
 import DatePicker from 'antd/lib/date-picker';
 import { clientConfig } from '@/services/auth';
+import { Moment } from '@/components/proptypes';
 
 const { RangePicker } = DatePicker;
 
@@ -32,18 +32,7 @@ export function DateTimeRangeInput({
 }
 
 DateTimeRangeInput.propTypes = {
-  value: (props, propName, componentName) => {
-    const value = props[propName];
-    if (
-      (value !== null) && !(
-        isArray(value) && (value.length === 2) &&
-        moment.isMoment(value[0]) && moment.isMoment(value[1])
-      )
-    ) {
-      return new Error('Prop `' + propName + '` supplied to `' + componentName +
-        '` should be an array of two Moment.js instances.');
-    }
-  },
+  value: PropTypes.arrayOf(Moment),
   withSeconds: PropTypes.bool,
   onSelect: PropTypes.func,
   className: PropTypes.string,
