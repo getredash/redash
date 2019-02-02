c56c4a0f67b01ea1078894ce60ea0909969a35a8
Levko Kravets <levko.ne@gmail.com>
2019-02-02 18:46:48 +0200

    Migrate "time ago" components to React (#3385)
    * Replace <am-time-ago> (angular-moment) and <rd-timer> with React component
    
    * PropTypes: Moment validation
    
    * Increase polling interval
    
    * Refine component implementation
    
    * Add tooltip with formatted date/time
    
    * Refine component implementation

--- client/app/components/proptypes.js
+++ client/app/components/proptypes.js
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+import { wrap } from 'lodash';
+import moment from 'moment';
 
 export const DataSource = PropTypes.shape({
   syntax: PropTypes.string,
@@ -49,3 +51,15 @@ export const Action = PropTypes.shape({
 export const AntdForm = PropTypes.shape({
   validateFieldsAndScroll: PropTypes.func,
 });
+
+function checkMoment(isRequired, props, propName, componentName) {
+  const value = props[propName];
+  const isRequiredValid = isRequired && (value !== null);
+  const isOptionalValid = !isRequired && ((value === null) || moment.isMoment(value));
+  if (!isRequiredValid && !isOptionalValid) {
+    return new Error('Prop `' + propName + '` supplied to `' + componentName + '` should be a Moment.js instance.');
+  }
+}
+
+export const Moment = wrap(false, checkMoment);
+Moment.isRequired = wrap(true, checkMoment);
