61fbb9307cbbf97cb463a1d2e5736cfc2ec2e9b2
Levko Kravets <levko.ne@gmail.com>
2019-02-06 00:19:31 +0200

    [Bug fix] Few small bugs on Queries list page (#3402)
    * Link to query page
    * Sidebar menu item title: `Archive` -> `Archived`
    * Whitespaces in empty state block

--- client/app/pages/queries-list/QueriesListEmptyState.jsx
+++ client/app/pages/queries-list/QueriesListEmptyState.jsx
@@ -24,8 +24,8 @@ export default function QueriesListEmptyState({ page, searchTerm, selectedTags }
     );
     case 'my': return (
       <div className="tiled bg-white p-15">
-        <a href="queries/new" className="btn btn-primary btn-sm">Create your first query</a> to populate My Queries
-        list. Need help? Check out our
+        <a href="queries/new" className="btn btn-primary btn-sm">Create your first query</a>
+        {' '}to populate My Queries list. Need help? Check out our{' '}
         <a href="https://redash.io/help/user-guide/querying/writing-queries">query writing documentation</a>.
       </div>
     );
