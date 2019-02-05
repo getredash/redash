3b82a9514d820011bb20d7894c6e33b4572a444d
Levko Kravets <levko.ne@gmail.com>
2019-02-05 21:13:32 +0200

    Migrate Dashboards/Queries/Users list pages to React (#3381)
    * Refine existing implementation of dashboards/queries/users lists and a common base controller
    
    * Migrate common list page controller to React and refactor it's logic
    
    * Migrate Dashboard list page to React
    
    * Migrate Queries list page to React
    
    * Migrate Users list page to React
    
    * Remove react-timeago dependency
    
    * Use composition instead of inheritance
    
    * Refine implementation
    
    * Merge sidebar into single component
    
    * Refine column definitions
    
    * Use simple controller instead of React context
    
    * Refine implementation
    
    * Restore changes from getredash/redash#2888
    
    * Tweak Users list page
    
    * Ability to render dynamically defined components
    
    * Tweak users list page
    
    * User list page for non-admins
    
    * Fix: ItemsTable ignores isAvailable field
    
    * Refine implementation
    
    * Refine implementation
    
    * Implement LiveItemsList as higher order component
    
    * Some fixes
    
    * Move some definitions to a better place
    
    * Some fixes
    
    * Refine components
    
    * Refine UsersList page
    
    * More comments for a god of comments
    
    * Fix wrong tables size on smaller screens
    
    * Tweak tables

---
+++ client/app/pages/dashboards/DashboardList.jsx
@@ -0,0 +1,164 @@
+import React from 'react';
+import { react2angular } from 'react2angular';
+
+import { PageHeader } from '@/components/PageHeader';
+import { Paginator } from '@/components/Paginator';
+import { DashboardTagsControl } from '@/components/tags-control/DashboardTagsControl';
+
+import { wrap as liveItemsList, createResourceFetcher, ControllerType } from '@/components/items-list/LiveItemsList';
+import LoadingState from '@/components/items-list/components/LoadingState';
+import * as Sidebar from '@/components/items-list/components/Sidebar';
+import ItemsTable, { Columns } from '@/components/items-list/components/ItemsTable';
+
+import { Dashboard } from '@/services/dashboard';
+import navigateTo from '@/services/navigateTo';
+import { routesToAngularRoutes } from '@/lib/utils';
+
+import DashboardListEmptyState from './DashboardListEmptyState';
+
+import './dashboard-list.css';
+
+class DashboardList extends React.Component {
+  static propTypes = {
+    controller: ControllerType.isRequired,
+  };
+
+  sidebarMenu = [
+    {
+      key: 'all',
+      href: 'dashboards',
+      title: 'All Dashboards',
+    },
+    {
+      key: 'favorites',
+      href: 'dashboards/favorites',
+      title: 'Favorites',
+      icon: () => <Sidebar.MenuIcon icon="fa fa-star" />,
+    },
+  ];
+
+  listColumns = [
+    Columns.favorites({ className: 'p-r-0' }),
+    Columns.custom.sortable((text, item) => (
+      <React.Fragment>
+        <a className="table-main-title" href={'dashboard/' + item.slug}>{ item.name }</a>
+        <DashboardTagsControl
+          className="d-block"
+          tags={item.tags}
+          isDraft={item.is_draft}
+          isArchived={item.is_archived}
+        />
+      </React.Fragment>
+    ), {
+      title: 'Name',
+      field: 'name',
+      width: null,
+    }),
+    Columns.avatar({ field: 'user', className: 'p-l-0 p-r-0' }, name => `Created by ${name}`),
+    Columns.dateTime.sortable({
+      title: 'Created At',
+      field: 'created_at',
+      className: 'text-nowrap',
+      width: '1%',
+    }),
+  ];
+
+  onTableRowClick = (event, item) => navigateTo('dashboard/' + item.slug);
+
+  renderSidebar() {
+    const { controller } = this.props;
+    return (
+      <React.Fragment>
+        <Sidebar.SearchInput
+          placeholder="Search Dashboards..."
+          value={controller.searchTerm}
+          onChange={controller.updateSearch}
+        />
+        <Sidebar.Menu items={this.sidebarMenu} selected={controller.currentPage} />
+        <Sidebar.Tags url="api/dashboards/tags" onChange={controller.updateSelectedTags} />
+        <Sidebar.PageSizeSelect
+          options={controller.pageSizeOptions}
+          value={controller.itemsPerPage}
+          onChange={itemsPerPage => controller.updatePagination({ itemsPerPage })}
+        />
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    const sidebar = this.renderSidebar();
+    const { controller } = this.props;
+    return (
+      <div className="container">
+        <PageHeader title={controller.title} />
+        <div className="row">
+          <div className="col-md-3 list-control-t">{sidebar}</div>
+          <div className="list-content col-md-9">
+            {!controller.isLoaded && <LoadingState />}
+            {
+              controller.isLoaded && controller.isEmpty && (
+                <DashboardListEmptyState
+                  page={controller.currentPage}
+                  searchTerm={controller.searchTerm}
+                  selectedTags={controller.selectedTags}
+                />
+              )
+            }
+            {
+              controller.isLoaded && !controller.isEmpty && (
+                <div className="bg-white tiled table-responsive">
+                  <ItemsTable
+                    items={controller.pageItems}
+                    columns={this.listColumns}
+                    onRowClick={this.onTableRowClick}
+                    orderByField={controller.orderByField}
+                    orderByReverse={controller.orderByReverse}
+                    toggleSorting={controller.toggleSorting}
+                  />
+                  <Paginator
+                    totalCount={controller.totalItemsCount}
+                    itemsPerPage={controller.itemsPerPage}
+                    page={controller.page}
+                    onChange={page => controller.updatePagination({ page })}
+                  />
+                </div>
+              )
+            }
+          </div>
+          <div className="col-md-3 list-control-r-b">{sidebar}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default function init(ngModule) {
+  ngModule.component('pageDashboardList', react2angular(liveItemsList(DashboardList, {
+    defaultOrderBy: '-created_at',
+    doRequest: createResourceFetcher(
+      ({ currentPage }) => ({
+        all: Dashboard.query.bind(Dashboard),
+        favorites: Dashboard.favorites.bind(Dashboard),
+      }[currentPage]),
+      item => new Dashboard(item),
+    ),
+  })));
+
+  return routesToAngularRoutes([
+    {
+      path: '/dashboards',
+      title: 'Dashboards',
+      key: 'all',
+    },
+    {
+      path: '/dashboards/favorites',
+      title: 'Favorite Dashboards',
+      key: 'favorites',
+    },
+  ], {
+    template: '<page-dashboard-list current-page="$resolve.currentPage"></page-dashboard-list>',
+    reloadOnSearch: false,
+  });
+}
+
+init.init = true;
---
+++ client/app/pages/queries-list/QueriesList.jsx
@@ -0,0 +1,192 @@
+import React from 'react';
+import { react2angular } from 'react2angular';
+
+import { PageHeader } from '@/components/PageHeader';
+import { Paginator } from '@/components/Paginator';
+import { QueryTagsControl } from '@/components/tags-control/QueryTagsControl';
+import { SchedulePhrase } from '@/components/queries/SchedulePhrase';
+
+import { wrap as liveItemsList, createResourceFetcher, ControllerType } from '@/components/items-list/LiveItemsList';
+import LoadingState from '@/components/items-list/components/LoadingState';
+import * as Sidebar from '@/components/items-list/components/Sidebar';
+import ItemsTable, { Columns } from '@/components/items-list/components/ItemsTable';
+
+import { Query } from '@/services/query';
+import { currentUser } from '@/services/auth';
+import navigateTo from '@/services/navigateTo';
+import { routesToAngularRoutes } from '@/lib/utils';
+
+import QueriesListEmptyState from './QueriesListEmptyState';
+
+import './queries-list.css';
+
+class QueriesList extends React.Component {
+  static propTypes = {
+    controller: ControllerType.isRequired,
+  };
+
+  sidebarMenu = [
+    {
+      key: 'all',
+      href: 'queries',
+      title: 'All Queries',
+    },
+    {
+      key: 'favorites',
+      href: 'queries/favorites',
+      title: 'Favorites',
+      icon: () => <Sidebar.MenuIcon icon="fa fa-star" />,
+    },
+    {
+      key: 'archive',
+      href: 'queries/archive',
+      title: 'Archive',
+      icon: () => <Sidebar.MenuIcon icon="fa fa-archive" />,
+    },
+    {
+      key: 'my',
+      href: 'queries/my',
+      title: 'My Queries',
+      icon: () => <Sidebar.ProfileImage user={currentUser} />,
+      isAvailable: () => currentUser.hasPermission('create_query'),
+    },
+  ];
+
+  listColumns = [
+    Columns.favorites({ className: 'p-r-0' }),
+    Columns.custom.sortable((text, item) => (
+      <React.Fragment>
+        <a className="table-main-title" href={'dashboard/' + item.slug}>{ item.name }</a>
+        <QueryTagsControl
+          className="d-block"
+          tags={item.tags}
+          isDraft={item.is_draft}
+          isArchived={item.is_archived}
+        />
+      </React.Fragment>
+    ), {
+      title: 'Name',
+      field: 'name',
+      width: null,
+    }),
+    Columns.avatar({ field: 'user', className: 'p-l-0 p-r-0' }, name => `Created by ${name}`),
+    Columns.dateTime.sortable({ title: 'Created At', field: 'created_at' }),
+    Columns.duration.sortable({ title: 'Runtime', field: 'runtime' }),
+    Columns.dateTime.sortable({ title: 'Last Executed At', field: 'retrieved_at', orderByField: 'executed_at' }),
+    Columns.custom.sortable(
+      (text, item) => <SchedulePhrase schedule={item.schedule} isNew={item.isNew()} />,
+      { title: 'Update Schedule', field: 'schedule' },
+    ),
+  ];
+
+  onTableRowClick = (event, item) => navigateTo('queries/' + item.id);
+
+  renderSidebar() {
+    const { controller } = this.props;
+    return (
+      <React.Fragment>
+        <Sidebar.SearchInput
+          placeholder="Search Queries..."
+          value={controller.searchTerm}
+          onChange={controller.updateSearch}
+        />
+        <Sidebar.Menu items={this.sidebarMenu} selected={controller.currentPage} />
+        <Sidebar.Tags url="api/queries/tags" onChange={controller.updateSelectedTags} />
+        <Sidebar.PageSizeSelect
+          options={controller.pageSizeOptions}
+          value={controller.itemsPerPage}
+          onChange={itemsPerPage => controller.updatePagination({ itemsPerPage })}
+        />
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    const sidebar = this.renderSidebar();
+    const { controller } = this.props;
+    return (
+      <div className="container">
+        <PageHeader title={controller.title} />
+        <div className="row">
+          <div className="col-md-3 list-control-t">{sidebar}</div>
+          <div className="list-content col-md-9">
+            {!controller.isLoaded && <LoadingState />}
+            {
+              controller.isLoaded && controller.isEmpty && (
+                <QueriesListEmptyState
+                  page={controller.currentPage}
+                  searchTerm={controller.searchTerm}
+                  selectedTags={controller.selectedTags}
+                />
+              )
+            }
+            {
+              controller.isLoaded && !controller.isEmpty && (
+                <div className="bg-white tiled table-responsive">
+                  <ItemsTable
+                    items={controller.pageItems}
+                    columns={this.listColumns}
+                    onRowClick={this.onTableRowClick}
+                    orderByField={controller.orderByField}
+                    orderByReverse={controller.orderByReverse}
+                    toggleSorting={controller.toggleSorting}
+                  />
+                  <Paginator
+                    totalCount={controller.totalItemsCount}
+                    itemsPerPage={controller.itemsPerPage}
+                    page={controller.page}
+                    onChange={page => controller.updatePagination({ page })}
+                  />
+                </div>
+              )
+            }
+          </div>
+          <div className="col-md-3 list-control-r-b">{sidebar}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default function init(ngModule) {
+  ngModule.component('pageQueriesList', react2angular(liveItemsList(QueriesList, {
+    defaultOrderBy: '-created_at',
+    doRequest: createResourceFetcher(
+      ({ currentPage }) => ({
+        all: Query.query.bind(Query),
+        my: Query.myQueries.bind(Query),
+        favorites: Query.favorites.bind(Query),
+        archive: Query.archive.bind(Query),
+      }[currentPage]),
+      item => new Query(item),
+    ),
+  })));
+
+  return routesToAngularRoutes([
+    {
+      path: '/queries',
+      title: 'Queries',
+      key: 'all',
+    },
+    {
+      path: '/queries/favorites',
+      title: 'Favorite Queries',
+      key: 'favorites',
+    },
+    {
+      path: '/queries/archive',
+      title: 'Archived Queries',
+      key: 'archive',
+    },
+    {
+      path: '/queries/my',
+      title: 'My Queries',
+      key: 'my',
+    },
+  ], {
+    template: '<page-queries-list current-page="$resolve.currentPage"></page-queries-list>',
+    reloadOnSearch: false,
+  });
+}
+
+init.init = true;
