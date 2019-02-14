467de31d19dfaceef8ba87e06a2a3ac7f81fbfb2
Ran Byron <ranbena@gmail.com>
2019-02-14 15:53:04 +0200

    Fixed “typo”

--- client/app/services/HelpTrigger.jsx
+++ client/app/services/HelpTrigger.jsx
@@ -47,7 +47,7 @@ export default class HelpTrigger extends React.PureComponent {
   }
 
   componentWillUnmount() {
-    this.onDrawerClosed();
+    this.removeIframeListeners();
   }
 
   get helpUrl() {
