e72ba07c26a90306fc0655af3312e41b47c64768
Gabriel Dutra <nesk.frz@gmail.com>
2019-02-15 19:05:30 -0200

    Change percy waiting to be with resources instead of time

--- cypress/integration/percy/page_screenshots.js
+++ cypress/integration/percy/page_screenshots.js
@@ -1,19 +1,55 @@
 const pages = [
-  { name: 'Create Data Source - Types', url: '/data_sources/new' },
-  { name: 'Edit Data Source - PostgreSQL', url: '/data_sources/1' },
-  { name: 'Users', url: '/users' },
-  { name: 'Groups', url: '/groups' },
-  { name: 'Group', url: '/groups/1' },
-  { name: 'Create Destination - Types', url: '/destinations/new' },
-  { name: 'Organization Settings', url: '/settings/organization' },
+  {
+    name: 'Create Data Source - Types',
+    url: '/data_sources/new',
+    resources: [ '/api/data_sources/types' ],
+  },
+  {
+    name: 'Edit Data Source - PostgreSQL',
+    url: '/data_sources/1',
+    resources: [ '/api/data_sources/1' ],
+  },
+  {
+    name: 'Users',
+    url: '/users',
+    resources: [ '/api/users**' ],
+  },
+  {
+    name: 'Groups',
+    url: '/groups',
+    resources: [ '/api/groups' ],
+  },
+  {
+    name: 'Group',
+    url: '/groups/1',
+    resources: [ '/api/groups/1' ],
+  },
+  {
+    name: 'Create Destination - Types',
+    url: '/destinations/new',
+    resources: [ '/api/destinations/types' ],
+  },
+  {
+    name: 'Organization Settings',
+    url: '/settings/organization',
+    resources: [ '/api/settings/organization' ],
+  },
 ];
 
 describe('Percy Page Screenshots', () => {
   pages.forEach((page) => {
     it(`takes a screenshot of ${page.name}`, () => {
+      cy.server();
+      page.resources.forEach((resource) => {
+        cy.route(resource).as(resource);
+      });
+
       cy.login();
       cy.visit(page.url);
-      cy.wait(1000);
+      
+      page.resources.forEach((resource) => {
+        cy.wait(`@${resource}`);
+      });
       cy.percySnapshot(page.name);
     });
   });
