5c1c31e1f9a893fb9300ec4ca1cd2b7cdabfb5d8
Gabriel Dutra <nesk.frz@gmail.com>
2019-02-20 16:20:53 -0300

    Render the form after type selection

--- client/app/pages/data-sources/CreateDataSource.jsx
+++ client/app/pages/data-sources/CreateDataSource.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Steps from 'antd/lib/steps';
+import { find } from 'lodash';
 import { react2angular } from 'react2angular';
 import { DataSource } from '@/services/data-source';
 import TypePicker from '@/components/TypePicker';
+import { DynamicForm } from '@/components/dynamic-form/DynamicForm';
+import helper from '@/components/dynamic-form/dynamicFormHelper';
 
 const { Step } = Steps;
 
@@ -21,7 +24,27 @@ class CreateDataSource extends React.Component {
       imgSrc: `${DataSource.IMG_ROOT}/${dataSourceType.type}.png`,
       onClick: () => this.setState({ selectedType: dataSourceType.type }),
     }));
+
     return (<TypePicker types={types} />);
+  }
+
+  renderForm() {
+    const { dataSourceTypes, selectedType } = this.state;
+    const type = find(dataSourceTypes, { type: selectedType });
+    const dataSource = new DataSource({ options: {} });
+    const fields = helper.getFields(type.configuration_schema, dataSource);
+
+    return (
+      <div>
+        <div className="text-center">
+          <img src={`${DataSource.IMG_ROOT}/${selectedType}.png`} alt={type.name} width="64" />
+          <h3>{type.name}</h3>
+        </div>
+        <div className="col-md-4 col-md-offset-4">
+          <DynamicForm fields={fields} feedbackIcons />
+        </div>
+      </div>
+    );
   }
 
   render() {
@@ -29,9 +52,9 @@ class CreateDataSource extends React.Component {
     const currentStep = selectedType ? 1 : 0;
 
     return (
-      <div>
-        <h3 className="text-center m-b-20">New Data Source</h3>
-        <Steps className="m-b-20" current={currentStep}>
+      <div className="row">
+        <h3 className="text-center">New Data Source</h3>
+        <Steps className="p-20" current={currentStep}>
           <Step
             title="Select the Type"
             style={currentStep > 0 ? { cursor: 'pointer' } : null}
@@ -40,7 +63,7 @@ class CreateDataSource extends React.Component {
           <Step title="Configure it" />
           <Step title="Done" />
         </Steps>
-        {!selectedType && this.renderTypes()}
+        {selectedType ? this.renderForm() : this.renderTypes()}
       </div>
     );
   }
