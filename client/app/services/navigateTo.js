27be946b4b5a238700b3123dc056e3bb0f9a7347
Levko Kravets <levko.ne@gmail.com>
2019-03-06 20:28:17 +0200

    Use native events to properly replicate browsers behavior

--- client/app/services/navigateTo.js
+++ client/app/services/navigateTo.js
@@ -11,12 +11,59 @@ export default function navigateTo(url, replace = false) {
   }
 }
 
-export function handleNavigationEvent(event, url, replace = false) {
+function cloneEvent(event) {
+  try {
+    return new event.constructor(event.type, event);
+  } catch (e) {
+    // fallback for IE; also works in modern browsers, but is deprecated
+    const result = document.createEvent(event.constructor.name);
+    if (result.initMouseEvent) {
+      result.initMouseEvent(
+        event.type,
+        event.bubbles,
+        event.cancelable,
+        window,
+        0,
+        event.screenX || 0,
+        event.screenY || 0,
+        event.clientX || 0,
+        event.clientY || 0,
+        event.ctrlKey || false,
+        event.altKey || false,
+        event.shiftKey || false,
+        event.metaKey || false,
+        event.button || 0,
+        event.relatedTarget || null,
+      );
+    } else {
+      result.initEvent(
+        event.type,
+        event.bubbles,
+        event.cancelable,
+      );
+    }
+    return result;
+  }
+}
+
+export function handleNavigationEvent(event, url) {
+  // cancel this event - we're going to forward it to <a> tag to keep
+  // native browser's behavior when modifier keys pressed (ctrl/shift/meta)
   event.preventDefault();
   event.stopPropagation();
-  if (event.ctrlKey || event.metaKey) {
-    window.open(url, '_blank');
-  } else {
-    navigateTo(url, replace);
-  }
+
+  // create <a> tag - need to append it to <body> to get angular-router working
+  const link = document.createElement('a');
+  link.setAttribute('href', url);
+  link.style.position = 'absolute';
+  link.style.left = '-100px';
+  link.style.top = '-100px';
+  document.body.appendChild(link);
+  // we cannot re-dispatch the same event, so create a copy
+  link.dispatchEvent(cloneEvent(event.nativeEvent));
+
+  // cleanup
+  setTimeout(() => {
+    document.body.removeChild(link);
+  }, 1000);
 }
