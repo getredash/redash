554baec2b61808445a75f762db58af5a2e000e7a
Gabriel Dutra <nesk.frz@gmail.com>
2019-03-07 17:33:47 -0300

    Fix DynamicForm import in CreateUserDialog

--- client/app/components/users/CreateUserDialog.jsx
+++ client/app/components/users/CreateUserDialog.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'antd/lib/modal';
 import Alert from 'antd/lib/alert';
-import { DynamicForm } from '@/components/dynamic-form/DynamicForm';
+import DynamicForm from '@/components/dynamic-form/DynamicForm';
 import { wrap as wrapDialog, DialogPropType } from '@/components/DialogWrapper';
 import recordEvent from '@/services/recordEvent';
 
