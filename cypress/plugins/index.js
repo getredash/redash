fbe2b5655be796c8187cf86d3d6d68d159353acc
Ran Byron <ranbena@gmail.com>
2019-03-11 22:47:50 +0200

    Added snapshot plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,1 +1,6 @@
-module.exports = () => {};
+const { initPlugin } = require('cypress-plugin-snapshots/plugin');
+
+module.exports = (on, config) => {
+  initPlugin(on, config);
+  return config;
+};
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,1 +1,2 @@
 import './commands';
+import 'cypress-plugin-snapshots/commands';
