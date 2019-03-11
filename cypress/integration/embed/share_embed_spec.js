10306b7b990f17193c5e47cdb33e2d63279446b5
Omer Lachish <omer@rauchy.net>
2019-03-11 10:28:50 +0200

    first e2e test for sharing embeds

---
+++ cypress/integration/embed/share_embed_spec.js
@@ -0,0 +1,22 @@
+describe('Embedded Queries', () => {
+  beforeEach(() => {
+    cy.login();
+    cy.visit('/queries/new');
+  });
+
+  it('are shared with text parameters', () => {
+    cy.getByTestId('QueryEditor')
+      .get('.ace_text-input')
+      .type('SELECT * FROM organizations WHERE id=\'{{}{{}id}}\'{esc}', { force: true });
+
+    cy.getByTestId('TextParamInput').type('1');
+    cy.getByTestId('ExecuteButton').click();
+    cy.getByTestId('SaveButton').click();
+    cy.getByTestId('ShowEmbedDialogButton').click({ force: true });
+    cy.getByTestId('EmbedIframe').invoke('text').then((iframe) => {
+      const embedUrl = iframe.match(/"(.*?)"/)[1]
+      cy.logout();
+      cy.visit(embedUrl);
+    });
+  });
+})
