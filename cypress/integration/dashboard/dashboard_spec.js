39d7edd7f4d7372a28493b5e3347e5d43630e87c
Ran Byron <ranbena@gmail.com>
2019-03-12 20:25:41 +0200

    Added Textbox testing - add, remove, edit

--- cypress/integration/dashboard/dashboard_spec.js
+++ cypress/integration/dashboard/dashboard_spec.js
@@ -36,6 +36,14 @@ function archiveCurrentDashboard() {
   cy.get('.label-tag-archived').should('exist');
 }
 
+function addTextbox() {
+  cy.contains('a', 'Add Textbox').click();
+  cy.get('.add-textbox').within(() => {
+    cy.get('textarea').type('Hello world!');
+  });
+  cy.contains('button', 'Add to Dashboard').click();
+}
+
 describe('Dashboard', () => {
   beforeEach(() => {
     cy.login();
@@ -54,6 +62,68 @@ describe('Dashboard', () => {
       cy.getByTestId('DashboardLayoutContent').within(() => {
         cy.getByTestId(slug).should('not.exist');
       });
+    });
+  });
+
+  describe.only('Textbox', () => {
+    beforeEach(() => {
+      createNewDashboard('Foo Bar');
+      cy.contains('button', 'Apply Changes').click();
+      cy.getByTestId('DashboardMoreMenu')
+        .click()
+        .within(() => {
+          cy.get('li')
+            .contains('Edit')
+            .click();
+        });
+    });
+
+    it('adds and removes textbox (from button)', () => {
+      addTextbox();
+
+      cy.get('.widget-text').within(() => {
+        cy.get('.widget-menu-remove').click();
+      });
+
+      cy.get('.widget-text').should('not.exist');
+    });
+
+    it('adds and removes textbox (from menu)', () => {
+      addTextbox();
+      cy.contains('button', 'Apply Changes').click();
+
+      cy.get('.widget-text').within(() => {
+        cy.get('.widget-menu-regular').click({ force: true }).within(() => {
+          cy.get('li a').contains('Remove From Dashboard').click({ force: true });
+        });
+      });
+
+      cy.get('.widget-text').should('not.exist');
+    });
+
+    it('adds, opens edit dialog and removes textbox', () => {
+      addTextbox();
+      cy.contains('button', 'Apply Changes').click();
+
+      cy.get('.widget-text').within(() => {
+        cy.get('.widget-menu-regular').click({ force: true }).within(() => {
+          cy.get('li a').contains('Edit').click({ force: true });
+        });
+      });
+
+      const newContent = '[edited]';
+      cy.get('edit-text-box').should('exist').within(() => {
+        cy.get('textarea').clear().type(newContent);
+        cy.contains('button', 'Save').click();
+      });
+
+      cy.get('.widget-text')
+        .should('contain', newContent)
+        .within(() => {
+          cy.get('.widget-menu-regular').click({ force: true }).within(() => {
+            cy.get('li a').contains('Remove From Dashboard').click({ force: true });
+          });
+        });
     });
   });
 });
