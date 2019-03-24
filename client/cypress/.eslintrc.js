887382652c7f99a3d38311cf237f8188590e3b9f
Ran Byron <ranbena@gmail.com>
2019-03-24 21:31:56 +0200

    Allowing chai assertion

--- client/cypress/.eslintrc.js
+++ client/cypress/.eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
   root: true,
-  extends: ["plugin:cypress/recommended", '../.eslintrc.js'],
-  plugins: ["cypress"],
+  extends: ["plugin:cypress/recommended", "../.eslintrc.js"],
+  plugins: ["cypress", "chai-friendly"],
   env: {
     "cypress/globals": true,
   },
   rules: {
-    "func-names": ["error", "never"]
+    "func-names": ["error", "never"],
+    "no-unused-expressions": 0,
+    "chai-friendly/no-unused-expressions": 2
   }
 };
