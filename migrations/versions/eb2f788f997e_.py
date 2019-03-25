41a70d1c70ff6fd7a23a94cf4112721ac8e4f28e
Marina Samuel <msamuel@mozilla.com>
2019-03-25 15:01:59 -0400

    Add empty migration to replace the removed schedule_until migration

---
+++ migrations/versions/eb2f788f997e_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: eb2f788f997e
+Revises: d1eae8b9893e
+Create Date: 2017-03-02 12:20:00.029066
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eb2f788f997e'
+down_revision = 'd1eae8b9893e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
