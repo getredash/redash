0334b92f365c5e518530bfc641afb5ad42b4abd1
Gabriel Dutra <nesk.frz@gmail.com>
2019-03-27 19:23:25 -0300

    Add eslint-disable for cy.wait

--- client/cypress/integration/data-source/create_data_source_spec.js
+++ client/cypress/integration/data-source/create_data_source_spec.js
@@ -6,7 +6,7 @@ describe('Create Data Source', () => {
 
   it('renders the page and takes a screenshot', () => {
     cy.getByTestId('CreateSourceDialog').should('contain', 'PostgreSQL');
-    cy.wait(1000);
+    cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
     cy.percySnapshot('Create Data Source - Types');
   });
 
