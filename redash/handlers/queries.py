c88d58ad1b33ad96afdff862bfdba6a71443fc0b
Omer Lachish <omer@rauchy.net>
2019-03-28 13:46:35 +0200

    fix bad rebase

--- redash/handlers/queries.py
+++ redash/handlers/queries.py
@@ -183,10 +183,6 @@ def require_access_to_dropdown_queries(user, query_def):
             abort(400, message="You are trying to associate a dropdown query that does not have a matching group."
                                "Please verify the dropdown query id you are trying to associate with this query.")
 
-        object = lambda: None
-        object.groups = dict(groups)
-        require_access(object, user, view_only)
-
         require_access(dict(groups), user, view_only)
 
 class QueryListResource(BaseQueryListResource):
