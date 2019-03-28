23e8f2ec684ccb8f4a0c1f5bdb209191d436d321
Levko Kravets <levko.ne@gmail.com>
2019-03-28 13:37:15 +0200

    Migrate Timer component to React; update TimeAgo component (use hooks + fix bug)

---
+++ client/app/components/Timer.jsx
@@ -0,0 +1,40 @@
+import moment from 'moment';
+import { useState, useMemo, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { react2angular } from 'react2angular';
+import { Moment } from '@/components/proptypes';
+
+function timeFrom(startTime) {
+  return moment.utc(moment.now() - startTime).format('HH:mm:ss');
+}
+
+export function Timer({ from }) {
+  const startTime = useMemo(() => moment(from).valueOf(), [from]);
+  const [currentTime, setCurrentTime] = useState(timeFrom(startTime));
+
+  useEffect(() => {
+    const timer = setInterval(() => setCurrentTime(timeFrom(startTime)), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  return currentTime;
+}
+
+Timer.propTypes = {
+  from: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.instanceOf(Date),
+    Moment,
+  ]),
+};
+
+Timer.defaultProps = {
+  from: null,
+};
+
+export default function init(ngModule) {
+  ngModule.component('rdTimer', react2angular(Timer));
+}
+
+init.init = true;
