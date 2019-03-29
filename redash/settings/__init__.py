a89d19ca6c5b0c8a6cc37b5c327dbee084f5a162
_AZ_ <a.zarutsky@gmail.com>
2019-03-29 15:19:46 +0100

    feature: add couchbase query runner

--- redash/settings/__init__.py
+++ redash/settings/__init__.py
@@ -168,6 +168,7 @@ default_query_runners = [
     'redash.query_runner.google_spreadsheets',
     'redash.query_runner.graphite',
     'redash.query_runner.mongodb',
+    'redash.query_runner.couchbase',
     'redash.query_runner.mysql',
     'redash.query_runner.pg',
     'redash.query_runner.url',
