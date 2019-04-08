6cf862023b57108589a19fc4c2bbe75047720e37
Omer Lachish <omer@rauchy.net>
2019-04-08 10:44:51 +0300

    put tests inside a describe block

--- client/app/services/query-string.test.js
+++ client/app/services/query-string.test.js
@@ -1,9 +1,12 @@
 import queryString from '@/services/query-string';
 
-it('removes prefixes', () => {
-  expect(queryString.fromString('p_a=b')).toHaveProperty('a');
+describe('fromString', () => {
+  it('removes prefixes', () => {
+    expect(queryString.fromString('p_a=b')).toHaveProperty('a');
+  });
+
+  it('ignores non-parameters', () => {
+    expect(queryString.fromString('p_a=b&utm=123')).not.toHaveProperty('utm');
+  });
 });
 
-it('ignores non-parameters', () => {
-  expect(queryString.fromString('p_a=b&utm=123')).not.toHaveProperty('utm');
-});
