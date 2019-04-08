2b46bd8612c7b1728662ed58c7259d559c7940c2
Omer Lachish <omer@rauchy.net>
2019-04-08 11:20:29 +0300

    add date ranges tests

--- client/app/services/query-string.test.js
+++ client/app/services/query-string.test.js
@@ -2,17 +2,38 @@ import queryString from '@/services/query-string';
 
 describe('fromString', () => {
   it('removes prefixes', () => {
-    expect(queryString.fromString('p_a=b')).toHaveProperty('a');
+    expect(queryString.fromString('p_a=b'))
+      .toHaveProperty('a');
   });
 
   it('ignores non-parameters', () => {
-    expect(queryString.fromString('p_a=b&utm=123')).not.toHaveProperty('utm');
+    expect(queryString.fromString('p_a=b&utm=123'))
+      .not.toHaveProperty('utm');
+  });
+
+  it('handles date ranges', () => {
+    expect(queryString.fromString('p_created_at.start=2019-01-01&p_created_at.end=2019-02-01')).toEqual({
+      created_at: {
+        start: '2019-01-01',
+        end: '2019-02-01',
+      },
+    });
   });
 });
 
 describe('toString', () => {
   it('adds prefixes', () => {
-    expect(queryString.toString({ a: 'b' })).toEqual('p_a=b');
+    expect(queryString.toString({
+      a: 'b',
+    })).toEqual('p_a=b');
+  });
+
+  it('handles date ranges', () => {
+    expect(queryString.toString({
+      created_at: {
+        start: '2019-01-01',
+        end: '2019-02-01',
+      },
+    })).toEqual('p_created_at.start=2019-01-01&p_created_at.end=2019-02-01');
   });
 });
-
