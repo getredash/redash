f1f1d8d80a3f4ce41f3092d115d86accb50ce920
Omer Lachish <omer@rauchy.net>
2019-04-08 10:59:21 +0300

    extract prefix and option constants

--- client/app/services/query-string.js
+++ client/app/services/query-string.js
@@ -1,8 +1,12 @@
 import qs from 'qs';
 
-const parse = queryString => qs.parse(queryString, { allowDots: true, ignoreQueryPrefix: true });
-const onlyParameters = ([k]) => k.startsWith('p_');
-const removePrefix = ([k, v]) => [k.slice(2), v];
+const prefix = 'p_';
+const options = { allowDots: true, ignoreQueryPrefix: true };
+
+const parse = queryString => qs.parse(queryString, options);
+const onlyParameters = ([k]) => k.startsWith(prefix);
+const removePrefix = ([k, v]) => [k.slice(prefix.length), v];
+const addPrefix = ([k, v]) => [`${prefix}${k}`, v];
 const toObject = (obj, [k, v]) => Object.assign(obj, { [k]: v });
 
 const fromString = queryString => Object.entries(parse(queryString))
