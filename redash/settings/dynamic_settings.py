52e16c0f365bfa7c86b34413d2430129a272ef14
Omer Lachish <omer@rauchy.net>
2019-04-15 11:39:02 +0300

    really important newlines according to CodeClimate

--- redash/settings/dynamic_settings.py
+++ redash/settings/dynamic_settings.py
@@ -1,6 +1,7 @@
 import os
 from .helpers import int_or_none
 
+
 # Replace this method with your own implementation in case you want to limit the time limit on certain queries or users.
 def query_time_limit(is_scheduled, user_id, org_id):
     scheduled_time_limit = int_or_none(os.environ.get('REDASH_SCHEDULED_QUERY_TIME_LIMIT', None))
