7a88d8af0fdc3262c2a8238402b7ac9b6763c1c9
Ran Byron <ranbena@gmail.com>
2019-04-22 09:51:56 +0300

    Code beautifying 🤩

--- client/cypress/integration/dashboard/dashboard_spec.js
+++ client/cypress/integration/dashboard/dashboard_spec.js
@@ -310,21 +310,21 @@ describe('Dashboard', () => {
         });
 
         it('stays put when dragged under snap threshold', () => {
-          dragBy(cy.get('@textboxEl'), 90).should(($el) => {
-            expect($el.offset().left).to.eq(15); // no change, 15 -> 15
-          });
+          dragBy(cy.get('@textboxEl'), 90)
+            .invoke('offset')
+            .should('have.property', 'left', 15); // no change, 15 -> 15
         });
 
         it('moves one column when dragged over snap threshold', () => {
-          dragBy(cy.get('@textboxEl'), 110).should(($el) => {
-            expect($el.offset().left).to.eq(215); //  moved by 200, 15 -> 215
-          });
+          dragBy(cy.get('@textboxEl'), 110)
+            .invoke('offset')
+            .should('have.property', 'left', 215); //  moved by 200, 15 -> 215
         });
 
         it('moves two columns when dragged over snap threshold', () => {
-          dragBy(cy.get('@textboxEl'), 330).should(($el) => {
-            expect($el.offset().left).to.eq(415); //  moved by 400, 15 -> 415
-          });
+          dragBy(cy.get('@textboxEl'), 330)
+            .invoke('offset')
+            .should('have.property', 'left', 415); //  moved by 400, 15 -> 415
         });
       });
 
@@ -333,9 +333,8 @@ describe('Dashboard', () => {
         cy.route('POST', 'api/widgets/*').as('WidgetSave');
 
         editDashboard();
-        dragBy(cy.get('@textboxEl'), 330).then(() => {
-          cy.wait('@WidgetSave');
-        });
+        dragBy(cy.get('@textboxEl'), 330);
+        cy.wait('@WidgetSave');
       });
     });
 
@@ -348,25 +347,22 @@ describe('Dashboard', () => {
         it('stays put when dragged under snap threshold', () => {
           resizeBy(cy.get('@textboxEl'), 90)
             .then(() => cy.get('@textboxEl'))
-            .should(($el) => {
-              expect($el.width()).to.eq(600); // no change, 600 -> 600
-            });
+            .invoke('width')
+            .should('eq', 600); // no change, 600 -> 600
         });
 
         it('moves one column when dragged over snap threshold', () => {
           resizeBy(cy.get('@textboxEl'), 110)
             .then(() => cy.get('@textboxEl'))
-            .should(($el) => {
-              expect($el.width()).to.eq(800); // resized by 200, 600 -> 800
-            });
+            .invoke('width')
+            .should('eq', 800); // resized by 200, 600 -> 800
         });
 
         it('moves two columns when dragged over snap threshold', () => {
           resizeBy(cy.get('@textboxEl'), 400)
             .then(() => cy.get('@textboxEl'))
-            .should(($el) => {
-              expect($el.width()).to.eq(1000); // resized by 400, 600 -> 1000
-            });
+            .invoke('width')
+            .should('eq', 1000); // resized by 400, 600 -> 1000
         });
       });
 
@@ -378,17 +374,15 @@ describe('Dashboard', () => {
         it('stays put when dragged under snap threshold', () => {
           resizeBy(cy.get('@textboxEl'), 0, 10)
             .then(() => cy.get('@textboxEl'))
-            .should(($el) => {
-              expect($el.height()).to.eq(135); // no change, 135 -> 135
-            });
+            .invoke('height')
+            .should('eq', 135); // no change, 135 -> 135
         });
 
         it('moves one row when dragged over snap threshold', () => {
           resizeBy(cy.get('@textboxEl'), 0, 30)
             .then(() => cy.get('@textboxEl'))
-            .should(($el) => {
-              expect($el.height()).to.eq(185); // resized by 50, , 135 -> 185
-            });
+            .invoke('height')
+            .should('eq', 185); // resized by 50, , 135 -> 185
         });
 
         it('shrinks to minimum', () => {
@@ -407,9 +401,8 @@ describe('Dashboard', () => {
         cy.route('POST', 'api/widgets/*').as('WidgetSave');
 
         editDashboard();
-        resizeBy(cy.get('@textboxEl'), 200).then(() => {
-          cy.wait('@WidgetSave');
-        });
+        resizeBy(cy.get('@textboxEl'), 200);
+        cy.wait('@WidgetSave');
       });
     });
   });
