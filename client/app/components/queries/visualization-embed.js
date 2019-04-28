43c65500b7c2a1bbb834cef7a2e1ce101c98115a
Omer Lachish <omer@rauchy.net>
2019-04-28 14:50:55 +0300

    Redirect to default parameter values when parameters are missing in embedded visualizations

--- client/app/components/queries/visualization-embed.js
+++ client/app/components/queries/visualization-embed.js
@@ -38,7 +38,8 @@ export default function init(ngModule) {
     return session($http, $route, Auth).then(() => {
       const queryId = $route.current.params.queryId;
       const query = $http.get(`api/queries/${queryId}`).then(response => response.data);
-      const queryResult = $http.post(`api/queries/${queryId}/results`, { parameters: queryStringParameters() }).then(response => response.data);
+      // eslint-disable-next-line no-unused-vars
+      const queryResult = $http.post(`api/queries/${queryId}/results`, { parameters: queryStringParameters() }).then(response => response.data, _ => ({}));
       return $q.all([query, queryResult]);
     });
   }
