bc926eb2ba75091692de379a1c01526e0b4d01d7
Omer Lachish <omer@rauchy.net>
2019-04-29 10:07:05 +0300

    load all data after page is loaded

--- client/app/components/queries/visualization-embed.js
+++ client/app/components/queries/visualization-embed.js
@@ -8,47 +8,40 @@ const VisualizationEmbed = {
   bindings: {
     data: '<',
   },
-  controller($routeParams, Query, QueryResult) {
+  controller($http, $q, $routeParams, Auth, Query, QueryResult) {
     'ngInject';
 
     document.querySelector('body').classList.add('headless');
     const visualizationId = parseInt($routeParams.visualizationId, 10);
     this.showQueryDescription = $routeParams.showDescription;
-    this.apiKey = $routeParams.api_key;
     this.logoUrl = logoUrl;
-    this.query = new Query(this.data[0]);
-    this.queryResult = new QueryResult(this.data[1]);
-    this.visualization =
-      find(this.query.visualizations, visualization => visualization.id === visualizationId);
+
+    this.apiKey = $routeParams.api_key;
+    Auth.setApiKey(this.apiKey);
+    Auth.loadConfig().then(() => {
+      const queryId = $routeParams.queryId;
+
+      const query = $http.get(`api/queries/${queryId}`).then(response => response.data);
+      const queryResult = $http.post(`api/queries/${queryId}/results`, {
+        parameters: queryStringParameters(),
+      }).then(response => response.data, /* eslint-disable-line no-unused-vars */ _ => ({}));
+
+      $q.all([query, queryResult]).then((data) => {
+        this.query = new Query(data[0]);
+        this.queryResult = new QueryResult(data[1]);
+        this.visualization =
+          find(this.query.visualizations, visualization => visualization.id === visualizationId);
+      });
+    });
   },
 };
 
 export default function init(ngModule) {
   ngModule.component('visualizationEmbed', VisualizationEmbed);
 
-  function session($http, $route, Auth) {
-    'ngInject';
-
-    const apiKey = $route.current.params.api_key;
-    Auth.setApiKey(apiKey);
-    return Auth.loadConfig();
-  }
-
-  function loadData($http, $route, $q, Auth) {
-    return session($http, $route, Auth).then(() => {
-      const queryId = $route.current.params.queryId;
-      const query = $http.get(`api/queries/${queryId}`).then(response => response.data);
-      const queryResult = $http.post(`api/queries/${queryId}/results`, { parameters: queryStringParameters() }).then(response => response.data);
-      return $q.all([query, queryResult]);
-    });
-  }
-
   ngModule.config(($routeProvider) => {
     $routeProvider.when('/embed/query/:queryId/visualization/:visualizationId', {
       template: '<visualization-embed data="$resolve.data"></visualization-embed>',
-      resolve: {
-        data: loadData,
-      },
     });
   });
 }
