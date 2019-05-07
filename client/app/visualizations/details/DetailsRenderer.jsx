b4fe0ea0f7c456144919add0a601d79beaa91236
Arik Fraimovich <arik@arikfr.com>
2019-05-07 21:57:51 +0300

    Details visualization

---
+++ client/app/visualizations/details/DetailsRenderer.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import map from 'lodash/map';
+import Pagination from 'antd/lib/pagination';
+
+import './details.less';
+
+export default function DetailsRenderer({ data }) {
+  const [page, setPage] = useState(0);
+
+  if (!data || !data.rows) {
+    return null;
+  }
+
+  // We use columsn to maintain order of columns in the view.
+  const columns = data.columns.map(column => column.name);
+  const row = data.rows[page];
+
+  return (
+    <div>
+      <table className="table table-bordered details-viz">
+        <tbody>
+          {map(columns, key => (
+            <tr key={key}>
+              <th>{key}</th>
+              <td>{'' + row[key]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {data.rows.length > 1 && (
+        <div className="paginator-container">
+          <Pagination current={page + 1} defaultPageSize={1} total={data.rows.length} onChange={p => setPage(p - 1)} />
+        </div>
+      )}
+    </div>
+  );
+}
