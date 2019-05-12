059a9c02c4edf3f406bc05ab51e33f0f8af1d24b
Arik Fraimovich <arik@arikfr.com>
2019-05-12 21:06:09 +0300

    Presto: ignore blank passwords
    PyHive expects only None as no password.

--- redash/query_runner/presto.py
+++ redash/query_runner/presto.py
@@ -103,7 +103,7 @@ class Presto(BaseQueryRunner):
             port=self.configuration.get('port', 8080),
             protocol=self.configuration.get('protocol', 'http'),
             username=self.configuration.get('username', 'redash'),
-            password=self.configuration.get('password'),
+            password=(self.configuration.get('password') or None),
             catalog=self.configuration.get('catalog', 'hive'),
             schema=self.configuration.get('schema', 'default'))
 
