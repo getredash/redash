286dcf32aa4a616aee4373e2e85c134bc0ffd4ba
Omer Lachish <omer@rauchy.net>
2019-05-12 12:48:01 +0300

    Fix Ability to Add Query-based Parameters to Existing Queries (#3716)
    * propagate `isDirty` down to `QueryBasedParameterInput`
    
    * go to /api/:id/dropdown while editing a query, since dropdown queries might still not be associated with the parent. see #3711
    
    * show helpful error message if dropdown values cannot be fetched
    
    * use backticks instead of line concatenation
    
    * remove requirement to have direct access to dropdown query in order validate it. parent query association checks are sufficient
    
    * remove isDirty-based implementation and allow dropdown queries through nested ACL even if they aren't associated yet (given that the user has _direct_ access to the dropdown query)
    
    * fix tests to cover all cases for /api/queries/:id/dropdowns/:id
    
    * fix indentation
    
    * require access to the query, not the data source
    
    * use require_access instead of has_access

--- redash/handlers/query_results.py
+++ redash/handlers/query_results.py
@@ -108,18 +108,22 @@ ONE_YEAR = 60 * 60 * 24 * 365.25
 
 class QueryResultDropdownResource(BaseResource):
     def get(self, query_id):
+        query = get_object_or_404(models.Query.get_by_id_and_org, query_id, self.current_org)
+        require_access(query.data_source, current_user, view_only)
         return dropdown_values(query_id)
 
 
 class QueryDropdownsResource(BaseResource):
     def get(self, query_id, dropdown_query_id):
         query = get_object_or_404(models.Query.get_by_id_and_org, query_id, self.current_org)
+        require_access(query, current_user, view_only)
 
         related_queries_ids = [p['queryId'] for p in query.parameters if p['type'] == 'query']
         if int(dropdown_query_id) not in related_queries_ids:
-            abort(403)
+            dropdown_query = get_object_or_404(models.Query.get_by_id_and_org, dropdown_query_id, self.current_org)
+            require_access(dropdown_query.data_source, current_user, view_only)
 
-        return dropdown_values(dropdown_query_id, should_require_access=False)
+        return dropdown_values(dropdown_query_id)
 
 
 class QueryResultResource(BaseResource):
