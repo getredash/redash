d6a29769f91cd9caa2b6e179085f386e5e6c0440
Gabriel Dutra <nesk.frz@gmail.com>
2019-05-11 22:44:51 -0300

    Add inputs to change iframe size

--- client/app/components/queries/EmbedQueryDialog.jsx
+++ client/app/components/queries/EmbedQueryDialog.jsx
@@ -1,59 +1,87 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'antd/lib/button';
 import Alert from 'antd/lib/alert';
+import Button from 'antd/lib/button';
+import Form from 'antd/lib/form';
+import InputNumber from 'antd/lib/input-number';
 import Modal from 'antd/lib/modal';
 import { wrap as wrapDialog, DialogPropType } from '@/components/DialogWrapper';
 import { clientConfig } from '@/services/auth';
 import CodeBlock from '@/components/CodeBlock';
 
-function EmbedQueryDialog({ dialog, query, visualization }) {
-  const embedUrl = `${clientConfig.basePath}embed/query/${query.id}/visualization/${
-    visualization.id}?api_key=${query.api_key}&${query.getParameters().toUrlParams()}`;
+class EmbedQueryDialog extends React.Component {
+  static propTypes = {
+    dialog: DialogPropType.isRequired,
+    query: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+    visualization: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  };
 
-  let snapshotUrl;
-  if (window.snapshotUrlBuilder) {
-    snapshotUrl = window.snapshotUrlBuilder(query, visualization);
+  state = {
+    iframeWidth: 720,
+    iframeHeight: 391,
+  };
+
+  constructor(props) {
+    super(props);
+    const { query, visualization } = props;
+    this.embedUrl = `${clientConfig.basePath}embed/query/${query.id}/visualization/${
+      visualization.id}?api_key=${query.api_key}&${query.getParameters().toUrlParams()}`;
+
+    if (window.snapshotUrlBuilder) {
+      this.snapshotUrl = window.snapshotUrlBuilder(query, visualization);
+    }
   }
 
-  return (
-    <Modal {...dialog.props} title="Embed Code" footer={(<Button onClick={dialog.dismiss}>Close</Button>)}>
-      {query.is_safe ? (
-        <React.Fragment>
-          <h5 className="m-t-0">Public URL</h5>
-          <div className="m-b-10">
-            <CodeBlock copyable>
-              {embedUrl}
-            </CodeBlock>
-          </div>
-          <h5 className="m-t-0">IFrame Embed</h5>
-          <div className="m-b-10">
-            <CodeBlock copyable>
-              {`<iframe src="${embedUrl}" width="720" height="391"></iframe>`}
-            </CodeBlock>
-          </div>
-          (height should be adjusted)
-          {snapshotUrl && (
-            <React.Fragment>
-              <h5>Image Embed</h5>
-              <CodeBlock copyable>{snapshotUrl}</CodeBlock>
-            </React.Fragment>
-          )}
-        </React.Fragment>
-      ) : (
-        <Alert
-          message="Currently it is not possible to embed queries that contain text parameters."
-          type="error"
-        />
-      )}
-    </Modal>
-  );
-}
+  render() {
+    const { query, dialog } = this.props;
+    const { iframeWidth, iframeHeight } = this.state;
 
-EmbedQueryDialog.propTypes = {
-  dialog: DialogPropType.isRequired,
-  query: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-  visualization: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-};
+    return (
+      <Modal {...dialog.props} title="Embed Code" footer={(<Button onClick={dialog.dismiss}>Close</Button>)}>
+        {query.is_safe ? (
+          <React.Fragment>
+            <h5 className="m-t-0">Public URL</h5>
+            <div className="m-b-10">
+              <CodeBlock copyable>
+                {this.embedUrl}
+              </CodeBlock>
+            </div>
+            <h5 className="m-t-0">IFrame Embed</h5>
+            <div className="m-b-10">
+              <CodeBlock copyable>
+                {`<iframe src="${this.embedUrl}" width="${iframeWidth}" height="${iframeHeight}"></iframe>`}
+              </CodeBlock>
+              <Form className="m-t-10 m-l-5" layout="inline">
+                <Form.Item className="f-300" label="Width">
+                  <InputNumber
+                    value={iframeWidth}
+                    onChange={value => this.setState({ iframeWidth: value })}
+                  />
+                </Form.Item>
+                <Form.Item label="Height">
+                  <InputNumber
+                    value={iframeHeight}
+                    onChange={value => this.setState({ iframeHeight: value })}
+                  />
+                </Form.Item>
+              </Form>
+            </div>
+            {this.snapshotUrl && (
+              <React.Fragment>
+                <h5>Image Embed</h5>
+                <CodeBlock copyable>{this.snapshotUrl}</CodeBlock>
+              </React.Fragment>
+            )}
+          </React.Fragment>
+        ) : (
+          <Alert
+            message="Currently it is not possible to embed queries that contain text parameters."
+            type="error"
+          />
+        )}
+      </Modal>
+    );
+  }
+}
 
 export default wrapDialog(EmbedQueryDialog);
