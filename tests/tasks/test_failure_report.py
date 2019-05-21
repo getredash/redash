d504c2b7e513f03c4aaf79bf20ccad6f31882e75
Omer Lachish <omer@rauchy.net>
2019-05-21 11:52:05 +0300

    test for indicating when approaching report limits + refactor

--- tests/tasks/test_failure_report.py
+++ tests/tasks/test_failure_report.py
@@ -5,20 +5,32 @@ import mock
 from tests import BaseTestCase
 from redash import redis_connection, models, settings
 from redash.tasks.failure_report import notify_of_failure
+from redash.utils import json_loads
 
 class TestSendAggregatedErrorsTask(BaseTestCase):
-    def test_schedules_email_if_failure_count_is_beneath_limit(self):
-        query = self.factory.create_query(schedule_failures=settings.MAX_FAILURE_REPORTS_PER_QUERY - 1)
-
+    def notify(self, **kwargs):
+        query = self.factory.create_query(**kwargs)
         notify_of_failure("Oh no, I failed!", query)
+        return "aggregated_failures:{}".format(query.user.email)
 
-        email_pending = redis_connection.get("aggregated_failures:{}:pending".format(query.user.email))
+    def test_schedules_email_if_failure_count_is_beneath_limit(self):
+        key = self.notify(schedule_failures=settings.MAX_FAILURE_REPORTS_PER_QUERY - 1)
+        email_pending = redis_connection.get("{}:pending".format(key))
         self.assertTrue(email_pending)
 
     def test_does_not_report_if_failure_count_is_beyond_limit(self):
-        query = self.factory.create_query(schedule_failures=settings.MAX_FAILURE_REPORTS_PER_QUERY)
-
-        notify_of_failure("Oh no, I failed!", query)
-
-        email_pending = redis_connection.get("aggregated_failures:{}:pending".format(query.user.email))
+        key = self.notify(schedule_failures=settings.MAX_FAILURE_REPORTS_PER_QUERY)
+        email_pending = redis_connection.get("{}:pending".format(key))
         self.assertFalse(email_pending)
+
+    def test_does_not_indicate_when_not_near_limit_for_a_query(self):
+        key = self.notify(schedule_failures=settings.MAX_FAILURE_REPORTS_PER_QUERY / 2)
+        failure = json_loads(redis_connection.lrange(key, 0, -1)[0])
+        comment = failure.get('comment')
+        self.assertFalse(comment)
+
+    def test_indicates_when_near_limit_for_a_query(self):
+        key = self.notify(schedule_failures=settings.MAX_FAILURE_REPORTS_PER_QUERY - 1)
+        failure = json_loads(redis_connection.lrange(key, 0, -1)[0])
+        comment = failure.get('comment')
+        self.assertTrue(comment)
