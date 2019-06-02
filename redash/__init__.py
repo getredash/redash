53ad27f9ccecb83efb76b19df932e96367fa227b
Arik Fraimovich <arik@arikfr.com>
2019-06-02 11:46:48 +0300

    Revert "Update version to 8.0.0-beta."
    This reverts commit c8fa74967fcce49aabc86c13b39fb93653ed9b32.

--- redash/__init__.py
+++ redash/__init__.py
@@ -14,7 +14,7 @@ from statsd import StatsClient
 from . import settings
 from .app import create_app  # noqa
 
-__version__ = '8.0.0-beta'
+__version__ = '7.0.0'
 
 
 if os.environ.get("REMOTE_DEBUG"):
