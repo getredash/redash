0c93212214196da8156de24e8c1d02c199b53167
Peng-Yuan Lai <peng-yuan.lai@appfolio.com>
2019-06-03 16:22:45 -0700

    check float if scale > 0 in snowflake query result

--- redash/query_runner/snowflake.py
+++ redash/query_runner/snowflake.py
@@ -60,6 +60,13 @@ class Snowflake(BaseQueryRunner):
     def enabled(cls):
         return enabled
 
+    @classmethod
+    def determine_type(cls, data_type, scale):
+        t = TYPES_MAP.get(data_type, None)
+        if t == TYPE_INTEGER and scale > 0:
+            return TYPE_FLOAT
+        return t
+
     def run_query(self, query, user):
         region = self.configuration.get('region')
 
@@ -84,7 +91,7 @@ class Snowflake(BaseQueryRunner):
             cursor.execute(query)
 
             columns = self.fetch_columns(
-                [(i[0], TYPES_MAP.get(i[1], None)) for i in cursor.description])
+                    [(i[0], self.determine_type(i[1], i[5])) for i in cursor.description])
             rows = [dict(zip((c['name'] for c in columns), row))
                     for row in cursor]
 
