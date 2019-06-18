059570e8b2ab056c5701680f19e760ce5a181d07
Ran Byron <ranbena@gmail.com>
2019-06-18 19:01:45 +0300

    Added new tests

--- client/cypress/integration/query/parameter_spec.js
+++ client/cypress/integration/query/parameter_spec.js
@@ -1,6 +1,22 @@
 import { createQuery } from '../../support/redash-api';
 
 describe('Parameter', () => {
+  const expectDirtyStateChange = (edit) => {
+    cy.getByTestId('ParameterName-test-parameter')
+      .find('.parameter-input')
+      .should(($el) => {
+        assert.isUndefined($el.data('dirty'));
+      });
+
+    edit();
+
+    cy.getByTestId('ParameterName-test-parameter')
+      .find('.parameter-input')
+      .should(($el) => {
+        assert.isTrue($el.data('dirty'));
+      });
+  };
+
   beforeEach(() => {
     cy.login();
   });
@@ -33,6 +49,14 @@ describe('Parameter', () => {
 
       cy.getByTestId('DynamicTable')
         .should('contain', 'New value');
+    });
+
+    it('sets dirty state when edited', () => {
+      expectDirtyStateChange(() => {
+        cy.getByTestId('ParameterName-test-parameter')
+          .find('input')
+          .type('Redash');
+      });
     });
   });
 
@@ -70,6 +94,14 @@ describe('Parameter', () => {
       cy.getByTestId('DynamicTable')
         .should('contain', 31415);
     });
+
+    it('sets dirty state when edited', () => {
+      expectDirtyStateChange(() => {
+        cy.getByTestId('ParameterName-test-parameter')
+          .find('input')
+          .type('{selectall}42');
+      });
+    });
   });
 
   describe('Dropdown Parameter', () => {
@@ -104,6 +136,17 @@ describe('Parameter', () => {
 
       cy.getByTestId('DynamicTable')
         .should('contain', 'value1');
+    });
+
+    it('sets dirty state when edited', () => {
+      expectDirtyStateChange(() => {
+        cy.getByTestId('ParameterName-test-parameter')
+          .find('.ant-select')
+          .click();
+
+        cy.contains('li.ant-select-dropdown-menu-item', 'value1')
+          .click();
+      });
     });
   });
 
@@ -147,6 +190,18 @@ describe('Parameter', () => {
 
       cy.getByTestId('DynamicTable')
         .should('contain', '15/01/19');
+    });
+
+    it('sets dirty state when edited', () => {
+      expectDirtyStateChange(() => {
+        cy.getByTestId('ParameterName-test-parameter')
+          .find('input')
+          .click();
+
+        cy.get('.ant-calendar-date-panel')
+          .contains('.ant-calendar-date', '15')
+          .click();
+      });
     });
   });
 
@@ -209,6 +264,91 @@ describe('Parameter', () => {
 
       cy.getByTestId('DynamicTable')
         .should('contain', '2019-01-01 00:00');
+    });
+
+    it('sets dirty state when edited', () => {
+      expectDirtyStateChange(() => {
+        cy.getByTestId('ParameterName-test-parameter')
+          .find('input')
+          .click();
+
+        cy.get('.ant-calendar-date-panel')
+          .contains('Now')
+          .click();
+      });
+    });
+  });
+
+  describe('Apply Button', () => {
+    beforeEach(() => {
+      const queryData = {
+        name: 'Testing Apply Button',
+        query: "SELECT '{{test-parameter-1}} {{ test-parameter-2 }}'",
+      };
+
+      createQuery(queryData, false)
+        .then(({ id }) => cy.visit(`/queries/${id}/source`));
+
+      cy.getByTestId('ParameterApplyButton').as('ApplyButton');
+    });
+
+    it('shows and hides according to parameter dirty state', () => {
+      cy.get('@ApplyButton')
+        .should('not.be', 'visible');
+
+      cy.getByTestId('ParameterName-test-parameter-1')
+        .find('input')
+        .as('Param')
+        .type('Redash');
+
+      cy.get('@ApplyButton')
+        .should('be', 'visible');
+
+      cy.get('@Param')
+        .clear();
+
+      cy.get('@ApplyButton')
+        .should('not.be', 'visible');
+    });
+
+    it('updates dirty counter', () => {
+      cy.getByTestId('ParameterName-test-parameter-1')
+        .find('input')
+        .type('Redash');
+
+      cy.get('@ApplyButton')
+        .find('.ant-badge-count p.current')
+        .should('contain', '1');
+
+      cy.getByTestId('ParameterName-test-parameter-2')
+        .find('input')
+        .type('Redash');
+
+      cy.get('@ApplyButton')
+        .find('.ant-badge-count p.current')
+        .should('contain', '2');
+    });
+
+    it('applies parameter changes', () => {
+      cy.getByTestId('ParameterName-test-parameter-1')
+        .find('input')
+        .type('Redash');
+
+      cy.getByTestId('ParameterName-test-parameter-2')
+        .find('input')
+        .type('Redash');
+
+      cy.location('search').should('not.contain', 'Redash');
+
+      // listen to results
+      cy.server();
+      cy.route('POST', 'api/queries/*/results').as('Results');
+
+      cy.get('@ApplyButton')
+        .click();
+
+      cy.location('search').should('contain', 'Redash');
+      cy.wait('@Results');
     });
   });
 });
