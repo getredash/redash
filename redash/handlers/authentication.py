a8ff2500be089f0e7c7718846977d42439da541f
k-tomoyasu <blhicr1303@gmail.com>
2019-07-11 19:23:06 +0900

    Build custom alert message (#3137)
    * build custom alert message
    
    * fit button color tone
    
    * pass existing test
    
    * fix typos
    
    * follow code style
    
    * add webhook alert description and avoid key error
    
    * refactor: create alert template module
    
    * follow code style
    
    * use es6 class, fix template display
    
    * use alerts.options, use mustache
    
    * fix email description
    
    * alert custom subject
    
    * add alert state to template context, sanitized preview
    
    * remove console.log :bow:
    
    * chatwork custom_subject
    
    * add alert custom message. pagerduty, mattermost, hangoutschat
    
    * Pass custom subject in webhook destination
    
    * Add log message when checking alert.
    
    * Add feature flag for extra alert options.

--- redash/handlers/authentication.py
+++ redash/handlers/authentication.py
@@ -227,6 +227,7 @@ def client_config():
         'showPermissionsControl': current_org.get_setting("feature_show_permissions_control"),
         'allowCustomJSVisualizations': settings.FEATURE_ALLOW_CUSTOM_JS_VISUALIZATIONS,
         'autoPublishNamedQueries': settings.FEATURE_AUTO_PUBLISH_NAMED_QUERIES,
+        'extendedAlertOptions': settings.FEATURE_EXTENDED_ALERT_OPTIONS,
         'mailSettingsMissing': not settings.email_server_is_configured(),
         'dashboardRefreshIntervals': settings.DASHBOARD_REFRESH_INTERVALS,
         'queryRefreshIntervals': settings.QUERY_REFRESH_INTERVALS,
