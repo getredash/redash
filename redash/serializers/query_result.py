df2691a4ed06305b3ec56b92abb24f3df97162b7
Omer Lachish <omer@rauchy.net>
2019-07-16 11:18:46 +0300

    lint

--- redash/serializers/query_result.py
+++ redash/serializers/query_result.py
@@ -60,7 +60,7 @@ def _get_column_lists(columns):
 def serialize_query_result(query_result, is_api_user):
     if is_api_user:
         publicly_needed_keys = ['data', 'retrieved_at']
-        return { key: query_result.to_dict()[key] for key in publicly_needed_keys }
+        return {key: query_result.to_dict()[key] for key in publicly_needed_keys}
     else:
         return query_result.to_dict()
 
