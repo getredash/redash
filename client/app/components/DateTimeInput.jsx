633a4afd9ed5d3f5d6d30477e8b20ddb75c36b9b
Gabriel Dutra <nesk.frz@gmail.com>
2019-07-26 16:40:13 -0300

    Add Dynamic Values to Date and Date Range Parameters (#3904)
    * Draft for Date Dynamic values
    
    * Use value with prefix instead of specific attr
    
    * Fix not possible to select static value
    
    * Update antd version
    
    * Cleanup and DateRangeParameter
    
    * Dynamic DateTimeRange
    
    * Add Dynamic options to Date Parameters
    
    * UI refinements
    
    * Add getDynamicValue function
    
    * Add 'This' options and prevent text clipping
    
    * Make allowClear available
    
    * Update ScheduleDialog snapshot
    
    * Add some protections and separate Date/DateRange
    
    * Accept null values on date or daterange parameters
    
    * Handle undefined values on Moment propType
    
    * Move export to end of files
    
    * Remove Today/Now option
    
    * Update with Apply Changes
    
    * Show name instead of value for dynamic values
    
    * Add comment about supporting useCurrentDateTime
    
    * Cypress Tests: Date Parameters
    
    * Cypress Tests: Date Range Parameters
    
    * Don't put null params in the url
    
    * Add workaround comments to Cypress tests
    
    Co-Authored-By: Ran Byron <ranbena@gmail.com>
    
    * Fix Dynamic Value as default for global parameters
    
    * Update Back to Static Value
    
    * Add isValid to value on Date and DateRange inputs
    
    * CR suggestions
    
    * Fix Back to Static Value for Dates
    
    * Update Dynamic Value Styling
    
    * Fix failing Date tests
    
    * Fix selectedDynamicValue
    
    * Parameter spec: Remove date range clickThrough
    
    * Add transition
    
    * Fix failing Cypress tests
    
    * Back with 'width: auto'
    
    * Check value is valid on Back to Static value
    
    * CR
    
    * Update Date Range width

--- client/app/components/DateTimeInput.jsx
+++ client/app/components/DateTimeInput.jsx
@@ -6,16 +6,21 @@ import { clientConfig } from '@/services/auth';
 import { Moment } from '@/components/proptypes';
 
 export function DateTimeInput({
+  defaultValue,
   value,
   withSeconds,
   onSelect,
   className,
+  ...props
 }) {
   const format = (clientConfig.dateFormat || 'YYYY-MM-DD') +
     (withSeconds ? ' HH:mm:ss' : ' HH:mm');
   const additionalAttributes = {};
-  if (value && value.isValid()) {
-    additionalAttributes.defaultValue = value;
+  if (defaultValue && defaultValue.isValid()) {
+    additionalAttributes.defaultValue = defaultValue;
+  }
+  if (value === null || (value && value.isValid())) {
+    additionalAttributes.value = value;
   }
   return (
     <DatePicker
@@ -25,11 +30,13 @@ export function DateTimeInput({
       format={format}
       placeholder="Select Date and Time"
       onChange={onSelect}
+      {...props}
     />
   );
 }
 
 DateTimeInput.propTypes = {
+  defaultValue: Moment,
   value: Moment,
   withSeconds: PropTypes.bool,
   onSelect: PropTypes.func,
@@ -37,7 +44,8 @@ DateTimeInput.propTypes = {
 };
 
 DateTimeInput.defaultProps = {
-  value: null,
+  defaultValue: null,
+  value: undefined,
   withSeconds: false,
   onSelect: () => {},
   className: '',
--- client/app/components/DateTimeRangeInput.jsx
+++ client/app/components/DateTimeRangeInput.jsx
@@ -9,16 +9,21 @@ import { Moment } from '@/components/proptypes';
 const { RangePicker } = DatePicker;
 
 export function DateTimeRangeInput({
+  defaultValue,
   value,
   withSeconds,
   onSelect,
   className,
+  ...props
 }) {
   const format = (clientConfig.dateFormat || 'YYYY-MM-DD') +
     (withSeconds ? ' HH:mm:ss' : ' HH:mm');
   const additionalAttributes = {};
-  if (isArray(value) && value[0].isValid() && value[1].isValid()) {
-    additionalAttributes.defaultValue = value;
+  if (isArray(defaultValue) && defaultValue[0].isValid() && defaultValue[1].isValid()) {
+    additionalAttributes.defaultValue = defaultValue;
+  }
+  if (value === null || (isArray(value) && value[0].isValid() && value[1].isValid())) {
+    additionalAttributes.value = value;
   }
   return (
     <RangePicker
@@ -27,11 +32,13 @@ export function DateTimeRangeInput({
       {...additionalAttributes}
       format={format}
       onChange={onSelect}
+      {...props}
     />
   );
 }
 
 DateTimeRangeInput.propTypes = {
+  defaultValue: PropTypes.arrayOf(Moment),
   value: PropTypes.arrayOf(Moment),
   withSeconds: PropTypes.bool,
   onSelect: PropTypes.func,
@@ -39,7 +46,8 @@ DateTimeRangeInput.propTypes = {
 };
 
 DateTimeRangeInput.defaultProps = {
-  value: null,
+  defaultValue: null,
+  value: undefined,
   withSeconds: false,
   onSelect: () => {},
   className: '',
