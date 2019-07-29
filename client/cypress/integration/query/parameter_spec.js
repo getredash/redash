95f1a42cf0a960b19909abc085201df0f3fd153b
Gabriel Dutra <nesk.frz@gmail.com>
2019-07-29 18:28:37 -0300

    Add Parameter Settings title change test

--- client/cypress/integration/query/parameter_spec.js
+++ client/cypress/integration/query/parameter_spec.js
@@ -535,4 +535,36 @@ describe('Parameter', () => {
         .each(($label, index) => expect($label).to.have.text(expectedOrder[index]));
     });
   });
+
+  describe('Parameter Settings', () => {
+    beforeEach(() => {
+      const queryData = {
+        name: 'Draggable',
+        query: "SELECT '{{parameter}}' AS parameter",
+        options: {
+          parameters: [
+            { name: 'parameter', title: 'Parameter', type: 'text' },
+          ],
+        },
+      };
+
+      createQuery(queryData, false)
+        .then(({ id }) => cy.visit(`/queries/${id}/source`));
+
+      cy.getByTestId('ParameterSettings-parameter').click();
+    });
+
+    it('changes the parameter title', () => {
+      cy.getByTestId('ParameterTitleInput')
+        .type('{selectall}New Parameter Name');
+      cy.getByTestId('SaveParameterSettings')
+        .click();
+
+      cy.contains('Query saved');
+      cy.reload();
+
+      cy.getByTestId('ParameterName-parameter')
+        .contains('label', 'New Parameter Name');
+    });
+  });
 });
