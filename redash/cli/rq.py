974a5c5ea45263c201a7799896ae27cf6d503947
Omer Lachish <omer@rauchy.net>
2019-08-22 00:12:13 +0300

    add rq_scheduler and an rq_scheduler service

--- redash/cli/rq.py
+++ redash/cli/rq.py
@@ -5,8 +5,19 @@ from redash import redis_connection, settings
 from click import argument
 from flask.cli import AppGroup
 from rq import Connection, Worker
+from rq_scheduler import Scheduler
+
 
 manager = AppGroup(help="RQ management commands.")
+
+
+@manager.command()
+def scheduler():
+    scheduler = Scheduler(connection=redis_connection,
+                          queue_name="periodic_jobs",
+                          interval=5)
+
+    scheduler.run()
 
 
 @manager.command()
