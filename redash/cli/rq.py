83ecc6730ba265dd73293560b4d666d3342443b2
Omer Lachish <omer@rauchy.net>
2019-09-08 23:52:14 +0300

    add a  command which errors if any of the workers running inside the current machine haven't been active in the last minute

--- redash/cli/rq.py
+++ redash/cli/rq.py
@@ -1,4 +1,7 @@
 from __future__ import absolute_import
+import socket
+import sys
+import datetime
 
 from redash import redis_connection
 from redash.schedule import rq_scheduler, schedule_periodic_jobs
@@ -22,3 +25,17 @@ def worker(queues='default'):
     with Connection(redis_connection):
         w = Worker(queues)
         w.work()
+
+
+@manager.command()
+def healthcheck():
+    hostname = socket.gethostname()
+    with Connection(redis_connection):
+        all_workers = Worker.all()
+
+        local_workers = [w for w in all_workers if w.hostname == hostname]
+        heartbeats = [w.last_heartbeat for w in local_workers]
+        time_since_seen = [datetime.datetime.utcnow() - hb for hb in heartbeats]
+        active = [t.seconds < 60 for t in time_since_seen]
+
+        sys.exit(not int(all(active)))
