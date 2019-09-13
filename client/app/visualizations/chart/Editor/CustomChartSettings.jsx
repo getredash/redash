eb9ac849291e24354c30ff24995410ea8dbe6d10
Levko Kravets <levko.ne@gmail.com>
2019-09-13 17:37:48 +0300

    Custom Chart settings

---
+++ client/app/visualizations/chart/Editor/CustomChartSettings.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Switch from 'antd/lib/switch';
+import Input from 'antd/lib/input';
+import { EditorPropTypes } from '@/visualizations';
+
+const { TextArea } = Input;
+
+export default function CustomChartSettings({ options, onOptionsChange }) {
+  return (
+    <React.Fragment>
+      <div className="m-b-15">
+        <label htmlFor="chart-editor-custom-code">Custom code</label>
+        <TextArea
+          id="chart-editor-custom-code"
+          className="form-control v-resizable"
+          rows="10"
+          defaultValue={options.customCode}
+          onChange={event => onOptionsChange({ customCode: event.target.value })}
+        />
+      </div>
+
+      <div className="m-b-15">
+        <label className="d-flex align-items-center" htmlFor="chart-editor-enable-console-logs">
+          <Switch
+            id="chart-editor-enable-console-logs"
+            data-test="Chart.Custom.EnableConsoleLogs"
+            defaultChecked={options.enableConsoleLogs}
+            onChange={enableConsoleLogs => onOptionsChange({ enableConsoleLogs })}
+          />
+          <span className="m-l-10">Show errors in the console</span>
+        </label>
+      </div>
+
+      <div className="m-b-15">
+        <label className="d-flex align-items-center" htmlFor="chart-editor-auto-update-custom-chart">
+          <Switch
+            id="chart-editor-auto-update-custom-chart"
+            data-test="Chart.Custom.AutoUpdate"
+            defaultChecked={options.autoRedraw}
+            onChange={autoRedraw => onOptionsChange({ autoRedraw })}
+          />
+          <span className="m-l-10">Auto update graph</span>
+        </label>
+      </div>
+    </React.Fragment>
+  );
+}
+
+CustomChartSettings.propTypes = EditorPropTypes;
--- client/app/visualizations/chart/Editor/index.jsx
+++ client/app/visualizations/chart/Editor/index.jsx
@@ -9,6 +9,7 @@ import YAxisSettings from './YAxisSettings';
 import SeriesSettings from './SeriesSettings';
 import ColorsSettings from './ColorsSettings';
 import DataLabelsSettings from './DataLabelsSettings';
+import CustomChartSettings from './CustomChartSettings';
 
 export default function Editor(props) {
   const { options, onOptionsChange } = props;
@@ -17,26 +18,39 @@ export default function Editor(props) {
     onOptionsChange(merge({}, options, newOptions));
   };
 
+  const isCustomChart = options.globalSeriesType === 'custom';
+
   return (
     <Tabs animated={false} tabBarGutter={0}>
       <Tabs.TabPane key="general" tab={<span data-test="Chart.EditorTabs.General">General</span>}>
         <GeneralSettings {...props} onOptionsChange={optionsChanged} />
+        {isCustomChart && <CustomChartSettings {...props} onOptionsChange={optionsChanged} />}
       </Tabs.TabPane>
-      <Tabs.TabPane key="x-axis" tab={<span data-test="Chart.EditorTabs.XAxis">X Axis</span>}>
-        <XAxisSettings {...props} onOptionsChange={optionsChanged} />
-      </Tabs.TabPane>
-      <Tabs.TabPane key="y-axis" tab={<span data-test="Chart.EditorTabs.YAxis">Y Axis</span>}>
-        <YAxisSettings {...props} onOptionsChange={optionsChanged} />
-      </Tabs.TabPane>
-      <Tabs.TabPane key="series" tab={<span data-test="Chart.EditorTabs.Series">Series</span>}>
-        <SeriesSettings {...props} onOptionsChange={optionsChanged} />
-      </Tabs.TabPane>
-      <Tabs.TabPane key="colors" tab={<span data-test="Chart.EditorTabs.Colors">Colors</span>}>
-        <ColorsSettings {...props} onOptionsChange={optionsChanged} />
-      </Tabs.TabPane>
-      <Tabs.TabPane key="data labels" tab={<span data-test="Chart.EditorTabs.DataLabels">Data Labels</span>}>
-        <DataLabelsSettings {...props} onOptionsChange={optionsChanged} />
-      </Tabs.TabPane>
+      {!isCustomChart && (
+        <Tabs.TabPane key="x-axis" tab={<span data-test="Chart.EditorTabs.XAxis">X Axis</span>}>
+          <XAxisSettings {...props} onOptionsChange={optionsChanged} />
+        </Tabs.TabPane>
+      )}
+      {!isCustomChart && (
+        <Tabs.TabPane key="y-axis" tab={<span data-test="Chart.EditorTabs.YAxis">Y Axis</span>}>
+          <YAxisSettings {...props} onOptionsChange={optionsChanged} />
+        </Tabs.TabPane>
+      )}
+      {!isCustomChart && (
+        <Tabs.TabPane key="series" tab={<span data-test="Chart.EditorTabs.Series">Series</span>}>
+          <SeriesSettings {...props} onOptionsChange={optionsChanged} />
+        </Tabs.TabPane>
+      )}
+      {!isCustomChart && (
+        <Tabs.TabPane key="colors" tab={<span data-test="Chart.EditorTabs.Colors">Colors</span>}>
+          <ColorsSettings {...props} onOptionsChange={optionsChanged} />
+        </Tabs.TabPane>
+      )}
+      {!isCustomChart && (
+        <Tabs.TabPane key="data labels" tab={<span data-test="Chart.EditorTabs.DataLabels">Data Labels</span>}>
+          <DataLabelsSettings {...props} onOptionsChange={optionsChanged} />
+        </Tabs.TabPane>
+      )}
     </Tabs>
   );
 }
