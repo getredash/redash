42e86e9fc4efb52d0df21cbdfafb15bc42bd1696
Gabriel Dutra <nesk.frz@gmail.com>
2019-09-20 17:59:04 -0300

    Randomly select a few options to be advanced

--- redash/query_runner/clickhouse.py
+++ redash/query_runner/clickhouse.py
@@ -39,6 +39,7 @@ class ClickHouse(BaseSQLQueryRunner):
                 }
             },
             "required": ["dbname"],
+            "advanced": ["timeout"],
             "secret": ["password"]
         }
 
