45886b595f1e6e98f2f89266a56e96de5b1abfda
Ran Byron <ranbena@gmail.com>
2019-09-22 13:13:30 +0300

    Better handling of promises

--- client/cypress/integration/alert/view_alert_spec.js
+++ client/cypress/integration/alert/view_alert_spec.js
@@ -54,61 +54,59 @@ describe('View Alert', () => {
     });
 
     it('hides remove button from non-author', function () {
-      addDestinationSubscription(this.alertId, 'Test Email Destination');
-
       cy.server();
       cy.route('GET', 'api/alerts/*/subscriptions').as('Subscriptions');
-      cy.visit(this.alertUrl);
 
-      // verify remove button appears for author
-      cy.wait(['@Subscriptions']);
-      cy.getByTestId('AlertDestinations')
-        .contains('Test Email Destination')
-        .parent().within(() => {
-          cy.get('.remove-button').should('exist');
-        });
+      addDestinationSubscription(this.alertId, 'Test Email Destination')
+        .then(() => {
+          cy.visit(this.alertUrl);
 
-      cy.logout();
-      cy.login('user@redash.io', 'password');
-      cy.reload();
+          // verify remove button appears for author
+          cy.wait(['@Subscriptions']);
+          cy.getByTestId('AlertDestinations')
+            .contains('Test Email Destination')
+            .parent()
+            .within(() => {
+              cy.get('.remove-button').as('RemoveButton').should('exist');
+            });
 
-      // verify remove button not shown for non-author
-      cy.wait(['@Subscriptions']);
-      cy.getByTestId('AlertDestinations')
-        .contains('Test Email Destination')
-        .parent().within(() => {
-          cy.get('.remove-button').should('not.exist');
+          return cy.logout().then(() => cy.login('user@redash.io', 'password'));
+        })
+        .then(() => {
+          cy.visit(this.alertUrl);
+
+          // verify remove button not shown for non-author
+          cy.wait(['@Subscriptions']);
+          cy.get('@RemoveButton').should('not.exist');
         });
     });
 
     it('shows remove button for non-author admin', function () {
-      cy.logout();
-      cy.login('user@redash.io', 'password');
-
-      addDestinationSubscription(this.alertId, 'Test Email Destination');
-
       cy.server();
       cy.route('GET', 'api/alerts/*/subscriptions').as('Subscriptions');
-      cy.visit(this.alertUrl);
 
-      // verify remove button appears for author
-      cy.wait(['@Subscriptions']);
-      cy.getByTestId('AlertDestinations')
-        .contains('Test Email Destination')
-        .parent().within(() => {
-          cy.get('.remove-button').should('exist');
-        });
+      cy.logout()
+        .then(() => cy.login('user@redash.io', 'password'))
+        .then(() => addDestinationSubscription(this.alertId, 'Test Email Destination'))
+        .then(() => {
+          cy.visit(this.alertUrl);
 
-      cy.logout();
-      cy.login(); // as admin
-      cy.reload();
+          // verify remove button appears for author
+          cy.wait(['@Subscriptions']);
+          cy.getByTestId('AlertDestinations')
+            .contains('Test Email Destination')
+            .parent().within(() => {
+              cy.get('.remove-button').as('RemoveButton').should('exist');
+            });
 
-      // verify remove button still appears for admin
-      cy.wait(['@Subscriptions']);
-      cy.getByTestId('AlertDestinations')
-        .contains('Test Email Destination')
-        .parent().within(() => {
-          cy.get('.remove-button').should('exist');
+          return cy.logout().then(() => cy.login()); // as admin
+        })
+        .then(() => {
+          cy.visit(this.alertUrl);
+
+          // verify remove button also appears for admin
+          cy.wait(['@Subscriptions']);
+          cy.get('@RemoveButton').should('exist');
         });
     });
   });
