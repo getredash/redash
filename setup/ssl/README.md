920f9ebf0f39f334edba7e6d2aa6c8fe898019c9
Chester Enright <amunchet@gmail.com>
2019-09-27 20:20:55 +0000

    Working self signed server

---
+++ setup/ssl/README.md
@@ -0,0 +1,3 @@
+- Run the generate self signed certificate bash script
+- docker-compose up --build (you may need to remove old docker images)
+
--- setup/ssl/generate_self_signed.sh
+++ setup/ssl/generate_self_signed.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-openssl req -new -newkey rsa:2048 -nodes -keyout server.key -out server.crt
-
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt
+openssl dhparam -out dhparam.pem 2048
