6227a1d07121a2dd34882fb5c59a743db69edd39
Arik Fraimovich <arik@arikfr.com>
2019-10-02 22:02:46 +0300

    Remove beta tag

--- redash/__init__.py
+++ redash/__init__.py
@@ -16,7 +16,7 @@ from .app import create_app  # noqa
 from .query_runner import import_query_runners
 from .destinations import import_destinations
 
-__version__ = '8.0.0-beta.2'
+__version__ = '8.0.0'
 
 
 if os.environ.get("REMOTE_DEBUG"):
