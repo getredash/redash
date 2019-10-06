7a59113c7e93ff577b0048341009dc9ba94015ab
Omer Lachish <omer@rauchy.net>
2019-10-06 10:57:39 +0300

    revert to calling a function in dynamic settings to allow periodic jobs to be scheduled after app has been loaded

--- redash/settings/dynamic_settings.py
+++ redash/settings/dynamic_settings.py
@@ -8,3 +8,24 @@ def query_time_limit(is_scheduled, user_id, org_id):
     adhoc_time_limit = int_or_none(os.environ.get('REDASH_ADHOC_QUERY_TIME_LIMIT', None))
 
     return scheduled_time_limit if is_scheduled else adhoc_time_limit
+
+
+def schedule_periodic_jobs(schedule):
+    """Schedule any custom periodic jobs here. For example:
+
+    from time import timedelta
+    from somewhere import some_job, some_other_job
+
+    jobs = [{
+        "func": some_job,
+        "interval": timedelta(hours=1)
+    }, {
+        "func": some_other_job,
+        "interval": timedelta(days=1)
+    }]
+    """
+
+    jobs = []
+
+    for job in jobs:
+        schedule(**job)
