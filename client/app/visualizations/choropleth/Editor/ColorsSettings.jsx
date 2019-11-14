aa06b32e17fa6377073f0cefa7fa95a7e9a21795
Levko Kravets <levko.ne@gmail.com>
2019-11-14 19:08:51 +0200

    Add some tests for Choropleth visualization (#4358)

--- client/app/visualizations/choropleth/Editor/ColorsSettings.jsx
+++ client/app/visualizations/choropleth/Editor/ColorsSettings.jsx
@@ -20,12 +20,13 @@ export default function ColorsSettings({ options, onOptionsChange }) {
           <Select
             id="choropleth-editor-clustering-mode"
             className="w-100"
+            data-test="Choropleth.Editor.ClusteringMode"
             defaultValue={options.clusteringMode}
             onChange={clusteringMode => onOptionsChange({ clusteringMode })}
           >
-            <Select.Option value="q">quantile</Select.Option>
-            <Select.Option value="e">equidistant</Select.Option>
-            <Select.Option value="k">k-means</Select.Option>
+            <Select.Option value="q" data-test="Choropleth.Editor.ClusteringMode.q">quantile</Select.Option>
+            <Select.Option value="e" data-test="Choropleth.Editor.ClusteringMode.e">equidistant</Select.Option>
+            <Select.Option value="k" data-test="Choropleth.Editor.ClusteringMode.k">k-means</Select.Option>
           </Select>
         </Grid.Col>
       </Grid.Row>
@@ -38,6 +39,7 @@ export default function ColorsSettings({ options, onOptionsChange }) {
           <InputNumber
             id="choropleth-editor-color-steps"
             className="w-100"
+            data-test="Choropleth.Editor.ColorSteps"
             min={3}
             max={11}
             defaultValue={options.steps}
@@ -53,6 +55,7 @@ export default function ColorsSettings({ options, onOptionsChange }) {
         <Grid.Col span={12}>
           <ColorPicker
             id="choropleth-editor-color-min"
+            data-test="Choropleth.Editor.Colors.Min"
             interactive
             presetColors={ColorPalette}
             placement="topRight"
@@ -69,6 +72,7 @@ export default function ColorsSettings({ options, onOptionsChange }) {
         <Grid.Col span={12}>
           <ColorPicker
             id="choropleth-editor-color-max"
+            data-test="Choropleth.Editor.Colors.Max"
             interactive
             presetColors={ColorPalette}
             placement="topRight"
@@ -85,6 +89,7 @@ export default function ColorsSettings({ options, onOptionsChange }) {
         <Grid.Col span={12}>
           <ColorPicker
             id="choropleth-editor-color-no-value"
+            data-test="Choropleth.Editor.Colors.NoValue"
             interactive
             presetColors={ColorPalette}
             placement="topRight"
@@ -101,6 +106,7 @@ export default function ColorsSettings({ options, onOptionsChange }) {
         <Grid.Col span={12}>
           <ColorPicker
             id="choropleth-editor-color-background"
+            data-test="Choropleth.Editor.Colors.Background"
             interactive
             presetColors={ColorPalette}
             placement="topRight"
@@ -117,6 +123,7 @@ export default function ColorsSettings({ options, onOptionsChange }) {
         <Grid.Col span={12}>
           <ColorPicker
             id="choropleth-editor-color-borders"
+            data-test="Choropleth.Editor.Colors.Borders"
             interactive
             presetColors={ColorPalette}
             placement="topRight"
