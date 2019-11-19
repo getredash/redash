7e42d789fedd25359f2ef66b477fdae8c54e7a01
Arik Fraimovich <arik@arikfr.com>
2019-11-19 14:27:30 +0200

    Format files

--- redash/query_runner/cloudwatch.py
+++ redash/query_runner/cloudwatch.py
@@ -7,21 +7,25 @@ from redash.utils import json_dumps
 
 
 def parse_response(results):
-    columns = [{'name': 'id', 'type': 'string'}, 
-               {'name': 'label', 'type': 'string'}, 
-               {'name': 'timestamp', 'type': 'datetime'},
-               {'name': 'value', 'type': 'float'}]
+    columns = [
+        {"name": "id", "type": "string"},
+        {"name": "label", "type": "string"},
+        {"name": "timestamp", "type": "datetime"},
+        {"name": "value", "type": "float"},
+    ]
 
     rows = []
 
     for metric in results:
-        for i, value in enumerate(metric['Values']):
-            rows.append({
-                'id': metric['Id'],
-                'label': metric['Label'],
-                'timestamp': metric['Timestamps'][i],
-                'value': value
-            })
+        for i, value in enumerate(metric["Values"]):
+            rows.append(
+                {
+                    "id": metric["Id"],
+                    "label": metric["Label"],
+                    "timestamp": metric["Timestamps"][i],
+                    "value": value,
+                }
+            )
 
     return rows, columns
 
@@ -36,50 +40,49 @@ class CloudWatch(BaseQueryRunner):
     @classmethod
     def configuration_schema(cls):
         return {
-            'type': 'object',
-            'properties': {
-                'region': {
-                    'type': 'string',
-                    'title': 'AWS Region'
-                },
-                'aws_access_key': {
-                    'type': 'string',
-                    'title': 'AWS Access Key'
-                },
-                'aws_secret_key': {
-                    'type': 'string',
-                    'title': 'AWS Secret Key'
-                },
+            "type": "object",
+            "properties": {
+                "region": {"type": "string", "title": "AWS Region"},
+                "aws_access_key": {"type": "string", "title": "AWS Access Key"},
+                "aws_secret_key": {"type": "string", "title": "AWS Secret Key"},
             },
-            'required': ['region', 'aws_access_key', 'aws_secret_key'],
-            'order': ['region', 'aws_access_key', 'aws_secret_key'],
-            'secret': ['aws_secret_key']
+            "required": ["region", "aws_access_key", "aws_secret_key"],
+            "order": ["region", "aws_access_key", "aws_secret_key"],
+            "secret": ["aws_secret_key"],
         }
 
     def __init__(self, configuration):
         super(CloudWatch, self).__init__(configuration)
-        self.syntax = 'yaml'
+        self.syntax = "yaml"
 
     def test_connection(self):
         self.get_schema()
 
     def _get_client(self):
-        cloudwatch = boto3.client('cloudwatch', region_name=self.configuration.get('region'), aws_access_key_id=self.configuration.get('aws_access_key'), aws_secret_access_key=self.configuration.get('aws_secret_key'))
+        cloudwatch = boto3.client(
+            "cloudwatch",
+            region_name=self.configuration.get("region"),
+            aws_access_key_id=self.configuration.get("aws_access_key"),
+            aws_secret_access_key=self.configuration.get("aws_secret_key"),
+        )
         return cloudwatch
 
     def get_schema(self, get_stats=False):
         client = self._get_client()
 
-        paginator = client.get_paginator('list_metrics')
+        paginator = client.get_paginator("list_metrics")
 
         metrics = {}
         for page in paginator.paginate():
-            for metric in page['Metrics']:
-                if metric['Namespace'] not in metrics:
-                    metrics[metric['Namespace']] = {'name': metric['Namespace'], 'columns': []}
+            for metric in page["Metrics"]:
+                if metric["Namespace"] not in metrics:
+                    metrics[metric["Namespace"]] = {
+                        "name": metric["Namespace"],
+                        "columns": [],
+                    }
 
-                if metric['MetricName'] not in metrics[metric['Namespace']]['columns']:
-                    metrics[metric['Namespace']]['columns'].append(metric['MetricName'])
+                if metric["MetricName"] not in metrics[metric["Namespace"]]["columns"]:
+                    metrics[metric["Namespace"]]["columns"].append(metric["MetricName"])
 
         return list(metrics.values())
 
@@ -89,13 +92,13 @@ class CloudWatch(BaseQueryRunner):
         query = yaml.safe_load(query)
 
         results = []
-        paginator = cloudwatch.get_paginator('get_metric_data')
+        paginator = cloudwatch.get_paginator("get_metric_data")
         for page in paginator.paginate(**query):
-            results += page['MetricDataResults']
+            results += page["MetricDataResults"]
 
         rows, columns = parse_response(results)
 
-        return json_dumps({'rows': rows, 'columns': columns}), None
+        return json_dumps({"rows": rows, "columns": columns}), None
 
 
 register(CloudWatch)
--- redash/query_runner/cloudwatch_insights.py
+++ redash/query_runner/cloudwatch_insights.py
@@ -13,7 +13,7 @@ TIMEOUT = 180
 
 
 def parse_response(response):
-    results = response['results']
+    results = response["results"]
     rows = []
     field_orders = {}
 
@@ -22,31 +22,39 @@ def parse_response(response):
         rows.append(record)
 
         for order, col in enumerate(row):
-            if col['field'] == '@ptr':
+            if col["field"] == "@ptr":
                 continue
-            field = col['field']
-            record[field] = col['value']
+            field = col["field"]
+            record[field] = col["value"]
             field_orders[field] = max(field_orders.get(field, -1), order)
 
     fields = sorted(field_orders, key=lambda f: field_orders[f])
-    cols = [{
-        'name': f,
-        'type': 'datetime' if f == '@timestamp' else 'string',
-        'friendly_name': f
-    } for f in fields]
-    return {'columns': cols, 'rows': rows, 'metadata': {'data_scanned': response['statistics']['bytesScanned']}}
+    cols = [
+        {
+            "name": f,
+            "type": "datetime" if f == "@timestamp" else "string",
+            "friendly_name": f,
+        }
+        for f in fields
+    ]
+    return {
+        "columns": cols,
+        "rows": rows,
+        "metadata": {"data_scanned": response["statistics"]["bytesScanned"]},
+    }
 
 
 def parse_query(query):
     query = yaml.safe_load(query)
 
-    for timeKey in ['startTime', 'endTime']:
+    for timeKey in ["startTime", "endTime"]:
         if isinstance(query.get(timeKey), str):
             query[timeKey] = int(parse_human_time(query[timeKey]).timestamp())
-    if not query.get('endTime'):
-        query['endTime'] = int(datetime.datetime.now().timestamp())
+    if not query.get("endTime"):
+        query["endTime"] = int(datetime.datetime.now().timestamp())
 
     return query
+
 
 class CloudWatchInsights(BaseQueryRunner):
     should_annotate_query = False
@@ -54,7 +62,7 @@ class CloudWatchInsights(BaseQueryRunner):
     @classmethod
     def name(cls):
         return "Amazon CloudWatch Logs Insights"
-    
+
     @classmethod
     def type(cls):
         return "cloudwatch_insights"
@@ -62,48 +70,51 @@ class CloudWatchInsights(BaseQueryRunner):
     @classmethod
     def configuration_schema(cls):
         return {
-            'type': 'object',
-            'properties': {
-                'region': {
-                    'type': 'string',
-                    'title': 'AWS Region'
-                },
-                'aws_access_key': {
-                    'type': 'string',
-                    'title': 'AWS Access Key'
-                },
-                'aws_secret_key': {
-                    'type': 'string',
-                    'title': 'AWS Secret Key'
-                },
+            "type": "object",
+            "properties": {
+                "region": {"type": "string", "title": "AWS Region"},
+                "aws_access_key": {"type": "string", "title": "AWS Access Key"},
+                "aws_secret_key": {"type": "string", "title": "AWS Secret Key"},
             },
-            'required': ['region', 'aws_access_key', 'aws_secret_key'],
-            'order': ['region', 'aws_access_key', 'aws_secret_key'],
-            'secret': ['aws_secret_key']
+            "required": ["region", "aws_access_key", "aws_secret_key"],
+            "order": ["region", "aws_access_key", "aws_secret_key"],
+            "secret": ["aws_secret_key"],
         }
 
     def __init__(self, configuration):
         super(CloudWatchInsights, self).__init__(configuration)
-        self.syntax = 'yaml'
-    
+        self.syntax = "yaml"
+
     def test_connection(self):
         self.get_schema()
 
     def _get_client(self):
-        cloudwatch = boto3.client('logs', region_name=self.configuration.get('region'), aws_access_key_id=self.configuration.get('aws_access_key'), aws_secret_access_key=self.configuration.get('aws_secret_key'))
+        cloudwatch = boto3.client(
+            "logs",
+            region_name=self.configuration.get("region"),
+            aws_access_key_id=self.configuration.get("aws_access_key"),
+            aws_secret_access_key=self.configuration.get("aws_secret_key"),
+        )
         return cloudwatch
 
     def get_schema(self, get_stats=False):
         client = self._get_client()
 
         log_groups = []
-        paginator = client.get_paginator('describe_log_groups')
+        paginator = client.get_paginator("describe_log_groups")
 
         for page in paginator.paginate():
-            for group in page['logGroups']:
-                group_name = group['logGroupName']
+            for group in page["logGroups"]:
+                group_name = group["logGroupName"]
                 fields = client.get_log_group_fields(logGroupName=group_name)
-                log_groups.append({'name': group_name, 'columns': [field['name'] for field in fields['logGroupFields']]})
+                log_groups.append(
+                    {
+                        "name": group_name,
+                        "columns": [
+                            field["name"] for field in fields["logGroupFields"]
+                        ],
+                    }
+                )
 
         return log_groups
 
@@ -111,18 +122,22 @@ class CloudWatchInsights(BaseQueryRunner):
         logs = self._get_client()
 
         query = parse_query(query)
-        query_id = logs.start_query(**query)['queryId']
+        query_id = logs.start_query(**query)["queryId"]
 
         elapsed = 0
         while True:
             result = logs.get_query_results(queryId=query_id)
-            if result['status'] == 'Complete':
+            if result["status"] == "Complete":
                 data = parse_response(result)
                 break
-            if result['status'] in ('Failed', 'Timeout', 'Unknown', 'Cancelled'):
-                raise Exception("CloudWatch Insights Query Execution Status: {}".format(result['status']))
+            if result["status"] in ("Failed", "Timeout", "Unknown", "Cancelled"):
+                raise Exception(
+                    "CloudWatch Insights Query Execution Status: {}".format(
+                        result["status"]
+                    )
+                )
             elif elapsed > TIMEOUT:
-                raise Exception('Request exceeded timeout.')
+                raise Exception("Request exceeded timeout.")
             else:
                 time.sleep(POLL_INTERVAL)
                 elapsed += POLL_INTERVAL
