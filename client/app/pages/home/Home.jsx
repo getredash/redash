dff9d09b9a97cce225d01c6b838a8950215cd4c8
Gabriel Dutra <nesk.frz@gmail.com>
2019-11-20 17:32:53 -0300

    Use Antd Alert

--- client/app/pages/home/Home.jsx
+++ client/app/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { includes, isEmpty } from 'lodash';
 import { react2angular } from 'react2angular';
+import Alert from 'antd/lib/alert';
 import Icon from 'antd/lib/icon';
 import EmptyState from '@/components/empty-state/EmptyState';
 import DynamicComponent from '@/components/DynamicComponent';
@@ -15,18 +16,24 @@ import { Query } from '@/services/query';
 
 function DeprecatedEmbedFeatureAlert() {
   return (
-    <div className="alert alert-warning">
-      You have enabled <code>ALLOW_PARAMETERS_IN_EMBEDS</code>. This setting is
-      now deprecated and should be turned off. Parameters in embeds are supported
-      by default.{' '}
-      <a
-        href="https://discuss.redash.io/t/support-for-parameters-in-embedded-visualizations/3337"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Read more
-      </a>.
-    </div>
+    <Alert
+      className="m-b-15"
+      type="warning"
+      message={(
+        <>
+          You have enabled <code>ALLOW_PARAMETERS_IN_EMBEDS</code>. This setting is
+          now deprecated and should be turned off. Parameters in embeds are supported
+          by default.{' '}
+          <a
+            href="https://discuss.redash.io/t/support-for-parameters-in-embedded-visualizations/3337"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read more
+          </a>.
+        </>
+      )}
+    />
   );
 }
 
@@ -38,11 +45,17 @@ function EmailNotVerifiedAlert() {
   };
 
   return (
-    <div className="alert alert-warning">
-      We have sent an email with a confirmation link to your email address. Please
-      follow the link to verify your email address.{' '}
-      <a className="clickable" onClick={verifyEmail}>Resend email</a>.
-    </div>
+    <Alert
+      className="m-b-15"
+      type="warning"
+      message={(
+        <>
+          We have sent an email with a confirmation link to your email address. Please
+          follow the link to verify your email address.{' '}
+          <a className="clickable" onClick={verifyEmail}>Resend email</a>.
+        </>
+      )}
+    />
   );
 }
 
