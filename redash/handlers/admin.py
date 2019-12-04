66f3db912b4a580cb416aed13aa93be1670ebeb0
Omer Lachish <omer@rauchy.net>
2019-12-04 21:36:43 +0000

    Revert "remove Celery monitoring"
    This reverts commit 37a74ea403301a21db9c36b3f05fbdc26cbd3530.

--- redash/handlers/admin.py
+++ redash/handlers/admin.py
@@ -8,7 +8,7 @@ from redash.handlers.base import json_response, record_event
 from redash.permissions import require_super_admin
 from redash.serializers import QuerySerializer
 from redash.utils import json_loads
-from redash.monitor import rq_status
+from redash.monitor import celery_tasks, rq_status
 
 
 @routes.route('/api/admin/queries/outdated', methods=['GET'])
@@ -35,6 +35,22 @@ def outdated_queries():
         'queries': QuerySerializer(outdated_queries, with_stats=True, with_last_modified_by=False).serialize(),
         'updated_at': manager_status['last_refresh_at'],
     }
+    return json_response(response)
+
+
+@routes.route('/api/admin/queries/tasks', methods=['GET'])
+@require_super_admin
+@login_required
+def queries_tasks():
+    record_event(current_org, current_user._get_current_object(), {
+        'action': 'list',
+        'object_type': 'celery_tasks'
+    })
+
+    response = {
+        'tasks': celery_tasks(),
+    }
+
     return json_response(response)
 
 
