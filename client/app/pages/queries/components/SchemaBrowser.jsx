e40c5ac04455cc80907d3d2b8fb9799e9c2d8cb2
Levko Kravets <levko.ne@gmail.com>
2019-12-11 14:19:23 +0200

    Use react-virtualized (virtual list) compoonent in schema browser

--- client/app/pages/queries/components/SchemaBrowser.jsx
+++ client/app/pages/queries/components/SchemaBrowser.jsx
@@ -1,10 +1,12 @@
-import { isNil, map, filter, find, includes } from 'lodash';
-import React, { useState, useCallback, useMemo } from 'react';
+import { isNil, map, filter, some, includes } from 'lodash';
+import React, { useState, useCallback, useMemo, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDebouncedCallback } from 'use-debounce';
 import Input from 'antd/lib/input';
 import Button from 'antd/lib/button';
 import Tooltip from 'antd/lib/tooltip';
+import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
+import List from 'react-virtualized/dist/commonjs/List';
 
 const SchemaItemType = PropTypes.shape({
   name: PropTypes.string.isRequired,
@@ -12,9 +14,10 @@ const SchemaItemType = PropTypes.shape({
   columns: PropTypes.arrayOf(PropTypes.string).isRequired,
 });
 
-function SchemaItem({ item, onSelect }) {
-  const [isExpanded, setIsExpanded] = useState(false);
+const schemaTableHeight = 22;
+const schemaColumnHeight = 18;
 
+function SchemaItem({ item, expanded, onToggle, onSelect, ...props }) {
   const handleSelect = useCallback((event, ...args) => {
     event.preventDefault();
     event.stopPropagation();
@@ -26,8 +29,8 @@ function SchemaItem({ item, onSelect }) {
   }
 
   return (
-    <div>
-      <div className="table-name" onClick={() => setIsExpanded(!isExpanded)}>
+    <div {...props}>
+      <div className="table-name" onClick={onToggle}>
         <i className="fa fa-table m-r-5" />
         <strong>
           <span title="{{table.name}}">{item.name}</span>
@@ -35,7 +38,7 @@ function SchemaItem({ item, onSelect }) {
         </strong>
         <i className="fa fa-angle-double-right copy-to-editor" aria-hidden="true" onClick={e => handleSelect(e, item.name)} />
       </div>
-      {isExpanded && (
+      {expanded && (
         <div>
           {map(item.columns, column => (
             <div key={column} className="table-open">
@@ -51,11 +54,15 @@ function SchemaItem({ item, onSelect }) {
 
 SchemaItem.propTypes = {
   item: SchemaItemType,
+  expanded: PropTypes.bool,
+  onToggle: PropTypes.func,
   onSelect: PropTypes.func,
 };
 
 SchemaItem.defaultProps = {
   item: null,
+  expanded: false,
+  onToggle: () => {},
   onSelect: () => {},
 };
 
@@ -64,30 +71,26 @@ function applyFilter(schema, filterString) {
 
   // Empty string: return original schema
   if (filters.length === 0) {
-    return map(schema, item => ({ ...item, matchesFilter: true }));
+    return schema;
   }
 
   // Single word: matches table or column
   if (filters.length === 1) {
     const nameFilter = filters[0];
     const columnFilter = filters[0];
-    return map(schema, item => ({
-      ...item,
-      matchesFilter: (
-        includes(item.name.toLowerCase(), nameFilter) ||
-        find(item.columns, column => includes(column.toLowerCase(), columnFilter))
-      ),
-    }));
+    return filter(schema, item => (
+      includes(item.name.toLowerCase(), nameFilter) ||
+      some(item.columns, column => includes(column.toLowerCase(), columnFilter))
+    ));
   }
 
   // Two (or more) words: first matches table, seconds matches column
   const nameFilter = filters[0];
   const columnFilter = filters[1];
   return filter(map(schema, (item) => {
-    item = { ...item, matchesFilter: false };
     if (includes(item.name.toLowerCase(), nameFilter)) {
-      item.columns = filter(item.columns, column => includes(column.toLowerCase(), columnFilter));
-      item.matchesFilter = item.columns.length > 0;
+      item = { ...item, columns: filter(item.columns, column => includes(column.toLowerCase(), columnFilter)) };
+      return item.columns.length > 0 ? item : null;
     }
   }));
 }
@@ -95,11 +98,27 @@ function applyFilter(schema, filterString) {
 export default function SchemaBrowser({ schema, onRefresh, ...props }) {
   const [filterString, setFilterString] = useState('');
   const filteredSchema = useMemo(() => applyFilter(schema, filterString), [schema, filterString]);
-
+  const [expandedFlags, setExpandedFlags] = useState({});
   const [handleFilterChange] = useDebouncedCallback(setFilterString, 500);
+  const [listRef, setListRef] = useState(null);
+
+  useEffect(() => { setExpandedFlags({}); }, [schema]);
+
+  useEffect(() => {
+    if (listRef) {
+      listRef.recomputeRowHeights();
+    }
+  }, [listRef, filteredSchema, expandedFlags]);
 
   if (schema.length === 0) {
     return null;
+  }
+
+  function toggleTable(tableName) {
+    setExpandedFlags({
+      ...expandedFlags,
+      [tableName]: !expandedFlags[tableName],
+    });
   }
 
   return (
@@ -119,7 +138,35 @@ export default function SchemaBrowser({ schema, onRefresh, ...props }) {
         </Tooltip>
       </div>
       <div className="schema-browser">
-        {map(filteredSchema, item => <SchemaItem key={item.name} item={item.matchesFilter ? item : null} />)}
+        <AutoSizer>
+          {({ width, height }) => (
+            <List
+              ref={setListRef}
+              width={width}
+              height={height}
+              rowCount={filteredSchema.length}
+              rowHeight={({ index }) => {
+                const item = filteredSchema[index];
+                const columnCount = (expandedFlags[item.name] ? item.columns.length : 0);
+                return schemaTableHeight + schemaColumnHeight * columnCount;
+              }}
+              rowRenderer={
+                ({ key, index, style }) => {
+                  const item = filteredSchema[index];
+                  return (
+                    <SchemaItem
+                      key={key}
+                      style={style}
+                      item={item}
+                      expanded={expandedFlags[item.name]}
+                      onToggle={() => toggleTable(item.name)}
+                    />
+                  );
+                }
+              }
+            />
+          )}
+        </AutoSizer>
       </div>
     </div>
   );
