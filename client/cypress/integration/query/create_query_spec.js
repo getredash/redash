28e5d557a92dc03ae8a8142d931e77cb48b46fd4
Restyled.io <commits@restyled.io>
2019-12-18 23:11:23 +0000

    Restyled by prettier

--- client/cypress/integration/query/create_query_spec.js
+++ client/cypress/integration/query/create_query_spec.js
@@ -1,25 +1,26 @@
-describe('Create Query', () => {
+describe("Create Query", () => {
   beforeEach(() => {
     cy.login();
-    cy.visit('/queries/new');
+    cy.visit("/queries/new");
   });
 
-  it('executes and saves a query', () => {
-    cy.getByTestId('SelectDataSource')
+  it("executes and saves a query", () => {
+    cy.getByTestId("SelectDataSource")
       .click()
-      .contains('Test PostgreSQL').click();
+      .contains("Test PostgreSQL")
+      .click();
 
-    cy.getByTestId('QueryEditor')
-      .get('.ace_text-input')
-      .type('SELECT id, name FROM organizations{esc}', { force: true });
+    cy.getByTestId("QueryEditor")
+      .get(".ace_text-input")
+      .type("SELECT id, name FROM organizations{esc}", { force: true });
 
-    cy.getByTestId('ExecuteButton').click();
+    cy.getByTestId("ExecuteButton").click();
 
-    cy.getByTestId('TableVisualization').should('exist');
+    cy.getByTestId("TableVisualization").should("exist");
     cy.wait(200); // eslint-disable-line cypress/no-unnecessary-waiting
-    cy.percySnapshot('Edit Query');
+    cy.percySnapshot("Edit Query");
 
-    cy.getByTestId('SaveButton').click();
-    cy.url().should('match', /\/queries\/\d+\/source/);
+    cy.getByTestId("SaveButton").click();
+    cy.url().should("match", /\/queries\/\d+\/source/);
   });
 });
