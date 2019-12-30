29582e3212b13425e65ca059840edea13b22bc08
Gabriel Dutra <nesk.frz@gmail.com>
2019-12-30 14:40:56 -0300

    Run prettier on cypress folder (#4510)
    * Run prettier on cypress folder
    
    * Test Restyled
    
    * Revert "Test Restyled"
    
    This reverts commit 13d43968fe1e1af77d4186ec999188a0c6242e26.

--- client/cypress/integration/data-source/create_data_source_spec.js
+++ client/cypress/integration/data-source/create_data_source_spec.js
@@ -1,26 +1,28 @@
-describe('Create Data Source', () => {
+describe("Create Data Source", () => {
   beforeEach(() => {
     cy.login();
-    cy.visit('/data_sources/new');
+    cy.visit("/data_sources/new");
   });
 
-  it('renders the page and takes a screenshot', () => {
-    cy.getByTestId('CreateSourceDialog').should('contain', 'PostgreSQL');
+  it("renders the page and takes a screenshot", () => {
+    cy.getByTestId("CreateSourceDialog").should("contain", "PostgreSQL");
     cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
-    cy.percySnapshot('Create Data Source - Types');
+    cy.percySnapshot("Create Data Source - Types");
   });
 
-  it('creates a new PostgreSQL data source', () => {
-    cy.getByTestId('SearchSource').type('PostgreSQL');
-    cy.getByTestId('CreateSourceDialog').contains('PostgreSQL').click();
+  it("creates a new PostgreSQL data source", () => {
+    cy.getByTestId("SearchSource").type("PostgreSQL");
+    cy.getByTestId("CreateSourceDialog")
+      .contains("PostgreSQL")
+      .click();
 
-    cy.getByTestId('Name').type('Redash');
-    cy.getByTestId('Host').type('postgres');
-    cy.getByTestId('User').type('postgres');
-    cy.getByTestId('Password').type('postgres');
-    cy.getByTestId('Database Name').type('postgres{enter}');
-    cy.getByTestId('CreateSourceButton').click();
+    cy.getByTestId("Name").type("Redash");
+    cy.getByTestId("Host").type("postgres");
+    cy.getByTestId("User").type("postgres");
+    cy.getByTestId("Password").type("postgres");
+    cy.getByTestId("Database Name").type("postgres{enter}");
+    cy.getByTestId("CreateSourceButton").click();
 
-    cy.contains('Saved.');
+    cy.contains("Saved.");
   });
 });
