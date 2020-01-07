94e3de1e3c825177e5a3da3a36c06e4e770f3e59
Levko Kravets <levko.ne@gmail.com>
2020-01-07 14:30:05 +0200

    Update layout on route change

--- client/app/components/ApplicationArea/index.jsx
+++ client/app/components/ApplicationArea/index.jsx
@@ -1,17 +1,47 @@
-import React, { useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import ApplicationHeader from "@/components/ApplicationHeader";
 
 import Router from "./Router";
 
+const layouts = {
+  default: {
+    showHeader: true,
+    bodyClass: false,
+  },
+  fixed: {
+    showHeader: true,
+    bodyClass: "fixed-layout",
+  },
+  defaultSignedOut: {
+    showHeader: false,
+  },
+};
+
+function selectLayout(route) {
+  let layout = layouts.default;
+  if (route.layout) {
+    layout = layouts[route.layout] || layouts.default;
+  } else if (!route.authenticated) {
+    layout = layouts.defaultSignedOut;
+  }
+  return layout;
+}
+
 export default function ApplicationArea() {
+  const [showHeader, setShowHeader] = useState(false);
+
   const handleRouteChange = useCallback(route => {
-    console.log(route);
-    // TODO: Apply layout (show/hide header, set custom <body> class, etc.)
+    const layout = selectLayout(route);
+    setShowHeader(layout.showHeader);
+    document.body.className = layout.bodyClass;
+    if (route.title) {
+      document.title = route.title;
+    }
   }, []);
 
   return (
     <div>
-      <ApplicationHeader />
+      {showHeader && <ApplicationHeader />}
       <Router onRouteChange={handleRouteChange} />
     </div>
   );
