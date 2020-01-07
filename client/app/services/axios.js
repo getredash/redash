0ac577d5e17743b907e330ec62b1558d3564ca64
Gabriel Dutra <nesk.frz@gmail.com>
2020-01-06 21:06:20 -0300

    Remove $applyAsync for query angular pages

--- client/app/services/axios.js
+++ client/app/services/axios.js
@@ -3,15 +3,9 @@ import { $rootScope } from "./ng";
 
 export const axios = axiosLib.create();
 
-const getData = ({ data }) => {
-  $rootScope.$applyAsync(); // ANGULAR_REMOVE_ME - remove when React Query pages are merged
-  return data;
-};
+const getData = ({ data }) => data;
+const getResponse = ({ response }) => Promise.reject(response);
 
-const getResponse = ({ response }) => {
-  $rootScope.$applyAsync(); // ANGULAR_REMOVE_ME - remove when React Query pages are merged
-  return Promise.reject(response);
-};
 axios.interceptors.response.use(getData, getResponse);
 
 // TODO: revisit this definition when auth is updated
