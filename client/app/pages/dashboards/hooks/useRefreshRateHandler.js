e2ee7074ebc5c2396414cdcdd7790b5b2706149c
Levko Kravets <levko.ne@gmail.com>
2020-01-21 13:10:47 +0200

    Update URL and timer only when refresh rate changes

--- client/app/pages/dashboards/hooks/useRefreshRateHandler.js
+++ client/app/pages/dashboards/hooks/useRefreshRateHandler.js
@@ -1,5 +1,5 @@
 import { isNaN, max, min } from "lodash";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useMemo } from "react";
 import location from "@/services/location";
 import { policy } from "@/services/policy";
 
@@ -16,13 +16,25 @@ function getRefreshRateFromUrl() {
 export default function useRefreshRateHandler(refreshDashboard) {
   const [refreshRate, setRefreshRate] = useState(getRefreshRateFromUrl());
 
+  // `refreshDashboard` may change quite frequently (on every update of `dashboard` instance), but we
+  // have to keep the same timer running, because timer will restart when re-creating, and instead of
+  // running refresh every N seconds - it will run refresh every N seconds after last dashboard update
+  // (which is not right obviously)
+  const refreshDashboardRef = useRef();
+  refreshDashboardRef.current = refreshDashboard;
+
+  // URL and timer should be updated only when `refreshRate` changes
   useEffect(() => {
     location.setSearch({ refresh: refreshRate || null }, true);
     if (refreshRate) {
-      const refreshTimer = setInterval(refreshDashboard, refreshRate * 1000);
+      const refreshTimer = setInterval(() => {
+        refreshDashboardRef.current();
+      }, refreshRate * 1000);
       return () => clearInterval(refreshTimer);
     }
-  }, [refreshDashboard, refreshRate]);
+  }, [refreshRate]);
 
-  return [refreshRate, rate => setRefreshRate(getLimitedRefreshRate(rate)), () => setRefreshRate(null)];
+  return useMemo(() => [refreshRate, rate => setRefreshRate(getLimitedRefreshRate(rate)), () => setRefreshRate(null)], [
+    refreshRate,
+  ]);
 }
