19d7f1b78d4a29a99ce93fcea71af2080bb07ec0
Levko Kravets <levko.ne@gmail.com>
2020-01-28 18:30:46 +0200

    Fix checkbox label styles

--- client/app/components/visualizations/editor/index.js
+++ client/app/components/visualizations/editor/index.js
@@ -1,7 +1,8 @@
+import React from "react";
 import AntSelect from "antd/lib/select";
 import AntInput from "antd/lib/input";
 import AntInputNumber from "antd/lib/input-number";
-import Checkbox from "antd/lib/checkbox";
+import AntCheckbox from "antd/lib/checkbox";
 
 import RedashColorPicker from "@/components/ColorPicker";
 import RedashTextAlignmentSelect from "@/components/TextAlignmentSelect";
@@ -12,7 +13,15 @@ import Section from "./Section";
 import Switch from "./Switch";
 import ContextHelp from "./ContextHelp";
 
-export { Section, ControlLabel, Checkbox, Switch, ContextHelp, withControlLabel, createTabbedEditor };
+export function Checkbox(props) {
+  return (
+    <div className="visualization-editor-control-label">
+      <AntCheckbox {...props} />
+    </div>
+  );
+}
+
+export { Section, ControlLabel, Switch, ContextHelp, withControlLabel, createTabbedEditor };
 export const Select = withControlLabel(AntSelect);
 export const Input = withControlLabel(AntInput);
 export const TextArea = withControlLabel(AntInput.TextArea);
