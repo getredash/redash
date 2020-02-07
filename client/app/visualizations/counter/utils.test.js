985edc69b2ba702d7d76f09034486929781dd5a8
Gabriel Dutra <nesk.frz@gmail.com>
2020-02-07 13:45:39 -0300

    Update utils.test.js - added Sum/Min/Max - changed visualisation -> visualization for consistence with the rest

--- client/app/visualizations/counter/utils.test.js
+++ client/app/visualizations/counter/utils.test.js
@@ -11,9 +11,9 @@ describe("Visualizations -> Counter -> Utils", () => {
         { city: "Tokyo", population: 38140000 },
       ],
       options: {},
-      visualisationName: "Visualisation Name",
+      visualizationName: "Visualization Name",
       result: {
-        counterLabel: "Visualisation Name",
+        counterLabel: "Visualization Name",
         counterValue: "",
         targetValue: null,
         counterValueTooltip: "",
@@ -23,9 +23,9 @@ describe("Visualizations -> Counter -> Utils", () => {
   });
 
   describe("getCounterData()", () => {
-    describe('"Count rows" option is disabled', () => {
+    describe('Counter type is not "Count Rows"', () => {
       test("No target and counter values return empty result", () => {
-        const result = getCounterData(dummy.rows, dummy.options, dummy.visualisationName);
+        const result = getCounterData(dummy.rows, dummy.options, dummy.visualizationName);
         expect(result).toEqual({
           ...dummy.result,
           showTrend: false,
@@ -33,7 +33,7 @@ describe("Visualizations -> Counter -> Utils", () => {
       });
 
       test('"Counter label" overrides vizualization name', () => {
-        const result = getCounterData(dummy.rows, { counterLabel: "Counter Label" }, dummy.visualisationName);
+        const result = getCounterData(dummy.rows, { counterLabel: "Counter Label" }, dummy.visualizationName);
         expect(result).toEqual({
           ...dummy.result,
           counterLabel: "Counter Label",
@@ -42,13 +42,13 @@ describe("Visualizations -> Counter -> Utils", () => {
       });
 
       test('"Counter Value Column Name" must be set to a correct non empty value', () => {
-        const result = getCounterData(dummy.rows, { rowNumber: 3 }, dummy.visualisationName);
+        const result = getCounterData(dummy.rows, { rowNumber: 3 }, dummy.visualizationName);
         expect(result).toEqual({
           ...dummy.result,
           showTrend: false,
         });
 
-        const result2 = getCounterData(dummy.rows, { counterColName: "missingColumn" }, dummy.visualisationName);
+        const result2 = getCounterData(dummy.rows, { counterColName: "missingColumn" }, dummy.visualizationName);
         expect(result2).toEqual({
           ...dummy.result,
           showTrend: false,
@@ -56,7 +56,7 @@ describe("Visualizations -> Counter -> Utils", () => {
       });
 
       test('"Counter Value Column Name" uses correct column', () => {
-        const result = getCounterData(dummy.rows, { counterColName: "population" }, dummy.visualisationName);
+        const result = getCounterData(dummy.rows, { counterColName: "population" }, dummy.visualizationName);
         expect(result).toEqual({
           ...dummy.result,
           counterValue: "18,604,000.000",
@@ -74,7 +74,7 @@ describe("Visualizations -> Counter -> Utils", () => {
             targetRowNumber: 2,
             targetColName: "population",
           },
-          dummy.visualisationName
+          dummy.visualizationName
         );
         expect(result).toEqual({
           ...dummy.result,
@@ -94,7 +94,7 @@ describe("Visualizations -> Counter -> Utils", () => {
             targetRowNumber: 1,
             targetColName: "population",
           },
-          dummy.visualisationName
+          dummy.visualizationName
         );
         expect(result2).toEqual({
           ...dummy.result,
@@ -108,7 +108,7 @@ describe("Visualizations -> Counter -> Utils", () => {
       });
     });
 
-    describe('"Count rows" option is enabled', () => {
+    describe('Counter type is "Count Rows"', () => {
       beforeEach(() => {
         dummy.result = {
           ...dummy.result,
@@ -119,7 +119,7 @@ describe("Visualizations -> Counter -> Utils", () => {
       });
 
       test("Rows are counted correctly", () => {
-        const result = getCounterData(dummy.rows, { counterType: "countRows" }, dummy.visualisationName);
+        const result = getCounterData(dummy.rows, { counterType: "countRows" }, dummy.visualizationName);
         expect(result).toEqual(dummy.result);
       });
 
@@ -131,7 +131,7 @@ describe("Visualizations -> Counter -> Utils", () => {
             rowNumber: 3,
             counterColName: "population",
           },
-          dummy.visualisationName
+          dummy.visualizationName
         );
         expect(result).toEqual(dummy.result);
       });
@@ -144,7 +144,7 @@ describe("Visualizations -> Counter -> Utils", () => {
             targetRowNumber: 2,
             targetColName: "population",
           },
-          dummy.visualisationName
+          dummy.visualizationName
         );
         expect(result).toEqual({
           ...dummy.result,
@@ -156,12 +156,63 @@ describe("Visualizations -> Counter -> Utils", () => {
       });
 
       test("Empty rows return counter value 0", () => {
-        const result = getCounterData([], { counterType: "countRows" }, dummy.visualisationName);
+        const result = getCounterData([], { counterType: "countRows" }, dummy.visualizationName);
         expect(result).toEqual({
           ...dummy.result,
           counterValue: "0.000",
           counterValueTooltip: "0",
         });
+      });
+    });
+
+    describe('Counter type is "Sum Values"', () => {
+      beforeEach(() => {
+        dummy.options = { counterType: "sumRows", counterColName: "population" };
+        dummy.result = {
+          ...dummy.result,
+          counterValue: "81,228,000.000",
+          counterValueTooltip: "81,228,000",
+          showTrend: false,
+        };
+      });
+
+      test("Rows are summed up correctly", () => {
+        const result = getCounterData(dummy.rows, dummy.options, dummy.visualizationName);
+        expect(result).toEqual(dummy.result);
+      });
+    });
+
+    describe('Counter type is "Min Value"', () => {
+      beforeEach(() => {
+        dummy.options = { counterType: "minValue", counterColName: "population" };
+        dummy.result = {
+          ...dummy.result,
+          counterValue: "18,604,000.000",
+          counterValueTooltip: "18,604,000",
+          showTrend: false,
+        };
+      });
+
+      test("The min value from rows is returned", () => {
+        const result = getCounterData(dummy.rows, dummy.options, dummy.visualizationName);
+        expect(result).toEqual(dummy.result);
+      });
+    });
+
+    describe('Counter type is "Max Value"', () => {
+      beforeEach(() => {
+        dummy.options = { counterType: "maxValue", counterColName: "population" };
+        dummy.result = {
+          ...dummy.result,
+          counterValue: "38,140,000.000",
+          counterValueTooltip: "38,140,000",
+          showTrend: false,
+        };
+      });
+
+      test("The max value from rows is returned", () => {
+        const result = getCounterData(dummy.rows, dummy.options, dummy.visualizationName);
+        expect(result).toEqual(dummy.result);
       });
     });
   });
