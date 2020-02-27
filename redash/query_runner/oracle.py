5d533a327710b0afb797911b755406eff6ca60e7
juanvasquezreyes <juan.reyes-vasquez@outlook.com>
2020-02-27 09:50:46 -0600

    Oracle: update DSN construction to support special characters in user/password. (#4659)
    * Update oracle.py
    
    The reason I propose this change is to fix an issue when oracle password has an @
    example of connection string: user/p@ssword@host
    
    * Update oracle.py
    
    Fixing init after comments
    
    * Remove empty constructor.
    
    Co-authored-by: Arik Fraimovich <arik@arikfr.com>

--- redash/query_runner/oracle.py
+++ redash/query_runner/oracle.py
@@ -62,19 +62,6 @@ class Oracle(BaseSQLQueryRunner):
     def type(cls):
         return "oracle"
 
-    def __init__(self, configuration):
-        super(Oracle, self).__init__(configuration)
-
-        dsn = cx_Oracle.makedsn(
-            self.configuration["host"],
-            self.configuration["port"],
-            service_name=self.configuration["servicename"],
-        )
-
-        self.connection_string = "{}/{}@{}".format(
-            self.configuration["user"], self.configuration["password"], dsn
-        )
-
     def _get_tables(self, schema):
         query = """
         SELECT
@@ -130,7 +117,12 @@ class Oracle(BaseSQLQueryRunner):
                 )
 
     def run_query(self, query, user):
-        connection = cx_Oracle.connect(self.connection_string)
+        dsn = cx_Oracle.makedsn(
+            self.configuration["host"],
+            self.configuration["port"],
+            service_name=self.configuration["servicename"])
+        
+        connection = cx_Oracle.connect(user=self.configuration["user"],password=self.configuration["password"], dsn=dsn)
         connection.outputtypehandler = Oracle.output_handler
 
         cursor = connection.cursor()
