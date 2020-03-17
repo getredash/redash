52911b7be30c184a2a4f91544c07e4d3ac0ae65c
Levko Kravets <levko.ne@gmail.com>
2020-03-17 13:42:45 +0200

    Cohort appearance settings (#4597)
    * Cohort: add settings for tooltips, value formats and placeholder
    
    * Cohort: add settings for colors
    
    * Cohort: change all settings tabs to use horizontal inputs
    
    * Cohort: show color labels in editor

--- client/app/visualizations/cohort/getOptions.js
+++ client/app/visualizations/cohort/getOptions.js
@@ -1,3 +1,6 @@
+import { merge } from "lodash";
+import ColorPalette from "@/visualizations/ColorPalette";
+
 const DEFAULT_OPTIONS = {
   timeInterval: "daily",
   mode: "diagonal",
@@ -5,8 +8,25 @@ const DEFAULT_OPTIONS = {
   stageColumn: "day_number",
   totalColumn: "total",
   valueColumn: "value",
+
+  showTooltips: true,
+  percentValues: true,
+
+  timeColumnTitle: "Time",
+  peopleColumnTitle: "Users",
+  stageColumnTitle: "{{ @ }}",
+
+  numberFormat: "0,0[.]00",
+  percentFormat: "0.00%",
+  noValuePlaceholder: "-",
+
+  colors: {
+    min: "#ffffff",
+    max: ColorPalette["Dark Blue"],
+    steps: 7,
+  },
 };
 
 export default function getOptions(options) {
-  return { ...DEFAULT_OPTIONS, ...options };
+  return merge({}, DEFAULT_OPTIONS, options);
 }
