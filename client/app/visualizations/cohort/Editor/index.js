0e6327ec325d69ac01d940e17f5b64cfc068b529
Levko Kravets <levko.ne@gmail.com>
2020-03-17 13:42:45 +0200

    Cohort appearance settings (#4597)
    * Cohort: add settings for tooltips, value formats and placeholder
    
    * Cohort: add settings for colors
    
    * Cohort: change all settings tabs to use horizontal inputs
    
    * Cohort: show color labels in editor

--- client/app/visualizations/cohort/Editor/index.js
+++ client/app/visualizations/cohort/Editor/index.js
@@ -2,8 +2,12 @@ import createTabbedEditor from "@/components/visualizations/editor/createTabbedE
 
 import ColumnsSettings from "./ColumnsSettings";
 import OptionsSettings from "./OptionsSettings";
+import ColorsSettings from "./ColorsSettings";
+import AppearanceSettings from "./AppearanceSettings";
 
 export default createTabbedEditor([
   { key: "Columns", title: "Columns", component: ColumnsSettings },
   { key: "Options", title: "Options", component: OptionsSettings },
+  { key: "Colors", title: "Colors", component: ColorsSettings },
+  { key: "Appearance", title: "Appearance", component: AppearanceSettings },
 ]);
