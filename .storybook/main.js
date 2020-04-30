4694158da4244eaf6a6f15b2f847b3402b0980df
Gabriel Dutra <nesk.frz@gmail.com>
2020-04-30 19:13:10 -0300

    Move storybook package to Redash package.json

---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ["../viz-lib/src/**/*.stories.[tj]s"],
+  addons: ["@storybook/addon-knobs"]
+};
--- viz-lib/.storybook/main.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  stories: ["../src/**/*.stories.[tj]s"],
-  addons: ["@storybook/addon-knobs"],
-};
