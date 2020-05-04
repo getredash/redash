2505abcccf3292652ed395a7e9ac1bd9b17ed709
Gabriel Dutra <nesk.frz@gmail.com>
2020-05-04 19:17:09 -0300

    Add getOptions to Editor

--- viz-lib/src/visualizations/Editor.jsx
+++ viz-lib/src/visualizations/Editor.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
 import { EditorPropTypes } from "@/visualizations/prop-types";
 import registeredVisualizations from "@/visualizations/registeredVisualizations";
 
-export default function Editor({ type, ...otherProps }) {
-  const { Editor } = registeredVisualizations[type];
-  return <Editor {...otherProps} />;
+export default function Editor({ type, options: optionsProp, data, ...otherProps }) {
+  const { Editor, getOptions } = registeredVisualizations[type];
+  const options = useMemo(() => getOptions(optionsProp, data), [optionsProp, data]);
+
+  return <Editor options={options} data={data} {...otherProps} />;
 }
 
 Editor.propTypes = {
