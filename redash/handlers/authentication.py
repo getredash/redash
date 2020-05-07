6fbc7b4f79410df0bd14013087591d05ccdfb58e
yinchunyang <yangyinchun@gmail.com>
2020-05-07 21:11:47 +0800

    add microsoft setting on ui

--- redash/handlers/authentication.py
+++ redash/handlers/authentication.py
@@ -304,6 +304,7 @@ def client_config():
         "dashboardRefreshIntervals": settings.DASHBOARD_REFRESH_INTERVALS,
         "queryRefreshIntervals": settings.QUERY_REFRESH_INTERVALS,
         "googleLoginEnabled": settings.GOOGLE_OAUTH_ENABLED,
+        "microsoftLoginEnabled": settings.MICROSOFT_OAUTH_ENABLED,
         "ldapLoginEnabled": settings.LDAP_LOGIN_ENABLED,
         "pageSize": settings.PAGE_SIZE,
         "pageSizeOptions": settings.PAGE_SIZE_OPTIONS,
