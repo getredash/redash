18b0dc294407e7968d77e4fc00f07285a984f165
Patrick Yang <patrick.yang@databricks.com>
2020-05-14 16:02:27 -0700

    test flippy floppy oops

--- tests/metrics/test_database.py
+++ tests/metrics/test_database.py
@@ -4,6 +4,6 @@ from tests import BaseTestCase
 
 @patch("statsd.StatsClient.timing")
 class TestDatabaseMetrics(BaseTestCase):
-    def test_flask_request_records_statsd_metrics(self, timing):
-        self.client.get("/ping")
-        timing.assert_called_once_with("requests.redash_ping.get", ANY)
+    def test_db_request_records_statsd_metrics(self, timing):
+        self.factory.create_query()
+        timing.assert_called_with("db.changes.insert", ANY)
--- tests/metrics/test_request.py
+++ tests/metrics/test_request.py
@@ -5,5 +5,5 @@ from tests import BaseTestCase
 @patch("statsd.StatsClient.timing")
 class TestRequestMetrics(BaseTestCase):
     def test_flask_request_records_statsd_metrics(self, timing):
-        self.factory.create_query()
-        timing.assert_called_with("db.queries.Insert", ANY)
+        self.client.get("/ping")
+        timing.assert_called_once_with("requests.redash_ping.get", ANY)
