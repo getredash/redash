5ee32f72b7a161f00a342cb7a2b737538d9302ef
Levko Kravets <levko.ne@gmail.com>
2020-05-19 09:27:03 +0300

    Extract desktop navbar component; move mobile navbar and its styles to ApplicationLayout folder

--- client/app/components/ApplicationArea/ApplicationLayout/index.jsx
+++ client/app/components/ApplicationArea/ApplicationLayout/index.jsx
@@ -1,134 +1,24 @@
-import React, { useState } from "react";
+import React, { useRef, useCallback } from "react";
 import PropTypes from "prop-types";
-import Button from "antd/lib/button";
-import Menu from "antd/lib/menu";
-import Icon from "antd/lib/icon";
-import HelpTrigger from "@/components/HelpTrigger";
-import { Auth, currentUser } from "@/services/auth";
-import CreateDashboardDialog from "@/components/dashboards/CreateDashboardDialog";
-import logoUrl from "@/assets/images/redash_icon_small.png";
-
-import ApplicationHeader from "../ApplicationHeader";
+import DesktopNavbar from "./DesktopNavbar";
+import MobileNavbar from "./MobileNavbar";
 
 import "./index.less";
 
 export default function ApplicationLayout({ children }) {
-  const [collapsed, setCollapsed] = useState(true);
+  const mobileNavbarContainerRef = useRef();
+
+  const getMobileNavbarPopupContainer = useCallback(() => mobileNavbarContainerRef.current, []);
+
   return (
     <React.Fragment>
-      <div className="application-layout-menu">
-        <div className="header-logo">
-          <a href="./">
-            <img src={logoUrl} alt="Redash" style={{ height: collapsed ? "20px" : "40px" }} />
-          </a>
-        </div>
-        <Menu mode="inline" selectable={false} inlineCollapsed={collapsed} theme="dark">
-          {/* <Menu.Item key="home" className="header-logo">
-            <a href="./">
-              <img src={logoUrl} alt="Redash" />
-            </a>
-          </Menu.Item> */}
-
-          {currentUser.hasPermission("list_dashboards") && (
-            <Menu.Item key="dashboards">
-              <a href="dashboards">
-                <Icon type="desktop" />
-                <span>Dashboards</span>
-              </a>
-            </Menu.Item>
-          )}
-          {currentUser.hasPermission("view_query") && (
-            <Menu.Item key="queries">
-              <a href="queries">
-                <Icon type="code" />
-                <span>Queries</span>
-              </a>
-            </Menu.Item>
-          )}
-          {currentUser.hasPermission("list_alerts") && (
-            <Menu.Item key="alerts">
-              <a href="alerts">
-                <Icon type="alert" />
-                <span>Alerts</span>
-              </a>
-            </Menu.Item>
-          )}
-
-          <Menu.Divider />
-        </Menu>
-
-        <Menu mode="inline" inlineCollapsed={true} theme="dark" className="create-menu menu-inline-icon-only">
-          <Menu.SubMenu
-            key="create"
-            title={
-              <span>
-                <Icon type="plus"></Icon>
-              </span>
-            }>
-            {currentUser.hasPermission("create_query") && (
-              <Menu.Item key="new-query">
-                <a href="queries/new">New Query</a>
-              </Menu.Item>
-            )}
-            {currentUser.hasPermission("create_dashboard") && (
-              <Menu.Item key="new-dashboard">
-                <a onMouseUp={() => CreateDashboardDialog.showModal()}>New Dashboard</a>
-              </Menu.Item>
-            )}
-            {currentUser.hasPermission("list_alerts") && (
-              <Menu.Item key="new-alert">
-                <a href="alerts/new">New Alert</a>
-              </Menu.Item>
-            )}
-          </Menu.SubMenu>
-        </Menu>
-        <Menu mode="inline" selectable={false} inlineCollapsed={collapsed} theme="dark">
-          <Menu.Item key="help">
-            <HelpTrigger showTooltip={false} type="HOME">
-              <Icon type="question-circle" />
-              <span>Help</span>
-            </HelpTrigger>
-          </Menu.Item>
-          {currentUser.isAdmin && (
-            <Menu.Item key="settings">
-              <a href="data_sources">
-                <Icon type="setting" />
-                <span>Settings</span>
-              </a>
-            </Menu.Item>
-          )}
-          <Menu.Divider />
-        </Menu>
-        <Menu mode="vertical" theme="dark">
-          <Menu.SubMenu
-            key="profile"
-            title={
-              <span>
-                <img className="profile__image_thumb" src={currentUser.profile_image_url} alt={currentUser.name} />
-                {/* <span>{currentUser.name}</span> */}
-              </span>
-            }>
-            <Menu.Item key="profile">
-              <a href="users/me">Profile</a>
-            </Menu.Item>
-            {currentUser.hasPermission("super_admin") && (
-              <Menu.Item key="status">
-                <a href="admin/status">System Status</a>
-              </Menu.Item>
-            )}
-            <Menu.Divider />
-
-            <Menu.Item key="logout">
-              <a onClick={() => Auth.logout()}>Log out</a>
-            </Menu.Item>
-          </Menu.SubMenu>
-        </Menu>
-        <Button onClick={() => setCollapsed(!collapsed)} className="collapse-button">
-          <Icon type={collapsed ? "menu-unfold" : "menu-fold"} />
-        </Button>
+      <div className="application-layout-side-menu">
+        <DesktopNavbar />
       </div>
       <div className="application-layout-content">
-        <ApplicationHeader />
+        <nav className="application-layout-top-menu" ref={mobileNavbarContainerRef}>
+          <MobileNavbar getPopupContainer={getMobileNavbarPopupContainer} />
+        </nav>
         {children}
       </div>
     </React.Fragment>
