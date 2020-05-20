6da7d708dd2f64cffec0f7fcbc180953a65bce88
chulucninh09 <43906170+chulucninh09@users.noreply.github.com>
2020-05-20 22:44:09 +0700

    Re-implement float nan serialization fix for consistency

--- redash/utils/__init__.py
+++ redash/utils/__init__.py
@@ -71,11 +71,6 @@ def generate_token(length):
 class JSONEncoder(simplejson.JSONEncoder):
     """Adapter for `simplejson.dumps`."""
     
-    def __init__(self, *args, **kwargs):
-        super().__init__()
-        # Float value nan or inf in Python should be render to None or null in json.
-        # Using ignore_nan = False will make Python render nan as NaN, leading to parse error in front-end
-        self.ignore_nan = True
     
     def default(self, o):
         # Some SQLAlchemy collections are lazy.
@@ -120,6 +115,9 @@ def json_dumps(data, *args, **kwargs):
     simplejson.dumps function."""
     kwargs.setdefault("cls", JSONEncoder)
     kwargs.setdefault("encoding", None)
+    # Float value nan or inf in Python should be render to None or null in json.
+    # Using ignore_nan = False will make Python render nan as NaN, leading to parse error in front-end
+    kwargs.setdefault('ignore_nan', True)
     return simplejson.dumps(data, *args, **kwargs)
 
 
