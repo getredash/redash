2c974e0341b10430ee40aefe0391d53ef1366e07
Levko Kravets <levko.ne@gmail.com>
2020-05-21 12:04:43 +0300

    getredash/redash#4692 When resizing chart to a certain size it errors out

--- viz-lib/src/visualizations/chart/Renderer/PlotlyChart.jsx
+++ viz-lib/src/visualizations/chart/Renderer/PlotlyChart.jsx
@@ -1,4 +1,4 @@
-import { isArray, isObject } from "lodash";
+import { isArray, isString, isObject, startsWith } from "lodash";
 import React, { useState, useEffect, useContext } from "react";
 import useMedia from "use-media";
 import { ErrorBoundaryContext } from "@/components/ErrorBoundary";
@@ -13,6 +13,11 @@ function catchErrors(func, errorHandler) {
     try {
       return func(...args);
     } catch (error) {
+      // This error happens only when chart width is 20px and looks that
+      // it's safe to just ignore it: 1px less or more and chart will get fixed.
+      if (isString(error) && startsWith(error, "ax.dtick error")) {
+        return;
+      }
       errorHandler.handleError(error);
     }
   };
