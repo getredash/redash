0a90cd8ae73e1481690235d4f077cd29690d9ac0
Gabriel Dutra <nesk.frz@gmail.com>
2020-05-27 12:58:53 -0300

    Remove type from super()
    Co-authored-by: Jannis Leidel <jannis@leidel.info>

--- redash/utils/requests_session.py
+++ redash/utils/requests_session.py
@@ -6,7 +6,7 @@ class ConfiguredSession(requests.Session):
     def request(self, *args, **kwargs):
         if not settings.REQUESTS_ALLOW_REDIRECTS:
             kwargs.update({"allow_redirects": False})
-        return super(ConfiguredSession, self).request(*args, **kwargs)
+        return super().request(*args, **kwargs)
 
 
 requests_session = ConfiguredSession()
