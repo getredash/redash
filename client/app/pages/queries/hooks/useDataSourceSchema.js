a307f677305beee63fdbacbd1aef4f3875d06d7a
Gabriel Dutra <nesk.frz@gmail.com>
2020-06-01 15:33:52 -0300

    Return cached data source schema when available

--- client/app/pages/queries/hooks/useDataSourceSchema.js
+++ client/app/pages/queries/hooks/useDataSourceSchema.js
@@ -1,11 +1,11 @@
-import { reduce } from "lodash";
+import { reduce, has } from "lodash";
 import { useCallback, useEffect, useRef, useState, useMemo } from "react";
 import { axios } from "@/services/axios";
 import DataSource, { SCHEMA_NOT_SUPPORTED } from "@/services/data-source";
 import notification from "@/services/notification";
 
 function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function getSchema(dataSource, refresh = undefined) {
@@ -13,24 +13,27 @@ function getSchema(dataSource, refresh = undefined) {
     return Promise.resolve([]);
   }
 
-  const fetchSchemaFromJob = (data) => {
-    return sleep(1000).then(() => {
-      return axios.get(`api/jobs/${data.job.id}`).then((data) => {
-        if (data.job.status < 3) {
-          return fetchSchemaFromJob(data);
-        } else if (data.job.status === 3) {
-          return data.job.result;
-        } else if (data.job.status === 4 && data.job.error.code === SCHEMA_NOT_SUPPORTED) {
-          return [];
-        } else {
-          return Promise.reject(new Error(data.job.error));
-        }
-      });
+  const fetchSchemaFromJob = data => {
+    return axios.get(`api/jobs/${data.job.id}`).then(data => {
+      if (data.job.status < 3) {
+        return sleep(1000).then(() => fetchSchemaFromJob(data));
+      } else if (data.job.status === 3) {
+        return data.job.result;
+      } else if (data.job.status === 4 && data.job.error.code === SCHEMA_NOT_SUPPORTED) {
+        return [];
+      } else {
+        return Promise.reject(new Error(data.job.error));
+      }
     });
   };
 
   return DataSource.fetchSchema(dataSource, refresh)
-    .then(fetchSchemaFromJob)
+    .then(data => {
+      if (has(data, "job")) {
+        return fetchSchemaFromJob(data);
+      }
+      return has(data, "schema") ? data.schema : Promise.reject();
+    })
     .catch(() => {
       notification.error("Schema refresh failed.", "Please try again later.");
       return Promise.resolve([]);
@@ -48,9 +51,11 @@ export default function useDataSourceSchema(dataSource) {
 
   const reloadSchema = useCallback(
     (refresh = undefined) => {
-      const refreshToken = Math.random().toString(36).substr(2);
+      const refreshToken = Math.random()
+        .toString(36)
+        .substr(2);
       refreshSchemaTokenRef.current = refreshToken;
-      getSchema(dataSource, refresh).then((data) => {
+      getSchema(dataSource, refresh).then(data => {
         if (refreshSchemaTokenRef.current === refreshToken) {
           setSchema(prepareSchema(data));
         }
