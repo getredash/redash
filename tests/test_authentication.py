88b3080f02ce26266565409ee5b589025104e2f9
Fernando Salcido <fsr313@gmail.com>
2020-06-03 17:38:09 -0700

    fix text indentation

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -198,14 +198,14 @@ class TestCreateAndLoginUser(BaseTestCase):
             login_user_mock.assert_called_once_with(user, remember=True)
     
     def test_create_user_long_url(self):
-    email = "test@example.com"
-    name = "Test user"
-    url = '@'*321
-    with patch("redash.authentication.login_user") as login_user_mock:
-        create_and_login_user(self.factory.org, name, email, url)
-        self.assertTrue(login_user_mock.called)
-        user = models.User.query.filter(models.User.email == email).one()
-        self.assertEqual(user.email, email)
+        email = "test@example.com"
+        name = "Test user"
+        url = '@'*321
+        with patch("redash.authentication.login_user") as login_user_mock:
+            create_and_login_user(self.factory.org, name, email, url)
+            self.assertTrue(login_user_mock.called)
+            user = models.User.query.filter(models.User.email == email).one()
+            self.assertEqual(user.email, email)
 
 
 class TestVerifyProfile(BaseTestCase):
