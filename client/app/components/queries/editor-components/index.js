c0d4a1cf8e1b234aa246b22cefeee98354c53528
Gabriel Dutra <nesk.frz@gmail.com>
2020-06-06 12:35:59 -0300

    Initial idea on register query components per DS

---
+++ client/app/components/queries/editor-components/index.js
@@ -0,0 +1,10 @@
+import SchemaBrowser from "@/components/queries/SchemaBrowser";
+import QueryEditor from "@/components/queries/QueryEditor";
+
+import { registerEditorComponent, useEditorComponents } from "./editorComponents";
+
+// default query editor components
+registerEditorComponent("SchemaBrowser", SchemaBrowser);
+registerEditorComponent("QueryEditor", QueryEditor);
+
+export { useEditorComponents };
