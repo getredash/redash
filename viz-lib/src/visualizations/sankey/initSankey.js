bdb62365b189dfee88661bf8b25ecdb299c54409
Gabriel Dutra <nesk.frz@gmail.com>
2020-06-11 06:28:45 -0300

    Fix Sankey issue to render 1 and 5 stages (#4965)
    * Sankey: Make sure last stage has "Exit"
    
    * Sankey: Use 2 as min stage width size to render
    
    * Use null instead of "Exit"
    
    * Add comment about corresponding exit node
    
    * Add multiple stages on Cypress test

--- viz-lib/src/visualizations/sankey/initSankey.js
+++ viz-lib/src/visualizations/sankey/initSankey.js
@@ -69,6 +69,7 @@ function graph(data) {
     addLink(row[dataKeys[1]], row[dataKeys[2]], row.value || 0, 2);
     addLink(row[dataKeys[2]], row[dataKeys[3]], row.value || 0, 3);
     addLink(row[dataKeys[3]], row[dataKeys[4]], row.value || 0, 4);
+    addLink(row[dataKeys[4]], null, row.value || 0, 5); // this line ensures that the last stage has a corresponding exit node
   });
 
   const color = d3.scale.category20();
