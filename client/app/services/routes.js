3e99829eb757c675a6dec612277490d517ba4285
Levko Kravets <levko.ne@gmail.com>
2020-06-24 17:36:05 +0300

    Refine code

--- client/app/services/routes.js
+++ client/app/services/routes.js
@@ -1,27 +1,21 @@
-import { isString, filter, map } from "lodash";
+import { isString, isObject, filter } from "lodash";
 
 class Routes {
-  _routes = [];
-
   items = [];
 
-  _updateItems() {
-    this.items = map(this._routes, ({ route }) => route);
+  register(id, route) {
+    id = isString(id) ? id : null;
+    this.unregister(id);
+    if (isObject(route)) {
+      this.items = [...this.items, { ...route, id }];
+    }
   }
 
-  register = (key, route) => {
-    key = isString(key) ? key : null;
-    this.unregister(key);
-    this._routes = [...this._routes, { key, route }];
-    this._updateItems();
-  };
-
-  unregister = key => {
-    if (isString(key)) {
-      this._routes = filter(this._routes, item => item.key !== key);
-      this._updateItems();
+  unregister(id) {
+    if (isString(id)) {
+      this.items = filter(this.items, item => item.id !== id);
     }
-  };
+  }
 }
 
 export default new Routes();
