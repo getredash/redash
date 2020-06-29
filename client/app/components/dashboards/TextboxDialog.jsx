898a0027c59c0ff2a53ae2b8249ef828b84558a5
Levko Kravets <levko.ne@gmail.com>
2020-06-29 15:17:46 +0300

    Textbox: confirm close if text was changed

--- client/app/components/dashboards/TextboxDialog.jsx
+++ client/app/components/dashboards/TextboxDialog.jsx
@@ -40,11 +40,31 @@ function TextboxDialog({ dialog, isNew, ...props }) {
     });
   }, [dialog, isNew, text]);
 
+  const confirmDialogDismiss = useCallback(() => {
+    const originalText = props.text;
+    if (text !== originalText) {
+      Modal.confirm({
+        title: "Confirm action",
+        content: "Closing this dialog will discard all changes you've made. Are you sure?",
+        okText: "Yes, discard changes",
+        okType: "danger",
+        cancelText: "No, get me back",
+        onOk: () => dialog.dismiss(),
+        maskClosable: true,
+        autoFocusButton: null,
+        style: { top: 170 },
+      });
+    } else {
+      dialog.dismiss();
+    }
+  }, [dialog, text, props.text]);
+
   return (
     <Modal
       {...dialog.props}
       title={isNew ? "Add Textbox" : "Edit Textbox"}
       onOk={saveWidget}
+      onCancel={confirmDialogDismiss}
       okText={isNew ? "Add to Dashboard" : "Save"}
       width={500}
       wrapProps={{ "data-test": "TextboxDialog" }}>
