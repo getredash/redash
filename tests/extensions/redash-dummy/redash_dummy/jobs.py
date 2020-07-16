988ca104a6222ce16a55527b4b8a78e670799d9b
Jannis Leidel <jannis@leidel.info>
2020-07-16 14:26:28 +0200

    Fix bundle-extensions script to work on recent importlib-resources.
    Also adds a test case for running the script.

--- tests/extensions/redash-dummy/redash_dummy/jobs.py
+++ tests/extensions/redash-dummy/redash_dummy/jobs.py
@@ -1,17 +1,5 @@
 from datetime import timedelta
 
-module_attribute = "hello!"
-
-
-def extension(app):
-    """This extension will work"""
-    return "extension loaded"
-
-
-def assertive_extension(app):
-    """This extension won't work"""
-    assert False
-
 
 def job_callback():
     return "result"
