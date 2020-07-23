6705dec0d41c06a2c7842addd8b0833530491bd8
Omer Lachish <omer@rauchy.net>
2020-07-23 10:29:05 +0300

    load extensions when creating the database

--- redash/cli/database.py
+++ redash/cli/database.py
@@ -8,6 +8,7 @@ from sqlalchemy.exc import DatabaseError
 from sqlalchemy.sql import select
 from sqlalchemy_utils.types.encrypted.encrypted_type import FernetEngine
 
+from redash import settings
 from redash.models.base import Column
 from redash.models.types import EncryptedConfiguration
 from redash.utils.configuration import ConfigurationContainer
@@ -34,6 +35,12 @@ def is_db_empty():
     return len(table_names) == 0
 
 
+def load_extensions(db):
+    with db.engine.connect() as connection:
+        for extension in settings.dynamic_settings.database_extensions:
+            connection.execute(f'CREATE EXTENSION IF NOT EXISTS "{extension}";')
+
+
 @manager.command()
 def create_tables():
     """Create the database tables."""
@@ -44,6 +51,8 @@ def create_tables():
     # We need to make sure we run this only if the DB is empty, because otherwise calling
     # stamp() will stamp it with the latest migration value and migrations won't run.
     if is_db_empty():
+        load_extensions(db)
+
         # To create triggers for searchable models, we need to call configure_mappers().
         sqlalchemy.orm.configure_mappers()
         db.create_all()
