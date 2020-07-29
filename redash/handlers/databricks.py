b411b130f26322062468017c2f9f3ede30e7be73
Gabriel Dutra <nesk.frz@gmail.com>
2020-07-28 21:00:09 -0300

    Back with RQ

--- redash/handlers/databricks.py
+++ redash/handlers/databricks.py
@@ -6,11 +6,14 @@ from redash.permissions import (
     require_access,
     view_only,
 )
+from redash.tasks.databricks import (
+    get_databricks_databases,
+    get_databricks_tables,
+    get_database_tables_with_columns,
+    get_databricks_table_columns,
+)
 from redash.serializers import serialize_job
 from redash.utils import json_loads, json_dumps
-
-
-DATABRICKS_REDIS_EXPIRATION_TIME = 3600
 
 
 def _get_databricks_data_source(data_source_id, user, org):
@@ -56,15 +59,10 @@ class DatabricksDatabaseListResource(BaseResource):
             if cached_databases is not None:
                 return cached_databases
 
-        try:
-            databases = data_source.query_runner.get_databases()
-            redis_connection.set(_databases_key(data_source_id), json_dumps(databases))
-            redis_connection.expire(
-                _databases_key(data_source_id), DATABRICKS_REDIS_EXPIRATION_TIME
-            )
-            return databases
-        except Exception:
-            return {"error": {"code": 2, "message": "Error retrieving database list."}}
+        job = get_databricks_databases.delay(
+            data_source.id, redis_key=_databases_key(data_source_id)
+        )
+        return serialize_job(job)
 
 
 class DatabricksSchemaResource(BaseResource):
@@ -80,29 +78,13 @@ class DatabricksSchemaResource(BaseResource):
             if cached_tables is not None:
                 return {"schema": cached_tables, "has_columns": True}
 
-            return {
-                "schema": data_source.query_runner.get_database_tables(database_name),
-                "has_columns": False,
-            }
+            job = get_databricks_tables.delay(data_source.id, database_name)
+            return serialize_job(job)
 
-        try:
-            tables = data_source.query_runner.get_database_tables_with_columns(
-                database_name
-            )
-            # check for tables since it doesn't return an error when the requested database doesn't exist
-            if tables or redis_connection.exists(
-                _tables_key(data_source_id, database_name)
-            ):
-                redis_connection.set(
-                    _tables_key(data_source_id, database_name), json_dumps(tables)
-                )
-                redis_connection.expire(
-                    _tables_key(data_source_id, database_name),
-                    DATABRICKS_REDIS_EXPIRATION_TIME,
-                )
-            return {"schema": tables, "has_columns": True}
-        except Exception:
-            return {"error": {"code": 2, "message": "Error retrieving schema."}}
+        job = get_database_tables_with_columns.delay(
+            data_source.id, database_name, redis_key=_tables_key(data_source_id, database_name)
+        )
+        return serialize_job(job)
 
 
 class DatabricksTableColumnListResource(BaseResource):
@@ -111,7 +93,5 @@ class DatabricksTableColumnListResource(BaseResource):
             data_source_id, user=self.current_user, org=self.current_org
         )
 
-        try:
-            return data_source.query_runner.get_table_columns(database_name, table_name)
-        except Exception:
-            return {"error": {"code": 2, "message": "Error retrieving table columns."}}
+        job = get_databricks_table_columns.delay(data_source.id, database_name, table_name)
+        return serialize_job(job)
