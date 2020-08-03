efa9278abc005c1586552b23fc70d9928dca4ac5
Lingkai Kong <lingkai.kong@databricks.com>
2020-08-03 13:12:17 -0700

    add default limit 1000

--- redash/query_runner/__init__.py
+++ redash/query_runner/__init__.py
@@ -1,6 +1,8 @@
 import logging
 
 from contextlib import ExitStack
+
+import sqlparse
 from dateutil import parser
 from functools import wraps
 import socket
@@ -10,7 +12,7 @@ from urllib.parse import urlparse
 from six import text_type
 from sshtunnel import open_tunnel
 from redash import settings
-from redash.utils import json_loads
+from redash.utils import json_loads, query_is_select_no_limit, add_limit_to_query
 from rq.timeouts import JobTimeoutException
 
 from redash.utils.requests_session import requests, requests_session
@@ -83,7 +85,7 @@ class BaseQueryRunner(object):
         """Returns this query runner's configured host.
         This is used primarily for temporarily swapping endpoints when using SSH tunnels to connect to a data source.
 
-        `BaseQueryRunner`'s naïve implementation supports query runner implementations that store endpoints using `host` and `port` 
+        `BaseQueryRunner`'s naïve implementation supports query runner implementations that store endpoints using `host` and `port`
         configuration values. If your query runner uses a different schema (e.g. a web address), you should override this function.
         """
         if "host" in self.configuration:
@@ -96,7 +98,7 @@ class BaseQueryRunner(object):
         """Sets this query runner's configured host.
         This is used primarily for temporarily swapping endpoints when using SSH tunnels to connect to a data source.
 
-        `BaseQueryRunner`'s naïve implementation supports query runner implementations that store endpoints using `host` and `port` 
+        `BaseQueryRunner`'s naïve implementation supports query runner implementations that store endpoints using `host` and `port`
         configuration values. If your query runner uses a different schema (e.g. a web address), you should override this function.
         """
         if "host" in self.configuration:
@@ -109,7 +111,7 @@ class BaseQueryRunner(object):
         """Returns this query runner's configured port.
         This is used primarily for temporarily swapping endpoints when using SSH tunnels to connect to a data source.
 
-        `BaseQueryRunner`'s naïve implementation supports query runner implementations that store endpoints using `host` and `port` 
+        `BaseQueryRunner`'s naïve implementation supports query runner implementations that store endpoints using `host` and `port`
         configuration values. If your query runner uses a different schema (e.g. a web address), you should override this function.
         """
         if "port" in self.configuration:
@@ -122,7 +124,7 @@ class BaseQueryRunner(object):
         """Sets this query runner's configured port.
         This is used primarily for temporarily swapping endpoints when using SSH tunnels to connect to a data source.
 
-        `BaseQueryRunner`'s naïve implementation supports query runner implementations that store endpoints using `host` and `port` 
+        `BaseQueryRunner`'s naïve implementation supports query runner implementations that store endpoints using `host` and `port`
         configuration values. If your query runner uses a different schema (e.g. a web address), you should override this function.
         """
         if "port" in self.configuration:
@@ -190,6 +192,13 @@ class BaseQueryRunner(object):
             **({"deprecated": True} if cls.deprecated else {}),
         }
 
+    @property
+    def supports_auto_limit(self):
+        return False
+
+    def apply_auto_limit(self, query_text):
+        return query_text
+
 
 class BaseSQLQueryRunner(BaseQueryRunner):
     def get_schema(self, get_stats=False):
@@ -208,6 +217,17 @@ class BaseSQLQueryRunner(BaseQueryRunner):
                 res = self._run_query_internal("select count(*) as cnt from %s" % t)
                 tables_dict[t]["size"] = res[0]["cnt"]
 
+    @property
+    def supports_auto_limit(self):
+        return True
+
+    def apply_auto_limit(self, query_text):
+        query_list = sqlparse.split(query_text)
+        # we only check for last one in the list because it is the one that we show result
+        last_query = query_list[-1]
+        if query_is_select_no_limit(last_query):
+            query_list[-1] = add_limit_to_query(last_query)
+        return "\n".join(query_list)
 
 def is_private_address(url):
     hostname = urlparse(url).hostname
---
+++ tests/query_runner/test_basesql_queryrunner.py
@@ -0,0 +1,74 @@
+import unittest
+
+from redash.query_runner import BaseSQLQueryRunner, BaseQueryRunner
+
+
+class TestBaseSQLQueryRunner(unittest.TestCase):
+
+    def setUp(self):
+        self.query_runner = BaseSQLQueryRunner(BaseQueryRunner)
+
+    def test_apply_auto_limit_origin_no_limit_1(self):
+        origin_query_text = "SELECT 2"
+        query_text = self.query_runner.apply_auto_limit(origin_query_text)
+        self.assertEqual("SELECT 2 LIMIT 1000", query_text)
+
+    def test_apply_auto_limit_origin_have_limit_1(self):
+        origin_query_text = "SELECT 2 LIMIT 100"
+        query_text = self.query_runner.apply_auto_limit(origin_query_text)
+        self.assertEqual(origin_query_text, query_text)
+
+    def test_apply_auto_limit_origin_have_limit_2(self):
+        origin_query_text = "SELECT * FROM fake WHERE id IN (SELECT id FROM fake_2 LIMIT 200) LIMIT 200"
+        query_text = self.query_runner.apply_auto_limit(origin_query_text)
+        self.assertEqual(origin_query_text, query_text)
+
+    def test_apply_auto_limit_origin_no_limit_2(self):
+        origin_query_text = "SELECT * FROM fake WHERE id IN (SELECT id FROM fake_2 LIMIT 200)"
+        query_text = self.query_runner.apply_auto_limit(origin_query_text)
+        self.assertEqual(origin_query_text + " LIMIT 1000", query_text)
+
+    def test_apply_auto_limit_non_select_query(self):
+        origin_query_text = "create table execution_times as " \
+                            "(select id, retrieved_at, data_source_id, query, runtime, query_hash " \
+                            "from query_results order by 1 desc)"
+        query_text = self.query_runner.apply_auto_limit(origin_query_text)
+        self.assertEqual(origin_query_text, query_text)
+
+    def test_apply_auto_limit_error_query(self):
+        origin_query_text = "dklsk jdhsajhdiwc kkdsakjdwi mdklsjal"
+        query_text = self.query_runner.apply_auto_limit(origin_query_text)
+        self.assertEqual(origin_query_text, query_text)
+
+    def test_apply_auto_limit_multi_query_add_limit_1(self):
+        origin_query_text = "insert into execution_times (id, retrieved_at, data_source_id, query, runtime, query_hash) " \
+                            "select id, retrieved_at, data_source_id, query, runtime, query_hash from query_results " \
+                            "where id > (select max(id) from execution_times);\n" \
+                            "select max(id), 'execution_times' as table_name from execution_times " \
+                            "union all " \
+                            "select max(id), 'query_results' as table_name from query_results"
+        query_text = self.query_runner.apply_auto_limit(origin_query_text)
+        self.assertEqual(origin_query_text + " LIMIT 1000", query_text)
+
+    def test_apply_auto_limit_multi_query_add_limit_2(self):
+        origin_query_text = "use database demo;\n" \
+                            "select * from data"
+        query_text = self.query_runner.apply_auto_limit(origin_query_text)
+        self.assertEqual(origin_query_text + " LIMIT 1000", query_text)
+
+    def test_apply_auto_limit_multi_query_end_with_punc(self):
+        origin_query_text = "select * from table1;\n" \
+                            "select * from table2;"
+        query_text = self.query_runner.apply_auto_limit(origin_query_text)
+        self.assertEqual("select * from table1;\n" \
+                         "select * from table2 LIMIT 1000;", query_text)
+
+    def test_apply_auto_limit_multi_query_last_not_select(self):
+        origin_query_text = "select * from table1;\n" \
+                            "CREATE TABLE Persons (PersonID int)"
+        query_text = self.query_runner.apply_auto_limit(origin_query_text)
+        self.assertEqual(origin_query_text, query_text)
+
+
+if __name__ == '__main__':
+    unittest.main()
