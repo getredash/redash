57b41a48ae03de854f8b0f3fd5cfbea6f88acedd
Gabriel Dutra <nesk.frz@gmail.com>
2020-08-07 16:50:46 -0300

    Prevent ResizeObserver error from failing tests

--- client/cypress/support/index.js
+++ client/cypress/support/index.js
@@ -1,1 +1,8 @@
 import "./commands";
+
+Cypress.on("uncaught:exception", err => {
+  // Prevent ResizeObserver error from failing tests
+  if (err && Cypress._.includes(err.message, "ResizeObserver loop limit exceeded")) {
+    return false;
+  }
+});
