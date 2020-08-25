7f1a9df80e9d04c593ab5d9024f2753f87bf2206
Arik Fraimovich <arik@arikfr.com>
2020-08-25 12:53:46 +0300

    Add TypeScript definitions for DialogComponent.

--- client/app/services/routes.ts
+++ client/app/services/routes.ts
@@ -1,16 +1,37 @@
 import { isString, isObject, filter, sortBy } from "lodash";
+import React from "react";
+import { Context, Route as UniversalRouterRoute } from "universal-router";
 import pathToRegexp from "path-to-regexp";
 
-function getRouteParamsCount(path) {
+export interface CurrentRoute<P> {
+  id: string | null;
+  key?: string;
+  title: string;
+  routeParams: P;
+}
+
+export interface RedashRoute<P = {}, C extends Context = Context, R = any> extends UniversalRouterRoute<C, R> {
+  path: string; // we don't use other UniversalRouterRoute options, path should be available and should be a string
+  key?: string; // generated in Router.jsx
+  title: string;
+  render?: (currentRoute: CurrentRoute<P>) => React.ReactNode;
+  getApiKey?: () => string;
+}
+
+interface RouteItem extends RedashRoute<any> {
+  id: string | null;
+}
+
+function getRouteParamsCount(path: string) {
   const tokens = pathToRegexp.parse(path);
   return filter(tokens, isObject).length;
 }
 
 class Routes {
-  _items = [];
+  _items: RouteItem[] = [];
   _sorted = false;
 
-  get items() {
+  get items(): RouteItem[] {
     if (!this._sorted) {
       this._items = sortBy(this._items, [
         item => getRouteParamsCount(item.path), // simple definitions first, with more params - last
@@ -22,19 +43,19 @@ class Routes {
     return this._items;
   }
 
-  register(id, route) {
-    id = isString(id) ? id : null;
-    this.unregister(id);
+  public register<P>(id: string, route: RedashRoute<P>) {
+    const idOrNull = isString(id) ? id : null;
+    this.unregister(idOrNull);
     if (isObject(route)) {
-      this._items = [...this._items, { ...route, id }];
+      this._items = [...this.items, { ...route, id: idOrNull }];
       this._sorted = false;
     }
   }
 
-  unregister(id) {
+  public unregister(id: string | null) {
     if (isString(id)) {
       // removing item does not break their order (if already sorted)
-      this._items = filter(this._items, item => item.id !== id);
+      this._items = filter(this.items, item => item.id !== id);
     }
   }
 }
