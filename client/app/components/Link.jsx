278f56d400d8d39f6ff9b96efeed4194584edc97
Levko Kravets <levko.ne@gmail.com>
2020-08-25 21:48:25 +0300

    Simplify Link component implementation

--- client/app/components/Link.jsx
+++ client/app/components/Link.jsx
@@ -1,20 +1,9 @@
-import React, { useMemo } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import Button from "antd/lib/button";
 
 function DefaultLinkComponent(props) {
-  const href = useMemo(() => Link.modifyHref(props.href), [props.href]);
-
-  return <a href={href} {...props} />; // eslint-disable-line jsx-a11y/anchor-has-content
+  return <a {...props} />; // eslint-disable-line jsx-a11y/anchor-has-content
 }
-
-DefaultLinkComponent.propTypes = {
-  href: PropTypes.string,
-};
-
-DefaultLinkComponent.defaultProps = {
-  href: null,
-};
 
 function Link(props) {
   return <Link.Component {...props} />;
@@ -23,18 +12,8 @@ function Link(props) {
 Link.Component = DefaultLinkComponent;
 
 function DefaultButtonLinkComponent(props) {
-  const href = useMemo(() => Link.modifyHref(props.href), [props.href]);
-
-  return <Button href={href} {...props} />;
+  return <Button {...props} />;
 }
-
-DefaultButtonLinkComponent.propTypes = {
-  href: PropTypes.string,
-};
-
-DefaultButtonLinkComponent.defaultProps = {
-  href: null,
-};
 
 function ButtonLink(props) {
   return <ButtonLink.Component {...props} />;
@@ -43,7 +22,5 @@ function ButtonLink(props) {
 ButtonLink.Component = DefaultButtonLinkComponent;
 
 Link.Button = ButtonLink;
-
-Link.modifyHref = href => href;
 
 export default Link;
