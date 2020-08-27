99766bbabd8a33a4b8e83d6c03430d85ee86bba5
Restyled.io <commits@restyled.io>
2020-08-27 02:06:57 +0000

    Restyled by prettier

--- client/app/visualizations/table/utils.js
+++ client/app/visualizations/table/utils.js
@@ -1,6 +1,6 @@
-import Icon from 'antd/lib/icon';
-import Tooltip from 'antd/lib/tooltip';
-import cx from 'classnames';
+import Icon from "antd/lib/icon";
+import Tooltip from "antd/lib/tooltip";
+import cx from "classnames";
 import {
   each,
   filter,
@@ -9,84 +9,88 @@ import {
   map,
   some,
   sortBy,
-  toString
-} from 'lodash';
-import React from 'react';
+  toString,
+} from "lodash";
+import React from "react";
 
-import initBooleanColumn from './columns/boolean';
-import initDateTimeColumn from './columns/datetime';
-import initImageColumn from './columns/image';
-import initJsonColumn from './columns/json';
-import initLinkColumn from './columns/link';
-import initNumberColumn from './columns/number';
-import initTextColumn from './columns/text';
+import initBooleanColumn from "./columns/boolean";
+import initDateTimeColumn from "./columns/datetime";
+import initImageColumn from "./columns/image";
+import initJsonColumn from "./columns/json";
+import initLinkColumn from "./columns/link";
+import initNumberColumn from "./columns/number";
+import initTextColumn from "./columns/text";
 
 // this map should contain all possible values for `column.displayAs` property
 export const ColumnTypes = {
-  string : initTextColumn,
-  number : initNumberColumn,
-  datetime : initDateTimeColumn,
-  boolean : initBooleanColumn,
-  link : initLinkColumn,
-  image : initImageColumn,
-  json : initJsonColumn,
+  string: initTextColumn,
+  number: initNumberColumn,
+  datetime: initDateTimeColumn,
+  boolean: initBooleanColumn,
+  link: initLinkColumn,
+  image: initImageColumn,
+  json: initJsonColumn,
 };
 
 function nextOrderByDirection(direction) {
   switch (direction) {
-  case 'ascend':
-    return 'descend';
-  case 'descend':
-    return null;
-  default:
-    return 'ascend';
+    case "ascend":
+      return "descend";
+    case "descend":
+      return null;
+    default:
+      return "ascend";
   }
 }
 
 function toggleOrderBy(columnName, orderBy = [], multiColumnSort = false) {
-  const index = findIndex(orderBy, i => i.name === columnName);
-  const item = {name : columnName, direction : 'ascend'};
+  const index = findIndex(orderBy, (i) => i.name === columnName);
+  const item = { name: columnName, direction: "ascend" };
   if (index >= 0) {
     item.direction = nextOrderByDirection(orderBy[index].direction);
   }
 
   if (multiColumnSort) {
     if (!item.direction) {
-      return filter(orderBy, i => i.name !== columnName);
+      return filter(orderBy, (i) => i.name !== columnName);
     }
     if (index >= 0) {
       orderBy[index] = item;
     } else {
       orderBy.push(item);
     }
-    return [...orderBy ];
+    return [...orderBy];
   }
-  return item.direction ? [ item ] : [];
+  return item.direction ? [item] : [];
 }
 
 function getOrderByInfo(orderBy) {
   const result = {};
-  each(orderBy, ({name, direction},
-                 index) => { result[name] = {direction, index : index + 1}; });
+  each(orderBy, ({ name, direction }, index) => {
+    result[name] = { direction, index: index + 1 };
+  });
   return result;
 }
 
 export function prepareColumns(columns, searchInput, orderBy, onOrderByChange) {
-  columns = filter(columns, 'visible');
-  columns = sortBy(columns, 'order');
+  columns = filter(columns, "visible");
+  columns = sortBy(columns, "order");
 
   const isMultiColumnSort = orderBy.length > 1;
   const orderByInfo = getOrderByInfo(orderBy);
 
   let tableColumns = map(columns, (column) => {
-    const isAscend = orderByInfo[column.name] &&
-                     (orderByInfo[column.name].direction === 'ascend');
-    const isDescend = orderByInfo[column.name] &&
-                      (orderByInfo[column.name].direction === 'descend');
+    const isAscend =
+      orderByInfo[column.name] &&
+      orderByInfo[column.name].direction === "ascend";
+    const isDescend =
+      orderByInfo[column.name] &&
+      orderByInfo[column.name].direction === "descend";
 
-    const sortColumnIndex = isMultiColumnSort && orderByInfo[column.name]
-                                ? orderByInfo[column.name].index
-                                : null;
+    const sortColumnIndex =
+      isMultiColumnSort && orderByInfo[column.name]
+        ? orderByInfo[column.name].index
+        : null;
 
     const result = {
       key: column.name, // set this because we don't use `dataIndex`
@@ -99,16 +103,27 @@ export function prepareColumns(columns, searchInput, orderBy, onOrderByChange) {
       title: (
         <React.Fragment>
           <Tooltip placement="top" title={column.title}>
-            <div className="table-visualization-heading" data-sort-column-index={sortColumnIndex}>{column.title}</div>
+            <div
+              className="table-visualization-heading"
+              data-sort-column-index={sortColumnIndex}
+            >
+              {column.title}
+            </div>
           </Tooltip>
           <span className="ant-table-column-sorter">
             <div className="ant-table-column-sorter-inner ant-table-column-sorter-inner-full">
               <Icon
-    className = {`ant-table-column-sorter-up ${isAscend ? 'on' : 'off'}`} type =
-        "caret-up"
-    theme = "filled" / > < Icon
-    className = {`ant-table-column-sorter-down ${
-        isDescend ? 'on' : 'off'}`} type = "caret-down"
+                className={`ant-table-column-sorter-up ${
+                  isAscend ? "on" : "off"
+                }`}
+                type="caret-up"
+                theme="filled"
+              />{" "}
+              <Icon
+                className={`ant-table-column-sorter-down ${
+                  isDescend ? "on" : "off"
+                }`}
+                type="caret-down"
                 theme="filled"
               />
             </div>
@@ -117,10 +132,11 @@ export function prepareColumns(columns, searchInput, orderBy, onOrderByChange) {
       ),
       onHeaderCell: () => ({
         className: cx(
-          'ant-table-column-has-actions ant-table-column-has-sorters',
-          { 'table-visualization-column-is-sorted': isAscend || isDescend },
+          "ant-table-column-has-actions ant-table-column-has-sorters",
+          { "table-visualization-column-is-sorted": isAscend || isDescend }
         ),
-        onClick: event => onOrderByChange(toggleOrderBy(column.name, orderBy, event.shiftKey)),
+        onClick: (event) =>
+          onOrderByChange(toggleOrderBy(column.name, orderBy, event.shiftKey)),
       }),
     };
 
@@ -131,16 +147,16 @@ export function prepareColumns(columns, searchInput, orderBy, onOrderByChange) {
       props: { className: `display-as-${column.displayAs}` },
     });
 
-                return result;
+    return result;
   });
 
   tableColumns.push({
-    key : '###Redash::Visualizations::Table::Spacer###',
-    dataIndex : null,
-    title : '',
-    className : 'table-visualization-spacer',
-    render : () => '',
-    onHeaderCell : () => ({className : 'table-visualization-spacer'}),
+    key: "###Redash::Visualizations::Table::Spacer###",
+    dataIndex: null,
+    title: "",
+    className: "table-visualization-spacer",
+    render: () => "",
+    onHeaderCell: () => ({ className: "table-visualization-spacer" }),
   });
 
   if (searchInput) {
@@ -149,39 +165,42 @@ export function prepareColumns(columns, searchInput, orderBy, onOrderByChange) {
     // property to it and set `colSpan` to 0 for every child cell except of the
     // 1st one - which should be expanded.
     tableColumns = map(
-        tableColumns,
-        ({title, align, key, onHeaderCell, ...rest}, index) => ({
-          key : key + '(parent)',
-          title,
-          align,
-          onHeaderCell,
-          children : [ {
+      tableColumns,
+      ({ title, align, key, onHeaderCell, ...rest }, index) => ({
+        key: key + "(parent)",
+        title,
+        align,
+        onHeaderCell,
+        children: [
+          {
             ...rest,
-            key : key + '(child)',
+            key: key + "(child)",
             align,
-            colSpan : index === 0 ? tableColumns.length : 0,
-            title : index === 0 ? searchInput : null,
-            onHeaderCell : () => ({className : 'table-visualization-search'}),
-          } ],
-        }));
+            colSpan: index === 0 ? tableColumns.length : 0,
+            title: index === 0 ? searchInput : null,
+            onHeaderCell: () => ({ className: "table-visualization-search" }),
+          },
+        ],
+      })
+    );
   }
 
   return tableColumns;
 }
 
 export function filterRows(rows, searchTerm, searchColumns) {
-  if ((searchTerm !== '') && (searchColumns.length > 0)) {
+  if (searchTerm !== "" && searchColumns.length > 0) {
     searchTerm = searchTerm.toUpperCase();
     const matchFields = map(searchColumns, (column) => {
       const initColumn = ColumnTypes[column.displayAs];
-      const {prepareData} = initColumn(column);
+      const { prepareData } = initColumn(column);
       return (row) => {
-        const {text} = prepareData(row);
+        const { text } = prepareData(row);
         return toString(text).toUpperCase().indexOf(searchTerm) >= 0;
       };
     });
 
-    return filter(rows, row => some(matchFields, match => match(row)));
+    return filter(rows, (row) => some(matchFields, (match) => match(row)));
   }
   return rows;
 }
@@ -195,15 +214,15 @@ function checkIsFloat(num) {
 }
 
 export function sortRows(rows, orderBy) {
-  if ((orderBy.length === 0) || (rows.length === 0)) {
+  if (orderBy.length === 0 || rows.length === 0) {
     return rows;
   }
 
-  const directions = {ascend : 1, descend : -1};
+  const directions = { ascend: 1, descend: -1 };
 
   // Create a copy of array before sorting, because .sort() will modify original
   // array
-  return [...rows ].sort((a, b) => {
+  return [...rows].sort((a, b) => {
     let va;
     let vb;
     for (let i = 0; i < orderBy.length; i += 1) {
