a46652f8e6c8a2ce804db0c9aba0cbfb058864af
Pedro Lages <55694628+pxd3v@users.noreply.github.com>
2020-09-06 12:10:48 -0300

    [fix] * improve code legibility

--- client/app/pages/dashboards/hooks/useAddDashboardToRecentDashboards.js
+++ client/app/pages/dashboards/hooks/useAddDashboardToRecentDashboards.js
@@ -6,19 +6,25 @@ export default function useAddDashboardToRecentDashboards(dashboardId) {
     const currentRecentDashboards = localStorage.getItem("recentDashboards");
     if (!currentRecentDashboards) {
       localStorage.setItem("recentDashboards", JSON.stringify([{ id: dashboardId, priority: 1 }]));
-    } else {
-      const parsedCurrentRecentDashboards = JSON.parse(currentRecentDashboards);
-      const currentRecentDashboardsPrioritys = map(parsedCurrentRecentDashboards, recentDashboard => recentDashboard.priority);
-      const recentDashboardsIncludesCurrentDashboard =
-        find(parsedCurrentRecentDashboards, recentDashboard => recentDashboard.id === dashboardId) !== undefined;
-      const biggestPriorityValue = reduce(currentRecentDashboardsPrioritys, (a, b) => Math.max(a, b));
-      if (recentDashboardsIncludesCurrentDashboard) {
-        const newRecentDashboards = map(parsedCurrentRecentDashboards, recentDashboard => recentDashboard.id === dashboardId ? { id: dashboardId, priority: biggestPriorityValue + 1 } : recentDashboard);
-        localStorage.setItem("recentDashboards", JSON.stringify(newRecentDashboards));
-      } else {
-        parsedCurrentRecentDashboards.push({ id: dashboardId, priority: biggestPriorityValue + 1 });
-        localStorage.setItem("recentDashboards", JSON.stringify(parsedCurrentRecentDashboards));
-      }
+      return;
     }
+
+    const parsedCurrentRecentDashboards = JSON.parse(currentRecentDashboards);
+    const currentRecentDashboardsPrioritys = map(
+      parsedCurrentRecentDashboards,
+      recentDashboard => recentDashboard.priority
+    );
+    const recentDashboardsIncludesCurrentDashboard =
+      find(parsedCurrentRecentDashboards, recentDashboard => recentDashboard.id === dashboardId) !== undefined;
+    const biggestPriorityValue = reduce(currentRecentDashboardsPrioritys, (a, b) => Math.max(a, b));
+    const dashboardsToSave = recentDashboardsIncludesCurrentDashboard
+      ? map(parsedCurrentRecentDashboards, recentDashboard =>
+          recentDashboard.id === dashboardId ? { id: dashboardId, priority: biggestPriorityValue + 1 } : recentDashboard
+        )
+      : (function() {
+          parsedCurrentRecentDashboards.push({ id: dashboardId, priority: biggestPriorityValue + 1 });
+          return parsedCurrentRecentDashboards;
+        })();
+    localStorage.setItem("recentDashboards", JSON.stringify(dashboardsToSave));
   }, [dashboardId]);
 }
--- client/app/pages/queries/hooks/useAddQueryToRecentQueries.js
+++ client/app/pages/queries/hooks/useAddQueryToRecentQueries.js
@@ -6,21 +6,20 @@ export default function useAddQueryToRecentQueries(query) {
     const currentRecentQueries = localStorage.getItem("recentQueries");
     if (!currentRecentQueries) {
       localStorage.setItem("recentQueries", JSON.stringify([{ id: query.id, priority: 1 }]));
-    } else {
-      const parsedCurrentRecentQueries = JSON.parse(currentRecentQueries);
-      const currentRecentQueriesPrioritys = map(parsedCurrentRecentQueries, recentQuery => recentQuery.priority);
-      const recentQueriesIncludesCurrentQuery =
-        find(parsedCurrentRecentQueries, recentQuery => recentQuery.id === query.id) !== undefined;
-      const biggestPriorityValue = reduce(currentRecentQueriesPrioritys, (a, b) => Math.max(a, b));
-      if (recentQueriesIncludesCurrentQuery) {
-        const newRecentQueries = map(parsedCurrentRecentQueries, recentQuery =>
-          recentQuery.id === query.id ? { id: query.id, priority: biggestPriorityValue + 1 } : recentQuery
-        );
-        localStorage.setItem("recentQueries", JSON.stringify(newRecentQueries));
-      } else {
-        parsedCurrentRecentQueries.push({ id: query.id, priority: biggestPriorityValue + 1 });
-        localStorage.setItem("recentQueries", JSON.stringify(parsedCurrentRecentQueries));
-      }
+      return;
     }
+    const parsedCurrentRecentQueries = JSON.parse(currentRecentQueries);
+    const currentRecentQueriesPrioritys = map(parsedCurrentRecentQueries, recentQuery => recentQuery.priority);
+    const recentQueriesIncludesCurrentQuery =
+      find(parsedCurrentRecentQueries, recentQuery => recentQuery.id === query.id) !== undefined;
+    const biggestPriorityValue = reduce(currentRecentQueriesPrioritys, (a, b) => Math.max(a, b));
+    const queriesToSave = recentQueriesIncludesCurrentQuery ? map(parsedCurrentRecentQueries, recentQuery =>
+      recentQuery.id === query.id ? { id: query.id, priority: biggestPriorityValue + 1 } : recentQuery
+    ) :
+    function() {
+      parsedCurrentRecentQueries.push({ id: query.id, priority: biggestPriorityValue + 1 });
+      return parsedCurrentRecentQueries;
+    }()
+    localStorage.setItem("recentQueries", JSON.stringify(queriesToSave));
   }, [query.id]);
 }
