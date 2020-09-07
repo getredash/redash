6d0139f8cf5b58518024ecbcec62dc1f1ba38560
Restyled.io <commits@restyled.io>
2020-09-07 17:03:13 +0000

    Restyled by isort

--- redash/models/base.py
+++ redash/models/base.py
@@ -3,7 +3,7 @@ import functools
 from flask_sqlalchemy import BaseQuery, SQLAlchemy
 from sqlalchemy.orm import object_session
 from sqlalchemy.pool import NullPool
-from sqlalchemy_searchable import make_searchable, vectorizer, SearchQueryMixin
+from sqlalchemy_searchable import SearchQueryMixin, make_searchable, vectorizer
 
 from redash import settings
 from redash.utils import json_dumps
