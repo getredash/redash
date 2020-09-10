00279b1421e9c4ad54de4e0a78be2fcd54e17292
Restyled.io <commits@restyled.io>
2020-09-10 22:21:17 +0000

    Restyled by clang-format

--- client/app/visualizations/gridmap/index.js
+++ client/app/visualizations/gridmap/index.js
@@ -1,35 +1,35 @@
-import { registerVisualization } from '@/visualizations';
+import {registerVisualization} from '@/visualizations';
 
-import Renderer from './Renderer';
 import Editor from './Editor';
+import Renderer from './Renderer';
 
 const DEFAULT_OPTIONS = {
-  lonColName: 'lon',
-  latColName: 'lat',
-  tooltipLabel: 'Grid Count',
-  cellSize: 20,
-  maxCount: 500,
-  setMapCenter: false,
-  setMaxDomain: false,
-  zoom: 7,
-  centerLat: 28.54,
-  centerLon: 77.25,
-  bearing: 0,
-  pitch: 0,
-  isCenterSet: false,
+  lonColName : 'lon',
+  latColName : 'lat',
+  tooltipLabel : 'Grid Count',
+  cellSize : 20,
+  maxCount : 500,
+  setMapCenter : false,
+  setMaxDomain : false,
+  zoom : 7,
+  centerLat : 28.54,
+  centerLon : 77.25,
+  bearing : 0,
+  pitch : 0,
+  isCenterSet : false,
 };
 
 export default function init() {
   registerVisualization({
-    type: 'GRIDMAP',
-    name: 'Map (Screen Grid)',
-    getOptions: options => ({ ...DEFAULT_OPTIONS, ...options }),
+    type : 'GRIDMAP',
+    name : 'Map (Screen Grid)',
+    getOptions : options => ({...DEFAULT_OPTIONS, ...options}),
     Renderer,
     Editor,
 
-    defaultColumns: 3,
-    defaultRows: 8,
-    minColumns: 2,
+    defaultColumns : 3,
+    defaultRows : 8,
+    minColumns : 2,
   });
 }
 
--- client/app/visualizations/hexmap/index.js
+++ client/app/visualizations/hexmap/index.js
@@ -1,36 +1,36 @@
-import { registerVisualization } from '@/visualizations';
+import {registerVisualization} from '@/visualizations';
 
-import Renderer from './Renderer';
 import Editor from './Editor';
+import Renderer from './Renderer';
 
 const DEFAULT_OPTIONS = {
-  lonColName: 'lon',
-  latColName: 'lat',
-  radius: 2000,
-  tooltipLabel: 'Hexagon Count',
-  elevation: 0,
-  maxCount: 500,
-  setMapCenter: false,
-  setMaxDomain: false,
-  zoom: 7,
-  centerLat: 28.54,
-  centerLon: 77.25,
-  bearing: 0,
-  pitch: 0,
-  isCenterSet: false,
+  lonColName : 'lon',
+  latColName : 'lat',
+  radius : 2000,
+  tooltipLabel : 'Hexagon Count',
+  elevation : 0,
+  maxCount : 500,
+  setMapCenter : false,
+  setMaxDomain : false,
+  zoom : 7,
+  centerLat : 28.54,
+  centerLon : 77.25,
+  bearing : 0,
+  pitch : 0,
+  isCenterSet : false,
 };
 
 export default function init() {
   registerVisualization({
-    type: 'HEXMAP',
-    name: 'Map (Hexagon)',
-    getOptions: options => ({ ...DEFAULT_OPTIONS, ...options }),
+    type : 'HEXMAP',
+    name : 'Map (Hexagon)',
+    getOptions : options => ({...DEFAULT_OPTIONS, ...options}),
     Renderer,
     Editor,
 
-    defaultColumns: 3,
-    defaultRows: 8,
-    minColumns: 2,
+    defaultColumns : 3,
+    defaultRows : 8,
+    minColumns : 2,
   });
 }
 
