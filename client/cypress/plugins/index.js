8cbff164765d564fa839319fc386dc1fbb62060b
Gabriel Dutra <nesk.frz@gmail.com>
2020-09-12 15:12:13 -0300

    Add Code Coverage information to Cypress

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -1,5 +1,8 @@
 const percyHealthCheck = require("@percy/cypress/task"); // eslint-disable-line import/no-extraneous-dependencies, import/no-unresolved
 
-module.exports = on => {
+module.exports = (on, config) => {
+  require("@cypress/code-coverage/task")(on, config);
   on("task", percyHealthCheck);
+
+  return config;
 };
