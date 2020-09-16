d4ef21e1d21e9a72fad3d2e01455f52160a44893
Omer Lachish <omer@rauchy.net>
2020-09-16 14:33:11 +0300

    use axios' built-in cookie to header copy mechanism

--- client/app/services/axios.js
+++ client/app/services/axios.js
@@ -1,10 +1,11 @@
 import axiosLib from "axios";
 import { Auth } from "@/services/auth";
 import qs from "query-string";
-import Cookies from "js-cookie";
 
 export const axios = axiosLib.create({
   paramsSerializer: params => qs.stringify(params),
+  xsrfCookieName: "csrf_token",
+  xsrfHeaderName: "X-CSRF-TOKEN",
 });
 
 const getData = ({ data }) => data;
@@ -15,11 +16,6 @@ axios.interceptors.request.use(config => {
   const apiKey = Auth.getApiKey();
   if (apiKey) {
     config.headers.Authorization = `Key ${apiKey}`;
-  }
-
-  const csrfToken = Cookies.get("csrf_token");
-  if (csrfToken) {
-    config.headers.common["X-CSRF-TOKEN"] = csrfToken;
   }
 
   return config;
