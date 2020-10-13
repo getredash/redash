ec51a365c54b72bc1f51e5d4c58282c6775b3c5e
Gabriel Dutra <nesk.frz@gmail.com>
2020-10-13 14:32:24 -0300

    Update dynamic dates to allow the non-param use

--- client/app/components/dynamic-parameters/DateParameter.jsx
+++ client/app/components/dynamic-parameters/DateParameter.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import classNames from "classnames";
-import moment from "moment";
-import { includes } from "lodash";
-import { isDynamicDate, getDynamicDateFromString } from "@/services/parameters/DateParameter";
-import DateInput from "@/components/DateInput";
-import DateTimeInput from "@/components/DateTimeInput";
-import DynamicButton from "@/components/dynamic-parameters/DynamicButton";
-
-import "./DynamicParameters.less";
+import { getDynamicDateFromString } from "@/services/parameters/DateParameter";
+import DynamicDatePicker from "@/components/dynamic-parameters/DynamicDatePicker";
 
 const DYNAMIC_DATE_OPTIONS = [
   {
@@ -29,87 +22,24 @@ const DYNAMIC_DATE_OPTIONS = [
   },
 ];
 
-class DateParameter extends React.Component {
-  static propTypes = {
-    type: PropTypes.string,
-    className: PropTypes.string,
-    value: PropTypes.any, // eslint-disable-line react/forbid-prop-types
-    parameter: PropTypes.any, // eslint-disable-line react/forbid-prop-types
-    onSelect: PropTypes.func,
-  };
-
-  static defaultProps = {
-    type: "",
-    className: "",
-    value: null,
-    parameter: null,
-    onSelect: () => {},
-  };
-
-  constructor(props) {
-    super(props);
-    this.dateComponentRef = React.createRef();
-  }
-
-  onDynamicValueSelect = dynamicValue => {
-    const { onSelect, parameter } = this.props;
-    if (dynamicValue === "static") {
-      const parameterValue = parameter.getExecutionValue();
-      if (parameterValue) {
-        onSelect(moment(parameterValue));
-      } else {
-        onSelect(null);
-      }
-    } else {
-      onSelect(dynamicValue.value);
-    }
-    // give focus to the DatePicker to get keyboard shortcuts to work
-    this.dateComponentRef.current.focus();
-  };
-
-  render() {
-    const { type, value, className, onSelect } = this.props;
-    const hasDynamicValue = isDynamicDate(value);
-    const isDateTime = includes(type, "datetime");
-
-    const additionalAttributes = {};
-
-    let DateComponent = DateInput;
-    if (isDateTime) {
-      DateComponent = DateTimeInput;
-      if (includes(type, "with-seconds")) {
-        additionalAttributes.withSeconds = true;
-      }
-    }
-
-    if (moment.isMoment(value) || value === null) {
-      additionalAttributes.value = value;
-    }
-
-    if (hasDynamicValue) {
-      const dynamicDate = value;
-      additionalAttributes.placeholder = dynamicDate && dynamicDate.name;
-      additionalAttributes.value = null;
-    }
-
-    return (
-      <div className="date-parameter">
-        <DateComponent
-          ref={this.dateComponentRef}
-          className={classNames("redash-datepicker", { "dynamic-value": hasDynamicValue }, className)}
-          onSelect={onSelect}
-          suffixIcon={null}
-          {...additionalAttributes}
-        />
-        <DynamicButton
-          options={DYNAMIC_DATE_OPTIONS}
-          selectedDynamicValue={hasDynamicValue ? value : null}
-          enabled={hasDynamicValue}
-          onSelect={this.onDynamicValueSelect}
-        />
-      </div>
-    );
-  }
+function DateParameter(props) {
+  return <DynamicDatePicker dynamicButtonOptions={{ options: DYNAMIC_DATE_OPTIONS }} {...props} />;
 }
+
+DateParameter.propTypes = {
+  type: PropTypes.string,
+  className: PropTypes.string,
+  value: PropTypes.any, // eslint-disable-line react/forbid-prop-types
+  parameter: PropTypes.any, // eslint-disable-line react/forbid-prop-types
+  onSelect: PropTypes.func,
+};
+
+DateParameter.defaultProps = {
+  type: "",
+  className: "",
+  value: null,
+  parameter: null,
+  onSelect: () => {},
+};
 
 export default DateParameter;
