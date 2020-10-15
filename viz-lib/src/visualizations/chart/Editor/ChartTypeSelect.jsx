8a6524c1bae3c94259a37194b0b86d42f51f7cd4
Rafael Wendel <rafawendel2010@gmail.com>
2020-10-15 15:34:38 -0300

    Add horizontal bar chart (#5154)
    * added bar chart boilerplate
    
    * added x/y manipulation
    
    * replaced x/y management to inner series preparer
    
    * added tests
    
    * moved axis inversion to all charts series
    
    * removed line and area
    
    * inverted labels ui
    
    * removed normalizer check, simplified inverted axes check
    
    * finished working hbar
    
    * minor review
    
    * added conditional title to YAxis
    
    * generalized horizontal chart for line charts, resetted state on globalSeriesType change
    
    * fixed updates
    
    * fixed updates to layout
    
    * fixed minor issues
    
    * removed right Y axis when axes inverted
    
    * ran prettier
    
    * fixed updater function conflict and misuse of getOptions
    
    * renamed inverted to swapped
    
    * created mappingtypes for swapped columns
    
    * removed unused import
    
    * minor polishing
    
    * improved series behaviour in h-bar
    
    * minor fix
    
    * added basic filter to ChartTypeSelect
    
    * final setup of filtered chart types
    
    * Update viz-lib/src/components/visualizations/editor/createTabbedEditor.jsx
    
    * added proptypes and renamed ChartTypeSelect props
    
    * Add missing import
    
    * fixed import, moved result array to global scope
    
    * merged import
    
    * clearer naming in ChartTypeSelect
    
    * better lodash map syntax
    
    * fixed global modification
    
    * moved result inside useMemo
    
    Co-authored-by: Gabriel Dutra <nesk.frz@gmail.com>
    Co-authored-by: Levko Kravets <levko.ne@gmail.com>

--- viz-lib/src/visualizations/chart/Editor/ChartTypeSelect.jsx
+++ viz-lib/src/visualizations/chart/Editor/ChartTypeSelect.jsx
@@ -1,23 +1,30 @@
-import { map } from "lodash";
+import { filter, includes, map } from "lodash";
 import React, { useMemo } from "react";
+import PropTypes from "prop-types";
 import { Select } from "@/components/visualizations/editor";
 import { visualizationsSettings } from "@/visualizations/visualizationsSettings";
 
-export default function ChartTypeSelect(props) {
+const allChartTypes = [
+  { type: "line", name: "Line", icon: "line-chart" },
+  { type: "column", name: "Bar", icon: "bar-chart" },
+  { type: "area", name: "Area", icon: "area-chart" },
+  { type: "pie", name: "Pie", icon: "pie-chart" },
+  { type: "scatter", name: "Scatter", icon: "circle-o" },
+  { type: "bubble", name: "Bubble", icon: "circle-o" },
+  { type: "heatmap", name: "Heatmap", icon: "th" },
+  { type: "box", name: "Box", icon: "square-o" },
+];
+
+export default function ChartTypeSelect({ hiddenChartTypes, ...props }) {
   const chartTypes = useMemo(() => {
-    const result = [
-      { type: "line", name: "Line", icon: "line-chart" },
-      { type: "column", name: "Bar", icon: "bar-chart" },
-      { type: "area", name: "Area", icon: "area-chart" },
-      { type: "pie", name: "Pie", icon: "pie-chart" },
-      { type: "scatter", name: "Scatter", icon: "circle-o" },
-      { type: "bubble", name: "Bubble", icon: "circle-o" },
-      { type: "heatmap", name: "Heatmap", icon: "th" },
-      { type: "box", name: "Box", icon: "square-o" },
-    ];
+    const result = [...allChartTypes];
 
     if (visualizationsSettings.allowCustomJSVisualizations) {
       result.push({ type: "custom", name: "Custom", icon: "code" });
+    }
+
+    if (hiddenChartTypes.length > 0) {
+      return filter(result, ({ type }) => !includes(hiddenChartTypes, type));
     }
 
     return result;
@@ -34,3 +41,11 @@ export default function ChartTypeSelect(props) {
     </Select>
   );
 }
+
+ChartTypeSelect.propTypes = {
+  hiddenChartTypes: PropTypes.arrayOf(PropTypes.oneOf(map(allChartTypes, "type"))),
+};
+
+ChartTypeSelect.defaultProps = {
+  hiddenChartTypes: [],
+};
