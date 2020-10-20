866b8ffd656ded63009594cb483a47e8d10e92ca
Josh Bohde <josh.bohde@sezzle.com>
2020-10-20 09:27:54 -0500

    Fix imports based upon review

--- redash/tasks/worker.py
+++ redash/tasks/worker.py
@@ -4,7 +4,7 @@ import signal
 import time
 from redash import statsd_client
 from rq import Queue as BaseQueue, get_current_job
-from rq.worker import HerokuWorker as BaseWorker
+from rq.worker import HerokuWorker # HerokuWorker implements graceful shutdown on SIGTERM
 from rq.utils import utcnow
 from rq.timeouts import UnixSignalDeathPenalty, HorseMonitorTimeoutException
 from rq.job import Job as BaseJob, JobStatus
@@ -41,7 +41,7 @@ class RedashQueue(StatsdRecordingQueue, CancellableQueue):
     pass
 
 
-class StatsdRecordingWorker(BaseWorker):
+class StatsdRecordingWorker(HerokuWorker):
     """
     RQ Worker Mixin that overrides `execute_job` to increment/modify metrics via Statsd
     """
@@ -59,7 +59,7 @@ class StatsdRecordingWorker(BaseWorker):
                 statsd_client.incr("rq.jobs.failed.{}".format(queue.name))
 
 
-class HardLimitingWorker(BaseWorker):
+class HardLimitingWorker(HerokuWorker):
     """
     RQ's work horses enforce time limits by setting a timed alarm and stopping jobs
     when they reach their time limits. However, the work horse may be entirely blocked
