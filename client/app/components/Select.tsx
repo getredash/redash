00eb3ff01db63023587e3109f7cfea6c0bc9299c
Rafael Wendel <rafawendel2010@gmail.com>
2020-10-21 16:43:26 -0300

    added check for property path

--- client/app/components/Select.tsx
+++ client/app/components/Select.tsx
@@ -1,11 +1,13 @@
 import React, { useRef, useState, useEffect } from "react";
-import { sortBy, map } from "lodash";
+import { sortBy, get } from "lodash";
 import AntdSelect, { SelectProps } from "antd/lib/select";
 
 function getItemOfPercentile<T>(list: Array<T>, percentile: number, sortIteratee: string | string[] = "length") {
+  if (get(list[0], sortIteratee, null) === null) {
+    return;
+  }
   const sortedList = sortBy(list, sortIteratee);
   const percentileIndex = Math.ceil((list.length - 1) * (percentile / 100));
-
   return sortedList[percentileIndex];
 }
 
@@ -18,8 +20,10 @@ function Select({ style, options, ...props }: SelectProps<any>): JSX.Element {
       if (selectEl.current) {
         fontSize = parseFloat(getComputedStyle((selectEl.current as unknown) as Element).fontSize);
       }
-      const width = String(getItemOfPercentile(options, 80, "label.length").label).length;
-      setDropdownMatchSelectWidth(width);
+      const itemOf80thPercentile = getItemOfPercentile(options, 80, "label.length");
+      if (itemOf80thPercentile) {
+        setDropdownMatchSelectWidth(String(itemOf80thPercentile.label).length);
+      }
     }
   }, [options]);
 
