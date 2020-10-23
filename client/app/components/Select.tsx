a992edcaf37add57bc5b3d27a103c96963177ed8
Rafael Wendel <rafawendel2010@gmail.com>
2020-10-23 12:08:01 -0300

    replaced to percentiles to max value

--- client/app/components/Select.tsx
+++ client/app/components/Select.tsx
@@ -1,26 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { sortBy, get } from "lodash";
+import { maxBy } from "lodash";
 import AntdSelect, { SelectProps } from "antd/lib/select";
 import { calculateTextWidth } from "@/lib/calculateTextWidth";
-
-function getItemOfPercentile<T>(list: Array<T>, percentile: number, sortIteratee = "length") {
-  if (get(list[0], sortIteratee, null) === null) {
-    return;
-  }
-  const sortedList = sortBy(list, sortIteratee);
-  const percentileIndex = Math.ceil((list.length - 1) * (percentile / 100));
-  return sortedList[percentileIndex];
-}
 
 function Select({ options, ...props }: SelectProps<any>): JSX.Element {
   const [dropdownMatchSelectWidth, setDropdownMatchSelectWidth] = useState<number | boolean>(true);
   useEffect(() => {
     if (options && options.length > 400) {
-      const itemOf80thPercentile = getItemOfPercentile(options, 80, "label.length");
+      const largestOpt = maxBy(options, "label.length");
 
-      if (itemOf80thPercentile) {
+      if (largestOpt) {
         const offset = 40;
-        const optionText = String(itemOf80thPercentile.label);
+        const optionText = String(largestOpt.label);
         const width = calculateTextWidth(optionText);
         if (width) {
           setDropdownMatchSelectWidth(width + offset);
