29a9114b5ef6675b4e26668b85e11d2a6a4c76fb
Gabriel Dutra <nesk.frz@gmail.com>
2020-11-05 14:54:49 -0300

    Policy updates

--- client/app/pages/queries/hooks/useQueryFlags.js
+++ client/app/pages/queries/hooks/useQueryFlags.js
@@ -1,6 +1,7 @@
 import { isNil, isEmpty } from "lodash";
 import { useMemo } from "react";
 import { currentUser } from "@/services/auth";
+import { policy } from "@/services/policy";
 
 export default function useQueryFlags(query, dataSource = null) {
   dataSource = dataSource || { view_only: true };
@@ -15,10 +16,11 @@ export default function useQueryFlags(query, dataSource = null) {
       // permissions flags
       canCreate: currentUser.hasPermission("create_query"),
       canView: currentUser.hasPermission("view_query"),
-      canEdit: currentUser.hasPermission("edit_query") && query.can_edit,
+      canEdit: currentUser.hasPermission("edit_query") && policy.canEdit(query),
       canViewSource: currentUser.hasPermission("view_source"),
       canExecute:
         !isEmpty(query.query) &&
+        policy.canRun(query) &&
         (query.is_safe || (currentUser.hasPermission("execute_query") && !dataSource.view_only)),
       canFork: currentUser.hasPermission("edit_query") && !dataSource.view_only,
       canSchedule: currentUser.hasPermission("schedule_query"),
