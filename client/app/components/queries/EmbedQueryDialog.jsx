83ec0592c3a3011f91291ba401fb6f1521a8911a
William Sheu <sheu.william@gmail.com>
2021-01-08 21:12:49 -0800

    Show safe embed links for queries w/ text params

--- client/app/components/queries/EmbedQueryDialog.jsx
+++ client/app/components/queries/EmbedQueryDialog.jsx
@@ -27,9 +27,10 @@ class EmbedQueryDialog extends React.Component {
   constructor(props) {
     super(props);
     const { query, visualization } = props;
-    this.embedUrl = `${clientConfig.basePath}embed/query/${query.id}/visualization/${visualization.id}?api_key=${
-      query.api_key
-    }&${query.getParameters().toUrlParams()}`;
+    const keyParam = query.is_safe ? `api_key=${query.api_key}&` : "";
+    this.embedUrl = `${clientConfig.basePath}embed/query/${query.id}/visualization/${
+      visualization.id
+    }?${keyParam}${query.getParameters().toUrlParams()}`;
 
     if (window.snapshotUrlBuilder) {
       this.snapshotUrl = window.snapshotUrlBuilder(query, visualization);
@@ -46,60 +47,63 @@ class EmbedQueryDialog extends React.Component {
         className="embed-query-dialog"
         title="Embed Query"
         footer={<Button onClick={dialog.dismiss}>Close</Button>}>
-        {query.is_safe ? (
+        <React.Fragment>
+          {!query.is_safe ? (
+            <div className="m-b-30">
+              <Alert
+                message="Text parameter queries are unsafe, so cannot be publicly shared."
+                type="warning"
+                data-test="EmbedErrorAlert"
+              />
+            </div>
+          ) : null}
           <React.Fragment>
-            <h5 className="m-t-0">Public URL</h5>
+            <h5 className="m-t-0">{query.is_safe ? "Public URL" : "URL (requires login)"}</h5>
             <div className="m-b-30">
               <CodeBlock data-test="EmbedIframe" copyable>
                 {this.embedUrl}
               </CodeBlock>
             </div>
-            <h5 className="m-t-0">IFrame Embed</h5>
-            <div>
-              <CodeBlock copyable>
-                {`<iframe src="${this.embedUrl}" width="${iframeWidth}" height="${iframeHeight}"></iframe>`}
-              </CodeBlock>
-              <Form className="m-t-10" layout="inline">
-                <Form.Item>
-                  <Checkbox
-                    checked={enableChangeIframeSize}
-                    onChange={e => this.setState({ enableChangeIframeSize: e.target.checked })}
-                  />
-                </Form.Item>
-                <Form.Item label="Width">
-                  <InputNumber
-                    className="size-input"
-                    value={iframeWidth}
-                    onChange={value => this.setState({ iframeWidth: value })}
-                    size="small"
-                    disabled={!enableChangeIframeSize}
-                  />
-                </Form.Item>
-                <Form.Item label="Height">
-                  <InputNumber
-                    className="size-input"
-                    value={iframeHeight}
-                    onChange={value => this.setState({ iframeHeight: value })}
-                    size="small"
-                    disabled={!enableChangeIframeSize}
-                  />
-                </Form.Item>
-              </Form>
-            </div>
-            {this.snapshotUrl && (
-              <React.Fragment>
-                <h5>Image Embed</h5>
-                <CodeBlock copyable>{this.snapshotUrl}</CodeBlock>
-              </React.Fragment>
-            )}
           </React.Fragment>
-        ) : (
-          <Alert
-            message="Currently it is not possible to embed queries that contain text parameters."
-            type="error"
-            data-test="EmbedErrorAlert"
-          />
-        )}
+          <h5 className="m-t-0">IFrame Embed</h5>
+          <div>
+            <CodeBlock copyable>
+              {`<iframe src="${this.embedUrl}" width="${iframeWidth}" height="${iframeHeight}"></iframe>`}
+            </CodeBlock>
+            <Form className="m-t-10" layout="inline">
+              <Form.Item>
+                <Checkbox
+                  checked={enableChangeIframeSize}
+                  onChange={(e) => this.setState({ enableChangeIframeSize: e.target.checked })}
+                />
+              </Form.Item>
+              <Form.Item label="Width">
+                <InputNumber
+                  className="size-input"
+                  value={iframeWidth}
+                  onChange={(value) => this.setState({ iframeWidth: value })}
+                  size="small"
+                  disabled={!enableChangeIframeSize}
+                />
+              </Form.Item>
+              <Form.Item label="Height">
+                <InputNumber
+                  className="size-input"
+                  value={iframeHeight}
+                  onChange={(value) => this.setState({ iframeHeight: value })}
+                  size="small"
+                  disabled={!enableChangeIframeSize}
+                />
+              </Form.Item>
+            </Form>
+          </div>
+          {this.snapshotUrl && (
+            <React.Fragment>
+              <h5>Image Embed</h5>
+              <CodeBlock copyable>{this.snapshotUrl}</CodeBlock>
+            </React.Fragment>
+          )}
+        </React.Fragment>
       </Modal>
     );
   }
