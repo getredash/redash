c288fbc97e0f628e2945dbd953065964ebd7394c
Rafael Wendel <rafawendel2010@gmail.com>
2021-02-05 20:19:09 -0300

    Improved accessibility of link component

--- client/app/components/Link.tsx
+++ client/app/components/Link.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Button, { ButtonProps } from "antd/lib/button";
 
-function DefaultLinkComponent(props: React.AnchorHTMLAttributes<any>) {
-  return <a {...props} />; // eslint-disable-line jsx-a11y/anchor-has-content
+type AnchorButtonProps = React.AnchorHTMLAttributes<any> & React.ButtonHTMLAttributes<any>;
+
+// TODO: adapt click handlers to keypress events
+function DefaultLinkComponent({ type, role, ...props }: AnchorButtonProps) {
+  return role === "button" ? (
+    <button type="button" style={{ all: "unset" }} {...props} />
+  ) : (
+    <a role={role} type={type} {...props} /> // eslint-disable-line jsx-a11y/anchor-has-content
+  );
 }
 
 Link.Component = DefaultLinkComponent;
 
-function Link({ tabIndex = 0, ...props }: React.AnchorHTMLAttributes<any>) {
-  return <Link.Component tabIndex={tabIndex} {...props} />;
+function Link({ tabIndex = 0, ...props }: AnchorButtonProps) {
+  return !props.href && !props.role ? (
+    <Link.Component tabIndex={tabIndex} role="link" {...props} />
+  ) : (
+    <Link.Component tabIndex={tabIndex} {...props} />
+  );
 }
 
 function DefaultButtonLinkComponent(props: ButtonProps) {
