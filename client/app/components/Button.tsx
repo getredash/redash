9e28d27827b6ca88ae46e0444ca3fbc7e653ed1c
Rafael Wendel <rafawendel2010@gmail.com>
2021-02-10 20:07:57 -0300

    Fixed Button for Button.Group uses and classNames

--- client/app/components/Button.tsx
+++ client/app/components/Button.tsx
@@ -1,4 +1,5 @@
 import AntdButton, { ButtonProps as AntdButtonProps } from "antd/lib/button";
+import classNames from "classnames";
 import React from "react";
 
 import "./Button.less";
@@ -7,8 +8,14 @@ interface ButtonProps extends Omit<AntdButtonProps, "type"> {
   type?: AntdButtonProps["type"] | "plain";
 }
 
-function Button({ type, ...props }: ButtonProps) {
-  return type === "plain" ? <button className="btn-plain" {...props} /> : <AntdButton type={type} {...props} />;
+function Button({ type, className, ...props }: ButtonProps) {
+  return type === "plain" ? (
+    <button className={classNames("btn-plain", "clickable", className)} type="button" {...props} />
+  ) : (
+    <AntdButton type={type} {...props} />
+  );
 }
+
+Button.Group = AntdButton.Group;
 
 export default Button;
