3614dbe379c4d90f046cb301251909a628c4d965
Rafael Wendel <rafawendel2010@gmail.com>
2021-03-23 15:59:29 -0300

    refactor: split external link into own component

--- client/app/components/Link.tsx
+++ client/app/components/Link.tsx
@@ -3,7 +3,10 @@ import Button, { ButtonProps as AntdButtonProps } from "antd/lib/button";
 
 interface LinkProps extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, "role" | "type" | "target"> {
   href: string;
-  type: "regular" | "external";
+}
+
+interface ExternalLinkProps extends LinkProps {
+  children: string;
   externalLink?: {
     icon: JSX.Element;
     alt: string;
@@ -20,24 +23,27 @@ function DefaultLinkComponent({ children, ...props }: React.AnchorHTMLAttributes
 
 Link.Component = DefaultLinkComponent;
 
-function Link({ tabIndex = 0, type, children, externalLink, ...props }: LinkProps) {
-  const isLinkExternal = type === "external";
-  const areChildrenText = typeof children === "string";
-  const externalLinkProps = isLinkExternal ? { target: "_blank", rel: "noopener noreferrer" } : {};
-
+function Link({ tabIndex = 0, children, ...props }: LinkProps) {
   return (
-    <Link.Component tabIndex={tabIndex} {...externalLinkProps} {...props}>
+    <Link.Component tabIndex={tabIndex} {...props}>
       {children}
-      {isLinkExternal && areChildrenText && (
-        <>
-          {" "}
-          {externalLink?.icon || <i className="fa fa-external-link" aria-hidden="true" />}
-          <span className="sr-only">{externalLink?.alt || "(opens in a new tab)"}</span>
-        </>
-      )}
     </Link.Component>
   );
 }
+
+function ExternalLink({ tabIndex = 0, children, externalLink, ...props }: ExternalLinkProps) {
+  const externalLinkProps = { target: "_blank", rel: "noopener noreferrer" };
+  const icon = externalLink?.icon || <i className="fa fa-external-link" aria-hidden="true" />;
+  const alt = externalLink?.alt || "(opens in a new tab)";
+
+  return (
+    <Link.Component tabIndex={tabIndex} {...externalLinkProps} {...props}>
+      {children} {icon} <span className="sr-only">{alt}</span>
+    </Link.Component>
+  );
+}
+
+Link.External = ExternalLink;
 
 // Ant Button will render an <a> if href is present.
 function DefaultButtonLinkComponent(props: ButtonProps) {
