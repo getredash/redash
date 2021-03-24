70681294a3363508be7bf8e65cea2e5b60751c5b
Sebastian Tramp <mail@sebastian.tramp.name>
2021-03-24 08:15:24 +0100

    Query Runner: eccenca Corporate Memory (SPARQL) - query RDF / Linked Data Knowledge Graphs with redash (#5415)
    * add Corporate Memory Runner based on cmempy 21.2.3
    
    * fix code style
    
    * apply some code nice ups
    
    * use extendedEnum, boolean and extra_options for schema description
    
    * use lower case sorting for data source types list
    
    This correctly orders data source names which starts with lower
    chars (such as eccenca Corporate Memory)
    
    * add missing dblogo

--- redash/settings/__init__.py
+++ redash/settings/__init__.py
@@ -372,6 +372,7 @@ default_query_runners = [
     "redash.query_runner.exasol",
     "redash.query_runner.cloudwatch",
     "redash.query_runner.cloudwatch_insights",
+    "redash.query_runner.corporate_memory",
 ]
 
 enabled_query_runners = array_from_string(
