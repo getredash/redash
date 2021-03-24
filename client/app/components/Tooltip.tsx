088c4ee368025d60e5631adcf434ba72107985fb
Rafael Wendel <rafawendel2010@gmail.com>
2021-03-24 15:04:16 -0300

    feature: add focus trigger

--- client/app/components/Tooltip.tsx
+++ client/app/components/Tooltip.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import AntTooltip, { TooltipProps } from "antd/lib/tooltip";
 
 export default function Tooltip(props: TooltipProps) {
-  return <AntTooltip {...props} />;
+  return <AntTooltip trigger={["hover", "focus"]} {...props} />;
 }
