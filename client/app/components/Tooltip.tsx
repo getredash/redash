871603d423c092c8e62ed70548e035d276972535
Rafael Wendel <rafawendel2010@gmail.com>
2021-03-24 18:44:33 -0300

    feature: add live reagions to tooltip

--- client/app/components/Tooltip.tsx
+++ client/app/components/Tooltip.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import AntTooltip, { TooltipProps } from "antd/lib/tooltip";
 
-export default function Tooltip(props: TooltipProps) {
-  return <AntTooltip trigger={["hover", "focus"]} {...props} />;
+export default function Tooltip({ title, ...restProps }: TooltipProps) {
+  const liveTitle = (
+    <span role="status" aria-live="assertive" aria-relevant="additions">
+      {title}
+    </span>
+  );
+
+  return <AntTooltip trigger={["hover", "focus"]} title={liveTitle} {...restProps} />;
 }
