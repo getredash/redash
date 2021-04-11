a27bab988dd472a6f9cd9f26de8ddc3a7966af09
Nolan Nichols <nnichols@mazetx.com>
2021-04-11 13:47:26 -0700

    add: register sparql endpoint runner

--- redash/settings/__init__.py
+++ redash/settings/__init__.py
@@ -373,6 +373,7 @@ default_query_runners = [
     "redash.query_runner.cloudwatch",
     "redash.query_runner.cloudwatch_insights",
     "redash.query_runner.corporate_memory",
+    "redash.query_runner.sparql_endpoint"
 ]
 
 enabled_query_runners = array_from_string(
