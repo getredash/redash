4dfb5f9527d9a17db9e11811f72883faa41969b0
Kapil Thangavelu <kapilt@gmail.com>
2021-05-21 12:26:01 -0400

    initialize with non empty db

--- redash/cli/database.py
+++ redash/cli/database.py
@@ -50,15 +50,15 @@ def create_tables():
 
     # We need to make sure we run this only if the DB is empty, because otherwise calling
     # stamp() will stamp it with the latest migration value and migrations won't run.
-    if is_db_empty():
-        load_extensions(db)
 
-        # To create triggers for searchable models, we need to call configure_mappers().
-        sqlalchemy.orm.configure_mappers()
-        db.create_all()
+    load_extensions(db)
 
-        # Need to mark current DB as up to date
-        stamp()
+    # To create triggers for searchable models, we need to call configure_mappers().
+    sqlalchemy.orm.configure_mappers()
+    db.create_all()
+
+    # Need to mark current DB as up to date
+    stamp()
 
 
 @manager.command()
