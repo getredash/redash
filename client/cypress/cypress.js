889d5f53ecb7c96bcbcf91b2744dced1f3cc12f3
Restyled.io <commits@restyled.io>
2021-09-01 12:30:39 +0000

    Restyled by prettier

--- client/cypress/cypress.js
+++ client/cypress/cypress.js
@@ -127,10 +127,16 @@ switch (command) {
     seedDatabase(seedData);
     break;
   case "run":
-    execSync("cypress run --config-file=./cypress.json", { stdio: "inherit", cwd: __dirname });
+    execSync("cypress run --config-file=./cypress.json", {
+      stdio: "inherit",
+      cwd: __dirname
+    });
     break;
   case "open":
-    execSync("cypress open --config-file=./cypress.json", { stdio: "inherit", cwd: __dirname });
+    execSync("cypress open --config-file=./cypress.json", {
+      stdio: "inherit",
+      cwd: __dirname
+    });
     break;
   case "run-ci":
     runCypressCI();
@@ -141,7 +147,10 @@ switch (command) {
   case "all":
     startServer();
     seedDatabase(seedData);
-    execSync("cypress run --config-file=./cypress.json", { stdio: "inherit", cwd: __dirname });
+    execSync("cypress run --config-file=./cypress.json", {
+      stdio: "inherit",
+      cwd: __dirname
+    });
     stopServer();
     break;
   default:
