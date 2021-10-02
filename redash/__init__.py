9c928bd1d3f00c994f37e1a87edaddd646259f56
Jesse Whitehouse <jesse@whitehouse.dev>
2021-10-01 21:13:13 -0500

    Bump version to 10.0.0

--- redash/__init__.py
+++ redash/__init__.py
@@ -15,7 +15,7 @@ from .app import create_app  # noqa
 from .query_runner import import_query_runners
 from .destinations import import_destinations
 
-__version__ = "10.0.0-beta"
+__version__ = "10.0.0"
 
 
 if os.environ.get("REMOTE_DEBUG"):
