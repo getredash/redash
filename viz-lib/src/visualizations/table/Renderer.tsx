4f9c8109fb67dfbb0f69cc42c4e40f0c9e564204
Aaron Rosenthal <Aaron.Rosenthal@ercot.com>
2022-04-19 07:04:23 -0500

    Add back a ts-expect-error

--- viz-lib/src/visualizations/table/Renderer.tsx
+++ viz-lib/src/visualizations/table/Renderer.tsx
@@ -122,6 +122,7 @@ export default function Renderer({ options, data }: any) {
         className="table-fixed-header"
         data-percy="show-scrollbars"
         data-test="TableVisualization"
+        // @ts-expect-error ts-migrate(2322) FIXME: Type '{ key: any; dataIndex: string; align: any; s... Remove this comment to see the full error message
         columns={tableColumns}
         dataSource={preparedRows}
         pagination={{
