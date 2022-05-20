93bf049178ac3d0b8074f21313ccb11bcc2f3fed
Eduard Nabokov <17691337+enabokov@users.noreply.github.com>
2022-05-20 22:07:30 +0800

    Dynamodb can assume role

--- redash/query_runner/dynamodb_sql.py
+++ redash/query_runner/dynamodb_sql.py
@@ -1,13 +1,16 @@
 import logging
-import sys
+import os
 
 from redash.query_runner import *
+from redash.settings import parse_boolean
 from redash.utils import json_dumps
 
 logger = logging.getLogger(__name__)
+ASSUME_ROLE = parse_boolean(os.environ.get("DYNAMODB_SQL_ASSUME_ROLE", "false"))
 
 try:
-    from dql import Engine, FragmentEngine
+    import boto3
+    from dql import FragmentEngine
     from dynamo3 import DynamoDBError
     from pyparsing import ParseException
 
@@ -38,16 +41,33 @@ class DynamoDBSQL(BaseSQLQueryRunner):
 
     @classmethod
     def configuration_schema(cls):
-        return {
+        schema = {
             "type": "object",
             "properties": {
                 "region": {"type": "string", "default": "us-east-1"},
                 "access_key": {"type": "string"},
                 "secret_key": {"type": "string"},
             },
-            "required": ["access_key", "secret_key"],
             "secret": ["secret_key"],
         }
+
+        if ASSUME_ROLE:
+            del schema["properties"]["access_key"]
+            del schema["properties"]["secret_key"]
+            schema["secret"] = []
+            schema["properties"].update(
+                {
+                    "iam_role": {"type": "string", "title": "IAM role to assume"},
+                    "external_id": {
+                        "type": "string",
+                        "title": "External ID to be used while STS assume role",
+                    },
+                }
+            )
+        else:
+            schema["required"] = ["access_key", "secret_key"]
+
+        return schema
 
     def test_connection(self):
         engine = self._connect()
@@ -61,9 +81,37 @@ class DynamoDBSQL(BaseSQLQueryRunner):
     def name(cls):
         return "DynamoDB (with DQL)"
 
+    def _get_iam_credentials(self, user=None):
+        if ASSUME_ROLE:
+            role_session_name = "redash" if user is None else user.email
+            sts = boto3.client("sts")
+            creds = sts.assume_role(
+                RoleArn=self.configuration["iam_role"],
+                RoleSessionName=role_session_name,
+                ExternalId=self.configuration.get("external_id"),
+            )
+            return {
+                "aws_access_key_id": creds["Credentials"]["AccessKeyId"],
+                "aws_secret_access_key": creds["Credentials"]["SecretAccessKey"],
+                "aws_session_token": creds["Credentials"]["SessionToken"],
+                "region_name": self.configuration["region"],
+            }
+        else:
+            return {
+                "aws_access_key_id": self.configuration.get("access_key", None),
+                "aws_secret_access_key": self.configuration.get("secret_key", None),
+                "region_name": self.configuration["region"],
+            }
+
     def _connect(self):
         engine = FragmentEngine()
         config = self.configuration.to_dict()
+
+        config["session"] = boto3.Session(**self._get_iam_credentials())._session
+
+        if ASSUME_ROLE:
+            del config["iam_role"]
+            del config["external_id"]
 
         if not config.get("region"):
             config["region"] = "us-east-1"
