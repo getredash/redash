13c98dbe968ea1e471ac94f4033013e79a79d84f
praful.tamrakar@auto1.com <praful.tamrakar@auto1.com>
2023-01-09 13:27:38 +0100

    OPS-16373 adding tf modules for redash

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -14,10 +14,6 @@ provider "vault" {
   address = var.vault_address
 }
 
-data "aws_ecs_cluster" "redash_cluster" {
-  cluster_name = var.cluster
-}
-
 data "vault_generic_secret" "redash" {
   path = local.vault_secret_path[terraform.workspace]
 }
---
+++ terraform/secrets.tf
@@ -0,0 +1,11 @@
+resource "aws_ssm_parameter" "secrets" {
+  for_each = var.ecs_task_secrets
+  name     = "/services/${terraform.workspace}/${var.service_name}/${each.key}"
+  type     = "SecureString"
+  value    = each.value
+  key_id   = data.aws_kms_alias.service.target_key_arn
+}
+
+data "aws_kms_alias" "service" {
+  name = "alias/dev/apps"
+}
