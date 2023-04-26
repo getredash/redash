f91345717e3e4ab52a7a1257a96c02743c25ab83
guyu <lee.gaecoli@gmail.com>
2023-04-26 17:16:46 +0800

    change code style

--- client/app/i18n/index.js
+++ client/app/i18n/index.js
@@ -1,8 +1,8 @@
-import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import { initReactI18next } from 'react-i18next';
-import en from './locales/en.json';
-import zh from './locales/zh.json';
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+import en from "./locales/en.json";
+import zh from "./locales/zh.json";
 
 i18n
   .use(initReactI18next)
@@ -12,8 +12,8 @@ i18n
       en: { translation: en },
       zh: { translation: zh },
     },
-    fallbackLng: 'zh',
-    preload: ['en', 'zh'],
+    fallbackLng: "zh",
+    preload: ["en", "zh"],
     interpolation: {
       escapeValue: false,
     },
