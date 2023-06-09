bbd83a1455a16cdee94e43424e960b0dedc727f9
kevinand11 <kevinfizu@gmail.com>
2023-06-09 18:41:08 +0100

    add versioning

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 remote_address="ec2-3-66-216-60.eu-central-1.compute.amazonaws.com"
-image_name="kevinand11/redash:latest"
+image_name="kevinand11/redash"
+version=$(docker images | awk '($1 == "kevinand11/redash") {print $2 += .01; exit}')
 
-docker build --platform linux/amd64 -t $image_name .
-docker push $image_name
+docker build --platform=linux/amd64 -t "$image_name:$version" .
+docker tag $image_name:"$version" $image_name:latest
+
+docker push $image_name:"$version"
+docker push $image_name:latest
 
 ssh -i deploy-keys.pem -T ubuntu@$remote_address <<'EOL'
 	cd /opt/redash
