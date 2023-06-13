641579bfeb9a573229430a9c1f78d0e67dc9dd3d
Muhammad-Tahir <mtz5prif@gmail.com>
2023-06-13 14:50:41 +0100

    change widget spinners

--- client/app/components/dashboards/dashboard-widget/VisualizationWidget.jsx
+++ client/app/components/dashboards/dashboard-widget/VisualizationWidget.jsx
@@ -302,15 +302,20 @@ class VisualizationWidget extends React.Component {
         );
       default:
         return (
-          <div
-            className="body-row-auto spinner-container"
-            role="status"
-            aria-live="polite"
-            aria-relevant="additions removals">
-            <div className="spinner">
-              <i className="zmdi zmdi-refresh zmdi-hc-spin zmdi-hc-5x" aria-hidden="true" />
-              <span className="sr-only">Loading...</span>
-            </div>
+          <div className="spinner-container">
+            <svg
+              className="animate-spin"
+              height={64}
+              width={64}
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24">
+              <circle opacity={0.25} cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+              <path
+                opacity={0.75}
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
           </div>
         );
     }
