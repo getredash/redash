b59d6478f083c28a881ee9ab7693a22d788b8c07
Peter Lee <yankeeguyu@gmail.com>
2023-07-03 19:15:03 +0800

    update code

--- redash/models/__init__.py
+++ redash/models/__init__.py
@@ -361,22 +361,13 @@ class QueryResult(db.Model, QueryResultPersistence, BelongsToOrgMixin):
     def get_latest(cls, data_source, query, max_age=0):
         query_hash = gen_query_hash(query)
 
+        if max_age == -1 and settings.QUERY_RESULTS_EXPIRED_TTL_ENABLED:
+            max_age = settings.QUERY_RESULTS_EXPIRED_TTL
+
         if max_age == -1:
-            if settings.QUERY_RESULTS_EXPIRED_TTL_ENABLED:
-                query = cls.query.filter(
-                    cls.query_hash == query_hash,
-                    cls.data_source == data_source,
-                    (
-                        db.func.timezone("utc", cls.retrieved_at) +
-                        datetime.timedelta(
-                            seconds=settings.QUERY_RESULTS_EXPIRED_TTL
-                        ) >= db.func.timezone("utc", db.func.now())
-                    )
-                )
-            else:
-                query = cls.query.filter(
-                    cls.query_hash == query_hash, cls.data_source == data_source
-                )
+            query = cls.query.filter(
+                cls.query_hash == query_hash, cls.data_source == data_source
+            )
         else:
             query = cls.query.filter(
                 cls.query_hash == query_hash,
