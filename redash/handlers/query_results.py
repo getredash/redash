cd236724a9b22549117c098998f0c56305808409
guyu <lee.gaecoli@gmail.com>
2023-07-21 11:21:17 +0800

    fix pubic dashboard not update result #6203

--- redash/handlers/query_results.py
+++ redash/handlers/query_results.py
@@ -255,7 +255,10 @@ class QueryResultResource(BaseResource):
         query = get_object_or_404(models.Query.get_by_id_and_org, query_id, self.current_org)
 
         allow_executing_with_view_only_permissions = query.parameterized.is_safe
-        should_apply_auto_limit = params.get("apply_auto_limit", False)
+        if "apply_auto_limit" in params:
+            should_apply_auto_limit = params.get("apply_auto_limit", False)
+        else:
+            should_apply_auto_limit = query.options.get("apply_auto_limit", False)
 
         if has_access(query, self.current_user, allow_executing_with_view_only_permissions):
             return run_query(
