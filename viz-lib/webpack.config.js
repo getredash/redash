ad39059558ca390e414b317fc89e508370a5d296
Justin Clift <justin@postgresql.org>
2023-07-31 21:12:37 +1000

    Fix webpack error in viz-lib (#6324)
    The config file format for webpack has changed slightly, so webpack was throwing this error:
    
      Invalid options object. Less Loader has been initialized using an
      options object that does not match the API schema.
    
    This commit updates the config file format so webpack works again.

--- viz-lib/webpack.config.js
+++ viz-lib/webpack.config.js
@@ -39,8 +39,10 @@ module.exports = {
           {
             loader: "less-loader",
             options: {
-              plugins: [new LessPluginAutoPrefix({ browsers: ["last 3 versions"] })],
-              javascriptEnabled: true,
+              lessOptions: {
+                plugins: [new LessPluginAutoPrefix({ browsers: ["last 3 versions"] })],
+                javascriptEnabled: true,
+	      },
             },
           },
         ],
