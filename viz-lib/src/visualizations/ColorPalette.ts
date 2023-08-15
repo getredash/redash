e3b777882a0d1a44253c712bcac6688ddd1b7cee
Muhammad-Tahir <mtz5prif@gmail.com>
2023-08-15 10:35:23 +0100

    make pie chart more like in product

--- viz-lib/src/visualizations/ColorPalette.ts
+++ viz-lib/src/visualizations/ColorPalette.ts
@@ -3,10 +3,10 @@ import { values } from "lodash";
 // The following colors will be used if you pick "Automatic" color
 export const BaseColors = {
   Cyan: "#00BCD4",
-  Blue: "#0091EA",
-  Red: "#EC407A",
-  Purple: "#B045E6",
   Yellow: "#FFD600",
+  Blue: "#0091EA",
+  Purple: "#B045E6",
+  Red: "#EC407A",
 };
 
 // Additional colors for the user to choose from
