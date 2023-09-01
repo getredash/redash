b0f81aad7d6240645462a78d3798fda69a5e098a
Christopher Elliott <christopher@aveasolutions.com>
2023-09-01 14:07:09 -0700

    Remove Redash logo

--- client/app/pages/dashboards/PublicDashboardPage.jsx
+++ client/app/pages/dashboards/PublicDashboardPage.jsx
@@ -89,12 +89,6 @@ class PublicDashboardPage extends React.Component {
           <PublicDashboard dashboard={dashboard} />
         )}
         <div id="footer">
-          <div className="text-center">
-            <Link href="https://redash.io">
-              <img alt="Redash Logo" src={logoUrl} width="38" />
-            </Link>
-          </div>
-          Powered by <Link href="https://redash.io/?ref=public-dashboard">Redash</Link>
         </div>
       </div>
     );
