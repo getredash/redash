94c5be0c50dae51c7206c38773ced9e25cdb934b
kevinand11 <kevinfizu@gmail.com>
2023-09-01 10:07:12 +0100

    add new visualization for regular redash table

--- viz-lib/src/visualizations/redash-table/index.ts
+++ viz-lib/src/visualizations/redash-table/index.ts
@@ -3,8 +3,8 @@ import Renderer from "./Renderer";
 import Editor from "./Editor";
 
 export default {
-  type: "TABLE",
-  name: "Table(Addressable)",
+  type: "REDASH-TABLE",
+  name: "Table(Redash)",
   getOptions,
   Renderer,
   Editor,
