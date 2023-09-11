29abb688de41fb9e50f47c890b1c6d44693156b8
Daniel Stevenson <26047171+daniel-cortez-stevenson@users.noreply.github.com>
2023-09-11 04:24:52 +0200

    Add test_get_schema_catalog_set test & fix tests

--- tests/query_runner/test_trino.py
+++ tests/query_runner/test_trino.py
@@ -3,40 +3,57 @@ Some test cases for Trino.
 """
 import re
 from unittest import TestCase
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 from redash.query_runner.trino import Trino
 
 
 class TestTrino(TestCase):
-    @patch("redash.query_runner.trino.trino")
+    catalog_name = "memory"
+    schema_name = "default"
+    table_name = "users"
+    column_name = "id"
+    column_type = "integer"
+
+    @staticmethod
+    def side_effect_run_query(query, user):
+        catalog_query_pattern = r"\s*SHOW\s+CATALOGS\s*"
+        schema_query_pattern = r"""
+        \s*SELECT\s+table_schema,\s+table_name,\s+column_name,\s+data_type
+        \s+FROM\s+(\w+\.)?information_schema\.columns
+        \s+WHERE\s+table_schema\s+NOT\s+IN\s+\('pg_catalog',\s+'information_schema'\)\s*
+        """
+        if re.search(catalog_query_pattern, query, re.IGNORECASE) is not None:
+            return (f'{{"rows": [{{"Catalog": "{TestTrino.catalog_name}"}}]}}', None)
+        elif re.search(schema_query_pattern, query, re.IGNORECASE) is not None:
+            return (
+                '{"rows": [{"table_schema": "default", "table_name": "users", "column_name": "id", "data_type": "integer"}]}',
+                None,
+            )
+        return (None, "Unknown query")
+
     @patch.object(Trino, "run_query")
-    def test_get_schema_no_catalog_set(self, mock_run_query, mock_trino):
-        def side_effect_run_query(query):
-            catalog_query_regex = "SHOW CATALOGS"
-            schema_query_regex = "SELECT.*information_schema"
-            if query.strip().match(catalog_query_regex, re.IGNORE_CASE) is not None:
-                return ('{"rows": [{"Catalog": "hive"}]}', None)
-            elif query.strip().match(schema_query_regex, re.IGNORE_CASE) is not None:
-                return (
-                    '{"rows": [{"table_schema": "public", "table_name": "users", "column_name": "id", "data_type": "integer"}]}',
-                    None,
-                )
-            return (None, "Unknown query")
-
-        # Mocking the response from run_query method
-        mock_run_query.return_value = side_effect_run_query
-
-        # Mocking the trino module's constants and exceptions
-        mock_trino.constants.DEFAULT_AUTH = Mock()
-        mock_trino.exceptions.DatabaseError = Exception
-
-        # Creating an instance of Trino class
+    def test_get_schema_no_catalog_set(self, mock_run_query):
+        mock_run_query.side_effect = self.side_effect_run_query
         runner = Trino({})
-
-        # Calling the get_schema method
         schema = runner.get_schema()
+        expected_schema = [
+            {
+                "name": f"{TestTrino.catalog_name}.{TestTrino.schema_name}.{TestTrino.table_name}",
+                "columns": [{"name": f"{TestTrino.column_name}", "type": f"{TestTrino.column_type}"}],
+            }
+        ]
+        self.assertEqual(schema, expected_schema)
 
-        # Verifying the output
-        expected_schema = [{"name": "hive.public.users", "columns": [{"name": "id", "type": "integer"}]}]
+    @patch.object(Trino, "run_query")
+    def test_get_schema_catalog_set(self, mock_run_query):
+        mock_run_query.side_effect = self.side_effect_run_query
+        runner = Trino({"catalog": TestTrino.catalog_name})
+        schema = runner.get_schema()
+        expected_schema = [
+            {
+                "name": f"{TestTrino.schema_name}.{TestTrino.table_name}",
+                "columns": [{"name": f"{TestTrino.column_name}", "type": f"{TestTrino.column_type}"}],
+            }
+        ]
         self.assertEqual(schema, expected_schema)
