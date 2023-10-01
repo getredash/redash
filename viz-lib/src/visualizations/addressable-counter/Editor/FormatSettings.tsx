9656142c624a599b7b12481cac8dde9abe6b9caf
Dani Kenan <danikenan@gmail.com>
2023-10-01 17:50:27 +0300

    number fomratting was w/o decimal

--- viz-lib/src/visualizations/addressable-counter/Editor/FormatSettings.tsx
+++ viz-lib/src/visualizations/addressable-counter/Editor/FormatSettings.tsx
@@ -8,29 +8,29 @@ export default function FormatSettings({ options, data, onOptionsChange }: any) 
   const inputsEnabled = isValueNumber(data.rows, options);
   return (
     <React.Fragment>
-      {/* @ts-expect-error ts-migrate(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message
+      {/* @ts-expect-error ts-migrate(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
       <Section>
         <InputNumber
           layout="horizontal"
           label="Formatting Decimal Place"
           data-test="Counter.Formatting.DecimalPlace"
-          defaultValue={options.stringDecimal}
-          disabled={!inputsEnabled}
+          defaultValue={2}
+          disabled={true}
           onChange={(stringDecimal: any) => onOptionsChange({ stringDecimal })}
         />
       </Section>
 
       {/* @ts-expect-error ts-migrate(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
-      {/* <Section>
+      <Section>
         <Input
+          disabled={true}
           layout="horizontal"
           label="Formatting Decimal Character"
           data-test="Counter.Formatting.DecimalCharacter"
           defaultValue={options.stringDecChar}
-          disabled={!inputsEnabled}
           onChange={(e: any) => onOptionsChange({ stringDecChar: e.target.value })}
         />
-      </Section> */}
+      </Section>
 
       {/* @ts-expect-error ts-migrate(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message
       <Section>
