81b9febc760aea48b0fa28636b6d64806f8ab51d
Madhur Jain <92226302+immortalcodes@users.noreply.github.com>
2023-10-08 12:39:49 +0530

    Update google_spreadsheets.py

--- redash/query_runner/google_spreadsheets.py
+++ redash/query_runner/google_spreadsheets.py
@@ -100,18 +100,28 @@ class WorksheetNotFoundByTitleError(Exception):
 def parse_query(query):
     values = query.split("|")
     key = values[0]  # key of the spreadsheet
-    worksheet_num_or_title = 0  # A default value for when a number of inputs is invalid
+    worksheet_num = 0  # A default value for when a number of inputs is invalid
+    worksheet_title = '' # No title initially
     if len(values) == 2:
         s = values[1].strip()
         if len(s) > 0:
             if re.match(r"^\"(.*?)\"$", s):
                 # A string quoted by " means a title of worksheet
-                worksheet_num_or_title = s[1:-1]
-            else:
+                worksheet_title = s[1:-1]
+            elif re.match(r"^\d+$", s):
                 # if spreadsheet contains more than one worksheet - this is the number of it
-                worksheet_num_or_title = int(s)
-
-    return key, worksheet_num_or_title
+                worksheet_num = int(s)
+            else:
+                # To match for non-quoted one-word file name
+                if len(s.split()) == 1 :
+                    worksheet_title = s
+            if worksheet_title != '':
+                return key, worksheet_title
+            else:
+                return key, worksheet_num
+                    
+        else:
+            return key, worksheet_num
 
 
 def parse_worksheet(worksheet):
