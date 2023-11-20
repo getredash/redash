2e0045dd902cbd6a081842879aeabd781f441506
Vladislav Denisov <denisov@sports.ru>
2023-11-20 15:33:21 +0300

    added tests

--- tests/query_runner/test_yandex_disk.py
+++ tests/query_runner/test_yandex_disk.py
@@ -1,9 +1,10 @@
 from io import BytesIO
-from unittest import TestCase
+from unittest import mock
 
 import pandas as pd
+import pytest
 
-from redash.query_runner.yandex_disk import EXTENSIONS_READERS
+from redash.query_runner.yandex_disk import EXTENSIONS_READERS, YandexDisk
 
 test_df = pd.DataFrame(
     [
@@ -18,28 +19,67 @@ test_df = pd.DataFrame(
 test_token = "AAAAQAA"
 
 
-class TestYandexDisk(TestCase):
-    def test_xlsx(self):
-        output = BytesIO()
-        writer = pd.ExcelWriter(output)
-        test_df.to_excel(writer, index=False)
-        writer.save()
+def test_yandex_disk_type():
+    assert YandexDisk.type() == "yandex_disk"
 
-        is_equals = test_df.equals(EXTENSIONS_READERS["xlsx"](output))
-        self.assertTrue(is_equals)
 
-    def test_csv(self):
-        output = BytesIO()
-        test_df.to_csv(output, index=False)
-        output.seek(0)
+def test_yandex_disk_name():
+    assert YandexDisk.name() == "Yandex Disk"
 
-        is_equals = test_df.equals(EXTENSIONS_READERS["csv"](output))
-        self.assertTrue(is_equals)
 
-    def test_tsv(self):
-        output = BytesIO()
-        test_df.to_csv(output, index=False, sep="\t")
-        output.seek(0)
+@mock.patch("requests.get")
+def test__send_query(mock_requests_get):
+    mock_requests_get.return_value.ok = True
+    mock_requests_get.return_value.json.return_value = {"foo": "bar"}
 
-        is_equals = test_df.equals(EXTENSIONS_READERS["tsv"](output))
-        self.assertTrue(is_equals)
+    configuration = {"token": test_token}
+    disk = YandexDisk(configuration)
+    response = disk._send_query("test_url")
+
+    assert response == {"foo": "bar"}
+    mock_requests_get.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "configuration, error_message",
+    [({"token": test_token}, None), ({"token": ""}, "Code: 400, message: Unauthorized")],
+)
+@mock.patch("requests.get")
+def test_test_connection(mock_requests_get, configuration, error_message):
+    if error_message:
+        mock_requests_get.return_value.ok = False
+        mock_requests_get.return_value.status_code = 400
+        mock_requests_get.return_value.text = "Unauthorized"
+    else:
+        mock_requests_get.return_value.ok = True
+
+    disk = YandexDisk(configuration)
+    if error_message:
+        with pytest.raises(Exception, match=error_message):
+            disk.test_connection()
+    else:
+        assert disk.test_connection() is None
+
+
+def test_read_xlsx():
+    output = BytesIO()
+    writer = pd.ExcelWriter(output)
+    test_df.to_excel(writer, index=False)
+    writer.save()
+    assert test_df.equals(EXTENSIONS_READERS["xlsx"](output))
+
+
+def test_read_csv():
+    output = BytesIO()
+    test_df.to_csv(output, index=False)
+    output.seek(0)
+
+    assert test_df.equals(EXTENSIONS_READERS["csv"](output))
+
+
+def test_tsv():
+    output = BytesIO()
+    test_df.to_csv(output, index=False, sep="\t")
+    output.seek(0)
+
+    assert test_df.equals(EXTENSIONS_READERS["tsv"](output))
