d3d90cccf36b58ede169aef7d417cea2b264fff6
Andrew Chubatiuk <andrew.chubatiuk@motional.com>
2024-01-04 14:17:03 +0200

    filter widged results to fix tests during repeatable execution

--- client/cypress/integration/dashboard/sharing_spec.js
+++ client/cypress/integration/dashboard/sharing_spec.js
@@ -58,6 +58,7 @@ describe("Dashboard Sharing", () => {
       editDashboard();
       cy.getByTestId("AddWidgetButton").click();
       cy.getByTestId("AddWidgetDialog").within(() => {
+        cy.get("input").type("Test Query");
         cy.get(`.query-selector-result[data-test="QueryId${queryId}"]`).click();
       });
       cy.contains("button", "Add to Dashboard").click();
