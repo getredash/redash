8aee9be99928c957375d57f6d53f0c6af5e7775a
Daniel DeWeerd <deweerdd@gmail.com>
2024-01-16 13:32:58 -0800

    Another test

--- cron/checkScheduler.py
+++ cron/checkScheduler.py
@@ -15,8 +15,8 @@ if 'updated_at' in api_response:
     time_difference = current_time - updated_at_datetime
 
     if time_difference.total_seconds() > 10 * 60:
-        print("The 'updated_at' value is older than 30 mintues.")
         exit(1)
+        print("The 'updated_at' value is older than 30 mintues.")
     else:
         print("The 'updated_at' value is within the last 30 minutes")
 else:
