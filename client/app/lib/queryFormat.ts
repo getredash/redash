4b7e2ce2d92d80eed964be4ee4adb1437ebc9c4f
kouya takahashi <3kamarin4skaya6@gmail.com>
2024-01-30 09:49:46 +0900

    [fix] set paramsType for the format of the query, including query parameters

--- client/app/lib/queryFormat.ts
+++ client/app/lib/queryFormat.ts
@@ -1,13 +1,14 @@
 import { trim } from "lodash";
 import { format } from "sql-formatter";
 import { formatterConfig } from './formatterConfig';
+import { defaultFormatterConfig } from './defaultFormatterConfig';
 
 interface QueryFormatterMap {
   [syntax: string]: (queryText: string) => string;
 }
 
 const QueryFormatters: QueryFormatterMap = {
-  sql: queryText => format(trim(queryText), formatterConfig),
+  sql: queryText => format(trim(queryText), Object.assign(defaultFormatterConfig, formatterConfig)),
   json: queryText => JSON.stringify(JSON.parse(queryText), null, 4),
 };
 
