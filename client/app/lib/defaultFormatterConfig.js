f7e976e03e8eac761f5d756c607e187c1091b077
kouya takahashi <3kamarin4skaya6@gmail.com>
2024-02-03 23:09:48 +0900

    [add] .\s- -> paramTypes:custom

--- client/app/lib/defaultFormatterConfig.js
+++ client/app/lib/defaultFormatterConfig.js
@@ -1,1 +1,1 @@
-export const defaultFormatterConfig = { paramTypes: { custom: [{ regex: "\\{\\{[a-zA-Z0-9_]+\\}\\}" }] } };
+export const defaultFormatterConfig = { paramTypes: { custom: [{ regex: "\\{\\{[a-zA-Z0-9\\s\\._-]+\\}\\}" }] } };
