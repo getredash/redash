bbdf73a7da81478fca2aeb41bba49981fd430a57
Restyled.io <commits@restyled.io>
2024-06-14 16:58:41 +0000

    Restyled by prettier

--- client/cypress/integration/visualizations/table/table_spec.js
+++ client/cypress/integration/visualizations/table/table_spec.js
@@ -8,7 +8,7 @@ import * as AllCellTypes from "./.mocks/all-cell-types";
 import * as MultiColumnSort from "./.mocks/multi-column-sort";
 import * as SearchInData from "./.mocks/search-in-data";
 import * as LargeDataset from "./.mocks/large-dataset";
-import * as WideDataSet from "./.mocks/wide-dataset"
+import * as WideDataSet from "./.mocks/wide-dataset";
 
 function prepareVisualization(query, type, name, options) {
   return cy
@@ -108,15 +108,17 @@ describe("Table", () => {
       cy.getByTestId("FixedColumns").click();
       cy.contains(".ant-select-item-option-content", "1").click();
       cy.contains("Save").click();
-      cy.wait(500);
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(500); //add some waiting to make sure table visualization is saved
 
-      cy.get('.ant-table-thead')
-        .find('th.ant-table-cell-fix-left')
-        .then((fixedCols) => {
-          expect(fixedCols.length).to.equal(1)});
+      cy.get(".ant-table-thead")
+        .find("th.ant-table-cell-fix-left")
+        .then(fixedCols => {
+          expect(fixedCols.length).to.equal(1);
+        });
 
-      cy.get('.ant-table-content').scrollTo('right', { duration: 1000 });
-      cy.get('.ant-table-content').scrollTo('left', { duration: 1000 });
+      cy.get(".ant-table-content").scrollTo("right", { duration: 1000 });
+      cy.get(".ant-table-content").scrollTo("left", { duration: 1000 });
     });
 
     it("doesn't let user fix too many columns", () => {
@@ -125,7 +127,7 @@ describe("Table", () => {
       cy.getByTestId("EditVisualization").click();
       cy.contains("span", "Grid").click();
       cy.getByTestId("FixedColumns").click();
-      cy.get(".ant-select-item-option-content")
+      cy.get(".ant-select-item-option-content");
       cy.contains(".ant-select-item-option-content", "3").should("not.exist");
       cy.contains(".ant-select-item-option-content", "4").should("not.exist");
     });
@@ -138,7 +140,7 @@ describe("Table", () => {
       cy.getByTestId("FixedColumns").click();
       cy.contains(".ant-select-item-option-content", "4").click();
       cy.contains("Save").click();
-    })
+    });
   });
 
   it("searches in multiple columns", () => {
