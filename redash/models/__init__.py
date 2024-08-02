e44fcdb653e275edcc91c32dd2f177660f9f654b
Ezra Odio <eodio@starfishstorage.com>
2024-08-02 19:47:29 +0000

    Fix alert evaluation logic and issue with calculating min and max of columns without numbers

--- redash/models/__init__.py
+++ redash/models/__init__.py
@@ -916,6 +916,8 @@ def next_state(op, value, threshold):
 
     if op(value, threshold):
         new_state = Alert.TRIGGERED_STATE
+    elif not value_is_number and op not in [OPERATORS.get("!="), OPERATORS.get("=="), OPERATORS.get("equals")]:
+        new_state = Alert.UNKNOWN_STATE
     else:
         new_state = Alert.OK_STATE
 
@@ -967,18 +969,22 @@ class Alert(TimestampMixin, BelongsToOrgMixin, db.Model):
             else:
                 selector = self.options["selector"]
 
-            if selector == "max":
-                max_val = float("-inf")
-                for i in range(0, len(data["rows"])):
-                    max_val = max(max_val, data["rows"][i][self.options["column"]])
-                value = max_val
-            elif selector == "min":
-                min_val = float("inf")
-                for i in range(0, len(data["rows"])):
-                    min_val = min(min_val, data["rows"][i][self.options["column"]])
-                value = min_val
-            else:
-                value = data["rows"][0][self.options["column"]]
+            try:
+                if selector == "max":
+                    max_val = float("-inf")
+                    for i in range(len(data["rows"])):
+                        max_val = max(max_val, float(data["rows"][i][self.options["column"]]))
+                    value = max_val
+                elif selector == "min":
+                    min_val = float("inf")
+                    for i in range(len(data["rows"])):
+                        min_val = min(min_val, float(data["rows"][i][self.options["column"]]))
+                    value = min_val
+                else:
+                    value = data["rows"][0][self.options["column"]]
+
+            except ValueError:
+                return self.UNKNOWN_STATE
 
             threshold = self.options["value"]
 
@@ -1007,7 +1013,6 @@ class Alert(TimestampMixin, BelongsToOrgMixin, db.Model):
         result_table = []  # A two-dimensional array which can rendered as a table in Mustache
         for row in data["rows"]:
             result_table.append([row[col["name"]] for col in data["columns"]])
-        print("OPTIONS", self.options)
         context = {
             "ALERT_NAME": self.name,
             "ALERT_URL": "{host}/alerts/{alert_id}".format(host=host, alert_id=self.id),
