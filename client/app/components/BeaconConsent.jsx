75029d7858d4c95456334ece98a5a5a08363c350
Restyled.io <commits@restyled.io>
2024-10-29 13:46:15 +0000

    Restyled by prettier

--- client/app/components/BeaconConsent.jsx
+++ client/app/components/BeaconConsent.jsx
@@ -22,7 +22,7 @@ function BeaconConsent() {
     setHide(true);
   };
 
-  const confirmConsent = confirm => {
+  const confirmConsent = (confirm) => {
     let message = "🙏 Thank you.";
 
     if (!confirm) {
@@ -47,7 +47,8 @@ function BeaconConsent() {
               <HelpTrigger type="USAGE_DATA_SHARING" />
             </>
           }
-          bordered={false}>
+          bordered={false}
+        >
           <Text>Help Redash improve by automatically sending anonymous usage data:</Text>
           <div className="m-t-5">
             <ul>
