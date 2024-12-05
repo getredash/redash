120cd2286cbe8452784b2e318d1f7516d00fa52b
Cory Johns <johnsca@gmail.com>
2024-12-05 12:56:18 -0800

    chore: support auth on redis (ENG-4206) (#69)
    Add support for AUTH on Redis.
    
    Part of: [ENG-4206](https://stacklet.atlassian.net/browse/ENG-4206)

--- redash/__init__.py
+++ redash/__init__.py
@@ -44,8 +44,9 @@ def setup_logging():
 
 setup_logging()
 
-redis_connection = redis.from_url(settings.REDIS_URL)
-rq_redis_connection = redis.from_url(settings.RQ_REDIS_URL)
+redis_token = os.environ.get("REDASH_REDIS_TOKEN") or None
+redis_connection = redis.from_url(settings.REDIS_URL, password=redis_token)
+rq_redis_connection = redis.from_url(settings.RQ_REDIS_URL, password=redis_token)
 mail = Mail()
 migrate = Migrate(compare_type=True)
 statsd_client = StatsClient(host=settings.STATSD_HOST, port=settings.STATSD_PORT, prefix=settings.STATSD_PREFIX)
