351a8254bfbbb6209145c8e68d47ad4fb5180df1
winebarrel <sugawara@winebarrel.jp>
2025-02-01 01:28:07 +0900

    Support AWS RDS IAM Authentication for Redash database

--- redash/settings/__init__.py
+++ redash/settings/__init__.py
@@ -467,3 +467,6 @@ CSRF_TIME_LIMIT = int(os.environ.get("REDASH_CSRF_TIME_LIMIT", 3600 * 6))
 
 # Email blocked domains, use delimiter comma to separated multiple domains
 BLOCKED_DOMAINS = set_from_string(os.environ.get("REDASH_BLOCKED_DOMAINS", "qq.com"))
+
+# AWS
+REDASH_DATABASE_IAM_AUTH = parse_boolean(os.environ.get("REDASH_DATABASE_IAM_AUTH", "false"))
