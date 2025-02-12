8afd656b2b8b0e9ee3eefb6f4b062245b584b549
Yoshioka Tsuneo (吉岡 恒夫) <yoshiokatsuneo@gmail.com>
2025-02-13 00:41:24 +0900

    Fix styling error reported by restyled

--- client/app/pages/queries/hooks/useAutocompleteFlags.js
+++ client/app/pages/queries/hooks/useAutocompleteFlags.js
@@ -5,7 +5,7 @@ export default function useAutocompleteFlags(schema) {
   const isAvailable = true;
   const [isEnabled, setIsEnabled] = useState(localOptions.get("liveAutocomplete", true));
 
-  const toggleAutocomplete = useCallback(state => {
+  const toggleAutocomplete = useCallback((state) => {
     setIsEnabled(state);
     localOptions.set("liveAutocomplete", state);
   }, []);
