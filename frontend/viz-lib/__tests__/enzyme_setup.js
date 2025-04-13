011464ca5924f381477b5149477409a5def064d3
Vladislav Denisov <vladislive@gmail.com>
2025-04-13 15:59:51 +0200

    React 17

--- frontend/viz-lib/__tests__/enzyme_setup.js
+++ frontend/viz-lib/__tests__/enzyme_setup.js
@@ -1,4 +1,4 @@
 import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 configure({ adapter: new Adapter() });
