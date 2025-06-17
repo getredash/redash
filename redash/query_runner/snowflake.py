273bdce09a1d54a9b30bcda20b2a4d477d5bde0d
gregosaurus <gregory.barry@wooga.net>
2025-06-17 16:24:09 +0200

    DE-4902: fixing javascrip overflow with int, snowflake connector with key file/pwd auth method

--- redash/query_runner/snowflake.py
+++ redash/query_runner/snowflake.py
@@ -52,18 +52,30 @@ class Snowflake(BaseSQLQueryRunner):
                     "default": False,
                 },
                 "host": {"type": "string"},
+                "private_key_file": {
+                    "type": "string",
+                    "title": "Private Key File",
+                    "description": "Path to the private key file for authentication.",
+                },
+                "private_key_file_pwd": {
+                    "type": "string",
+                    "title": "Private Key File Password",
+                    "description": "Password for the private key file.",
+                },
             },
             "order": [
                 "account",
                 "user",
                 "password",
+                "private_key_file",
+                "private_key_file_pwd",
                 "warehouse",
                 "database",
                 "region",
                 "host",
             ],
-            "required": ["user", "password", "account", "database", "warehouse"],
-            "secret": ["password"],
+            "required": ["user", "password", "account", "database", "warehouse", "private_key_file", "private_key_file_pwd"],
+            "secret": ["password", "private_key_file_pwd"],
             "extra_options": [
                 "host",
             ],
@@ -74,11 +86,14 @@ class Snowflake(BaseSQLQueryRunner):
         return enabled
 
     @classmethod
-    def determine_type(cls, data_type, scale):
+    def determine_type(cls, data_type, precision, scale):
         t = TYPES_MAP.get(data_type, None)
         if t == TYPE_INTEGER and scale > 0:
             return TYPE_FLOAT
-        return t
+        elif t == TYPE_INTEGER and precision >= 16:
+            return TYPE_STRING
+        else:
+            return t
 
     def _get_connection(self):
         region = self.configuration.get("region")
@@ -96,14 +111,26 @@ class Snowflake(BaseSQLQueryRunner):
             else:
                 host = "{}.snowflakecomputing.com".format(account)
 
-        connection = snowflake.connector.connect(
-            user=self.configuration["user"],
-            password=self.configuration["password"],
-            account=account,
-            region=region,
-            host=host,
-            application="Redash/{} (Snowflake)".format(__version__.split("-")[0]),
-        )
+        if self.configuration.__contains__("password"):
+            connection = snowflake.connector.connect(
+                user=self.configuration["user"],
+                password=self.configuration["password"],
+                account=account,
+                region=region,
+                host=host,
+                application="Redash/{} (Snowflake)".format(__version__.split("-")[0]),
+            )
+        else:
+            connection = snowflake.connector.connect(
+                user=self.configuration["user"],
+                password=self.configuration["password"],
+                account=account,
+                region=region,
+                host=host,
+                application="Redash/{} (Snowflake)".format(__version__.split("-")[0]),
+                private_key_file = self.configuration["private_key_file"],
+                private_key_file_pwd = self.configuration["private_key_file_pwd"],
+            )
 
         return connection
 
@@ -115,7 +142,7 @@ class Snowflake(BaseSQLQueryRunner):
 
     def _parse_results(self, cursor):
         columns = self.fetch_columns(
-            [(self._column_name(i[0]), self.determine_type(i[1], i[5])) for i in cursor.description]
+            [(self._column_name(i[0]), self.determine_type(i[1], i[4], i[5])) for i in cursor.description]
         )
         rows = [dict(zip((column["name"] for column in columns), row)) for row in cursor]
 
