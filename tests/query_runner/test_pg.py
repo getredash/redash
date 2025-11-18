c2b7921610553c70fa9945e466065cdf70d6e701
Sarvesh Vazarkar <sarveshvazarkar@gmail.com>
2025-11-18 23:06:09 +0100

    ran make format

--- tests/query_runner/test_pg.py
+++ tests/query_runner/test_pg.py
@@ -43,18 +43,54 @@ class TestBuildSchema(TestCase):
         )
 
     def test_build_schema_with_data_types_with_special_table_names(self):
-        results={
-                 'rows': [{'table_schema': 'public', 'table_name': 'org Table', 'column_name': 'id', 'data_type': 'integer'},
-                          {'table_schema': 'public', 'table_name': 'org Table', 'column_name': 'name', 'data_type': 'character varying'},
-                          {'table_schema': 'public', 'table_name': 'org Table', 'column_name': 'created_at', 'data_type': 'timestamp without time zone'},
-                          {'table_schema': 'public', 'table_name': 'org1Table', 'column_name': 'id', 'data_type': 'integer'},
-                          {'table_schema': 'public', 'table_name': 'org1Table', 'column_name': 'name', 'data_type': 'character varying'},
-                          {'table_schema': 'public', 'table_name': 'org1Table', 'column_name': 'created_at', 'data_type': 'timestamp without time zone'},
-                          {'table_schema': 'public', 'table_name': 'orgTable', 'column_name': 'id', 'data_type': 'integer'},
-                          {'table_schema': 'public', 'table_name': 'orgTable', 'column_name': 'name', 'data_type': 'character varying'},
-                          {'table_schema': 'public', 'table_name': 'org Tablemview', 'column_name': 'id', 'data_type': None}, #materialized view with no data types
-                          {'table_schema': 'public', 'table_name': 'org1Tablemview', 'column_name': 'created_at', 'data_type': None} #materialized view with no data types
-                          ]
+        results = {
+            "rows": [
+                {"table_schema": "public", "table_name": "org Table", "column_name": "id", "data_type": "integer"},
+                {
+                    "table_schema": "public",
+                    "table_name": "org Table",
+                    "column_name": "name",
+                    "data_type": "character varying",
+                },
+                {
+                    "table_schema": "public",
+                    "table_name": "org Table",
+                    "column_name": "created_at",
+                    "data_type": "timestamp without time zone",
+                },
+                {"table_schema": "public", "table_name": "org1Table", "column_name": "id", "data_type": "integer"},
+                {
+                    "table_schema": "public",
+                    "table_name": "org1Table",
+                    "column_name": "name",
+                    "data_type": "character varying",
+                },
+                {
+                    "table_schema": "public",
+                    "table_name": "org1Table",
+                    "column_name": "created_at",
+                    "data_type": "timestamp without time zone",
+                },
+                {"table_schema": "public", "table_name": "orgTable", "column_name": "id", "data_type": "integer"},
+                {
+                    "table_schema": "public",
+                    "table_name": "orgTable",
+                    "column_name": "name",
+                    "data_type": "character varying",
+                },
+                {
+                    "table_schema": "public",
+                    "table_name": "org Tablemview",
+                    "column_name": "id",
+                    "data_type": None,
+                },  # materialized view with no data types
+                {
+                    "table_schema": "public",
+                    "table_name": "org1Tablemview",
+                    "column_name": "created_at",
+                    "data_type": None,
+                },  # materialized view with no data types
+            ]
         }
 
         schema = {}
@@ -62,45 +98,74 @@ class TestBuildSchema(TestCase):
         build_schema(results, schema)
 
         self.assertListEqual(
-            schema["orgTable"]["columns"], [{"name": "id", "type": "integer"}, {"name": "name", "type": "character varying"}]
+            schema["orgTable"]["columns"],
+            [{"name": "id", "type": "integer"}, {"name": "name", "type": "character varying"}],
         )
 
         self.assertListEqual(
-            schema["org Table"]["columns"], [{"name": "id", "type": "integer"}, {"name": "name", "type": "character varying"}, {"name": "created_at", "type": "timestamp without time zone"}]
+            schema["org Table"]["columns"],
+            [
+                {"name": "id", "type": "integer"},
+                {"name": "name", "type": "character varying"},
+                {"name": "created_at", "type": "timestamp without time zone"},
+            ],
         )
 
         self.assertListEqual(
-            schema["org1Table"]["columns"], [{"name": "id", "type": "integer"}, {"name": "name", "type": "character varying"}, {"name": "created_at", "type": "timestamp without time zone"}]
+            schema["org1Table"]["columns"],
+            [
+                {"name": "id", "type": "integer"},
+                {"name": "name", "type": "character varying"},
+                {"name": "created_at", "type": "timestamp without time zone"},
+            ],
         )
 
         self.assertListEqual(
-            schema["orgTable"]["columns"], [{"name": "id", "type": "integer"}, {"name": "name", "type": "character varying"}]
+            schema["orgTable"]["columns"],
+            [{"name": "id", "type": "integer"}, {"name": "name", "type": "character varying"}],
         )
 
         self.assertListEqual(
-            schema["org Table"]["columns"], [{"name": "id", "type": "integer"}, {"name": "name", "type": "character varying"}, {"name": "created_at", "type": "timestamp without time zone"}]
+            schema["org Table"]["columns"],
+            [
+                {"name": "id", "type": "integer"},
+                {"name": "name", "type": "character varying"},
+                {"name": "created_at", "type": "timestamp without time zone"},
+            ],
         )
 
         self.assertListEqual(
-            schema["org1Table"]["columns"], [{"name": "id", "type": "integer"}, {"name": "name", "type": "character varying"}, {"name": "created_at", "type": "timestamp without time zone"}]
+            schema["org1Table"]["columns"],
+            [
+                {"name": "id", "type": "integer"},
+                {"name": "name", "type": "character varying"},
+                {"name": "created_at", "type": "timestamp without time zone"},
+            ],
         )
 
         self.assertListEqual(
-            schema["orgTable"]["columns"], [{"name": "id", "type": "integer"}, {"name": "name", "type": "character varying"}]
+            schema["orgTable"]["columns"],
+            [{"name": "id", "type": "integer"}, {"name": "name", "type": "character varying"}],
         )
 
         self.assertListEqual(
-            schema["org Table"]["columns"], [{"name": "id", "type": "integer"}, {"name": "name", "type": "character varying"}, {"name": "created_at", "type": "timestamp without time zone"}]
+            schema["org Table"]["columns"],
+            [
+                {"name": "id", "type": "integer"},
+                {"name": "name", "type": "character varying"},
+                {"name": "created_at", "type": "timestamp without time zone"},
+            ],
         )
 
         self.assertListEqual(
-            schema["org1Table"]["columns"], [{"name": "id", "type": "integer"}, {"name": "name", "type": "character varying"}, {"name": "created_at", "type": "timestamp without time zone"}]
+            schema["org1Table"]["columns"],
+            [
+                {"name": "id", "type": "integer"},
+                {"name": "name", "type": "character varying"},
+                {"name": "created_at", "type": "timestamp without time zone"},
+            ],
         )
 
-        self.assertListEqual(
-            schema["org Tablemview"]["columns"], ['id']
-        )
+        self.assertListEqual(schema["org Tablemview"]["columns"], ["id"])
 
-        self.assertListEqual(
-            schema["org1Tablemview"]["columns"], ['created_at']
-        )
+        self.assertListEqual(schema["org1Tablemview"]["columns"], ["created_at"])
