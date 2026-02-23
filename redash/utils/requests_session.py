4f82efdf233c9b68b412da0dde84083f7496a92b
Yoshioka Tsuneo (吉岡 恒夫) <yoshiokatsuneo@gmail.com>
2026-02-23 23:10:02 +0900

    Limit scope for supressing warning

--- redash/utils/requests_session.py
+++ redash/utils/requests_session.py
@@ -2,17 +2,18 @@ import warnings
 
 from redash import settings
 
-# Supress advocate warning below
-#   /usr/local/lib/python3.13/site-packages/advocate/api.py:102: SyntaxWarning: invalid escape sequence '\*'
-#   server-1     |   :param \*\*kwargs: Optional arguments that ``request`` takes.
-warnings.filterwarnings("ignore", category=SyntaxWarning, module=r".*advocate.*")
+with warnings.catch_warnings():
+    # Supress advocate warning below
+    #   /usr/local/lib/python3.13/site-packages/advocate/api.py:102: SyntaxWarning: invalid escape sequence '\*'
+    #   server-1     |   :param \*\*kwargs: Optional arguments that ``request`` takes.
+    warnings.filterwarnings("ignore", category=SyntaxWarning, module=r".*advocate.*")
 
-from advocate.exceptions import UnacceptableAddressException  # noqa: F401, E402
+    from advocate.exceptions import UnacceptableAddressException  # noqa: F401, E402
 
-if settings.ENFORCE_PRIVATE_ADDRESS_BLOCK:
-    import advocate as requests_or_advocate
-else:
-    import requests as requests_or_advocate
+    if settings.ENFORCE_PRIVATE_ADDRESS_BLOCK:
+        import advocate as requests_or_advocate
+    else:
+        import requests as requests_or_advocate
 
 
 class ConfiguredSession(requests_or_advocate.Session):
