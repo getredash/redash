bd57aad515b55761ab991508e11ee9aa7f956562
Vladislav Denisov <vladislive@gmail.com>
2026-03-01 17:14:41 +0100

    Switch from yarn to pnpm: ci

--- client/cypress/cypress.js
+++ client/cypress/cypress.js
@@ -107,6 +107,6 @@ switch (command) {
     stopServer();
     break;
   default:
-    console.log("Usage: yarn cypress [build|start|db-seed|open|run|stop]");
+    console.log("Usage: pnpm run cypress [build|start|db-seed|open|run|stop]");
     break;
 }
