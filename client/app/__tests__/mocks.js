774ab9bea6609827a2687227ee555dfb2f94cd45
Vladislav Denisov <vladislive@gmail.com>
2026-03-20 20:50:50 +0100

    lint fixed

--- client/app/__tests__/mocks.js
+++ client/app/__tests__/mocks.js
@@ -4,9 +4,10 @@ const date = new Date("2000-01-01T02:00:00.000");
 
 MockDate.set(date);
 
-if (typeof globalThis.MessageChannel === "undefined") {
+if (typeof global.MessageChannel === "undefined") {
   const { MessageChannel } = require("node:worker_threads");
-  globalThis.MessageChannel = MessageChannel;
+  global.MessageChannel = MessageChannel;
+  window.MessageChannel = MessageChannel;
 }
 
 // jsdom doesn't implement getComputedStyle with pseudo-elements.
